ff712f50d553720c2024671250785fa0d856fad5
David Chisnall <theraven@theravensnest.org>
2013-09-12 14:05:23 +0100

    Relax test to allow registers to be renamed.

--- clang/test/CodeGen/chericonst.c
+++ clang/test/CodeGen/chericonst.c
@@ -8,7 +8,9 @@ int callconst(void)
   struct x { int a,b,c; } x;
   __capability struct x *y = (__capability struct x*)&x;
   // Make sure we remove the write permissions from the capability
-  // CHECK: call i8 addrspace(200)* @llvm.cheri.and.cap.perms(i8 addrspace(200)* %2, i64 65495)
+  // CHECK: call i8 addrspace(200)* @llvm.cheri.and.cap.perms(
+  // CHECK: i64 65495)
   cst(y);
+  return 0;
 }
 
