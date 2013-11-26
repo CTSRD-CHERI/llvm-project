9ce8a1ada9e0535af953a66c198ff00242991e59
David Chisnall <theraven@theravensnest.org>
2013-11-26 14:40:10 +0000

    Fix the cheri-cget / set tests.

--- llvm/test/MC/Mips/cheri-cget.s
+++ llvm/test/MC/Mips/cheri-cget.s
@@ -3,17 +3,24 @@
 # Check that the assembler is able to handle capability get instructions.
 #
 
-# CHECK: cgetperm	$t0, $c1
+# CHECK: cgetperm	$12, $c1
+# CHECK: encoding: [0x48,0x0c,0x08,0x00]
 	cgetperm	$t0, $c1
-# CHECK: cgettype	$t0, $c1
+# CHECK: cgettype	$12, $c1
+# CHECK: encoding: [0x48,0x0c,0x08,0x01]
 	cgettype	$t0, $c1
-# CHECK: cgetbase	$t0, $c1
+# CHECK: cgetbase	$12, $c1
+# CHECK: encoding: [0x48,0x0c,0x08,0x02]
 	cgetbase	$t0, $c1
-# CHECK: cgetlen	$t0, $c1
+# CHECK: cgetlen	$12, $c1
+# CHECK: encoding: [0x48,0x0c,0x08,0x03]
 	cgetlen		$t0, $c1
-# CHECK: cgettag	$t0, $c1
+# CHECK: cgettag	$12, $c1
+# CHECK: encoding: [0x48,0x0c,0x08,0x05]
 	cgettag		$t0, $c1
-# CHECK: cgetunsealed	$t0, $c1
+# CHECK: cgetunsealed	$12, $c1
+# CHECK: encoding: [0x48,0x0c,0x08,0x06]
 	cgetunsealed	$t0, $c1
-# CHECK: cgetpcc	$t0($c1)
+# CHECK: cgetpcc	$12($c1)
+# CHECK: encoding: [0x48,0x0c,0x08,0x07]
 	cgetpcc		$t0($c1)
--- llvm/test/MC/Mips/cheri-cset.s
+++ llvm/test/MC/Mips/cheri-cset.s
@@ -9,13 +9,13 @@
 	cunseal		$c1, $c2, $c3
 # CHECK: csealcode	$c1, $c2
 	csealcode	$c1, $c2
-# CHECK: candperm	$c1, $c2, $t0
+# CHECK: candperm	$c1, $c2, $12
 	candperm	$c1, $c2, $t0
-# CHECK: csettype	$c1, $c2, $t0
+# CHECK: csettype	$c1, $c2, $12
 	csettype	$c1, $c2, $t0
-# CHECK: cincbase	$c1, $c2, $t0
+# CHECK: cincbase	$c1, $c2, $12
 	cincbase	$c1, $c2, $t0
-# CHECK: csetlen	$c1, $c2, $t0
+# CHECK: csetlen	$c1, $c2, $12
 	csetlen		$c1, $c2, $t0
 # CHECK: ccleartag	$c1
 	ccleartag	$c1
