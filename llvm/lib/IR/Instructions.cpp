67fb7033a3117326294bcfff573936f5161ca466
David Chisnall <theraven@theravensnest.org>
2013-11-27 14:24:49 +0000

    Remove an obsolete check.

--- llvm/lib/IR/Instructions.cpp
+++ llvm/lib/IR/Instructions.cpp
@@ -2245,11 +2245,6 @@ unsigned CastInst::isEliminableCastPair(
       if (!SrcIntPtrTy || DstIntPtrTy != SrcIntPtrTy)
         return 0;
       unsigned PtrSize = SrcIntPtrTy->getScalarSizeInBits();
-      // FIXME: This is probably too conservative for a number of platforms.
-      // FIXME: Remove this once addrspacecast things are in.
-      if (cast<PointerType>(DstTy)->getAddressSpace() !=
-          cast<PointerType>(SrcTy)->getAddressSpace())
-        return 0;
       if (MidSize >= PtrSize)
         return Instruction::BitCast;
       return 0;
