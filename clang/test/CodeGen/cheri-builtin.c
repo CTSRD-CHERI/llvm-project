6fc0f105a1df7586a6a7d280282774804fb92205
David Chisnall <theraven@theravensnest.org>
2013-12-02 14:24:52 +0000

    Fix CHERI builtins test.

--- clang/test/CodeGen/cheri-builtin.c
+++ clang/test/CodeGen/cheri-builtin.c
@@ -1,4 +1,4 @@
-
+// RUN: %clang %s -target cheri-unknown-freebsd  -S -o - -O0 -emit-llvm | FileCheck %s
 __capability void *results[12];
 
 long long test(__capability void* foo)
@@ -13,7 +13,7 @@ long long test(__capability void* foo)
 	// CHECK: call i8 addrspace(200)* @llvm.cheri.set.cap.type
 	// CHECK: call i8 addrspace(200)* @llvm.cheri.seal.cap.code
 	// CHECK: call i8 addrspace(200)* @llvm.cheri.seal.cap.data
-	// CHECK: call i8 addrspace(200)* @llvm.cheri.seal.cap.data
+	// CHECK: call i8 addrspace(200)* @llvm.cheri.unseal.cap
 	// CHECK: call void @llvm.cheri.set.cause(i64 42)
 	// CHECK: call i64 @llvm.cheri.get.cause()
 	x &= __builtin_cheri_get_cap_length(foo);
@@ -23,7 +23,7 @@ long long test(__capability void* foo)
 	results[0] = __builtin_cheri_set_cap_length(foo, 42);
 	results[1] = __builtin_cheri_and_cap_perms(foo, 12);
 	results[2] = __builtin_cheri_set_cap_type(foo, 0x23);
-	results[3] = __builtin_cheri_seal_cap_code(foo, foo);
+	results[3] = __builtin_cheri_seal_cap_code(foo);
 	results[4] = __builtin_cheri_seal_cap_data(foo, foo);
 	results[5] = __builtin_cheri_unseal_cap(foo, foo);
 	__builtin_cheri_set_cause(42);
