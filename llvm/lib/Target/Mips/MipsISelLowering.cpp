3877e994d424886b98c064727acb3f4bdd78e651
David Chisnall <theraven@theravensnest.org>
2014-01-06 15:13:19 +0000

    Manually expand unaligned i16 loads for CHERI.

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -2005,14 +2005,34 @@ SDValue MipsTargetLowering::lowerLOAD(SDValue Op, SelectionDAG &DAG) const {
   if (Subtarget->isCheri() && (ISD::NON_EXTLOAD != LD->getExtensionType()) &&
       Op.getValueType() == MVT::i32) {
     SDValue BasePtr = LD->getBasePtr();
+    SDLoc DL(Op);
+    SDValue Chain = LD->getChain();
     if (BasePtr->getValueType(0) == MVT::iFATPTR) {
-      SDValue Chain = LD->getChain();
-      SDLoc DL(Op);
       const SDValue Load = DAG.getExtLoad(LD->getExtensionType(), DL, MVT::i64,
           Chain, BasePtr, LD->getPointerInfo(), LD->getMemoryVT(), LD->isVolatile(),
           LD->isNonTemporal(), LD->getAlignment());
       const SDValue Trunc = DAG.getNode(ISD::TRUNCATE, DL, Op.getValueType(), Load);
       return Trunc;
+    } else if ((MemVT == MVT::i16) && (LD->getAlignment() < 2)) {
+      // We have to manually expand unaligned i16 loads on CHERI, because
+      // SelectionDAG doesn't give us a way of only doing custom expansion on
+      // certain types of pointer.
+      ISD::LoadExtType HiExtType = LD->getExtensionType();
+      EVT VT = Op.getValueType();
+      SDValue Hi = DAG.getExtLoad(HiExtType, DL, VT, Chain, BasePtr, LD->getPointerInfo(),
+                          MVT::i8, LD->isVolatile(),
+                          LD->isNonTemporal(), 1, LD->getTBAAInfo());
+      SDValue Ptr = DAG.getNode(ISD::ADD, DL, BasePtr.getValueType(), BasePtr,
+                        DAG.getConstant(1, BasePtr.getValueType()));
+      SDValue Lo = DAG.getExtLoad(ISD::ZEXTLOAD, DL, VT, Chain, Ptr,
+                          LD->getPointerInfo().getWithOffset(1),
+                          MVT::i8, LD->isVolatile(),
+                          LD->isNonTemporal(), 1, LD->getTBAAInfo());
+      SDValue ShiftAmount = DAG.getConstant(8,
+          getShiftAmountTy(Hi.getValueType()));
+      SDValue Result = DAG.getNode(ISD::SHL, DL, VT, Hi, ShiftAmount);
+      Result = DAG.getNode(ISD::OR, DL, VT, Result, Lo);
+      return Result;
     }
   }
 
