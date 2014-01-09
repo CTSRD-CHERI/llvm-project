1058d3f80a6677a3fb8eaa46b4038a832e30951e
David Chisnall <theraven@theravensnest.org>
2014-01-09 13:34:46 +0000

    Make sure CHERI can handle loads of 16-64-byte values with any alignment.

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -1990,6 +1990,51 @@ static SDValue createLoadLR(unsigned Opc, SelectionDAG &DAG, LoadSDNode *LD,
   return DAG.getMemIntrinsicNode(Opc, DL, VTList, Ops, 3, MemVT,
                                  LD->getMemOperand());
 }
+namespace {
+inline SDValue loadFirstPart(SelectionDAG &DAG, LoadSDNode *LD, EVT VT) {
+  unsigned Align = LD->getAlignment();
+  ISD::LoadExtType ExtType = LD->getExtensionType();
+  // If this is a non-extending load (i.e. the register and memory types are
+  // the same) then it doesn't matter how we extend the first part, because
+  // we'll be discarding those bits anyway.
+  if (ExtType == ISD::NON_EXTLOAD)
+    ExtType = ISD::ZEXTLOAD;
+  MVT PartVT;
+  switch (Align)
+  {
+    case 1: PartVT = MVT::i8; break;
+    case 2: PartVT = MVT::i16; break;
+    case 4: PartVT = MVT::i32; break;
+    default: llvm_unreachable("Invalid alignment for partial load");
+  }
+  return DAG.getExtLoad(ExtType, SDLoc(LD), VT, LD->getChain(),
+      LD->getBasePtr(), LD->getPointerInfo(), PartVT, LD->isVolatile(),
+      LD->isNonTemporal(), Align, LD->getTBAAInfo());
+}
+SDValue loadNextPart(SelectionDAG &DAG, LoadSDNode *LD, SDValue &Hi, EVT &VT,
+                     SDValue &OutChain, unsigned Align, unsigned Offset) {
+  SDValue BasePtr = LD->getBasePtr();
+  SDLoc DL(LD);
+  SDValue Chain = LD->getChain();
+  MVT PartVT;
+  switch (Align)
+  {
+    case 1: PartVT = MVT::i8; break;
+    case 2: PartVT = MVT::i16; break;
+    case 4: PartVT = MVT::i32; break;
+    default: llvm_unreachable("Invalid alignment for partial load");
+  }
+  SDValue Ptr = DAG.getPointerAdd(DL, BasePtr, Offset);
+  SDValue Lo = DAG.getExtLoad(ISD::ZEXTLOAD, DL, VT, Chain, Ptr,
+                      LD->getPointerInfo().getWithOffset(Offset),
+                          PartVT, LD->isVolatile(),
+                          LD->isNonTemporal(), Align, LD->getTBAAInfo());
+  SDValue ShiftAmount = DAG.getConstant(8 * Align, MVT::i64);
+  SDValue Shift = DAG.getNode(ISD::SHL, DL, VT, Hi, ShiftAmount);
+  OutChain = cast<LoadSDNode>(Lo)->getChain();
+  return DAG.getNode(ISD::OR, DL, VT, Shift, Lo);
+}
+}
 
 // Expand an unaligned 32 or 64-bit integer load node.
 SDValue MipsTargetLowering::lowerLOAD(SDValue Op, SelectionDAG &DAG) const {
@@ -2004,6 +2049,7 @@ SDValue MipsTargetLowering::lowerLOAD(SDValue Op, SelectionDAG &DAG) const {
     SDLoc DL(Op);
     SDValue BasePtr = LD->getBasePtr();
     SDValue Chain = LD->getChain();
+    EVT VT = Op.getValueType();
     if ((ISD::NON_EXTLOAD != LD->getExtensionType()) &&
       Op.getValueType() == MVT::i32 &&
       BasePtr->getValueType(0) == MVT::iFATPTR) {
@@ -2017,46 +2063,23 @@ SDValue MipsTargetLowering::lowerLOAD(SDValue Op, SelectionDAG &DAG) const {
       // We have to manually expand unaligned i16 loads on CHERI, because
       // SelectionDAG doesn't give us a way of only doing custom expansion on
       // certain types of pointer.
-      ISD::LoadExtType HiExtType = LD->getExtensionType();
-      EVT VT = Op.getValueType();
-      SDValue Hi = DAG.getExtLoad(HiExtType, DL, VT, Chain, BasePtr, LD->getPointerInfo(),
-                          MVT::i8, LD->isVolatile(),
-                          LD->isNonTemporal(), 1, LD->getTBAAInfo());
-      SDValue Ptr = DAG.getNode(ISD::ADD, DL, BasePtr.getValueType(), BasePtr,
-                        DAG.getConstant(1, BasePtr.getValueType()));
-      SDValue Lo = DAG.getExtLoad(ISD::ZEXTLOAD, DL, VT, Chain, Ptr,
-                          LD->getPointerInfo().getWithOffset(1),
-                          MVT::i8, LD->isVolatile(),
-                          LD->isNonTemporal(), 1, LD->getTBAAInfo());
-      SDValue ShiftAmount = DAG.getConstant(8,
-          getShiftAmountTy(Hi.getValueType()));
-      SDValue Result = DAG.getNode(ISD::SHL, DL, VT, Hi, ShiftAmount);
-      Result = DAG.getNode(ISD::OR, DL, VT, Result, Lo);
-      SDValue Ops[2] = {Result, cast<LoadSDNode>(Lo)->getChain()};
+      SDValue Hi = loadFirstPart(DAG, LD, VT);
+      SDValue Result = loadNextPart(DAG, LD, Hi, VT, Chain, 1, 1);
+      SDValue Ops[2] = {Result, Chain};
       return DAG.getMergeValues(Ops, 2, DL);
-    } else if ((MemVT == MVT::i64) && (LD->getAlignment() < 8) &&
+    } else if ((LD->getAlignment() < (MemVT.getSizeInBits() / 8)) &&
+               ((MemVT == MVT::i64) || (MemVT == MVT::i32)) &&
                (BasePtr->getValueType(0) == MVT::iFATPTR)) {
       // CHERI doesn't have capability versions of LDL / LDR, so we have to
-      // expand them the more prosaic way.
-      if (LD->getAlignment() == 4) {
-        EVT VT = Op.getValueType();
-        SDValue Hi = DAG.getExtLoad(ISD::ZEXTLOAD, DL, VT, Chain, BasePtr,
-            LD->getPointerInfo(), MVT::i32, LD->isVolatile(),
-            LD->isNonTemporal(), 4, LD->getTBAAInfo());
-        SDValue Ptr = DAG.getPointerAdd(DL, BasePtr, 4);
-        SDValue Lo = DAG.getExtLoad(ISD::ZEXTLOAD, DL, VT, Chain, Ptr,
-                            LD->getPointerInfo().getWithOffset(4),
-                            MVT::i32, LD->isVolatile(),
-                            LD->isNonTemporal(), 4, LD->getTBAAInfo());
-        SDValue ShiftAmount = DAG.getConstant(32,
-            getShiftAmountTy(Hi.getValueType()));
-        SDValue Result = DAG.getNode(ISD::SHL, DL, VT, Hi, ShiftAmount);
-        Result = DAG.getNode(ISD::OR, DL, VT, Result, Lo);
-        SDValue Ops[2] = {Result, cast<LoadSDNode>(Lo)->getChain()};
-        return DAG.getMergeValues(Ops, 2, DL);
-      }
-      assert(0 &&
-          "64-bit loads with less than 4-byte alignment not yet handled");
+      // expand them the more prosaic way.  We've already expanded i16, so now
+      // we just need to expand i32 and i64
+      SDValue Result = loadFirstPart(DAG, LD, VT);
+      unsigned Align = LD->getAlignment();
+      unsigned Size = (MemVT.getSizeInBits() / 8);
+      for (unsigned Offset=Align ; Offset<=(Size - Align) ; Offset+=Align)
+        Result = loadNextPart(DAG, LD, Result, VT, Chain, Align, Offset);
+      SDValue Ops[2] = {Result, Chain};
+      return DAG.getMergeValues(Ops, 2, DL);
     }
   }
 
