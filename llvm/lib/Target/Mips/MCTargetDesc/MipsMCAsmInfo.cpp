CTSRD-CHERI/llvm

MIPS IV is also not little endian.

lib/Target/Mips/MCTargetDesc/MipsMCAsmInfo.cpp
@@ -22,6 +22,7 @@ MipsMCAsmInfo::MipsMCAsmInfo(StringRef TT) {
   Triple TheTriple(TT);
   if ((TheTriple.getArch() == Triple::mips) ||
       (TheTriple.getArch() == Triple::mips64) ||
+      (TheTriple.getArch() == Triple::mips4) ||
       (TheTriple.getArch() == Triple::cheri))
     IsLittleEndian = false;
 

CTSRD-CHERI/llvm-project

llvm/lib/Target/Mips/MCTargetDesc/MipsMCAsmInfo.cpp
@@ -21,7 +21,8 @@ void MipsMCAsmInfo::anchor() { }
 MipsMCAsmInfo::MipsMCAsmInfo(StringRef TT) {
   Triple TheTriple(TT);
   if ((TheTriple.getArch() == Triple::mips) ||
-      (TheTriple.getArch() == Triple::mips64))
+      (TheTriple.getArch() == Triple::mips64) ||
+      (TheTriple.getArch() == Triple::mips4))
     IsLittleEndian = false;
 
   if ((TheTriple.getArch() == Triple::mips64el) ||