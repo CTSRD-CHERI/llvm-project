87b0008c9cda07d92310e58a6a87af25f7950470
Michael Roe <mroe@cornstalk.org.uk>
2014-02-25 14:17:05 +0000

    Added test cases for MIPS branch likely instructions.

--- llvm/test/MC/Mips/mips-jump-instructions.s
+++ llvm/test/MC/Mips/mips-jump-instructions.s
@@ -20,6 +20,8 @@
 # CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
 # CHECK32:   bgezal $6, 1332        # encoding: [0x4d,0x01,0xd1,0x04]
 # CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bltzal $6, 1332        # encoding: [0x4d,0x01,0xd0,0x04]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
 # CHECK32:   bgtz $6, 1332          # encoding: [0x4d,0x01,0xc0,0x1c]
 # CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
 # CHECK32:   blez $6, 1332          # encoding: [0x4d,0x01,0xc0,0x18]
@@ -44,6 +46,8 @@
 # CHECK64:   bgez $6, 1332          # encoding: [0x4d,0x01,0xc1,0x04]
 # CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
 # CHECK64:   bgezal $6, 1332        # encoding: [0x4d,0x01,0xd1,0x04]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bltzal $6, 1332        # encoding: [0x4d,0x01,0xd0,0x04]
 # CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
 # CHECK64:   bgtz $6, 1332          # encoding: [0x4d,0x01,0xc0,0x1c]
 # CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
@@ -72,6 +76,8 @@
          nop
          bgezal $6,1332
          nop
+         bltzal $6,1332
+         nop
          bgtz $6,1332
          nop
          blez $6,1332
@@ -86,6 +92,68 @@
          nop
 
 end_of_code:
+
+#------------------------------------------------------------------------------
+# Branch likely instructions
+#------------------------------------------------------------------------------
+
+# CHECK32:   bc1fl 1332             # encoding: [0x4d,0x01,0x02,0x45]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bc1tl 1332             # encoding: [0x4d,0x01,0x03,0x45]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   beql $9, $6, 1332      # encoding: [0x4d,0x01,0x26,0x51]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bgezl $6, 1332         # encoding: [0x4d,0X01,0xc3,0x04]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bgezall $6, 1332       # encoding: [0x4d,0x01,0xd3,0x04]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bltzall $6, 1332       # encoding: [0x4d,0x01,0xd2,0x04]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bgtzl $6, 1332         # encoding: [0x4d,0x01,0xc0,0x5c]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   blezl $6, 1332         # encoding: [0x4d,0x01,0xc0,0x58]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK32:   bnel $9, $6, 1332      # encoding: [0x4d,0x01,0x26,0x55]
+# CHECK32:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+
+# CHECK64:   bc1fl 1332             # encoding: [0x4d,0x01,0x02,0x45]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bc1tl 1332             # encoding: [0x4d,0x01,0x03,0x45]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   beql $9, $6, 1332      # encoding: [0x4d,0x01,0x26,0x51]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bgezl $6, 1332         # encoding: [0x4d,0X01,0xc3,0x04]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bgezall $6, 1332       # encoding: [0x4d,0x01,0xd3,0x04]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bltzall $6, 1332       # encoding: [0x4d,0x01,0xd2,0x04]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bgtzl $6, 1332         # encoding: [0x4d,0x01,0xc0,0x5c]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   blezl $6, 1332         # encoding: [0x4d,0x01,0xc0,0x58]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+# CHECK64:   bnel $9, $6, 1332      # encoding: [0x4d,0x01,0x26,0x55]
+# CHECK64:   nop                    # encoding: [0x00,0x00,0x00,0x00]
+
+        bc1fl 1332
+        nop
+	bc1tl 1332
+        nop
+        beql $9, $6, 1332
+        nop
+        bgezl $6, 1332
+        nop
+        bgezall $6, 1332
+        nop
+	bltzall $6, 1332
+        nop
+        bgtzl $6, 1332
+        nop
+        blezl $6, 1332
+        nop
+        bnel $9, $6, 1332
+        nop
+
 #------------------------------------------------------------------------------
 # Jump instructions
 #------------------------------------------------------------------------------
