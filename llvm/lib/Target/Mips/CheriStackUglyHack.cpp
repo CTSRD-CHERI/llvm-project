70673c9dd3ddc7d1c5042f29de7f64efc2050a89
David Chisnall <theraven@theravensnest.org>
2014-05-07 13:32:02 +0100

    Improve the capability stack pass so that it correctly generates C0-relative pointers when required.

--- llvm/lib/Target/Mips/CheriStackUglyHack.cpp
+++ llvm/lib/Target/Mips/CheriStackUglyHack.cpp
@@ -11,6 +11,7 @@
 #include "llvm/ADT/StringSwitch.h"
 #include "llvm/Support/CallSite.h"
 #include "llvm/InstVisitor.h"
+#include "llvm/Transforms/Utils/Local.h"
 
 #include <string>
 #include <utility>
@@ -26,29 +27,44 @@ class CheriStackHack : public FunctionPass,
                        public InstVisitor<CheriStackHack> {
   DataLayout *DL;
   Module *M;
+  /// Structure used to track an alloca and its possible replacements.
+  struct Replacement {
+    /// The original alloca instruction
+    Value  *Alloca;
+    /// The capability to use for capability references to this
+    Value *Cap;
+    /// The C pointer value for this alloca
+    Value *Ptr;
+  };
   llvm::SmallVector<AllocaInst*, 16> Allocas;
 
   Value *InitialBitCast;
   Value *CastToCap;
 
-  void replaceGEP(GetElementPtrInst *GEP, Value *Cap) {
+  void replaceGEP(GetElementPtrInst *GEP, const Replacement &R) {
     SmallVector<Value*, 8> Idxs;
     for (auto I=GEP->idx_begin(),E=GEP->idx_end() ; I!=E ; I++)
       Idxs.push_back(I->get());
     // The result of a GEP is another pointer, so we must do the replacement
     // here.
-    replaceUsers(GEP,
-        GetElementPtrInst::Create(Cap, Idxs, GEP->getName(), GEP));
-    GEP->eraseFromParent();
+    Value *NewGEP = GetElementPtrInst::Create(R.Cap, Idxs, GEP->getName(),
+        GEP);
+    Value *NewGEPAsPtr = new AddrSpaceCastInst(NewGEP, GEP->getType(),
+        GEP->getName(), GEP);
+
+    Replacement GEPR = { GEP, NewGEP, NewGEPAsPtr };
+    replaceUsers(GEP, GEPR);
+    RecursivelyDeleteTriviallyDeadInstructions(GEP);
+    RecursivelyDeleteTriviallyDeadInstructions(NewGEPAsPtr);
   }
-  void replaceStore(StoreInst *SI, Value *Cap) {
-    new StoreInst(SI->getOperand(0), Cap, SI->isVolatile(), SI->getAlignment(),
-        SI->getOrdering(), SI->getSynchScope(), SI);
+  void replaceStore(StoreInst *SI, const Replacement &R) {
+    new StoreInst(SI->getOperand(0), R.Cap, SI->isVolatile(),
+        SI->getAlignment(), SI->getOrdering(), SI->getSynchScope(), SI);
     // Stores can't have users...
     SI->eraseFromParent();
   }
-  void replaceLoad(LoadInst *LI, Value *Cap) {
-    LoadInst *CapLoad = new LoadInst(Cap, LI->getName(), LI->isVolatile(),
+  void replaceLoad(LoadInst *LI, const Replacement &R) {
+    LoadInst *CapLoad = new LoadInst(R.Cap, LI->getName(), LI->isVolatile(),
         LI->getAlignment(), LI->getOrdering(), LI->getSynchScope(), LI);
     // We shouldn't need to visit users of this, because once we've loaded the
     // value then it will all work correctly.  Note that this isn't the case if
@@ -56,16 +72,17 @@ class CheriStackHack : public FunctionPass,
     // In that case, everything will break.
     LI->replaceAllUsesWith(CapLoad);
   }
-  void replaceAddrSpaceCast(AddrSpaceCastInst *AI, Value *Cap) {
-    if (AI->getType() == Cap->getType())
-      AI->replaceAllUsesWith(Cap);
+  void replaceAddrSpaceCast(AddrSpaceCastInst *AI, const Replacement &R) {
+    if (AI->getType() == R.Cap->getType())
+      AI->replaceAllUsesWith(R.Cap);
     else {
-      BitCastInst *BI = new BitCastInst(Cap, AI->getType(), AI->getName(), AI);
+      BitCastInst *BI = new BitCastInst(R.Cap, AI->getType(), AI->getName(), AI);
       AI->replaceAllUsesWith(BI);
     }
     AI->eraseFromParent();
   }
-  void replaceCall(CallInst *CI, Value *Cap) {
+
+  void replaceCall(CallInst *CI, const Replacement &R) {
     if (CI == CastToCap)
       return;
     // We can safely ignore some intrinsics: they are only used for mid-level
@@ -79,33 +96,44 @@ class CheriStackHack : public FunctionPass,
           CI->eraseFromParent();
           return;
       }
-    CI->dump();
-    llvm_unreachable("Unable to replace call taking stack address as argument");
+    CI->replaceUsesOfWith(R.Alloca, R.Ptr);
   }
-  void replaceBitCast(BitCastInst *BI, Value *Cap) {
-    // What is the sensible thing to do here?  We can do a ptrtoint thing if
-    // it's sensible, otherwise a pointer bitcast?
-    BI->dump();
-    llvm_unreachable("Can't handle bitcasts yet");
+
+  void replacePtrToInt(PtrToIntInst *PI, const Replacement &R) {
+    PI->replaceUsesOfWith(R.Alloca, R.Ptr);
   }
-  void replaceUsers(Instruction *Inst, Value *Cap) {
-    for (auto U=Inst->use_begin(),E=Inst->use_end() ; U!=E ; ++U) {
-      if (GetElementPtrInst *GEP = dyn_cast<GetElementPtrInst>(*U))
-        replaceGEP(GEP, Cap);
-      else if (LoadInst *LI = dyn_cast<LoadInst>(*U))
-        replaceLoad(LI, Cap);
-      else if (StoreInst *LI = dyn_cast<StoreInst>(*U))
-        replaceStore(LI, Cap);
-      else if (CallInst *CI = dyn_cast<CallInst>(*U))
-        replaceCall(CI, Cap);
-      else if (AddrSpaceCastInst *AI = dyn_cast<AddrSpaceCastInst>(*U))
-        replaceAddrSpaceCast(AI, Cap);
-      else if (BitCastInst *BI = dyn_cast<BitCastInst>(*U)) {
+  void replaceBitCast(BitCastInst *BI, const Replacement &R) {
+    BI->replaceUsesOfWith(R.Alloca, R.Ptr);
+  }
+
+  void replaceUsers(Instruction *Inst, const Replacement &R) {
+    // We're going to delete users as we go, so we need to make sure that we
+    // finish iterating before we start deleting
+    SmallVector<Value *, 16> Users;
+    for (auto U=Inst->use_begin(),E=Inst->use_end() ; U!=E ; ++U)
+      Users.push_back(*U);
+    for (Value *U : Users) {
+      if (GetElementPtrInst *GEP = dyn_cast<GetElementPtrInst>(U))
+        replaceGEP(GEP, R);
+      else if (LoadInst *LI = dyn_cast<LoadInst>(U))
+        replaceLoad(LI, R);
+      else if (StoreInst *LI = dyn_cast<StoreInst>(U))
+        replaceStore(LI, R);
+      else if (CallInst *CI = dyn_cast<CallInst>(U))
+        replaceCall(CI, R);
+      else if (AddrSpaceCastInst *AI = dyn_cast<AddrSpaceCastInst>(U)) {
+        if (AI != R.Cap)
+          replaceAddrSpaceCast(AI, R);
+      }
+      else if (PtrToIntInst *PI = dyn_cast<PtrToIntInst>(U))
+        replacePtrToInt(PI, R);
+      else if (BitCastInst *BI = dyn_cast<BitCastInst>(U)) {
         if (BI != InitialBitCast)
-          replaceBitCast(BI, Cap);
+          replaceBitCast(BI, R);
       }
       else {
-        (*U)->dump();
+        U->dump();
+        llvm_unreachable("Unknown instruction using alloca!");
       }
     }
   }
@@ -146,7 +174,6 @@ class CheriStackHack : public FunctionPass,
         Type *AllocationTy = AllocaTy->getElementType();
         unsigned ElementSize = DL->getTypeAllocSize(AllocationTy);
         Value *Size = ConstantInt::get(Type::getInt64Ty(C), ElementSize);
-        AI->getArraySize()->dump();
         if (AI->isArrayAllocation())
           Size = B.CreateMul(Size, AI->getArraySize());
 
@@ -155,7 +182,10 @@ class CheriStackHack : public FunctionPass,
         CastToCap = B.CreateCall(CastFn, InitialBitCast);
         Value *AllocaAsCap = B.CreateCall2(SetLenFun, CastToCap, Size);
         AllocaAsCap = B.CreateBitCast(AllocaAsCap, CapTy);
-        replaceUsers(AI, AllocaAsCap);
+        Value *AllocaAsPtr = B.CreateAddrSpaceCast(CastToCap, AllocaTy);
+        Replacement R = { AI, AllocaAsCap, AllocaAsPtr };
+        replaceUsers(AI, R);
+        RecursivelyDeleteTriviallyDeadInstructions(AllocaAsPtr);
       }
       return true;
     }
