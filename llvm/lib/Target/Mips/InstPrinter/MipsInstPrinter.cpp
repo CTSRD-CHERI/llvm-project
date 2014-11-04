c412e3650b86cf52928aa111d9b1b49a8b4aa482
David Chisnall <theraven@theravensnest.org>
2014-11-04 14:30:26 +0000

    [mips] Fix two crashing cases in the MIPS disassembler.

--- llvm/lib/Target/Mips/InstPrinter/MipsInstPrinter.cpp
+++ llvm/lib/Target/Mips/InstPrinter/MipsInstPrinter.cpp
@@ -220,6 +220,10 @@ void MipsInstPrinter::printUnsignedImm8(const MCInst *MI, int opNum,
 
 void MipsInstPrinter::
 printMemOperand(const MCInst *MI, int opNum, raw_ostream &O) {
+  if (opNum+1 >= (int)MI->getNumOperands()) {
+    printOperand(MI, opNum, O);
+    return;
+  }
   // Load/Store memory operands -- imm($reg)
   // If PIC target the target is loaded as the
   // pattern lw $25,%call16($28)
