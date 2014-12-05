a43f4415f1e23903f949104eca13b1c6f0a54b55
David Chisnall <theraven@theravensnest.org>
2014-12-05 11:06:53 +0000

    Replace capabilities.h with memcap.h, better exposing implementation-agnostic capabilities.

---
+++ clang/lib/Headers/memcap.h
@@ -0,0 +1,134 @@
+/*===---- memcap.h - Header for memory capabilities -----------------------===*\
+ *
+ * Copyright (c) 2014 David Chisnall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+\*===----------------------------------------------------------------------===*/
+
+
+typedef unsigned short memcap_perms_t;
+typedef unsigned __INT32_TYPE__ memcap_type_t;
+#ifdef __cplusplus
+#define __memcap_bool bool
+#else
+#define __memcap_bool _Bool
+#endif
+
+#if __has_feature(capabilities)
+#define __IF_CAPS(x, y) x
+typedef __intcap_t intcap_t;
+typedef __uintcap_t uintcap_t;
+#define capability __capability
+#define output __output
+#define input __input
+#else
+#define __IF_CAPS(x, y) y
+typedef __INTPTR_TYPE__ intcap_t;
+typedef __INTPTR_TYPE__ uintcap_t;
+#define capability
+#define output 
+#define input
+#endif
+
+#define __MEMCAP_GET(__name, __type, __get, __default)            \
+  static inline __type                                                        \
+  memcap_##__name##__get(capability void *__cap)                              \
+  {                                                                           \
+    return __IF_CAPS(__builtin_memcap_##__name##__get(__cap), __default);     \
+  }
+
+#define __MEMCAP_SET(__name, __type, __set)                                   \
+  static inline capability void*                                              \
+  memcap_##__name##__set(const capability void *__cap, __type __val)          \
+  {                                                                           \
+    return __IF_CAPS(__builtin_memcap_##__name##__set(__cap, __val),          \
+                     (void*)__cap);                                           \
+  }                                                                           \
+
+#define __MEMCAP_ACCESSOR(__name, __type, __set, __get, __default)            \
+  __MEMCAP_GET(__name, __type, __get, __default)                              \
+  __MEMCAP_SET(__name, __type, __set)                                         \
+
+__MEMCAP_ACCESSOR(length, __SIZE_TYPE__, _set, _get, __SIZE_MAX__)
+__MEMCAP_ACCESSOR(base, __SIZE_TYPE__, _increment, _get, __SIZE_MAX__)
+__MEMCAP_ACCESSOR(offset, __SIZE_TYPE__, _set, _get, __SIZE_MAX__)
+__MEMCAP_ACCESSOR(type, memcap_type_t, _set, _get, 0)
+__MEMCAP_ACCESSOR(perms, memcap_perms_t, _and, _get, 0)
+__MEMCAP_GET(tag, _Bool, _get, 0)
+__MEMCAP_GET(unsealed, _Bool, _get, 0)
+
+static inline
+capability void *memcap_offset_increment(const capability void *__cap,
+                                         __PTRDIFF_TYPE__ __offset) {
+  return __IF_CAPS(__builtin_memcap_offset_increment(__cap, __offset),
+                   ((char*)__cap) + __offset);
+}
+static inline
+capability void *memcap_base_only_increment(const capability void *__cap,
+                                         __PTRDIFF_TYPE__ __offset) {
+  return __IF_CAPS(__builtin_memcap_base_only_increment(__cap, __offset),
+                   (void*)__cap);
+}
+
+static inline
+capability void *memcap_tag_clear(const capability void *__cap) {
+  return __IF_CAPS(__builtin_memcap_tag_clear(__cap), (void*)__cap);
+}
+
+static inline
+capability void *memcap_seal(const capability void *__cap,
+                             const capability void *__type) {
+  return __IF_CAPS(__builtin_memcap_seal(__cap, __type), (void*)__cap);
+}
+
+static inline
+capability void *memcap_unseal(const capability void *__cap,
+                               const capability void *__type) {
+  return __IF_CAPS(__builtin_memcap_unseal(__cap, __type), (void*)__cap);
+}
+
+static inline
+void memcap_perms_check(const capability void *__cap, memcap_perms_t __perms) {
+  __IF_CAPS(__builtin_memcap_perms_check(__cap, __perms), );
+}
+
+static inline
+void memcap_type_check(const capability void *__cap,
+                       const capability void *__type) {
+  __IF_CAPS(__builtin_memcap_type_check(__cap, __type), );
+}
+
+static inline
+capability void *memcap_global_data_get(void) {
+  return __IF_CAPS(__builtin_memcap_global_data_get(), 0);
+}
+
+static inline
+capability void *memcap_program_counter_get(void) {
+  return __IF_CAPS(__builtin_memcap_program_counter_get(), 0);
+}
+
+#undef __MEMCAP_ACCESSOR
+
+#undef __MEMCAP_ACCESSOR
+#undef __MEMCAP_GET
+#undef __MEMCAP_SET
+#undef __memcap_bool
+#undef __IF_CAPS
---
+++ clang/test/CodeGen/memcap.c
@@ -0,0 +1,95 @@
+// RUN: %clang %s -O1 -target cheri-unknown-freebsd -o - -emit-llvm -S | FileCheck %s --check-prefix=CAPS
+// RUN: %clang %s -O1 -target i386-unknown-freebsd -o - -emit-llvm -S | FileCheck %s --check-prefix=PTRS
+
+// Remove the static from all of the function prototypes so that this really exists.
+#define static
+#define inline 
+#include <memcap.h>
+
+// PTRS: define i32 @memcap_length_get(i8* nocapture readnone %__cap)
+// PTRS: ret i32 -1
+// PTRS: define i8* @memcap_length_set(i8* readnone %__cap, i32  %__val)
+// PTRS: ret i8* %__cap
+// PTRS: define i32 @memcap_base_get(i8* nocapture readnone %__cap)
+// PTRS: ret i32 -1
+// PTRS: define i8* @memcap_base_increment(i8* readnone %__cap, i32  %__val)
+// PTRS: ret i8* %__cap
+// PTRS: define i32 @memcap_offset_get(i8* nocapture readnone %__cap)
+// PTRS: ret i32 -1
+// PTRS: define i8* @memcap_offset_set(i8* readnone %__cap, i32  %__val)
+// PTRS: ret i8* %__cap
+// PTRS: define i32 @memcap_type_get(i8* nocapture readnone %__cap)
+// PTRS: ret i32 0
+// PTRS: define i8* @memcap_type_set(i8* readnone %__cap, i32  %__val)
+// PTRS: ret i8* %__cap
+// PTRS: define  zeroext i16 @memcap_perms_get(i8* nocapture readnone %__cap)
+// PTRS: ret i16 0
+// PTRS: define i8* @memcap_perms_and(i8* readnone %__cap, i16 zeroext  %__val)
+// PTRS: ret i8* %__cap
+// PTRS: define zeroext i1 @memcap_tag_get(i8* nocapture readnone %__cap)
+// PTRS: ret i1 false
+// PTRS: define zeroext i1 @memcap_unsealed_get(i8* nocapture readnone %__cap)
+// PTRS: ret i1 false
+// PTRS: define i8* @memcap_offset_increment(i8* readnone %__cap, i32 %__offset)
+// PTRS: %add.ptr = getelementptr inbounds i8* %__cap, i32 %__offset
+// PTRS: ret i8* %add.ptr
+// PTRS: define i8* @memcap_base_only_increment(i8* readnone %__cap, i32 %__offset)
+// PTRS: ret i8* %__cap
+// PTRS: define i8* @memcap_tag_clear(i8* readnone %__cap)
+// PTRS: ret i8* %__cap
+// PTRS: define i8* @memcap_seal(i8* readnone %__cap, i8* nocapture readnone %__type)
+// PTRS: ret i8* %__cap
+// PTRS: define i8* @memcap_unseal(i8* readnone %__cap, i8* nocapture readnone %__type)
+// PTRS: ret i8* %__cap
+// PTRS: define void @memcap_perms_check(i8* nocapture %__cap, i16 zeroext %__perms)
+// PTRS: ret void
+// PTRS: define void @memcap_type_check(i8* nocapture %__cap, i8* nocapture %__type)
+// PTRS: ret void
+// PTRS: define noalias i8* @memcap_global_data_get()
+// PTRS: ret i8* null
+// PTRS: define noalias i8* @memcap_program_counter_get()
+// PTRS: ret i8* null
+
+// CAPS: define i64 @memcap_length_get(i8 addrspace(200)* readnone %__cap)
+// CAPS: call i64 @llvm.mips.cap.length.get(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_length_set(i8 addrspace(200)* readnone %__cap, i64 zeroext %__val)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.length.set(i8 addrspace(200)* %__cap, i64 %__val)
+// CAPS: define i64 @memcap_base_get(i8 addrspace(200)* readnone %__cap)
+// CAPS: call i64 @llvm.mips.cap.base.get(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_base_increment(i8 addrspace(200)* readnone %__cap, i64 zeroext %__val)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.base.increment(i8 addrspace(200)* %__cap, i64 %__val)
+// CAPS: define i64 @memcap_offset_get(i8 addrspace(200)* readnone %__cap)
+// CAPS: call i64 @llvm.mips.cap.offset.get(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_offset_set(i8 addrspace(200)* readnone %__cap, i64 zeroext %__val)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.offset.set(i8 addrspace(200)* %__cap, i64 %__val)
+// CAPS: define i32 @memcap_type_get(i8 addrspace(200)* %__cap)
+// CAPS: call i64 @llvm.mips.cap.type.get(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_type_set(i8 addrspace(200)* readnone %__cap, i32 zeroext %__val)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.type.set(i8 addrspace(200)* %__cap, i64 %conv)
+// CAPS: define zeroext i16 @memcap_perms_get(i8 addrspace(200)* %__cap)
+// CAPS: call i64 @llvm.mips.cap.perms.get(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_perms_and(i8 addrspace(200)* readnone %__cap, i16 zeroext %__val)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.perms.and(i8 addrspace(200)* %__cap, i64 %conv)
+// CAPS: define zeroext i1 @memcap_tag_get(i8 addrspace(200)* readnone %__cap)
+// CAPS: call i64 @llvm.mips.cap.tag.get(i8 addrspace(200)* %__cap)
+// CAPS: define zeroext i1 @memcap_unsealed_get(i8 addrspace(200)* readnone %__cap)
+// CAPS: call i64 @llvm.mips.cap.unsealed.get(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_offset_increment(i8 addrspace(200)* readnone %__cap, i64 signext %__offset)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.offset.increment(i8 addrspace(200)* %__cap, i64 %__offset)
+// CAPS: define i8 addrspace(200)* @memcap_base_only_increment(i8 addrspace(200)* readnone %__cap, i64 signext %__offset)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.base.only.increment(i8 addrspace(200)* %__cap, i64 %__offset)
+// CAPS: define i8 addrspace(200)* @memcap_tag_clear(i8 addrspace(200)* readnone %__cap)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.tag.clear(i8 addrspace(200)* %__cap)
+// CAPS: define i8 addrspace(200)* @memcap_seal(i8 addrspace(200)* readnone %__cap, i8 addrspace(200)* readnone %__type)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.seal(i8 addrspace(200)* %__cap, i8 addrspace(200)* %__type)
+// CAPS: define i8 addrspace(200)* @memcap_unseal(i8 addrspace(200)* readnone %__cap, i8 addrspace(200)* readnone %__type)
+// CAPS: call i8 addrspace(200)* @llvm.mips.cap.unseal(i8 addrspace(200)* %__cap, i8 addrspace(200)* %__type)
+// CAPS: define void @memcap_perms_check(i8 addrspace(200)* %__cap, i16 zeroext %__perms)
+// CAPS: call void @llvm.mips.cap.perms.check(i8 addrspace(200)* %__cap, i64 %conv)
+// CAPS: define void @memcap_type_check(i8 addrspace(200)* %__cap, i8 addrspace(200)* %__type)
+// CAPS: call void @llvm.mips.cap.type.check(i8 addrspace(200)* %__cap, i8 addrspace(200)* %__type)
+// CAPS: define i8 addrspace(200)* @memcap_global_data_get()
+// CAPS: call i8 addrspace(200)* @llvm.mips.c0.get()
+// CAPS: define i8 addrspace(200)* @memcap_program_counter_get()
+// CAPS: call i8 addrspace(200)* @llvm.mips.pcc.get()
+
