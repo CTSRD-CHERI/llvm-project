3e03860f5c3ba17504b1cde1a5eabec27375a331
David Chisnall <theraven@theravensnest.org>
2015-03-10 13:18:46 +0000

    Fix the test for sema analysis main to be a sema test.

---
+++ clang/test/Sema/cheri-main.c
@@ -0,0 +1,39 @@
+// RUN: %clang_cc1 -DNO_ARGS=1 -triple cheri-unknown-freebsd -fsyntax-only -verify %s -target-abi sandbox
+// RUN: %clang_cc1 -DPTR_PTR=1 -triple cheri-unknown-freebsd -fsyntax-only -verify %s -target-abi sandbox
+// RUN: %clang_cc1 -DCONST_PTR_PTR=1 -triple cheri-unknown-freebsd -fsyntax-only -verify %s -target-abi sandbox
+// RUN: %clang_cc1 -DARR_PTR=1 -triple cheri-unknown-freebsd -fsyntax-only -verify %s -target-abi sandbox
+// RUN: %clang_cc1 -DCONST_ARR_PTR=1 -triple cheri-unknown-freebsd -fsyntax-only -verify %s -target-abi sandbox
+
+// expected-no-diagnostics
+
+// Ensure various legal main function protoypes are accepted.
+
+#ifdef NO_ARGS
+int main() {
+  return 0;
+}
+#endif
+
+#ifdef PTR_PTR
+int main(int argc, char **argv) {
+  return 0;
+}
+#endif
+
+#ifdef CONST_PTR_PTR
+int main(int argc, const char * const *argv) {
+  return 0;
+}
+#endif
+
+#ifdef ARR_PTR
+int main(int argc, char* argv[]) {
+  return 0;
+}
+#endif
+
+#ifdef CONST_ARR_PTR
+int main(int argc, const char* argv[]) {
+  return 0;
+}
+#endif
