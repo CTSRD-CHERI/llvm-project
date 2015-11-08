8b64ae088058c2115ea183f37bb6539f727b0e42
David Chisnall <theraven@theravensnest.org>
2015-11-08 13:37:02 -0800

    Run the function pointer size test on both ABIs.

--- clang/test/Sema/cheri-fnptrsize.c
+++ clang/test/Sema/cheri-fnptrsize.c
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -triple cheri-unknown-freebsd -fsyntax-only %s -target-abi sandbox
+// RUN: %clang_cc1 -triple cheri-unknown-freebsd -fsyntax-only %s
 
 typedef void(*ptr)(void);
 
