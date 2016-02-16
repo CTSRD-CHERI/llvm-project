6249d03447664740d38a1f49f706b0356e2ee838
David Chisnall <theraven@theravensnest.org>
2016-02-16 15:15:44 +0000

    Fix the intcap_t test.
    Now that we’re sensibly optimising intcap_t operations, we can’t rely on the optimiser to preserve the builtin calls.

--- clang/test/CodeGen/intcap_t.c
+++ clang/test/CodeGen/intcap_t.c
@@ -1,6 +1,6 @@
-// RUN: %clang %s -O1 -target cheri-unknown-freebsd -o - -emit-llvm -S | FileCheck %s
+// RUN: %clang %s -O0 -target cheri-unknown-freebsd -o - -emit-llvm -S | FileCheck %s
 // Check that we can actually compile this file...
-// RUN: %clang %s -O1 -target cheri-unknown-freebsd -o /dev/null -c
+// RUN: %clang %s -O0 -target cheri-unknown-freebsd -o /dev/null -c
 
 // CHECK: c1
 int c1(__capability void* x, __capability void* y)
@@ -46,9 +46,9 @@ int c5(__capability void* x, __capability void* y)
 // CHECK: ca1
 int ca1(__capability void* x, __capability void* y)
 {
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %x)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t a = (__intcap_t)x;
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %y)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t b = (__intcap_t)y;
   // CHECK: sub
   return a - b;
@@ -57,9 +57,9 @@ int ca1(__capability void* x, __capability void* y)
 // CHECK: ca2
 int ca2(__capability void* x, __capability void* y)
 {
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %x)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t a = (__intcap_t)x;
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %y)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t b = (__intcap_t)y;
   // CHECK: add
   return a + b;
@@ -68,9 +68,9 @@ int ca2(__capability void* x, __capability void* y)
 // CHECK: ca3
 int ca3(__capability void* x, __capability void* y)
 {
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %x)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t a = (__intcap_t)x;
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %y)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t b = (__intcap_t)y;
   // CHECK: mul
   return a * b;
@@ -79,9 +79,9 @@ int ca3(__capability void* x, __capability void* y)
 // CHECK: ca4
 int ca4(__capability void* x, __capability void* y)
 {
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %x)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t a = (__intcap_t)x;
-  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)* %y)
+  // CHECK: @llvm.mips.cap.offset.get(i8 addrspace(200)*
   __intcap_t b = (__intcap_t)y;
   // CHECK: sdiv
   return a / b;
@@ -105,7 +105,7 @@ __capability void *castc(__intcap_t a)
 
 void *castp(__intcap_t a)
 {
-  // CHECK: ptrtoint i8 addrspace(200)* %a to i64
+  // CHECK: ptrtoint i8 addrspace(200)* 
   // CHECK: inttoptr
   return (void*)a;
 }
@@ -131,16 +131,15 @@ __uintcap_t xor(__uintcap_t f)
 int capdiff(__capability int *a, __capability int *b)
 {
   // CHECK: @capdiff(i32 addrspace(200)* %a, i32 addrspace(200)* %b) #0 {
-  // CHECK: %0 = bitcast i32 addrspace(200)* %a to i8 addrspace(200)*
-  // CHECK: %1 = bitcast i32 addrspace(200)* %b to i8 addrspace(200)*
-  // CHECK: %2 = tail call i64 @llvm.mips.cap.base.get(i8 addrspace(200)* %0)
-  // CHECK: %3 = tail call i64 @llvm.mips.cap.base.get(i8 addrspace(200)* %1)
-  // CHECK: %4 = tail call i64 @llvm.mips.cap.offset.get(i8 addrspace(200)* %0)
-  // CHECK: %5 = tail call i64 @llvm.mips.cap.offset.get(i8 addrspace(200)* %1)
-  // CHECK: %sub.ptr.rhs.cast.neg = sub i64 %2, %3
-  // CHECK: %sub.ptr.lhs.cast = add i64 %sub.ptr.rhs.cast.neg, %4
-  // CHECK: %sub.ptr.sub = sub i64 %sub.ptr.lhs.cast, %5
-  // CHECK: %sub.ptr.div.1 = lshr exact i64 %sub.ptr.sub, 2
+  // CHECK: call i64 @llvm.mips.cap.base.get(i8 addrspace(200)*
+  // CHECK: call i64 @llvm.mips.cap.base.get(i8 addrspace(200)*
+  // CHECK: call i64 @llvm.mips.cap.offset.get(i8 addrspace(200)*
+  // CHECK: call i64 @llvm.mips.cap.offset.get(i8 addrspace(200)*
+  // CHECK: %sub.ptr.lhs.cast = add i64 %4, %6
+  // CHECK: %sub.ptr.rhs.cast = add i64 %5, %7
+  // CHECK: %sub.ptr.sub = sub i64 %sub.ptr.lhs.cast, %sub.ptr.rhs.cast
+  // CHECK: %sub.ptr.div = sdiv exact i64 %sub.ptr.sub, 4
+  // CHECK: %conv = trunc i64 %sub.ptr.div to i32
   return a-b;
 }
 
