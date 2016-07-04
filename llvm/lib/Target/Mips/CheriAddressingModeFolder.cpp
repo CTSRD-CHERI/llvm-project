4ad7ff6ec99a7c5fb024f9aff5a21c118abbf827
David Chisnall <theraven@theravensnest.org>
2016-07-04 15:47:38 +0100

    Turn c0-relative loads / stores into MIPS loads / stores.
    This also folds an addui into the immediate of the load where possible.  This should give a noticeable performance win on every global access, as we’re now doing an ld with a constant immediate from the GOT pointer and not an add, a cfromptr, and then a capability-relative load.

--- llvm/lib/Target/Mips/CheriAddressingModeFolder.cpp
+++ llvm/lib/Target/Mips/CheriAddressingModeFolder.cpp
@@ -55,6 +55,29 @@ struct CheriAddressingModeFolder : public MachineFunctionPass {
     offset = off;
     return true;
   }
+  unsigned MipsOpForCHERIOp(unsigned Op) {
+    switch (Op) {
+      default:
+        llvm_unreachable("No MIPS equivalent");
+      case Mips::CAPLOAD8: return Mips::LB64;
+      case Mips::CAPLOADU8: return Mips::LBu64;
+      case Mips::CAPLOAD16: return Mips::LH64;
+      case Mips::CAPLOADU16: return Mips::LHu64;
+      case Mips::CAPLOAD32: return Mips::LW;
+      case Mips::CAPLOADU32: return Mips::LWu;
+      case Mips::CAPLOAD832: return Mips::LB;
+      case Mips::CAPLOADU832: return Mips::LBu;
+      case Mips::CAPLOAD1632: return Mips::LH;
+      case Mips::CAPLOADU1632: return Mips::LHu;
+      case Mips::CAPLOAD3264: return Mips::LW64;
+      case Mips::CAPLOAD64: return Mips::LD;
+      case Mips::CAPSTORE8: return Mips::SB64;
+      case Mips::CAPSTORE16: return Mips::SH64;
+      case Mips::CAPSTORE32: return Mips::SW64;
+      case Mips::CAPSTORE64: return Mips::SD;
+    }
+  }
+
   bool foldMachineFunction(MachineFunction &MF) {
     if (DisableAddressingModeFolder)
       return false;
@@ -63,6 +86,7 @@ struct CheriAddressingModeFolder : public MachineFunctionPass {
     std::set<MachineInstr *> IncOffsets;
     std::set<MachineInstr *> Adds;
     std::set<std::pair<MachineBasicBlock *, MachineInstr *>> SetPCCOffsets;
+    llvm::SmallVector<std::pair<MachineInstr *, MachineInstr *>, 8> C0Ops;
     std::set<MachineInstr *> GetPCCs;
     bool modified = false;
     for (auto &MBB : MF)
@@ -110,9 +134,20 @@ struct CheriAddressingModeFolder : public MachineFunctionPass {
         // If the capability is formed by incrementing an offset, then try to
         // pull that calculation into the memory operation.
 
-        // Ignore ones that are not based on a CIncOffset op
         MachineInstr *IncOffset = RI.getUniqueVRegDef(I.getOperand(3).getReg());
-        if (!IncOffset || (IncOffset->getOpcode() != Mips::CIncOffset))
+        if (!IncOffset)
+          continue;
+        // If this is CFromPtr-relative load or store, then we may be able to
+        // fold it into a MIPS load.
+        if (IncOffset->getOpcode() == Mips::CFromPtr) {
+          if ((Op == Mips::LOADCAP) || (Op == Mips::STORECAP))
+            continue;
+          if (IncOffset->getOperand(1).getReg() == Mips::C0)
+            C0Ops.emplace_back(&I, IncOffset);
+          continue;
+        }
+        // Ignore ones that are not based on a CIncOffset op
+        if (IncOffset->getOpcode() != Mips::CIncOffset)
           continue;
         assert(IncOffset);
 
@@ -137,6 +172,35 @@ struct CheriAddressingModeFolder : public MachineFunctionPass {
         IncOffsets.insert(IncOffset);
         modified = true;
       }
+    for (auto &I : C0Ops) {
+      IncOffsets.insert(I.second);
+      unsigned BaseReg = I.second->getOperand(2).getReg();
+      // This can't be a symbolic address (yet) because we don't have
+      // relocations that fit.
+      MachineOperand &Offset = I.first->getOperand(2);
+      // If this was the result of a daddiu, fold the immediate into the result
+      // as well.
+      MachineInstr *AddInst = RI.getUniqueVRegDef(BaseReg);
+      if (AddInst && (AddInst->getOpcode() == Mips::DADDiu)) {
+        MachineOperand &MO = AddInst->getOperand(2);
+        // FIXME: We could probably fold the add into the load in some cases
+        // even when it's not initially zero, as our immediate field has just
+        // grown, but that's probably not the common case.
+        if (Offset.isImm() && (Offset.getImm() == 0)) {
+          Offset = MO;
+          BaseReg = AddInst->getOperand(1).getReg();
+        }
+      } else
+        AddInst = nullptr;
+      BuildMI(*I.first->getParent(), I.first, I.first->getDebugLoc(),
+          InstrInfo->get(MipsOpForCHERIOp(I.first->getOpcode())),
+          I.first->getOperand(0).getReg())
+        .addReg(BaseReg).addOperand(Offset);
+      I.first->eraseFromBundle();
+      if (AddInst)
+        IncOffsets.insert(AddInst);
+      modified = true;
+    }
     for (auto &I : SetPCCOffsets) {
       unsigned PCC =
           MF.getRegInfo().createVirtualRegister(&Mips::CheriRegsRegClass);
