3a1ceed4c7141da5edc9a6eab9f9c725155678bf
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2016-11-24 00:12:27 +0000

    Ensure function type template parameters are properly qualified

--- clang/lib/Sema/SemaTemplateDeduction.cpp
+++ clang/lib/Sema/SemaTemplateDeduction.cpp
@@ -3311,7 +3311,8 @@ static bool AdjustFunctionParmAndArgTypesForDeduction(
     //     function-to-pointer standard conversion (4.3) is used in place
     //     of A for type deduction; otherwise,
     else if (ArgType->isFunctionType())
-      ArgType = S.Context.getPointerType(ArgType);
+      ArgType = S.Context.getPointerType(ArgType,
+                      S.Context.getTargetInfo().areAllPointersCapabilities());
     else {
       // - If A is a cv-qualified type, the top level cv-qualifiers of A's
       //   type are ignored for type deduction.
---
+++ clang/test/SemaCXX/cheri-template-param-fn-type.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 -triple cheri-unknown-freebsd -target-abi sandbox -fsyntax-only -ast-dump %s | FileCheck %s
+void snprintf (char * s) { }
+
+template<typename P >
+void as_string(P sprintf_like) { }
+
+void to_string() {
+  // CHECK: ParmVarDecl {{.*}} {{.*}} {{.*}} sprintf_like 'void (* __capability)(char * __capability)':'void (* __capability)(char * __capability)'
+  as_string(snprintf);
+}
