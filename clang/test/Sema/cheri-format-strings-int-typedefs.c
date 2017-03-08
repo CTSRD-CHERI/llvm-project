a91e06c12c4bc2a47cf89126b506383c04c06628
Alex Richardson <arichardson.kde@gmail.com>
2017-03-08 12:38:22 +0000

    Update test/Sema/cheri-format-strings-int-typedefs.c
    After the last commit the typedefs are now correctly qualified with
    __capability.

--- clang/test/Sema/cheri-format-strings-int-typedefs.c
+++ clang/test/Sema/cheri-format-strings-int-typedefs.c
@@ -11,20 +11,20 @@ void test(void) {
   printf("%zu", 42.0); // expected-warning {{format specifies type 'size_t' (aka 'unsigned long')}}
   printf("%td", 42.0); // expected-warning {{format specifies type 'ptrdiff_t' (aka 'long')}}
   printf("%lc", 42.0); // expected-warning {{format specifies type 'wint_t' (aka 'int')}}
-  printf("%ls", 42.0); // expected-warning {{format specifies type 'wchar_t *' (aka 'int *')}}
-  printf("%S", 42.0);  // expected-warning {{format specifies type 'wchar_t *' (aka 'int *')}}
+  printf("%ls", 42.0); // expected-warning {{format specifies type 'wchar_t *' (aka 'int * __capability')}}
+  printf("%S", 42.0);  // expected-warning {{format specifies type 'wchar_t *' (aka 'int * __capability')}}
   printf("%C", 42.0);  // expected-warning {{format specifies type 'wchar_t' (aka 'int')}}
 
-  scanf("%jd", 0); // expected-warning {{format specifies type 'intmax_t *' (aka 'long *')}}
-  scanf("%ju", 0); // expected-warning {{format specifies type 'uintmax_t *' (aka 'unsigned long *')}}
-  scanf("%Pd", 0); // expected-warning {{format specifies type 'intptr_t *' (aka '__intcap_t *')}}
-  scanf("%Pu", 0); // expected-warning {{format specifies type 'uintptr_t *' (aka '__uintcap_t *')}}
-  scanf("%zu", 0); // expected-warning {{format specifies type 'size_t *' (aka 'unsigned long *')}}
-  scanf("%td", 0); // expected-warning {{format specifies type 'ptrdiff_t *' (aka 'long *')}}
-  scanf("%lc", 0); // expected-warning {{format specifies type 'wchar_t *' (aka 'int *')}}
-  scanf("%ls", 0); // expected-warning {{format specifies type 'wchar_t *' (aka 'int *')}}
-  scanf("%S",  0);  // expected-warning {{format specifies type 'wchar_t *' (aka 'int *')}}
-  scanf("%C",  0);  // expected-warning {{format specifies type 'wchar_t *' (aka 'int *')}}
+  scanf("%jd", 0); // expected-warning {{format specifies type 'intmax_t *' (aka 'long * __capability')}}
+  scanf("%ju", 0); // expected-warning {{format specifies type 'uintmax_t *' (aka 'unsigned long * __capability')}}
+  scanf("%Pd", 0); // expected-warning {{format specifies type 'intptr_t *' (aka '__intcap_t * __capability')}}
+  scanf("%Pu", 0); // expected-warning {{format specifies type 'uintptr_t *' (aka '__uintcap_t * __capability')}}
+  scanf("%zu", 0); // expected-warning {{format specifies type 'size_t *' (aka 'unsigned long * __capability')}}
+  scanf("%td", 0); // expected-warning {{format specifies type 'ptrdiff_t *' (aka 'long * __capability')}}
+  scanf("%lc", 0); // expected-warning {{format specifies type 'wchar_t *' (aka 'int * __capability')}}
+  scanf("%ls", 0); // expected-warning {{format specifies type 'wchar_t *' (aka 'int * __capability')}}
+  scanf("%S",  0);  // expected-warning {{format specifies type 'wchar_t *' (aka 'int * __capability')}}
+  scanf("%C",  0);  // expected-warning {{format specifies type 'wchar_t *' (aka 'int * __capability')}}
 
 
   // typedef size_t et al. to something crazy.
