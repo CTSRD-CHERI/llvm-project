5532bf61093acc63d72c440a8213d291d79ba4ab
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-03-17 18:06:57 +0000

    Revert "LTO: Fix a potential race condition in the caching API." (Needs LLVM change)
    This reverts commit 0a62e298e730a0aa7c318eed1aa470d3e218d7f4.

--- lld/ELF/LTO.cpp
+++ lld/ELF/LTO.cpp
@@ -150,11 +150,10 @@ std::vector<InputFile *> BitcodeCompiler::compile() {
   // specified, configure LTO to use it as the cache directory.
   lto::NativeObjectCache Cache;
   if (!Config->ThinLTOCacheDir.empty())
-    Cache = check(
-        lto::localCache(Config->ThinLTOCacheDir,
-                        [&](size_t Task, std::unique_ptr<MemoryBuffer> MB) {
-                          Files[Task] = std::move(MB);
-                        }));
+    Cache = check(lto::localCache(
+        Config->ThinLTOCacheDir, [&](size_t Task, StringRef Path) {
+          Files[Task] = check(MemoryBuffer::getFile(Path));
+        }));
 
   checkError(LTOObj->run(
       [&](size_t Task) {
