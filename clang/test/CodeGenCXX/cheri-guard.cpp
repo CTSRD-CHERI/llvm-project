6578edd36d465d2aa067f3b786b67d1f5367821a
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-03-21 11:22:49 +0000

    Add test for guards

---
+++ clang/test/CodeGenCXX/cheri-guard.cpp
@@ -0,0 +1,14 @@
+// RUN:  %clang_cc1 -triple cheri-unknown-freebsd -target-abi sandbox -emit-llvm -std=c++11 -o - %s | FileCheck %s
+struct foo {
+  foo();
+};
+
+// CHECK: @_ZGVZ1fvE1x = internal addrspace(200) global i64 0, align 8
+foo *f() {
+	// CHECK: call i32 @__cxa_guard_acquire(i64 addrspace(200)* @_ZGVZ1fvE1x)
+  static foo x;
+	// CHECK: call void @__cxa_guard_release(i64 addrspace(200)* @_ZGVZ1fvE1x)
+  return &x;
+}
+
+// CHECK: declare i32 @__cxa_guard_acquire(i64 addrspace(200)*) #1
