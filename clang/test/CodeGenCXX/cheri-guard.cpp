02e2c2c2f078b17dd6c3109d37765bc77aaf0429
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-03-23 14:00:45 +0000

    Slightly extend cheri-guard.cpp test

--- clang/test/CodeGenCXX/cheri-guard.cpp
+++ clang/test/CodeGenCXX/cheri-guard.cpp
@@ -5,10 +5,15 @@ struct foo {
 
 // CHECK: @_ZGVZ1fvE1x = internal addrspace(200) global i64 0, align 8
 foo *f() {
-	// CHECK: call i32 @__cxa_guard_acquire(i64 addrspace(200)* @_ZGVZ1fvE1x)
+  // CHECK: %0 = load atomic i8, i8 addrspace(200)* bitcast (i64 addrspace(200)* @_ZGVZ1fvE1x to i8 addrspace(200)*) acquire, align 8
+  // CHECK: call i32 @__cxa_guard_acquire(i64 addrspace(200)* @_ZGVZ1fvE1x)
+  // CHECK: call void @_ZN3fooC1Ev(%struct.foo addrspace(200)* @_ZZ1fvE1x)
+  // CHECK: call void @__cxa_guard_release(i64 addrspace(200)* @_ZGVZ1fvE1x)
+  // CHECK: ret %struct.foo addrspace(200)* @_ZZ1fvE1x
   static foo x;
-	// CHECK: call void @__cxa_guard_release(i64 addrspace(200)* @_ZGVZ1fvE1x)
   return &x;
 }
 
 // CHECK: declare i32 @__cxa_guard_acquire(i64 addrspace(200)*) #1
+// CHECK: declare void @__cxa_guard_release(i64 addrspace(200)*) #1
+
