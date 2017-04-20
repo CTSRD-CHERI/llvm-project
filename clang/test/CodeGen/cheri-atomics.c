313fccf577c4701d264462f81146a1bf7421f575
Alex Richardson <arichardson.kde@gmail.com>
2017-04-20 12:47:41 +0100

    Make cheri-atomics pass with changed IR names

--- clang/test/CodeGen/cheri-atomics.c
+++ clang/test/CodeGen/cheri-atomics.c
@@ -1,41 +1,46 @@
 // RUN: %clang_cc1 %s -target-abi purecap -emit-llvm -triple cheri-unknown-freebsd -o - | FileCheck %s
 int main(void) {
   _Atomic(int*) p;
-	// CHECK:  %p = alloca i32 addrspace(200)*, align 32
-	// CHECK:  %.atomictmp = alloca i64, align 8
-	// CHECK:  %atomic-temp = alloca i32 addrspace(200)*, align 32
-	// CHECK:  %atomic-temp1 = alloca i32 addrspace(200)*, align 32
-	// CHECK:  %c = alloca i32 addrspace(200)*, align 32
-	// CHECK:  %atomic-temp2 = alloca i32 addrspace(200)*, align 32
-	// CHECK:  store i64 4, i64 addrspace(200)* %.atomictmp, align 8
-	// CHECK:  %0 = bitcast i32 addrspace(200)* addrspace(200)* %atomic-temp to i8 addrspace(200)*
-	// CHECK:  %1 = bitcast i64 addrspace(200)* %.atomictmp to i8 addrspace(200)*
-	// CHECK:  call void @llvm.memcpy.p200i8.p200i8.i64(i8 addrspace(200)* %0, i8 addrspace(200)* %1, i64 8, i32 8, i1 false)
-	// CHECK:  %2 = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
-	// CHECK:  %3 = bitcast i32 addrspace(200)* addrspace(200)* %atomic-temp to i8 addrspace(200)*
-	// CHECK:  %4 = bitcast i32 addrspace(200)* addrspace(200)* %atomic-temp1 to i8 addrspace(200)*
-	// CHECK:  call void @__atomic_fetch_add(i64 zeroext 32, i8 addrspace(200)* %2, i8 addrspace(200)* %3, i8 addrspace(200)* %4, i32 signext 5)
+  // CHECK:  %p = alloca i32 addrspace(200)*, align 32, addrspace(200)
+  // CHECK:  [[ATOMIC_TMP1:%.*]] = alloca i64, align 8, addrspace(200)
+  // CHECK:  [[ATOMIC_TMP2:%.*]] = alloca i32 addrspace(200)*, align 32
+  // CHECK:  [[ATOMIC_TMP3:%.*]] = alloca i32 addrspace(200)*, align 32
+  // CHECK:  %c = alloca i32 addrspace(200)*, align 32
+  // CHECK:  [[ATOMIC_TMP4:%.*]] = alloca i32 addrspace(200)*, align 32
+  // CHECK:  [[ATOMIC_TMP5:%.*]] = alloca i32 addrspace(200)*, align 32
+  // CHECK:  %expected = alloca i32 addrspace(200)*, align 32, addrspace(200)
+  // CHECK:  [[ATOMIC_TMP6:%.*]] = alloca i32 addrspace(200)*, align 32
+
+  // CHECK:  store i64 4, i64 addrspace(200)* [[ATOMIC_TMP1]], align 8
+  // CHECK:  [[AS_I8PTR:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP2]] to i8 addrspace(200)*
+  // CHECK:  [[AS_I8PTR2:%.*]] = bitcast i64 addrspace(200)* [[ATOMIC_TMP1]] to i8 addrspace(200)*
+  // CHECK:  call void @llvm.memcpy.p200i8.p200i8.i64(i8 addrspace(200)* [[AS_I8PTR]], i8 addrspace(200)* [[AS_I8PTR2]], i64 8, i32 8, i1 false)
+  // CHECK:  [[FETCHADD_ARG1:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
+  // CHECK:  [[FETCHADD_ARG2:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP2]] to i8 addrspace(200)*
+  // CHECK:  [[FETCHADD_ARG3:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP3]] to i8 addrspace(200)*
+  // CHECK:  call void @__atomic_fetch_add(i64 zeroext 32, i8 addrspace(200)* [[FETCHADD_ARG1]], i8 addrspace(200)* [[FETCHADD_ARG2]], i8 addrspace(200)* [[FETCHADD_ARG3]], i32 signext 5)
   __c11_atomic_fetch_add(&p, 1, __ATOMIC_SEQ_CST);
 
-	// CHECK:  %5 = load i32 addrspace(200)*, i32 addrspace(200)* addrspace(200)* %atomic-temp1, align 32
-	// CHECK:  %6 = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
-	// CHECK:  %7 = bitcast i32 addrspace(200)* addrspace(200)* %atomic-temp2 to i8 addrspace(200)*
-	// CHECK:  call void @__atomic_load(i64 zeroext 32, i8 addrspace(200)* %6, i8 addrspace(200)* %7, i32 signext 5)
-	// CHECK:  %8 = load i32 addrspace(200)*, i32 addrspace(200)* addrspace(200)* %atomic-temp2, align 32
-	// CHECK:  store i32 addrspace(200)* %8, i32 addrspace(200)* addrspace(200)* %c, align 32
+  // CHECK:  [[DEAD:%.*]] = load i32 addrspace(200)*, i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP3]], align 32
+  // CHECK:  [[LOAD_ARG1:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
+  // CHECK:  [[LOAD_ARG2:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP4]] to i8 addrspace(200)*
+  // CHECK:  call void @__atomic_load(i64 zeroext 32, i8 addrspace(200)* [[LOAD_ARG1]], i8 addrspace(200)* [[LOAD_ARG2]], i32 signext 5)
+  // CHECK:  [[TMP:%.*]] = load i32 addrspace(200)*, i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP4]], align 32
+  // CHECK:  store i32 addrspace(200)* [[TMP]], i32 addrspace(200)* addrspace(200)* %c, align 32
   int *c = __c11_atomic_load(&p, __ATOMIC_SEQ_CST);
 
-  // CHECK: store i32 addrspace(200)* null, i32 addrspace(200)* addrspace(200)* %.atomictmp3, align 32
-  // CHECK: %9 = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
-  // CHECK: %10 = bitcast i32 addrspace(200)* addrspace(200)* %.atomictmp3 to i8 addrspace(200)*
-  // CHECK: call void @__atomic_store(i64 zeroext 32, i8 addrspace(200)* %9, i8 addrspace(200)* %10, i32 signext 5)
-	__c11_atomic_store(&p, 0, __ATOMIC_SEQ_CST);
+  // CHECK: store i32 addrspace(200)* null, i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP5]], align 32
+  // CHECK: [[STORE_ARG1:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
+  // CHECK: [[STORE_ARG2:%.*]] = bitcast i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP5]] to i8 addrspace(200)*
+  // CHECK: call void @__atomic_store(i64 zeroext 32, i8 addrspace(200)* [[STORE_ARG1]], i8 addrspace(200)* [[STORE_ARG2]], i32 signext 5)
+  __c11_atomic_store(&p, 0, __ATOMIC_SEQ_CST);
 
-	// CHECK:  %11 = load i32 addrspace(200)*, i32 addrspace(200)* addrspace(200)* %c, align 32
-	// CHECK:  store i32 addrspace(200)* %11, i32 addrspace(200)* addrspace(200)* %.atomictmp4, align 32
-	// CHECK:  %12 = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
-	// CHECK:  %13 = bitcast i32 addrspace(200)* addrspace(200)* %.atomictmp4 to i8 addrspace(200)*
-	// CHECK:  %call = call zeroext i1 @__atomic_compare_exchange(i64 zeroext 32, i8 addrspace(200)* %12, i8 addrspace(200)* null, i8 addrspace(200)* %13, i32 signext 5, i32 signext 5)
-	__c11_atomic_compare_exchange_strong(&p, 0, c, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+  // CHECK:  store i32 addrspace(200)* null, i32 addrspace(200)* addrspace(200)* %expected, align 32
+  // CHECK:  [[CMPXCHG_ARG1:%.*]]  = bitcast i32 addrspace(200)* addrspace(200)* %p to i8 addrspace(200)*
+  // CHECK:  [[CMPXCHG_ARG2:%.*]]  = bitcast i32 addrspace(200)* addrspace(200)* %expected to i8 addrspace(200)*
+  // CHECK:  [[CMPXCHG_ARG3:%.*]]  = bitcast i32 addrspace(200)* addrspace(200)* [[ATOMIC_TMP6]] to i8 addrspace(200)*
+  // CHECK:  %call = call zeroext i1 @__atomic_compare_exchange(i64 zeroext 32, i8 addrspace(200)* [[CMPXCHG_ARG1]], i8 addrspace(200)* [[CMPXCHG_ARG2]], i8 addrspace(200)* [[CMPXCHG_ARG3]], i32 signext 5, i32 signext 5)
+  int* expected = 0;
+  __c11_atomic_compare_exchange_strong(&p, &expected, c, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
   return 0;
 }
