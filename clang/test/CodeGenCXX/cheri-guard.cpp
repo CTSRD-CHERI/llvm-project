d486da438bdca393ee5018169d59601f44fb4e6e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-04-26 17:02:14 +0000

    Make CodeGenCXX/cheri-guard.cpp pass on FreeBSD
    Again numbering starts at one instead of zero for some strange reason.
    
    There aren't any instructions before so I don't see why it would already
    be used.
    
    ```
    ; Function Attrs: noinline nounwind
    define %struct.foo addrspace(200)* @_Z1fv() #0 {
      %1 = load atomic i8, i8 addrspace(200)* bitcast (i64 addrspace(200)* @_ZGVZ1fvE1x to i8 addrspace(200)*) acquire, align 8
    
    ```

--- clang/test/CodeGenCXX/cheri-guard.cpp
+++ clang/test/CodeGenCXX/cheri-guard.cpp
@@ -5,7 +5,8 @@ struct foo {
 
 // CHECK: @_ZGVZ1fvE1x = internal addrspace(200) global i64 0, align 8
 foo *f() {
-  // CHECK: %0 = load atomic i8, i8 addrspace(200)* bitcast (i64 addrspace(200)* @_ZGVZ1fvE1x to i8 addrspace(200)*) acquire, align 8
+  // CHECK: define %struct.foo addrspace(200)* @_Z1fv() #0 {
+  // CHECK-NEXT: %{{.+}} = load atomic i8, i8 addrspace(200)* bitcast (i64 addrspace(200)* @_ZGVZ1fvE1x to i8 addrspace(200)*) acquire, align 8
   // CHECK: call i32 @__cxa_guard_acquire(i64 addrspace(200)* @_ZGVZ1fvE1x)
   // CHECK: call void @_ZN3fooC1Ev(%struct.foo addrspace(200)* @_ZZ1fvE1x)
   // CHECK: call void @__cxa_guard_release(i64 addrspace(200)* @_ZGVZ1fvE1x)
