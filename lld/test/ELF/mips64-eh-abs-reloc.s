8641190232a8636a54716c8f858f74f31cd5dd72
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-05-30 13:31:01 +0100

    Make test from https://reviews.llvm.org/D33670 pass

--- lld/test/ELF/mips64-eh-abs-reloc.s
+++ lld/test/ELF/mips64-eh-abs-reloc.s
@@ -36,8 +36,11 @@
 # NO-RELOCS-NEXT: ]
 
 # PIC-RELOCS: Relocations [
-# PIC-RELOCS-NEXT:  Section (7) .rela.dyn {
-# PIC-RELOCS-NEXT:    {{0x.+}} R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x10000
+# XXXAR: we use REL for FreeBSD instead of RELA:
+# UPSTREAM-PIC-RELOCS-NEXT:  Section (7) .rela.dyn {
+# UPSTREAM-PIC-RELOCS-NEXT:    {{0x.+}} R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x10000
+# PIC-RELOCS-NEXT:  Section (7) .rel.dyn {
+# PIC-RELOCS-NEXT:    {{0x.+}} R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0
 # PIC-RELOCS-NEXT:  }
 # PIC-RELOCS-NEXT:]
 
