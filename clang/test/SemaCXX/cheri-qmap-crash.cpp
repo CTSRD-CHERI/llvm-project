7d7c18d4ccc30a73b5a4eb4185f4f89d1ffa7f58
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-06-06 17:32:59 +0100

    Fix SemaCXX tests

--- clang/test/SemaCXX/cheri-qmap-crash.cpp
+++ clang/test/SemaCXX/cheri-qmap-crash.cpp
@@ -17,8 +17,8 @@ struct QMapNodeBase
     QMapNodeBase *parent() const { return reinterpret_cast<QMapNodeBase *>(p & ~Mask); } // expected-warning {{binary expression on capability and non-capability types}}
     void setParent(QMapNodeBase *pp) { p = (p & Mask) | quintptr(pp); } // expected-warning {{binary expression on capability and non-capability types}}
 
-    bool comparisonOp() const { return (p & Mask) == Black; }
-    bool comparisonOp2() const { return Red == (p & Mask); }
+    bool comparisonOp() const { return (p & Mask) == Black; } // expected-warning {{binary expression on capability and non-capability types}}
+    bool comparisonOp2() const { return Red == (p & Mask); } // expected-warning {{binary expression on capability and non-capability types}}
 
-    bool capRHS() const { return (Black | p) == p; }
+    bool capRHS() const { return (Black | p) == p; } // expected-warning {{binary expression on capability and non-capability types}}
 };
