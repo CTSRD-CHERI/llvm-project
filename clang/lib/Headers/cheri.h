5a815d3dfc091cbffca492dd61cf7d8be2535297
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-06-12 13:40:45 +0000

    Fix uses of __capability in lib/Headers/cheri.h

--- clang/lib/Headers/cheri.h
+++ clang/lib/Headers/cheri.h
@@ -49,14 +49,14 @@ typedef __INTPTR_TYPE__ uintcap_t;
 
 #define __CHERI_GET(__name, __type, __get, __default)            \
   static inline __type                                                        \
-  cheri_##__name##__get(capability void *__cap)                              \
+  cheri_##__name##__get(void * capability __cap)                              \
   {                                                                           \
     return __IF_CAPS(__builtin_cheri_##__name##__get(__cap), __default);     \
   }
 
 #define __CHERI_SET(__name, __type, __set)                                   \
-  static inline capability void*                                              \
-  cheri_##__name##__set(const capability void *__cap, __type __val)          \
+  static inline void* capability                                             \
+  cheri_##__name##__set(const void * capability __cap, __type __val)          \
   {                                                                           \
     return __IF_CAPS(__builtin_cheri_##__name##__set(__cap, __val),          \
                      (void*)__cap);                                           \
@@ -75,47 +75,47 @@ __CHERI_GET(tag, _Bool, _get, 0)
 __CHERI_GET(sealed, _Bool, _get, 0)
 
 static inline
-capability void *cheri_offset_increment(const capability void *__cap,
+void * capability cheri_offset_increment(const void *capability __cap,
                                          __PTRDIFF_TYPE__ __offset) {
   return __IF_CAPS(__builtin_cheri_offset_increment(__cap, __offset),
                    ((char*)__cap) + __offset);
 }
 
 static inline
-capability void *cheri_tag_clear(const capability void *__cap) {
+void * capability cheri_tag_clear(const void * capability __cap) {
   return __IF_CAPS(__builtin_cheri_tag_clear(__cap), (void*)__cap);
 }
 
 static inline
-capability void *cheri_seal(const capability void *__cap,
-                             const capability void *__type) {
+void * capability cheri_seal(const void * capability __cap,
+                             const void * capability __type) {
   return __IF_CAPS(__builtin_cheri_seal(__cap, __type), (void*)__cap);
 }
 
 static inline
-capability void *cheri_unseal(const capability void *__cap,
-                               const capability void *__type) {
+void * capability cheri_unseal(const void * capability __cap,
+                               const void * capability __type) {
   return __IF_CAPS(__builtin_cheri_unseal(__cap, __type), (void*)__cap);
 }
 
 static inline
-void cheri_perms_check(const capability void *__cap, cheri_perms_t __perms) {
+void cheri_perms_check(const void * capability __cap, cheri_perms_t __perms) {
   __IF_CAPS(__builtin_cheri_perms_check(__cap, __perms), );
 }
 
 static inline
-void cheri_type_check(const capability void *__cap,
-                       const capability void *__type) {
+void cheri_type_check(const void * capability __cap,
+                      const void * capability __type) {
   __IF_CAPS(__builtin_cheri_type_check(__cap, __type), );
 }
 
 static inline
-capability void *cheri_global_data_get(void) {
+void * capability cheri_global_data_get(void) {
   return __IF_CAPS(__builtin_cheri_global_data_get(), 0);
 }
 
 static inline
-capability void *cheri_program_counter_get(void) {
+void * capability cheri_program_counter_get(void) {
   return __IF_CAPS(__builtin_cheri_program_counter_get(), 0);
 }
 
