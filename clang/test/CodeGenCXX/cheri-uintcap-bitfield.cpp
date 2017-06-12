1701626601cb22e9739fca6d21c030a06efbbc17
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-06-12 12:11:11 +0100

    Improve test case for #137

--- clang/test/CodeGenCXX/cheri-uintcap-bitfield.cpp
+++ clang/test/CodeGenCXX/cheri-uintcap-bitfield.cpp
@@ -2,15 +2,30 @@
 // XFAIL: *
 // Bitfields with underlying type __uintcap_t used to cause assertions. Found while compiling qdatetime.cpp for purecap ABI
 
-typedef __intcap_t a;
-class b {
-  void c();
-  struct B {
-    a e : sizeof(void *);
-  };
+typedef __intcap_t qintptr;
+typedef __uintcap_t quintptr;
+
+struct ShortData {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        quintptr status : 8;
+#endif
+
+        qintptr msecs : sizeof(void *) * 8 - 8;
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        quintptr status : 8;
+#endif
+};
+
+class QDateTime {
+  void setMsecs(long f);
   struct {
-    B data;
+    ShortData data;
   } d;
 };
-long f;
-void b::c() { d.data.e = f; }
+
+void QDateTime::setMsecs(long f) {
+  d.data.msecs = f;
+  d.data.msecs = qintptr(f);
+  d.data.status = 24;
+}
