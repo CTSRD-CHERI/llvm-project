b64998905ea744e67c803d682494c26278e66cae
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-07-05 12:54:19 +0000

    Fix test case for #144

--- clang/test/CodeGen/cheri-function-assign-crash.c
+++ clang/test/CodeGen/cheri-function-assign-crash.c
@@ -1,7 +1,7 @@
 // check that it doesn't crash
 // RUN: %clang_cc1 -triple cheri-unknown-freebsd -emit-obj -target-cpu cheri -target-abi purecap -O2 -std=gnu99 -o /dev/null %s
 // next check that it emits sensible IR:
-// RUN: %clang_cc1 -triple cheri-unknown-freebsd -emit-obj -target-cpu cheri -target-abi purecap -O2 -std=gnu99 -o - -emit-llvm | FileCheck %s
+// RUN: %clang_cc1 -triple cheri-unknown-freebsd -emit-obj -target-cpu cheri -target-abi purecap -O2 -std=gnu99 -o - -emit-llvm %s | FileCheck %s
 
 // https://github.com/CTSRD-CHERI/clang/issues/144
 // XFAIL: *
