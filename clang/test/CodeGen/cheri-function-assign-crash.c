d6fc6f2ff17323fbca9cb64c465b1275c6fc0525
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-07-05 12:50:05 +0000

    Add test case for #144

---
+++ clang/test/CodeGen/cheri-function-assign-crash.c
@@ -0,0 +1,23 @@
+// check that it doesn't crash
+// RUN: %clang_cc1 -triple cheri-unknown-freebsd -emit-obj -target-cpu cheri -target-abi purecap -O2 -std=gnu99 -o /dev/null %s
+// next check that it emits sensible IR:
+// RUN: %clang_cc1 -triple cheri-unknown-freebsd -emit-obj -target-cpu cheri -target-abi purecap -O2 -std=gnu99 -o - -emit-llvm | FileCheck %s
+
+// https://github.com/CTSRD-CHERI/clang/issues/144
+// XFAIL: *
+
+int a();
+int d();
+int e();
+int *b() {
+  int *c;
+  if (a)
+    d();
+  c = a;
+  if (c == 0)
+    return 0;
+  e();
+  return c;
+}
+
+// CHECK: {{%.+}} = phi i32 addrspace(200)* [ {{%.+}}, {{%.+}} ], [ null, {{%.+}}]{{$}}
