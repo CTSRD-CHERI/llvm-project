1d06c15478c6bff804cfb5ec65cd9f2902b5e7ec
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-07-06 14:59:36 +0100

    Update two tests after the latest changes

--- clang/test/Sema/cheri-int-to-cap-cast.c
+++ clang/test/Sema/cheri-int-to-cap-cast.c
@@ -4,5 +4,9 @@ void * __capability c;
 void a(int x, long long y)
 {
 	b = (void * __capability)x; // expected-warning {{cast to 'void * __capability' from smaller integer type 'int'}}
+	// expected-warning@-1 {{cast from provenance-free integer type to pointer type will give pointer that can not be dereferenced}}
+	// expected-note@-2 {{insert cast to intptr_t to silence this warning}}
 	c = (void * __capability)y; // Should be no warning here - long long is 64 bits.
+	// expected-warning@-1 {{cast from provenance-free integer type to pointer type will give pointer that can not be dereferenced}}
+	// expected-note@-2 {{insert cast to intptr_t to silence this warning}}
 }
