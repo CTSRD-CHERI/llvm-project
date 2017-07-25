d80a63f2c4afbd5c0345688301e8e00b7320e23b
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-07-25 11:12:36 +0100

    Don't output obj code to stdout

--- clang/test/CodeGen/cheri-inregs-param-info.c
+++ clang/test/CodeGen/cheri-inregs-param-info.c
@@ -1,4 +1,4 @@
-// RUN:  not --crash %cheri_cc1 "-triple" "cheri-unknown-freebsd" "-emit-obj" "-target-feature" "+soft-float" "-target-abi" "purecap" "-O2" "-std=gnu99" "-cheri-linker" "-x" "c" %s -o -
+// RUN:  not --crash %cheri_cc1 "-triple" "cheri-unknown-freebsd" "-emit-obj" "-target-feature" "+soft-float" "-target-abi" "purecap" "-O2" "-std=gnu99" "-cheri-linker" "-x" "c" %s -o /dev/null
 // This crashes with an assertion: void llvm::CCState::getInRegsParamInfo(unsigned int, unsigned int &, unsigned int &) const: Assertion `InRegsParamRecordIndex < ByValRegs.size() && "Wrong ByVal parameter index"' failed.
 
 typedef struct { int err_msg[1024]; } Dwarf_Error;
