5eeab18c7345b973d4720aeaf0b62d4fd703b0f8
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-08-30 13:43:23 +0100

    Output error if cast increases required alignment.
    This happens when bitcasting from a pointer to a non-capability to a pointer to
    a capability. Here is an example:
    
    unsigned long *x; int * __capability * y = (int * __capability *)x;
    
    Here x has alignment 8 whereas x has alignment 32 on CHERI-256.
    
    The only time when an error will not be generated is when casting from char* or
    void*.

--- clang/test/Sema/cheri-cap-ptr-casts.c
+++ clang/test/Sema/cheri-cap-ptr-casts.c
@@ -1,12 +1,21 @@
+// RUN: %clang_cc1 %s -triple cheri-unknown-freebsd -DALIGN=1 -verify
 // RUN: %clang_cc1 %s -triple cheri-unknown-freebsd -fsyntax-only -ast-dump | FileCheck %s
+
+#ifdef ALIGN
 void f() {
+  unsigned long foo[8];
+  ((int * __capability *)foo)[0] = 0; // expected-error {{cast from 'unsigned long *' to 'int * __capability *' increases required alignment from 8 to 32}}
+}
+#else
+void g() {
   __capability char *x;
   // CHECK: CStyleCastExpr {{.*}} {{.*}} 'char *' <CHERICapabilityToPointer>
   char *y = (char *)x;
 }
 
-void g() {
+void h() {
   char *x;
   // CHECK: CStyleCastExpr {{.*}} {{.*}} 'char * __capability' <PointerToCHERICapability>
   __capability char *y = (__capability char *)x;
 }
+#endif
