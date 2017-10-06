db4e51b4c04ad131b1438073d1cf51ceb0948427
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-06 11:01:41 +0100

    Turn cheri-DECL-fail.c into a useful test

--- clang/test/Sema/cheri-DECL-fail.c
+++ clang/test/Sema/cheri-DECL-fail.c
@@ -1,3 +1,7 @@
-// RUN: %clang_cc1 -triple cheri-unknown-freebsd -emit-obj -disable-free -main-file-name opt.c -mrelocation-model pic -pic-level 2 -mthread-model posix -fno-signed-zeros -masm-verbose -mconstructor-aliases -target-cpu cheri -target-feature +soft-float -target-abi purecap -Wmips-cheri-prototypes -msoft-float -mfloat-abi soft -dwarf-column-info -debug-info-kind=standalone -dwarf-version=2 -debugger-tuning=gdb -coverage-notes-file /local/scratch/rmk35/ffmpeg/libavutil/opt.gcno -D _ISOC99_SOURCE -D _FILE_OFFSET_BITS=64 -D _LARGEFILE_SOURCE -D PIC -D HAVE_AV_CONFIG_H -O3 -Wdeclaration-after-statement -Wall -Wdisabled-optimization -Wpointer-arith -Wredundant-decls -Wwrite-strings -Wtype-limits -Wundef -Wmissing-prototypes -Wno-pointer-to-int-cast -Wstrict-prototypes -Wempty-body -Wno-parentheses -Wno-switch -Wno-format-zero-length -Wno-pointer-sign -Wno-unused-const-variable -Werror=implicit-function-declaration -Werror=missing-prototypes -Werror=return-type -std=c11 -fconst-strings -ferror-limit 19 -fmessage-length 159 -pthread -mstack-alignment=16 -fobjc-runtime=gnustep -fdiagnostics-show-option -fcolor-diagnostics -vectorize-loops -vectorize-slp -cheri-linker -x c %s -o - | FileCheck %s
-// XFAIL: *
+// RUN: %cheri_purecap_cc1 -fsyntax-only -verify %s
+// This used to crash the compiler instead of emitting an invalid syntax error
+
 __END_DECLS a __capability
+// expected-error@-1 {{unknown type name '__END_DECLS'}}
+// expected-error@-2 {{'__capability' type specifier must precede the declarator}}
+// expected-error@-3 {{expected ';' after top level declarator}}
