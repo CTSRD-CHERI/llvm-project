91c9a66a10e0156f66ead6b40c225dcd336c728b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-07 23:42:49 +0100

    Add a test case for #157

---
+++ clang/test/Sema/cheri-capability-attr-invalid-type.c
@@ -0,0 +1,26 @@
+// RUN: %cheri_cc1 -target-abi n64 -fsyntax-only %s
+// Test case for https://github.com/CTSRD-CHERI/clang/issues/157
+// XFAIL: *
+
+#define a(b)                                                                   \
+  {                                                                            \
+    typedef __typeof__(b) c;                                                   \
+    __capability c d; }
+int e() {
+  int *pipv = (a(pip));
+}
+
+
+#  define ZEXPORT
+#  define FAR
+typedef unsigned char  Byte;
+#  define Bytef Byte FAR
+#define cheri_ptr_to_bounded_cap(ptr)   __extension__({ \
+        typedef __typeof__(ptr) __ptr_type;             \
+        typedef __capability __ptr_type __cap_type;     \
+ z_stream;
+typedef __capability z_stream FAR *z_streamp;
+int ZEXPORT compress2 (dest, destLen, source, sourceLen, level)     Bytef *dest;
+{
+  z_stream stream;
+  z_streamp stream_cap = cheri_ptr_to_bounded_cap(&stream);
