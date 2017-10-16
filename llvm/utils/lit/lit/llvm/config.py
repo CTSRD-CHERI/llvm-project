f7be3b720e70bc71eb06cd15a1d8851c3ac35391
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-16 14:13:53 +0100

    Make the %clang substitutions work when used in LLVM
    We really shouldn't need them but some tests don't work with llc or opt

--- llvm/utils/lit/lit/llvm/config.py
+++ llvm/utils/lit/lit/llvm/config.py
@@ -431,8 +431,9 @@ class LLVMConfig(object):
             self.config.substitutions.append(
                 ('%target_itanium_abi_host_triple', ''))
 
-        self.config.substitutions.append(
-            ('%src_include_dir', self.config.clang_src_dir + '/include'))
+        if hasattr(self.config, "clang_src_dir"):
+            self.config.substitutions.append(
+                ('%src_include_dir', self.config.clang_src_dir + '/include'))
 
         # FIXME: Find nicer way to prohibit this.
         self.config.substitutions.append(
