dbffaa5b46da28fac577342cf4726545143263d0
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-21 11:28:07 +0100

    Add a clang builtin define to detect the new cap table mode
    If we are using the capability table then __CHERI_CAPABILITY_TABLE__ will
    be set to 1. This is useful to make e.g. crt_init_globals() work for
    both modes.

---
+++ clang/test/Preprocessor/cheri-cap-table.c
@@ -0,0 +1,6 @@
+// RUN: %cheri_purecap_cc1 -E -dM -x c /dev/null | FileCheck %s -check-prefix NOTABLE
+// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table -E -dM -x c /dev/null | FileCheck %s -check-prefix CAPTABLE
+
+// NOTABLE-NOT: __CHERI_CAPABILITY_TABLE__
+// CAPTABLE: __CHERI_CAPABILITY_TABLE__ 1
+
