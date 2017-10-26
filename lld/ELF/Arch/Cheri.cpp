4a7a7983acd44ea19f36b5b7c851194a98dd0e62
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-26 14:27:34 +0100

    Don't fail due to duplicate symbols

--- lld/ELF/Arch/Cheri.cpp
+++ lld/ELF/Arch/Cheri.cpp
@@ -384,6 +384,10 @@ template <class ELFT> void CheriCapTableSection::addCapTableSymbols() {
     StringRef Name = it.first->getName();
     if (Name.empty())
       continue;
+    // Avoid duplicate symbol name errors for unnamed string constants:
+    // XXXAR: maybe renumber them instead?
+    if (Name.startswith(".L.str"))
+      continue;
     Symtab->addRegular(Saver.save(Name + "@CAPTABLE"), STV_HIDDEN,
                        STT_OBJECT, it.second * Config->CapabilitySize,
                        Config->CapabilitySize, STB_LOCAL, this, nullptr);
