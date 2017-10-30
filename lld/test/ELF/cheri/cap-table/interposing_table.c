36499211b6928dce0c32850813173aceb7e231cc
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-30 15:08:53 +0000

    Add test case for libc interposing table not being initialized

--- lld/test/ELF/cheri/cap-table/interposing_table.c
+++ lld/test/ELF/cheri/cap-table/interposing_table.c
@@ -1,6 +1,62 @@
-// RUN: mkdir -p %S/helloworld_src
-// RUN: tar xJf %S/Inputs/helloworld.tar.xz -C %S/helloworld_src
-// RUN: cd %S/helloworld_src/reproduce && ld.lld @%S/helloworld_src/reproduce/response.txt -o helloworld.exe
-// RUN: cd %S/helloworld_src/reproduce && ld.lld @%S/helloworld_src/reproduce/response.txt -o %t.exe
-// RUN: llvm-objdump -t -C -s %t.exe > /dev/null
-// RUN: false
+// RUN: %cheri256_cc1 -emit-obj -O2 -target-feature +soft-float -msoft-float -target-abi purecap -mllvm -cheri-cap-table %s -o %t.o
+// RUN: ld.lld %t.o %S/Inputs/interposing_table.o  -o %t.exe
+// RUN: llvm-objdump -t -C %t.exe | FileCheck %s
+
+#define SLOT_SYS(name) int __sys_##name(void) { return 0; }
+#define SLOT_LIBC(name) int __libc_##name(void) { return 0; }
+#define SLOT(unused, name) int name(void) { return 0; }
+
+SLOT_SYS(accept)
+SLOT_SYS(accept4)
+SLOT_SYS(aio_suspend)
+SLOT_SYS(close)
+SLOT_SYS(connect)
+SLOT_SYS(fcntl)
+SLOT_SYS(fsync)
+SLOT_SYS(fork)
+SLOT_SYS(msync)
+SLOT_SYS(nanosleep)
+SLOT_SYS(openat)
+SLOT_SYS(poll)
+SLOT_SYS(pselect)
+SLOT_SYS(read)
+SLOT_SYS(readv)
+SLOT_SYS(recvfrom)
+SLOT_SYS(recvmsg)
+SLOT_SYS(select)
+SLOT_SYS(sendmsg)
+SLOT_SYS(sendto)
+SLOT_SYS(setcontext)
+SLOT_SYS(sigaction)
+SLOT_SYS(sigprocmask)
+SLOT_SYS(sigsuspend)
+SLOT_LIBC(sigwait)
+SLOT_SYS(sigtimedwait)
+SLOT_SYS(sigwaitinfo)
+SLOT_SYS(swapcontext)
+SLOT_LIBC(system)
+SLOT_LIBC(tcdrain)
+SLOT_SYS(wait4)
+SLOT_SYS(write)
+SLOT_SYS(writev)
+SLOT(_pthread_mutex_init_calloc_cb, _pthread_mutex_init_calloc_cb_stub)
+SLOT(spinlock, __libc_spinlock_stub)
+SLOT(spinunlock, __libc_spinunlock_stub)
+SLOT_SYS(kevent)
+SLOT_SYS(wait6)
+SLOT_SYS(ppoll)
+SLOT_LIBC(map_stacks_exec)
+SLOT_SYS(fdatasync)
+SLOT_SYS(clock_nanosleep)
+
+typedef int (*interpos_func_t)(void);
+interpos_func_t *__libc_interposing_slot(int interposno);
+extern interpos_func_t __libc_interposing[] __attribute__((visibility("hidden")));
+
+int __start(void) {
+  return __libc_interposing[1] != (void*)0;
+}
+
+// CHECK: CAPABILITY RELOCATION RECORDS:
+// CHECK: 0x0000000120020000	Base: __wrap_accept (0x0000000120010170)	Offset: 0x0000000000000000	Length: 0x0000000000000038	Permissions: 0x8000000000000000 (Function)
+// CHECK: 0000000120020000 l       .data		 00000540 .hidden __libc_interposing
