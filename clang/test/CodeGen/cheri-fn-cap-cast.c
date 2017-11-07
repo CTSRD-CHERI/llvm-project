d1aa530229278df81b0176491a749068931b7204
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-11-07 14:55:27 +0000

    Fix more CodeGen tests

--- clang/test/CodeGen/cheri-fn-cap-cast.c
+++ clang/test/CodeGen/cheri-fn-cap-cast.c
@@ -5,6 +5,6 @@ void cheri_codeptr(const fnptr ptr)
 {
   // Check that this cast is PCC-relative and not GDC-relative
   // CHECK: llvm.mips.pcc.get
-  c = ptr;
+  c = (void (* __capability)(void))ptr;
 }
 
