df075a17393a3d273c0c6bb6806424e25008fc2f
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2017-11-07 14:18:20 +0000

    Update cheritest_libcheri_trustedstack-crash.c test

--- clang/test/CodeGen/CHERI/cheritest_libcheri_trustedstack-crash.c
+++ clang/test/CodeGen/CHERI/cheritest_libcheri_trustedstack-crash.c
@@ -1,11 +1,8 @@
-// RUN: %cheri_cc1 -target-abi n64 -O2 -std=gnu99 -fconst-strings -vectorize-loops -vectorize-slp  -o - -O0 -emit-llvm  %s
-// XFAIL: *
-// This crashes crashes during code gen
-// XXXAR: do we want to make this code an error by default?
+// RUN: %cheri_cc1 -target-abi n64 -O2 -std=gnu99 -fconst-strings -vectorize-loops -vectorize-slp  -o - -O0 -emit-llvm -verify %s
 
 struct a {
   void *__capability b;
 } * c;
 _Bool cheritest_libcheri_userfn_getstack(struct a *csfp) {
-  return csfp->b != c;
+  return csfp->b != c; // expected-error {{converting non-capability type 'struct a *' to capability type 'void * __capability' without an explicit cast}}
 }
