778b2f2507eb2cdedea2f5f938771cc7af08739d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-01-14 16:21:06 +0000

    Enable new TBAA by default for CHERI triples
    If we don't do this we get IR verifier failures when compiling files
    containing a recursive CHERI CCALL

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -547,8 +547,10 @@ static bool ParseCodeGenArgs(CodeGenOptions &Opts, ArgList &Args, InputKind IK,
     OPT_fuse_register_sized_bitfield_access);
   Opts.RelaxedAliasing = Args.hasArg(OPT_relaxed_aliasing);
   Opts.StructPathTBAA = !Args.hasArg(OPT_no_struct_path_tbaa);
+  // XXXAR: If we don't enable new struct path TBAA we get LLVM verifier issues
+  // when compiling files containing recursive cheri ccall
   Opts.NewStructPathTBAA = !Args.hasArg(OPT_no_struct_path_tbaa) &&
-                           Args.hasArg(OPT_new_struct_path_tbaa);
+    (Args.hasArg(OPT_new_struct_path_tbaa) || Triple.getArch() == llvm::Triple::cheri);
   Opts.FineGrainedBitfieldAccesses =
       Args.hasFlag(OPT_ffine_grained_bitfield_accesses,
                    OPT_fno_fine_grained_bitfield_accesses, false);
--- clang/test/CodeGen/CHERI/cheri-recursive-ccall.c
+++ clang/test/CodeGen/CHERI/cheri-recursive-ccall.c
@@ -1,6 +1,7 @@
+// RUN: %cheri_purecap_cc1 -emit-llvm -O2 -o /dev/null %s
+
 // For some reason TBAA is broken with recursive cheri_ccalls (cheri_stdio.c)
-// RUN: not %cheri_purecap_cc1 -emit-llvm -O2 -o - %s
-// RUN: not %cheri_purecap_cc1 -emit-llvm -O2 -o - %s 2>&1 | FileCheck %s
+// RUN: not %cheri_purecap_cc1 -no-struct-path-tbaa -emit-llvm -O2 -o /dev/null %s 2>&1 | FileCheck %s
 // CHECK: Access type node must be a valid scalar type
 struct {
   void *a;
