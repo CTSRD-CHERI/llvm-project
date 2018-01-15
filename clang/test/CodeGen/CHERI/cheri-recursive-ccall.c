6ee6aa94a8aa3f7f45a796f3dfcc1f2990d7e252
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-01-15 11:22:10 +0000

    Revert "Enable new TBAA by default for CHERI triples"
    It fixes one issue but causes 10000 other ones
    
    This reverts commit 1da281b638648e10cf5c9f2bcc4f48747ce2a984.

--- clang/test/CodeGen/CHERI/cheri-recursive-ccall.c
+++ clang/test/CodeGen/CHERI/cheri-recursive-ccall.c
@@ -1,8 +1,8 @@
-// RUN: %cheri_purecap_cc1 -emit-llvm -O2 -o /dev/null %s
-
 // For some reason TBAA is broken with recursive cheri_ccalls (cheri_stdio.c)
 // RUN: not %cheri_purecap_cc1 -no-struct-path-tbaa -emit-llvm -O2 -o /dev/null %s 2>&1 | FileCheck %s
 // CHECK: Access type node must be a valid scalar type
+// But this works with the new TBAA
+// RUN: %cheri_purecap_cc1 -new-struct-path-tbaa -emit-llvm -O2 -o /dev/null %s
 struct {
   void *a;
   void *b;
