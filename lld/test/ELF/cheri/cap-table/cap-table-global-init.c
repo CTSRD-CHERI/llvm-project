a356e3e5aa62e1ddb121974fbf3faf45b1406452
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-01-24 17:31:42 +0000

    Make a test less fragile by compiling the source at -O0

--- lld/test/ELF/cheri/cap-table/cap-table-global-init.c
+++ lld/test/ELF/cheri/cap-table/cap-table-global-init.c
@@ -1,4 +1,4 @@
-// RUN: %cheri128_purecap_cc1 -pedantic -Weverything %s -emit-obj -mllvm -mxgot -mllvm -cheri-cap-table -x c -O3 -o %t.o
+// RUN: %cheri128_purecap_cc1 -pedantic -Wextra %s -emit-obj -mllvm -mxgot -mllvm -cheri-cap-table -x c -O0 -o %t.o
 // RUN: ld.lld %t.o -o %t.exe
 // RUN: llvm-objdump -t -d -s %t.exe | FileCheck %s
 
@@ -8,7 +8,7 @@
 int global = 1;
 long global2 = 3;
 
-int main();
+int main(void);
 
 static int _start(void) __attribute__((used));
 DEFINE_CHERI_START_FUNCTION(_start)
@@ -17,14 +17,15 @@ static int _start(void) {
   return main();
 }
 
-int main() {
+int main(void) {
   return global + global2;
 }
+
 // CHECK-LABEL: SYMBOL TABLE:
-// CHECK-DAG: 0000000120010050    l     F .text		 0000001c _start
-// CHECK-DAG: 0000000120010000    g     F .text		 00000050 __start
-// CHECK-DAG: 00000001200300{{2|4}}0         .cap_table		 00000000 .hidden __cap_table_end
-// CHECK-DAG: 0000000120030000         .cap_table		 00000020 .hidden __cap_table_start
-// TODO-CHECK-DAG: 0000000120030000 l    d  .cap_table		 00000020 .hidden .cap_table
-// CHECK-DAG: 0000000120030000 l       .cap_table		 00000010 .hidden global@CAPTABLE
-// CHECK-DAG: 00000001200300{{1|2}}0 l       .cap_table		 00000010 .hidden global2@CAPTABLE
+// CHECK-DAG: 0000000120010050    l        F .text		 00000034 _start
+// CHECK-DAG: 0000000120010000    g        F .text		 00000050 __start
+// CHECK-DAG: 0000000120030030               .cap_table		 00000000 .hidden __cap_table_end
+// CHECK-DAG: 0000000120030000               .cap_table		 00000030 .hidden __cap_table_start
+// CHECK-DAG: 0000000120030000 l             .cap_table		 00000010 main@CAPTABLE
+// CHECK-DAG: 0000000120030010 l             .cap_table		 00000010 global@CAPTABLE
+// CHECK-DAG: 0000000120030020 l             .cap_table		 00000010 global2@CAPTABLE
