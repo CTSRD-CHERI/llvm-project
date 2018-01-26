qwattash/lld

Don't warn about setting size 0 for __cap_relocs for weak NULL symbols

ELF/Arch/Cheri.cpp
@@ -298,15 +298,39 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &Location,
           "for cap_reloc at" + Location.toString<ELFT>());
     return 0;
   }
-  if (TargetSize == 0 && !Reloc.Target.Symbol->isUndefined() &&
-      !Reloc.Target.Symbol->IsPreemptible) {
+  auto TargetSym = Reloc.Target.Symbol;
+  if (TargetSize == 0 && !TargetSym->IsPreemptible) {
+    bool WasWeakSym = !TargetSym->isLocal() && TargetSym->symbol()->WasUndefWeak;
+    if (WasWeakSym)  {
+      // Symbols previously declared as weak can have size 0 (if they then resolve
+      // to NULL)
+      // For example __preinit_array_start, etc. are generated by the linker as ABS
+      // symbols with value 0
+      if (TargetSym->getVA(0) != 0) {
+        StringRef Name = TargetSym->getName();
+        bool IsSpecialEndSymbol = Name.startswith("__") &&
+                                  Name.endswith("_end");
+        if (Name == "end" || Name == "_end" || Name == "etext" ||
+            Name == "_etext" || Name == "edata" || Name == "_edata")
+          IsSpecialEndSymbol = true;
+        // All previously weak symbols that resolve to an address should have
+        // a size > 0. The only exception are e.g. __preinit_array_end, etc.
+        // which should actually be zero size symbol since they are just markers
+        // for the end of a section and not usable as a valid pointer
+        if (!IsSpecialEndSymbol)
+          error("Zero size weak symbol did not resolve to NULL but 0x" +
+                utohexstr(TargetSym->getVA(0)) + ": " +
+                verboseToString<ELFT>(TargetSym, 0));
+      }
+      return 0;
+    }
     bool WarnAboutUnknownSize = true;
     // currently clang doesn't emit the necessary symbol information for local
     // string constants such as: struct config_opt opts[] = { { ..., "foo" },
     // { ..., "bar" } }; As this pattern is quite common don't warn if the
     // target section is .rodata.str
     if (DefinedRegular *DefinedSym =
-      dyn_cast<DefinedRegular>(Reloc.Target.Symbol)) {
+      dyn_cast<DefinedRegular>(TargetSym)) {
       if (DefinedSym->isSection() &&
           DefinedSym->Section->Name.startswith(".rodata.str")) {
         WarnAboutUnknownSize = false;
@@ -319,10 +343,10 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &Location,
            verboseToString<ELFT>(Reloc.Target) + "\n>>> referenced by " +
            Location.toString<ELFT>());
     }
-    if (OutputSection *OS = Reloc.Target.Symbol->getOutputSection()) {
+    if (OutputSection *OS = TargetSym->getOutputSection()) {
       // For negative offsets use 0 instead (we wan the range of the full symbol in that case)
       int64_t Offset = std::max((int64_t)0, Reloc.Target.Offset);
-      uint64_t TargetVA = Reloc.Target.Symbol->getVA(Offset);
+      uint64_t TargetVA = TargetSym->getVA(Offset);
       assert(TargetVA >= OS->Addr);
       uint64_t OffsetInOS = TargetVA - OS->Addr;
       // Use less-or-equal here to account for __end_foo symbols which point 1 past the section
@@ -337,8 +361,9 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &Location,
     } else {
       warn("Could not find size for symbol " +
            verboseToString<ELFT>(Reloc.Target) +
-           " and could not determine section size. Using UINT64_MAX.");
-      TargetSize = std::numeric_limits<uint64_t>::max();
+           " and could not determine section size. Using 0.");
+      // TargetSize = std::numeric_limits<uint64_t>::max();
+      return 0;
     }
   }
   return TargetSize;

CTSRD-CHERI/llvm-project

lld/ELF/Arch/Cheri.cpp
@@ -291,15 +291,39 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &Location,
           "for cap_reloc at" + Location.toString());
     return 0;
   }
-  if (TargetSize == 0 && !Reloc.Target.Symbol->isUndefined() &&
-      !Reloc.Target.Symbol->IsPreemptible) {
+  auto TargetSym = Reloc.Target.Symbol;
+  if (TargetSize == 0 && !TargetSym->IsPreemptible) {
+    bool WasWeakSym = !TargetSym->isLocal() && TargetSym->WasUndefWeak;
+    if (WasWeakSym)  {
+      // Symbols previously declared as weak can have size 0 (if they then resolve
+      // to NULL)
+      // For example __preinit_array_start, etc. are generated by the linker as ABS
+      // symbols with value 0
+      if (TargetSym->getVA(0) != 0) {
+        StringRef Name = TargetSym->getName();
+        bool IsSpecialEndSymbol = Name.startswith("__") &&
+                                  Name.endswith("_end");
+        if (Name == "end" || Name == "_end" || Name == "etext" ||
+            Name == "_etext" || Name == "edata" || Name == "_edata")
+          IsSpecialEndSymbol = true;
+        // All previously weak symbols that resolve to an address should have
+        // a size > 0. The only exception are e.g. __preinit_array_end, etc.
+        // which should actually be zero size symbol since they are just markers
+        // for the end of a section and not usable as a valid pointer
+        if (!IsSpecialEndSymbol)
+          error("Zero size weak symbol did not resolve to NULL but 0x" +
+                utohexstr(TargetSym->getVA(0)) + ": " +
+                lld::verboseToString<ELFT>(TargetSym, 0));
+      }
+      return 0;
+    }
     bool WarnAboutUnknownSize = true;
     // currently clang doesn't emit the necessary symbol information for local
     // string constants such as: struct config_opt opts[] = { { ..., "foo" },
     // { ..., "bar" } }; As this pattern is quite common don't warn if the
     // target section is .rodata.str
     if (Defined *DefinedSym =
-      dyn_cast<Defined>(Reloc.Target.Symbol)) {
+      dyn_cast<Defined>(TargetSym)) {
       if (DefinedSym->isSection() &&
           DefinedSym->Section->Name.startswith(".rodata.str")) {
         WarnAboutUnknownSize = false;
@@ -312,10 +336,10 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &Location,
            verboseToString<ELFT>(Reloc.Target) + "\n>>> referenced by " +
            Location.toString());
     }
-    if (OutputSection *OS = Reloc.Target.Symbol->getOutputSection()) {
+    if (OutputSection *OS = TargetSym->getOutputSection()) {
       // For negative offsets use 0 instead (we wan the range of the full symbol in that case)
       int64_t Offset = std::max((int64_t)0, Reloc.Target.Offset);
-      uint64_t TargetVA = Reloc.Target.Symbol->getVA(Offset);
+      uint64_t TargetVA = TargetSym->getVA(Offset);
       assert(TargetVA >= OS->Addr);
       uint64_t OffsetInOS = TargetVA - OS->Addr;
       // Use less-or-equal here to account for __end_foo symbols which point 1 past the section
@@ -330,8 +354,9 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &Location,
     } else {
       warn("Could not find size for symbol " +
            verboseToString<ELFT>(Reloc.Target) +
-           " and could not determine section size. Using UINT64_MAX.");
-      TargetSize = std::numeric_limits<uint64_t>::max();
+           " and could not determine section size. Using 0.");
+      // TargetSize = std::numeric_limits<uint64_t>::max();
+      return 0;
     }
   }
   return TargetSize;