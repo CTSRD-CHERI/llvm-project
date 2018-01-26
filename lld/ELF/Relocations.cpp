qwattash/lld

Initial implementation of capability table for static linking
Dynamic linking will need plt generation and rtld bits but static linkage
should work for now

ELF/Relocations.cpp
@@ -361,7 +361,8 @@ static bool isStaticLinkTimeConstant(RelExpr E, RelType Type,
                      R_MIPS_GOT_OFF, R_MIPS_GOT_OFF32, R_MIPS_GOT_GP_PC,
                      R_MIPS_TLSGD, R_GOT_PAGE_PC, R_GOT_PC, R_GOTONLY_PC,
                      R_GOTONLY_PC_FROM_END, R_PLT_PC, R_TLSGD_PC, R_TLSGD,
-                     R_PPC_PLT_OPD, R_TLSDESC_CALL, R_TLSDESC_PAGE, R_HINT>(E))
+                     R_PPC_PLT_OPD, R_TLSDESC_CALL, R_TLSDESC_PAGE, R_HINT,
+                     R_CHERI_CAPABILITY_TABLE_INDEX>(E))
     return true;
 
   // These never do, except if the entire file is position dependent or if
@@ -851,6 +852,12 @@ static void addGotEntry(SymbolBody &Sym, bool Preemptible) {
     InX::Got->Relocations.push_back({R_ABS, Target->GotRel, Off, 0, &Sym});
 }
 
+template <class ELFT>
+static void addCapbilityTableEntry(SymbolBody &Sym, bool Preemptible) {
+  // FIXME: TLS Symbols should also go into the capability table!
+
+}
+
 // The reason we have to do this early scan is as follows
 // * To mmap the output file, we need to know the size
 // * For that, we need to know how many dynamic relocs we will have.
@@ -956,6 +963,29 @@ static void scanRelocs(InputSectionBase &Sec, ArrayRef<RelTy> Rels) {
       In<ELFT>::CapRelocs->addCapReloc({LocationSym, Offset}, Config->Pic,
                                        {&Body, 0u}, NeedsDynReloc, Addend);
       // TODO: check if it needs a plt stub
+      continue;
+    }
+    if (Expr == R_CHERI_CAPABILITY_TABLE_INDEX) {
+      assert(Config->ProcessCapRelocs);
+      uint32_t Index = InX::CheriCapTable->addEntry(Body);
+      assert(Addend == 0);
+      // FIXME: locking?
+      if (!ElfSym::CheriCapabilityTable)
+        ElfSym::CheriCapabilityTable = cast<DefinedRegular>(
+            Symtab
+                ->addRegular<ELFT>("_CHERI_CAPABILITY_TABLE_", STV_HIDDEN,
+                                   STT_SECTION,
+                                   /*Value=*/0, /*Size=*/0, STB_LOCAL,
+                                   InX::CheriCapTable, nullptr)
+                ->body());
+      In<ELFT>::CapRelocs->addCapReloc(
+        {ElfSym::CheriCapabilityTable, Index * Config->CapabilitySize}, Config->Pic,
+                                       {&Body, 0u}, Body.IsPreemptible, Addend);
+      if (Config->Pic) {
+        error("Cannot add capability table entries for PIC code yet!");
+      }
+      // Write out the index into the instruction
+      Sec.Relocations.push_back({Expr, Type, Offset, Addend, &Body});
       continue;
     }
 

ELF/Symbols.cpp
@@ -39,6 +39,7 @@ DefinedRegular *ElfSym::GlobalOffsetTable;
 DefinedRegular *ElfSym::MipsGp;
 DefinedRegular *ElfSym::MipsGpDisp;
 DefinedRegular *ElfSym::MipsLocalGp;
+DefinedRegular *ElfSym::CheriCapabilityTable;
 
 static uint64_t getSymVA(const SymbolBody &Body, int64_t &Addend) {
   switch (Body.kind()) {

ELF/Symbols.h
@@ -337,6 +337,11 @@ struct ElfSym {
   static DefinedRegular *MipsGp;
   static DefinedRegular *MipsGpDisp;
   static DefinedRegular *MipsLocalGp;
+
+
+  // The _CHERI_CAPABILITY_TABLE_ symbol points to the beginning of the
+  // .cap_table section
+  static DefinedRegular *CheriCapabilityTable;
 };
 
 // A real symbol object, SymbolBody, is usually stored within a Symbol. There's

CTSRD-CHERI/llvm-project

lld/ELF/Relocations.cpp
@@ -341,7 +341,7 @@ static bool isStaticLinkTimeConstant(RelExpr E, RelType Type, const Symbol &Sym,
                      R_MIPS_GOT_GP_PC, R_MIPS_TLSGD, R_GOT_PAGE_PC, R_GOT_PC,
                      R_GOTONLY_PC, R_GOTONLY_PC_FROM_END, R_PLT_PC, R_TLSGD_PC,
                      R_TLSGD, R_PPC_PLT_OPD, R_TLSDESC_CALL, R_TLSDESC_PAGE,
-                     R_HINT>(E))
+                     R_HINT, R_CHERI_CAPABILITY_TABLE_INDEX>(E))
     return true;
 
   // These never do, except if the entire file is position dependent or if
@@ -981,9 +981,9 @@ static void scanReloc(InputSectionBase &Sec, OffsetGetter &GetOffset, RelTy *&I,
       std::lock_guard<std::mutex> Lock(Mu);
       LocationSym = Symtab->find(SymbolHackName);
       if (!LocationSym) {
-        Symtab->addRegular(Saver.save(SymbolHackName), STV_DEFAULT, STT_SECTION,
-                           0, Sec.getOutputSection()->Size, STB_LOCAL, &Sec,
-                           Sec.File);
+        Symtab->addRegular<ELFT>(Saver.save(SymbolHackName), STV_DEFAULT,
+                                 STT_SECTION, 0, Sec.getOutputSection()->Size,
+                                 STB_LOCAL, &Sec, Sec.File);
         LocationSym = Symtab->find(SymbolHackName);
         assert(LocationSym);
       }
@@ -991,6 +991,22 @@ static void scanReloc(InputSectionBase &Sec, OffsetGetter &GetOffset, RelTy *&I,
     In<ELFT>::CapRelocs->addCapReloc({LocationSym, Offset}, Config->Pic,
                                      {&Sym, 0u}, NeedsDynReloc, Addend);
     // TODO: check if it needs a plt stub
+    return;
+  } else if (Expr == R_CHERI_CAPABILITY_TABLE_INDEX) {
+    assert(Config->ProcessCapRelocs);
+    uint32_t Index = InX::CheriCapTable->addEntry(Sym);
+    assert(Addend == 0);
+    // FIXME: locking?
+    if (!ElfSym::CheriCapabilityTable)
+      ElfSym::CheriCapabilityTable = cast<Defined>(Symtab->addRegular("_CHERI_CAPABILITY_TABLE_", STV_HIDDEN, STT_SECTION, /*Value=*/0, /*Size=*/0, STB_LOCAL, InX::CheriCapTable, nullptr));
+    In<ELFT>::CapRelocs->addCapReloc(
+      {ElfSym::CheriCapabilityTable, Index * Config->CapabilitySize}, Config->Pic,
+      {&Sym, 0u}, Sym.IsPreemptible, Addend);
+    if (Config->Pic) {
+      error("Cannot add capability table entries for PIC code yet!");
+    }
+    // Write out the index into the instruction
+    Sec.Relocations.push_back({Expr, Type, Offset, Addend, &Sym});
     return;
   }
 

lld/ELF/Symbols.cpp
@@ -39,6 +39,8 @@ Defined *ElfSym::GlobalOffsetTable;
 Defined *ElfSym::MipsGp;
 Defined *ElfSym::MipsGpDisp;
 Defined *ElfSym::MipsLocalGp;
+Defined *ElfSym::CheriCapabilityTable;
+
 
 static uint64_t getSymVA(const Symbol &Sym, int64_t &Addend) {
   switch (Sym.kind()) {

lld/ELF/Symbols.h
@@ -327,6 +327,10 @@ struct ElfSym {
   static Defined *MipsGp;
   static Defined *MipsGpDisp;
   static Defined *MipsLocalGp;
+
+  // The _CHERI_CAPABILITY_TABLE_ symbol points to the beginning of the
+  // .cap_table section
+  static Defined *CheriCapabilityTable;
 };
 
 // A buffer class that is large enough to hold any Symbol-derived