qwattash/lld

Generate correct __cap_relocs with unnamed symbols as the target
When multiple files define unnamed symbols that point to e.g. .data+0x10
I was only writing the first __cap_relocs out to the file. Now I use the
InputFile as part of the map key as well so all of them get written.

ELF/Relocations.cpp
@@ -944,6 +944,7 @@ static void scanRelocs(InputSectionBase &Sec, ArrayRef<RelTy> Rels) {
     if (Expr == R_CHERI_CAPABILITY) {
       assert(Config->ProcessCapRelocs);
       bool NeedsDynReloc = Body.IsPreemptible;
+
       std::string SymbolHackName = ("__caprelocs_hack_" + Sec.Name).str();
       auto LocationSym = Symtab->find(SymbolHackName);
       if (!LocationSym) {
@@ -955,13 +956,14 @@ static void scanRelocs(InputSectionBase &Sec, ArrayRef<RelTy> Rels) {
         if (!LocationSym) {
           Symtab->addRegular<ELFT>(Saver.save(SymbolHackName), STV_DEFAULT,
                                    STT_SECTION, 0, Sec.getOutputSection()->Size,
-                                   STB_LOCAL, &Sec, Sec.File);
+                                   STB_LOCAL, &Sec, nullptr);
           LocationSym = Symtab->find(SymbolHackName);
           assert(LocationSym);
         }
       }
-      In<ELFT>::CapRelocs->addCapReloc({LocationSym, Offset}, Config->Pic,
-                                       {&Body, 0u}, NeedsDynReloc, Addend);
+      In<ELFT>::CapRelocs->addCapReloc({LocationSym, Offset}, Sec.File,
+                                       Config->Pic, {&Body, 0u}, NeedsDynReloc,
+                                       Addend);
       // TODO: check if it needs a plt stub
       continue;
     }
@@ -979,8 +981,8 @@ static void scanRelocs(InputSectionBase &Sec, ArrayRef<RelTy> Rels) {
                                    InX::CheriCapTable, nullptr)
                 ->body());
       In<ELFT>::CapRelocs->addCapReloc(
-        {ElfSym::CheriCapabilityTable, Index * Config->CapabilitySize}, Config->Pic,
-                                       {&Body, 0u}, Body.IsPreemptible, Addend);
+        {ElfSym::CheriCapabilityTable, Index * Config->CapabilitySize}, nullptr,
+         Config->Pic, {&Body, 0u}, Body.IsPreemptible, Addend);
       if (Config->Pic) {
         static bool HasWarned = false;
         if (!HasWarned) {

CTSRD-CHERI/llvm-project

lld/ELF/Relocations.cpp
@@ -983,13 +983,14 @@ static void scanReloc(InputSectionBase &Sec, OffsetGetter &GetOffset, RelTy *&I,
       if (!LocationSym) {
         Symtab->addRegular(Saver.save(SymbolHackName), STV_DEFAULT, STT_SECTION,
                            0, Sec.getOutputSection()->Size, STB_GLOBAL, &Sec,
-                           Sec.File);
+                           nullptr);
         LocationSym = Symtab->find(SymbolHackName);
         assert(LocationSym);
       }
     }
-    In<ELFT>::CapRelocs->addCapReloc({LocationSym, Offset}, Config->Pic,
-                                     {&Sym, 0u}, NeedsDynReloc, Addend);
+    In<ELFT>::CapRelocs->addCapReloc({LocationSym, Offset}, Sec.File,
+                                     Config->Pic, {&Sym, 0u}, NeedsDynReloc,
+                                     Addend);
     // TODO: check if it needs a plt stub
     return;
   } else if (Expr == R_CHERI_CAPABILITY_TABLE_INDEX) {
@@ -1000,8 +1001,8 @@ static void scanReloc(InputSectionBase &Sec, OffsetGetter &GetOffset, RelTy *&I,
     if (!ElfSym::CheriCapabilityTable)
       ElfSym::CheriCapabilityTable = cast<Defined>(Symtab->addRegular("_CHERI_CAPABILITY_TABLE_", STV_HIDDEN, STT_SECTION, /*Value=*/0, /*Size=*/0, STB_LOCAL, InX::CheriCapTable, nullptr));
     In<ELFT>::CapRelocs->addCapReloc(
-      {ElfSym::CheriCapabilityTable, Index * Config->CapabilitySize}, Config->Pic,
-      {&Sym, 0u}, Sym.IsPreemptible, Addend);
+        {ElfSym::CheriCapabilityTable, Index * Config->CapabilitySize}, nullptr,
+        Config->Pic, {&Sym, 0u}, Sym.IsPreemptible, Addend);
     if (Config->Pic) {
       static bool HasWarned = false;
       if (!HasWarned) {