c386c42ddd27f5d2c656f37b16c2fefeceb2ab4f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-01-31 12:28:55 +0000

    Reenable address space check in the IR verifier
    We originally had to disable this check since the CheriPureCapABI pass used
    to transform all allocas from AS200 to AS0. This is no longer necessary
    since we have stopped using $sp.
    
    This fixes three tests that expect this check to fail on invalid IR.

--- llvm/lib/IR/Verifier.cpp
+++ llvm/lib/IR/Verifier.cpp
@@ -3235,14 +3235,11 @@ void Verifier::verifySwiftErrorValue(const Value *SwiftErrorVal) {
 
 void Verifier::visitAllocaInst(AllocaInst &AI) {
   SmallPtrSet<Type*, 4> Visited;
-#ifndef NDEBUG
-  // PointerType *PTy = AI.getType();
+  PointerType *PTy = AI.getType();
   // TODO: Relax this restriction?
-  // XXXAR: This assertion does not hold for cheri because we replace the allocaAS
-  // Assert(PTy->getAddressSpace() == DL.getAllocaAddrSpace(),
-  //       "Allocation instruction pointer not in the stack address space!",
-  //       &AI);
-#endif
+  Assert(PTy->getAddressSpace() == DL.getAllocaAddrSpace(),
+         "Allocation instruction pointer not in the stack address space!",
+         &AI);
   Assert(AI.getAllocatedType()->isSized(&Visited),
          "Cannot allocate unsized type", &AI);
   Assert(AI.getArraySize()->getType()->isIntegerTy(),
