f7ca8216dbd58a34695b87115d7884cfb880dc07
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-02-18 18:38:23 +0000

    Simplify test case for #245

--- clang/test/CodeGen/CHERI/vfs_cache-crash.c
+++ clang/test/CodeGen/CHERI/vfs_cache-crash.c
@@ -1,34 +1,49 @@
 // https://github.com/CTSRD-CHERI/llvm/issues/245
-// RUN: %cheri_purecap_cc1 "-emit-obj" "-disable-free" "-disable-llvm-verifier" "-discard-value-names" "-main-file-name" "vfs_cache.c" "-mrelocation-model" "pic" "-pic-level" "1" "-mthread-model" "posix" "-mdisable-fp-elim" "-masm-verbose" "-mconstructor-aliases" "-target-feature" "+soft-float" "-msoft-float" "-mfloat-abi" "soft" "-mllvm" "-mips-ssection-threshold=0" "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb" "-nostdsysteminc" "-nobuiltininc" "-sys-header-deps" "-D" "_KERNEL" "-D" "HAVE_KERNEL_OPTION_HEADERS" "-D" "KERNLOADADDR=0xffffffff80100000" "-D" "__printf__=__freebsd_kprintf__" "-O2" "-Wno-error" "-std=iso9899:1999" "-ferror-limit" "19" "-fmessage-length" "0" "-ffreestanding" "-fwrapv" "-fobjc-runtime=gnustep" "-fdiagnostics-show-option" "-fcolor-diagnostics" "-vectorize-loops" "-vectorize-slp" "-cheri-linker" %s -o /dev/null
+// RUN: %cheri_purecap_cc1 -emit-llvm -O2 %s -o %t.ll
+// RUN: cat %t.ll
+// RUN: %cheri_purecap_llc -O2 %t.ll -o /dev/null
+// RUN: %cheri_purecap_cc1 -emit-obj -O2 %s -o /dev/null  -S
+static int __attribute__((__section__(".data.read_mostly"))) a;
+int b() {
+  if (b)
+    return 0;
+c:
+  d();
+  a++;
+  goto c;
+}
 
+
+#if 0 // original test case:
 #define a(b) __attribute__((__section__(b)))
 #define d(h, ab, c) for (h = ab; 0;)
 struct {
   char bi;
-  long bj
+  long bj;
 } * e;
 struct bl {
   struct bm *bn
 };
 struct bm {
   struct {
-    int *f
+    int *f;
   };
   struct bl br;
   struct {
-    struct bl bs
+    struct bl bs;
   };
   char bt
 }
 #define bx() &bz[ca]
     * bz,
     g;
-static a(".data.read_mostly") ca, cd, t;
-cc;
+static int a(".data.read_mostly") ca, cd, t;
+int cc;
 #define ap(aq, ar) at ar
 #define au() ap(, (""));
 #define bc(c, b) au()
-i(){bc(, )} j(k) {
+int i(){bc(, );}
+int j(k) {
   int *l = 0;
   struct bl *co = 0;
   struct bm *cf;
@@ -67,3 +82,4 @@ ct:
   cd++;
   goto ct;
 }
+#endif
