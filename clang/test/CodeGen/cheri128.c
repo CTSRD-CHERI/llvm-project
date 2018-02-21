43e6267f1d14188e08b03605eb9a7491285fac4d
Alexander Richardson <arichardson.kde@gmail.com>
2018-02-21 17:34:20 +0000

    Update test for new datalayout string

--- clang/test/CodeGen/cheri128.c
+++ clang/test/CodeGen/cheri128.c
@@ -1,8 +1,8 @@
 // RUN: %cheri256_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefix=CHECK256  %s
 // RUN: %cheri128_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefix=CHECK128 %s
 
-// CHECK128: target datalayout = "E-m:e-pf200:128:128-i8:8:32-i16:16:32-i64:64-n32:64-S128"
-// CHECK256: target datalayout = "E-m:e-pf200:256:256-i8:8:32-i16:16:32-i64:64-n32:64-S128"
+// CHECK128: target datalayout = "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
+// CHECK256: target datalayout = "E-m:e-pf200:256:256:256:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
 void * __capability array[2];
 
 void * __capability get(int x)
