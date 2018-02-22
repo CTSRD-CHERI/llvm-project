e8321a73a76299bb06015867b72843dad8b3258c
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-02-22 16:37:13 +0000

    Update clang datalayout string for LLVM change

--- clang/test/CodeGen/cheri128.c
+++ clang/test/CodeGen/cheri128.c
@@ -1,8 +1,13 @@
-// RUN: %cheri256_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefix=CHECK256  %s
-// RUN: %cheri128_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefix=CHECK128 %s
+// RUN: %cheri256_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefixes=CHECK256,HYBRID256 %s
+// RUN: %cheri128_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefixes=CHECK128,HYBRID128 %s
+// RUN: %cheri256_purecap_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefixes=CHECK256,PURE256 %s
+// RUN: %cheri128_purecap_cc1 -S -o - -O1 -emit-llvm %s | FileCheck -check-prefixes=CHECK128,PURE128 %s
 
-// CHECK128: target datalayout = "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
-// CHECK256: target datalayout = "E-m:e-pf200:256:256:256:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
+// HYBRID128: target datalayout = "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
+// HYBRID256: target datalayout = "E-m:e-pf200:256:256:256:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
+// PURE128: target datalayout = "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128-A200-P200-G200"
+// PURE256: target datalayout = "E-m:e-pf200:256:256:256:64-i8:8:32-i16:16:32-i64:64-n32:64-S128-A200-P200-G200"
+
 void * __capability array[2];
 
 void * __capability get(int x)
