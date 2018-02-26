8e26013bf7a10f8b9e808fecf4980e6ad6c1af01
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-02-26 14:32:07 +0000

    Add a check that cheri256 capabilities are aligned correctly

---
+++ lld/test/ELF/cheri/chericap-alignment.s
@@ -0,0 +1,36 @@
+
+# RUN: %cheri256_purecap_llvm-mc %s -filetype=obj -defsym=FIRST=1 -o %t1.o
+# RUN: %cheri256_purecap_llvm-mc %s -filetype=obj -defsym=FIRST=0 -o %t2.o
+# RUN: ld.lld %t1.o %t2.o -shared -o %t.so
+# RUN: llvm-objdump -C -s -t %t.so | FileCheck %s
+
+.if FIRST
+
+.data
+.quad 0x1234
+
+.else
+
+.global foo
+.global cap
+.data
+cap:
+    .chericap foo
+
+.endif
+
+
+# CHECK-LABEL: CAPABILITY RELOCATION RECORDS:
+# CHECK-NEXT: 0x0000000000010020	Base:  (0x0000000000000000)	Offset: 0x0000000000000000	Length: 0x0000000000000000	Permissions: 0x00000000
+
+
+# CHECK-LABEL: Contents of section .data:
+# CHECK-NEXT:  10000 00000000 00001234 00000000 00000000  .......4........
+# CHECK-NEXT:  10010 00000000 00000000 00000000 00000000  ................
+# CHECK-NEXT:  10020 cacacaca cacacaca cacacaca cacacaca  ................
+# CHECK-NEXT:  10030 cacacaca cacacaca cacacaca cacacaca  ................
+
+# CHECK-LABEL: SYMBOL TABLE:
+# CHECK: 0000000000010020         .data		 00000000 cap
+#                   ^----- Ensure that this is aligned to 0x20
+# CHECK: 0000000000000000         *UND*		 00000000 foo
