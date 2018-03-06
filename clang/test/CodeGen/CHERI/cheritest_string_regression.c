6767bd2e7e6cae7411dd9f8cfd1ce1057bc65636
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2018-03-06 15:46:53 +0000

    Fix test following fix for #178

--- clang/test/CodeGen/CHERI/cheritest_string_regression.c
+++ clang/test/CodeGen/CHERI/cheritest_string_regression.c
@@ -1,5 +1,5 @@
-// RUN: not --crash %cheri_purecap_cc1 -o - -O0 -emit-llvm %s
-// REQUIRES: asserts
+// RUN: %cheri_purecap_cc1 -o - -O0 -emit-llvm %s | FileCheck %s
+// Ensure that the code below doesn't crash due to the __cheri_tocap to void*
 // Found after https://github.com/CTSRD-CHERI/cheribsd/commit/58379f87eda5569d6aae0d36154549725b50c1b2
 // See https://github.com/CTSRD-CHERI/clang/issues/178
 struct a {
@@ -8,5 +8,6 @@ struct a {
 void c(void) {
   struct a first;
   struct a second;
+  // CHECK: bitcast %struct.a addrspace(200)* %second to i8 addrspace(200)*
   first.ptr = (__cheri_tocap void *)&second;
 }
