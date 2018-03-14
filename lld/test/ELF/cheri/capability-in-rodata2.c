cdbbc5dd76b88622bf6c4b31bc78d745c66e7544
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-03-14 17:52:19 +0000

    Don't output useless stuff to stdout in capability-in-rodata2.c test

--- lld/test/ELF/cheri/capability-in-rodata2.c
+++ lld/test/ELF/cheri/capability-in-rodata2.c
@@ -3,7 +3,6 @@
 // as dynamic binaries. Currently the kernel just gives us a read-write mapping to
 // work around this but we really should just make this an error
 //
-// RUN: %cheri_purecap_cc1 %s -O0 -S -o -
 // RUN: %cheri_purecap_cc1 %s -emit-obj -o %t.o
 // RUNNOT: llvm-readobj -r %t.o
 // RUN: not ld.lld -shared %t.o -o %t.so 2>&1 | FileCheck %s
