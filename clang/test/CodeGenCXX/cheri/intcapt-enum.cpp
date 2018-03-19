ee788f8d5681f0c38986e36230de023420664fd4
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2018-03-19 14:42:22 +0000

    Fix emitting of enum values whereby the underlying integer type of the enum is [u]intcap_t
    Similar to commit b7d18c83c52836a1a03c7ce7abbd5f518759fc2a.

---
+++ clang/test/CodeGenCXX/cheri/intcapt-enum.cpp
@@ -0,0 +1,9 @@
+// RUN: %cheri_purecap_cc1 -x c++ -std=c++11 -emit-llvm -o - -O0 %s | %cheri_FileCheck %s
+// Check that enum values whereby the underlying enum type is [u]intcap_t are
+// converted to i8 addrspace(200)* and not left as i128/i256
+typedef __intcap_t a;
+enum : a { b, d };
+a c() {
+  // CHECK: ret i8 addrspace(200)* inttoptr (i[[@EXPR 8 * $CAP_SIZE]] 1 to i8 addrspace(200)*)
+  return d;
+}
