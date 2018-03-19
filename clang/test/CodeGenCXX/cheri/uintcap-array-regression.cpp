b8be9d8f8f0c3c0a64194d124e853fc494651458
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2018-03-19 14:34:24 +0000

    Fix test on MacOS

--- clang/test/CodeGenCXX/cheri/uintcap-array-regression.cpp
+++ clang/test/CodeGenCXX/cheri/uintcap-array-regression.cpp
@@ -3,5 +3,5 @@
 // This started crashing after fixing emission of uintcap_t arrays:
 enum { a };
 void b() { new int[a]; }
-// CHECK-LABEL: @_Z1bv() {
+// CHECK-LABEL: @_Z1bv()
 // CHECK: call i8 addrspace(200)* @_Znam(i64 zeroext 0) #2
