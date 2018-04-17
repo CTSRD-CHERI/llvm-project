ee9c27496caa098e5ddf4aeb3baa3129a461b581
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2018-04-17 12:20:28 +0100

    Add -no-mxcaptable option to disable the large cap table. Fix compile error when checking the mxcaptable flags.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1674,10 +1674,10 @@ void Clang::AddMIPSTargetArgs(const ArgList &Args,
       StringRef v = A->getValue();
       CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString("-cheri-cap-table-abi=" + v));
-      if (Args.hasFlag(options::OPT_cheri_large_cap_table)) {
-	  CmdArgs.push_back("-mllvm");
-	  CmdArgs.push_back("-mxcaptable");
-      }
+      bool MxCapTable = Args.hasFlag(options::OPT_cheri_large_cap_table,
+				     options::OPT_no_cheri_large_cap_table, false);
+      CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(MxCapTable ? "-mxcaptable=true" : "-mxcaptable=false");
       A->claim();
   }
 }
