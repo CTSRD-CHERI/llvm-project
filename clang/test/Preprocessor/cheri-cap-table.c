b93d150ad6e7283e670b2c5ef22ddf5fc2fd460b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-05-11 14:57:07 +0100

    Fix the __CHERI_CAPABILITY_TABLE__ test

--- clang/test/Preprocessor/cheri-cap-table.c
+++ clang/test/Preprocessor/cheri-cap-table.c
@@ -2,10 +2,10 @@
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=legacy -E -dM -x c /dev/null | FileCheck %s -check-prefix NOTABLE
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=plt -E -dM -x c /dev/null | FileCheck %s -check-prefix CAPTABLE-PLT
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel -E -dM -x c /dev/null | FileCheck %s -check-prefix CAPTABLE-PCREL
-// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=fndesc -E -dM -x c /dev/null | FileCheck %s -check-prefix CAPTABLE-FNDESC
+// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=fn-desc -E -dM -x c /dev/null | FileCheck %s -check-prefix CAPTABLE-FNDESC
 
 // NOTABLE-NOT: __CHERI_CAPABILITY_TABLE__
-// CAPTABLE-PLT: __CHERI_CAPABILITY_TABLE__ 1
-// CAPTABLE-PCREL: __CHERI_CAPABILITY_TABLE__ 2
-// CAPTABLE-FNDESC: __CHERI_CAPABILITY_TABLE__ 3
+// CAPTABLE-PLT: __CHERI_CAPABILITY_TABLE__ 2
+// CAPTABLE-PCREL: __CHERI_CAPABILITY_TABLE__ 3
+// CAPTABLE-FNDESC: __CHERI_CAPABILITY_TABLE__ 4
 
