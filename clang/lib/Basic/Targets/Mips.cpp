d747eb4a8985c0e43c536918a5c866bc17dd952f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-05-11 13:38:58 +0100

    Define different __CHERI_CAPABITLIY_TABLE__ values for different ABIs

--- clang/lib/Basic/Targets/Mips.cpp
+++ clang/lib/Basic/Targets/Mips.cpp
@@ -172,9 +172,12 @@ void MipsTargetInfo::getTargetDefines(const LangOptions &Opts,
     if (CapabilityABI) {
       Builder.defineMacro("__CHERI_SANDBOX__", Twine(4));
       Builder.defineMacro("__CHERI_PURE_CAPABILITY__", Twine(2));
+      auto CapTableABI = llvm::MCTargetOptions::cheriCapabilityTableABI();
+      if (CapTableABI != llvm::CheriCapabilityTableABI::Legacy) {
+        Builder.defineMacro("__CHERI_CAPABILITY_TABLE__",
+                            Twine(((int)CapTableABI) + 1));
+      }
     }
-    if (llvm::MCTargetOptions::cheriUsesCapabilityTable())
-      Builder.defineMacro("__CHERI_CAPABILITY_TABLE__", Twine(1));
 
     // Macros for use with the set and get permissions builtins.
     Builder.defineMacro("__CHERI_CAP_PERMISSION_GLOBAL__", Twine(1<<0));
