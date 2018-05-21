7764221b6bb8b74d10857bd10d57c4e0e7509b9d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-05-21 11:50:27 +0100

    Zero $c13 on return from a function with on-stack arguments
    This way we can be certain that any function without on-stack arguments
    always has a NULL $c13 even if it ends up calling variadics or functions
    with on-stack args.

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -4223,6 +4223,20 @@ MipsTargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
       RetOps.push_back(DAG.getRegister(Mips::C3, CapType));
     }
   }
+  if (ABI.IsCheriPureCap()) {
+    // If this function used $c13 we want to zero it before returning so that
+    // all functions without on-stack arguments can assume that $c13 is null
+    // on entry
+    if (MF.getRegInfo().isLiveIn(Mips::C13) || IsVarArg) {
+      DEBUG(dbgs() << "Lowering return for function with $c13 live-in: "
+                   << MF.getName()
+                   << "(is varargs: " << MF.getFunction().isVarArg() << ")\n");
+      Chain = DAG.getCopyToReg(Chain, DL, Mips::C13,
+                               DAG.getConstant(0, DL, CapType), Flag);
+      Flag = Chain.getValue(1);
+      RetOps.push_back(DAG.getRegister(Mips::C13, CapType));
+    }
+  }
 
   // The mips ABIs for returning structs by value requires that we copy
   // the sret argument into $v0 for the return. We saved the argument into
