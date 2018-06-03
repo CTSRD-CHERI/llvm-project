64d115cad4e8e1ea62780ea68ed782b869f753ed
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-06-03 19:01:55 +0100

    Fix which operand in ctoptr refers to $ddc (it's ct not cb)

--- llvm/test/MC/Mips/cheri/stop-using-c0-dcc.s
+++ llvm/test/MC/Mips/cheri/stop-using-c0-dcc.s
@@ -17,7 +17,7 @@ cgetepcc $c0    # CHECK: [[@LINE]]:10: error: register name $c0 is invalid as th
 
 # Using $c0 as a non-source-address register should be an error:
 cfromptr $c0, $c1, $3  # CHECK: 39:10: error: register name $c0 is invalid as this operand.
-ctoptr $3, $c1, $c0    # CHECK: [[@LINE]]:17: error: register name $c0 is invalid as this operand.
+ctoptr $3, $c0, $c2    # CHECK: [[@LINE]]:12: error: register name $c0 is invalid as this operand.
 clc $c0, $zero, 0($c2) # CHECK: [[@LINE]]:5: error: register name $c0 is invalid as this operand.
 csc $c0, $zero, 0($c2) # CHECK: [[@LINE]]:5: error: register name $c0 is invalid as this operand.
 cllc $c0, $c1          # CHECK: [[@LINE]]:6: error: register name $c0 is invalid as this operand.
@@ -27,7 +27,7 @@ cscc $2, $c0, $c1      # CHECK: [[@LINE]]:10: error: register name $c0 is invali
 
 # However, cfromptr, ctoptr, cl* and cs* should still work with $c0 as the address register
 cfromptr $c1, $c0, $3 # CHECK: [[@LINE]]:15: warning: register name $c0 is deprecated. Use $ddc instead.
-ctoptr $3, $c0, $c1    # CHECK: [[@LINE]]:12: warning: register name $c0 is deprecated. Use $ddc instead.
+ctoptr $3, $c1, $c0    # CHECK: [[@LINE]]:17: warning: register name $c0 is deprecated. Use $ddc instead.
 clc $c1, $zero, 0($c0)  # CHECK: [[@LINE]]:19: warning: register name $c0 is deprecated. Use $ddc instead.
 clcbi $c1, 0($c0)  # CHECK: [[@LINE]]:14: warning: register name $c0 is deprecated. Use $ddc instead.
 clw $3, $zero, 0($c0)  # CHECK: [[@LINE]]:18: warning: register name $c0 is deprecated. Use $ddc instead.
