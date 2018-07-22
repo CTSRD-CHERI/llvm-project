a87ed7bc978fa2dfd4bfb74d66b8cce5761c68b9
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-07-22 12:05:52 +0100

    Fix test

--- clang/test/Driver/cheri/bounds-mode.c
+++ clang/test/Driver/cheri/bounds-mode.c
@@ -2,8 +2,11 @@
 
 // RUN: %cheri_purecap_cc1 -cheri-bounds=conservative -emit-llvm %s -o /dev/null
 // RUN: %cheri_purecap_cc1 -cheri-bounds=references-only -emit-llvm %s -o /dev/null
-// RUN: %cheri_purecap_cc1 -cheri-bounds=subobject-safe -emit-llvm %s -o /dev/null
 // RUN: %cheri_purecap_cc1 -cheri-bounds=aggressive -emit-llvm %s -o /dev/null
 
 // RUN: not %cheri_purecap_cc1 -cheri-bounds=typo -emit-llvm %s -o /dev/null 2>&1 | FileCheck %s -check-prefix ERR
 // ERR: error: invalid value 'typo' in '-cheri-bounds=typo'
+
+// This one is not implemented yet:
+// RUN: not %cheri_purecap_cc1 -cheri-bounds=subobject-safe -emit-llvm %s -o /dev/null 2>&1 | FileCheck %s -check-prefix NOTYET
+// NOTYET: error: unsupported argument 'subobject-safe' to option '-cheri-bounds=subobject-safe'
