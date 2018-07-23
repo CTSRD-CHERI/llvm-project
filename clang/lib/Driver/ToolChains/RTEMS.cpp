f1c642719758e8256e050f363fed23f2e3ef1cd5
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-07-23 21:07:30 +0100

    Fix the rtems driver tests

--- clang/lib/Driver/ToolChains/RTEMS.cpp
+++ clang/lib/Driver/ToolChains/RTEMS.cpp
@@ -85,9 +85,15 @@ void rtems::Linker::ConstructJob(Compilation &C, const JobAction &JA,
     if (!Args.hasArg(options::OPT_q_rtems)) {
 
       // RTEMS must provide a --sysroot path to its installed C library
-      assert(!D.SysRoot.empty());
-
-      CmdArgs.push_back(Args.MakeArgString(D.SysRoot + "/lib/crt0.o"));
+      const char* arg = nullptr;
+      if (D.SysRoot.empty()) {
+        ToolChain.getDriver().Diag(diag::err_drv_argument_not_allowed_with)
+          << "missing --sysroot= option" << "rtems driver";
+        arg = Args.MakeArgString(ToolChain.GetFilePath("crt0.o"));
+      } else {
+        arg = Args.MakeArgString(D.SysRoot + "/lib/crt0.o");
+      }
+      CmdArgs.push_back(arg);
     }
     // Otherwise, RTEMS build system should provide its own start file
   }
