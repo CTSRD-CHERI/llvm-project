970c233983f38ac4fc4508beb0f1bdfd56b34dec
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-08-10 13:23:52 +0200

    Update cheri-cap-tls.c to LLVM option parsing change

--- clang/test/Preprocessor/cheri-cap-tls.c
+++ clang/test/Preprocessor/cheri-cap-tls.c
@@ -1,7 +1,12 @@
-// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel -E -dM -x c /dev/null | FileCheck %s -check-prefix LEGACY
-// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-tls-abi=cap-equiv -E -dM -x c /dev/null | FileCheck %s -check-prefix LEGACY
-// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-tls-abi=legacy -E -dM -x c /dev/null | FileCheck %s -check-prefix LEGACY
+// -mllvm -cheri-cap-table-abi != legacy defaults to cap-equiv (but can be overriden:)
+// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel -E -dM -x c /dev/null | FileCheck %s -check-prefix CAP-EQUIV
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel -mllvm -cheri-cap-tls-abi=cap-equiv -E -dM -x c /dev/null | FileCheck %s -check-prefix CAP-EQUIV
+// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel -mllvm -cheri-cap-tls-abi=legacy -E -dM -x c /dev/null | FileCheck %s -check-prefix LEGACY
+// If -cheri-cap-table-abi=legacy we ignore the cap-tls-abi flag:
+// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=legacy -mllvm -cheri-cap-tls-abi=cap-equiv -E -dM -x c /dev/null | FileCheck %s -check-prefix LEGACY
+
+// Without any flags we still default to legacy:
+// RUN: %cheri_purecap_cc1 -E -dM -x c /dev/null | FileCheck %s -check-prefix LEGACY
 
 // LEGACY-NOT: __CHERI_CAPABILITY_TLS__
 // CAP-EQUIV: __CHERI_CAPABILITY_TLS__ 1
