80aa3782061e14d2d7f19820d07dce3149fb2a8f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-09-10 21:59:48 +0100

    Update test

--- lld/test/ELF/cheri/cap-table/weak-symbols.c
+++ lld/test/ELF/cheri/cap-table/weak-symbols.c
@@ -31,7 +31,7 @@ TEST(_DYNAMIC)
 int __start(void) {
   return 0;
 }
-// DYNAMIC-EXE: 0000000000000238 .dynamic		 00000170 .hidden _DYNAMIC
+// DYNAMIC-EXE: 0000000000000238 .dynamic		 00000190 .hidden _DYNAMIC
 // DYNAMIC-EXE: 0000000000010000 .text 00000000 .hidden __init_array_end
 // DYNAMIC-EXE: 0000000000010000 .text 00000000 .hidden __init_array_start
 // STATIC-EXE: 0000000120010000 .text 00000000 .hidden __init_array_end
