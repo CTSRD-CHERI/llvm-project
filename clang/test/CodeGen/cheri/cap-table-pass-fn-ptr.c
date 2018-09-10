9fe88b081b0f8e508d1ea5db292cdd61e9940844
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-09-10 09:07:29 +0100

    Update test for relocation change

--- clang/test/CodeGen/cheri/cap-table-pass-fn-ptr.c
+++ clang/test/CodeGen/cheri/cap-table-pass-fn-ptr.c
@@ -9,10 +9,11 @@ void snprintf_func(void) {
   // CHECK:       %call = tail call i32 @a(void () addrspace(200)* nonnull @snprintf_func) #2
 }
 
-// ASM:      clcbi	$c3, %capcall20(snprintf_func)($c26)
+
+// This should use captab20 for loading the address of snprintf_func
+// ASM:      clcbi	$c3, %captab20(snprintf_func)($c26)
 // ASM-NEXT: clcbi $c12, %capcall20(a)($c26)
 // ASM-NEXT: cjalr	$c12, $c17
 // ASM-NEXT: nop
 
-// Checking the asm output should be done in LLVM but the IR will probably change
 
