439d1820ac8c2c4931a4f2dd499c44a2ff0ef15b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-10-11 09:36:21 +0100

    This test no longer xfails
    Seems like the optimizer got a bit better.

--- clang/test/CodeGen/intcapswitch-inefficient.c
+++ clang/test/CodeGen/intcapswitch-inefficient.c
@@ -1,7 +1,6 @@
 // RUN: %clang %s -mabi=purecap -cheri-linker -target cheri-unknown-freebsd -o - -emit-llvm -S | FileCheck %s
 // RUN: %clang %s -mabi=purecap -cheri-linker -target cheri-unknown-freebsd -o - -emit-llvm -S -O2 | FileCheck %s -check-prefix CHECK-OPT
 // RUN: %clang %s -mabi=purecap -cheri-linker -target cheri-unknown-freebsd -o - -emit-llvm -S -O3 | FileCheck %s -check-prefix CHECK-OPT
-// XFAIL: *
 
 #define A (__intcap_t)1
 #define B (__intcap_t)(void*)2
@@ -11,7 +10,7 @@
 int y(void) {
   // FIXME: C + 1 is not constant folded
   __intcap_t foo = C + 1;
-  // CHECK: switch i64 %6, label %sw.epilog [
+  // CHECK: switch i64  %intcap.vaddr, label %sw.epilog [
   switch (foo)
   {
     // CHECK-NEXT: i64 1, label %sw.bb
@@ -24,7 +23,7 @@ int y(void) {
     case C...D: return 6;
   }
   // with optimization this switch gets constant folded:
-  // CHECK-OPT:      define i32 @y() local_unnamed_addr #0 {
+  // CHECK-OPT:      define signext i32 @y() local_unnamed_addr #0 {
   // CHECK-OPT-NEXT: entry:
   // CHECK-OPT-NEXT:   ret i32 6
   // CHECK-OPT-NEXT: }
