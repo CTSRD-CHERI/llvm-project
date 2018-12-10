973532f82ba9c9d4bf41b91a563eeb74a4d4308f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-12-10 20:49:12 +0000

    Add debug output to libunwind_01 test

--- libunwind/test/libunwind_01.pass.cpp
+++ libunwind/test/libunwind_01.pass.cpp
@@ -1,5 +1,6 @@
 #include <libunwind.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void backtrace(int lower_bound) {
   unw_context_t context;
@@ -8,13 +9,21 @@ void backtrace(int lower_bound) {
   unw_cursor_t cursor;
   unw_init_local(&cursor, &context);
 
-  int n = 0;
-  do {
-    ++n;
+  char buffer[1024];
+  unw_word_t offset = 0;
+
+  int n = 1;
+  while (unw_step(&cursor) > 0) {
+    n++;
+    if (unw_get_proc_name(&cursor, buffer, sizeof(buffer), &offset) == 0) {
+      fprintf(stderr, "Frame %d: %s+%p\n", n, buffer, (void*)offset);
+    } else {
+      fprintf(stderr, "Frame %d: Could not get name for cursor\n", n);
+    }
     if (n > 100) {
       abort();
     }
-  } while (unw_step(&cursor) > 0);
+  };
 
   if (n < lower_bound) {
     abort();
@@ -22,15 +31,18 @@ void backtrace(int lower_bound) {
 }
 
 void test1(int i) {
+  fprintf(stderr, "starting %s\n", __func__);
   backtrace(i);
 }
 
 void test2(int i, int j) {
+  fprintf(stderr, "starting %s\n", __func__);
   backtrace(i);
   test1(j);
 }
 
 void test3(int i, int j, int k) {
+  fprintf(stderr, "starting %s\n", __func__);
   backtrace(i);
   test2(j, k);
 }
