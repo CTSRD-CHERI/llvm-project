a373e3402bf82d7944d40db32d635f113e09f6d7
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-12-20 22:10:00 +0100

    Ensure that the tests link against the static libunwind from the build dir
    Maybe this will fix the tests in jenkins

--- libunwind/test/libunwind/test/config.py
+++ libunwind/test/libunwind/test/config.py
@@ -58,7 +58,16 @@ class Configuration(LibcxxConfiguration):
         # Ensure that libunwind is always added to the linker flags
         # This should be the case for most TargetInfo classes anyway but some
         # of them don't add it.
-        self.cxx.link_flags += ['-lunwind']
+        self.cxx.link_flags += ['-nodefaultlibs', '-lc']
+        if self.link_shared:
+            self.cxx.link_flags += ['-lunwind']
+        else:
+            libname = self.make_static_lib_name('unwind')
+            abs_path = os.path.join(self.libunwind_obj_root, "lib", libname)
+            assert os.path.exists(abs_path) and "static libunwind library does not exist", abs_path
+            self.cxx.link_flags += [abs_path]
+
+        print(self.cxx.link_flags)
 
     def configure_compile_flags_header_includes(self):
         self.configure_config_site_header()
