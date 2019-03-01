fd479536a75e8acba108cc4dc83e4d8193aded19
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-03-01 14:08:57 +0000

    Let -Wcheri-unaligned stay a warning even with -Werror
    If you pass -Werror=cheri-unaligned it will still be turned into an error,
    it's just not turned into an error if you pass -Werror without a specific
    warning flag.

--- clang/test/CodeGen/cheri/memcpy-unaligned.c
+++ clang/test/CodeGen/cheri/memcpy-unaligned.c
@@ -1,6 +1,10 @@
 // RUN: %cheri128_purecap_cc1 -O0 -o - -emit-llvm %s -verify | FileCheck %s
 // RUN: %cheri128_purecap_cc1 -DBUILTIN -O0 -o - -emit-llvm %s -verify | FileCheck %s
 
+// If we are using -Werror this warning should not fail the build, only if it is explicitly added:
+// RUN: %cheri128_purecap_cc1 -O0 -o /dev/null -emit-llvm %s -Werror 2> /dev/null
+// RUN: not %cheri128_purecap_cc1 -O0 -o /dev/null -emit-llvm %s -Werror=cheri-misaligned 2>/dev/null
+
 #ifdef BUILTIN
 #define memcpy __builtin_memcpy
 #define memmove __builtin_memmove
