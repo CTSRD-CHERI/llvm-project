bbe4bbde1dfed5e7c8a1b941c6807725f913e579
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-03-14 10:13:36 +0000

    GVN: Disable coercing between capabilities and non-capabilities
    Fixes https://github.com/CTSRD-CHERI/llvm-project/issues/286
    
    Previously only a store of a fat pointer couldn't be converted to i128/i256
    but the same also applies for i128/i256 -> i8 addrspace(200)*

--- llvm/lib/Transforms/Utils/VNCoercion.cpp
+++ llvm/lib/Transforms/Utils/VNCoercion.cpp
@@ -44,12 +44,10 @@ bool canCoerceMustAliasedValueToLoad(Value *StoredVal, Type *LoadTy,
   }
 
   // We can't coerce a store of a fat pointer to a load of anything that isn't
-  // a fat pointer
-  if (auto *PT = dyn_cast<PointerType>(StoredVal->getType()))
-    if (DL.isFatPointer(PT->getPointerAddressSpace())) {
-      auto *LPTy = dyn_cast<PointerType>(LoadTy);
-      return LPTy ? (DL.isFatPointer(LPTy->getPointerAddressSpace())) : false;
-    }
+  // a fat pointer. We also can't coerce a store of a non fat pointer to
+  // anything that is a fat pointer.
+  if (DL.isFatPointer(StoredVal->getType()) != DL.isFatPointer(LoadTy))
+    return false;
 
   return true;
 }
