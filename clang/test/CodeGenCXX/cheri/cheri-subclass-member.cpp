7184c503c0c4f3dca1b9562e52fd4b16a80e9cf1
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-04-30 13:15:31 +0100

    Fix test for cheri256

--- clang/test/CodeGenCXX/cheri/cheri-subclass-member.cpp
+++ clang/test/CodeGenCXX/cheri/cheri-subclass-member.cpp
@@ -1,4 +1,4 @@
-// RUN: %cheri_cc1 -ast-dump -o /dev/null %s 2>&1 | FileCheck %s -check-prefix AST
+// RUN: %cheri_cc1 -ast-dump -o /dev/null %s 2>&1 | %cheri_FileCheck %s -check-prefix AST
 // RUN: %cheri_cc1 -emit-llvm -o - %s | FileCheck %s
 
 // Check that a UncheckedDerivedToBase cast does not remove the __capability qualifier
@@ -21,7 +21,7 @@ struct s2 : public s1 {
 const s2 *__capability c_s2;
 
 // CHECK-LABEL: _Z4funcv
-// CHECK: load %struct.s2 addrspace(200)*, %struct.s2 addrspace(200)** @c_s2, align 16
+// CHECK: load %struct.s2 addrspace(200)*, %struct.s2 addrspace(200)** @c_s2, align [[$CAP_SIZE]]
 const long &__capability func() {
   return c_s2->size;
 }
