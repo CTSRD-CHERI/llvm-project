CTSRD-CHERI/llvm-project

commit e874c99fa813e2cab4c1595704270e4319f70fcc

Fix crash on __uintcap_t template parameters
Fixes https://github.com/CTSRD-CHERI/llvm-project/issues/320

clang/lib/Sema/SemaTemplate.cpp
@@ -6929,7 +6929,10 @@ Sema::BuildExpressionFromIntegralTemplateArgument(const TemplateArgument &Arg,
   } else if (T->isNullPtrType()) {
     E = new (Context) CXXNullPtrLiteralExpr(Context.NullPtrTy, Loc);
   } else {
-    E = IntegerLiteral::Create(Context, Arg.getAsIntegral(), T, Loc);
+    auto IntValue = Arg.getAsIntegral();
+    if (T->isIntCapType())
+      IntValue = IntValue.extOrTrunc(Context.getIntWidth(T));
+    E = IntegerLiteral::Create(Context, IntValue, T, Loc);
   }
 
   if (OrigT->isEnumeralType()) {

clang/test/SemaCXX/cheri/template-uintcap-arg.cpp
@@ -1,0 +1,56 @@
+// RUN: %cheri_purecap_cc1 -std=gnu++1z -o - -emit-llvm -O0 %s | FileCheck %s
+
+template <__uintcap_t c> struct d {
+  static int x() { return (int)c; }
+};
+
+extern "C" int f() { return d<0>::x(); }
+extern "C" int f2() { return d<1>::x(); }
+extern "C" int f3() { return d<__UINT64_MAX__ + 1>::x(); }
+extern "C" int f4() { return d<__UINT64_MAX__ + 4>::x(); }
+
+
+// CHECK: define signext i32 @f() addrspace(200) #0 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   %call = call signext i32 @_ZN1dILu11__uintcap_t0EE1xEv()
+// CHECK-NEXT:   ret i32 %call
+// CHECK-NEXT: }
+// CHECK-EMPTY:
+// CHECK-NEXT: ; Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define linkonce_odr signext i32 @_ZN1dILu11__uintcap_t0EE1xEv() addrspace(200) #0 comdat align 2 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   ret i32 0
+// CHECK-NEXT: }
+// CHECK-EMPTY:
+// CHECK-NEXT:  Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define signext i32 @f2() addrspace(200) #0 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   %call = call signext i32 @_ZN1dILu11__uintcap_t1EE1xEv()
+// CHECK-NEXT:   ret i32 %call
+// CHECK-NEXT: }
+// CHECK-EMPTY:
+// CHECK-NEXT: ; Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define linkonce_odr signext i32 @_ZN1dILu11__uintcap_t1EE1xEv() addrspace(200) #0 comdat align 2 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   ret i32 1
+// CHECK-NEXT: }
+// CHECK-EMPTY:
+// CHECK-NEXT:  Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define signext i32 @f3() addrspace(200) #0 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   %call = call signext i32 @_ZN1dILu11__uintcap_t0EE1xEv()
+// CHECK-NEXT:   ret i32 %call
+// CHECK-NEXT: }
+// CHECK-EMPTY:
+// CHECK-NEXT:  Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define signext i32 @f4() addrspace(200) #0 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   %call = call signext i32 @_ZN1dILu11__uintcap_t3EE1xEv()
+// CHECK-NEXT:   ret i32 %call
+// CHECK-NEXT: }
+// CHECK-EMPTY:
+// CHECK-NEXT:  Function Attrs: noinline nounwind optnone
+// CHECK-NEXT: define linkonce_odr signext i32 @_ZN1dILu11__uintcap_t3EE1xEv() addrspace(200) #0 comdat align 2 {
+// CHECK-NEXT: entry:
+// CHECK-NEXT:   ret i32 3
+// CHECK-NEXT: }

commit bf0bc1617d56e657bd425313f9af100327d45466

Fix another crash with __uintcap_t template parameters
We need to use getIntRange() and not getIntWidth() for __uintcap_t.
This fixes a bug introduced in e874c99fa813e2cab4c1595704270e4319f70fcc
that caused uintcap_t template parameters to use 128-bit APInt values.

clang/lib/AST/Expr.cpp
@@ -782,7 +782,7 @@ IntegerLiteral::IntegerLiteral(const ASTContext &C, const llvm::APInt &V,
          false, false),
     Loc(l) {
   assert(type->isIntegerType() && "Illegal type in IntegerLiteral");
-  assert(V.getBitWidth() == C.getIntWidth(type) &&
+  assert(V.getBitWidth() == C.getIntRange(type) &&
          "Integer type is not the correct size for constant.");
   setValue(C, V);
 }

clang/lib/AST/ExprConstant.cpp
@@ -2108,7 +2108,7 @@ static bool HandleOverflow(EvalInfo &Info, const Expr *E,
 static bool HandleFloatToIntCast(EvalInfo &Info, const Expr *E,
                                  QualType SrcType, const APFloat &Value,
                                  QualType DestType, APSInt &Result) {
-  unsigned DestWidth = Info.Ctx.getIntWidth(DestType);
+  unsigned DestWidth = Info.Ctx.getIntRange(DestType);
   // Determine whether we are converting to unsigned or signed.
   bool DestSigned = DestType->isSignedIntegerOrEnumerationType();
 
@@ -7412,7 +7412,7 @@ public:
   bool Success(const llvm::APInt &I, const Expr *E, APValue &Result) {
     assert(E->getType()->isIntegralOrEnumerationType() &&
            "Invalid evaluation result.");
-    assert(I.getBitWidth() == Info.Ctx.getIntWidth(E->getType()) &&
+    assert(I.getBitWidth() == Info.Ctx.getIntRange(E->getType()) &&
            "Invalid evaluation result.");
     Result = APValue(APSInt(I));
     Result.getInt().setIsUnsigned(
@@ -7553,7 +7553,7 @@ class FixedPointExprEvaluator
 
   bool Success(const APFixedPoint &V, const Expr *E) {
     assert(E->getType()->isFixedPointType() && "Invalid evaluation result.");
-    assert(V.getWidth() == Info.Ctx.getIntWidth(E->getType()) &&
+    assert(V.getWidth() == Info.Ctx.getIntRange(E->getType()) &&
            "Invalid evaluation result.");
     Result = APValue(V);
     return true;
@@ -7641,7 +7641,7 @@ bool IntExprEvaluator::CheckReferencedDecl(const Expr* E, const Decl* D) {
     bool SameSign = (ECD->getInitVal().isSigned()
                      == E->getType()->isSignedIntegerOrEnumerationType());
     bool SameWidth = (ECD->getInitVal().getBitWidth()
-                      == Info.Ctx.getIntWidth(E->getType()));
+                      == Info.Ctx.getIntRange(E->getType()));
     if (SameSign && SameWidth)
       return Success(ECD->getInitVal(), E);
     else {
@@ -9135,7 +9135,7 @@ bool DataRecursiveIntBinOpEvaluator::
   // Set up the width and signedness manually, in case it can't be deduced
   // from the operation we're performing.
   // FIXME: Don't do this in the cases where we can deduce it.
-  APSInt Value(Info.Ctx.getIntWidth(E->getType()),
+  APSInt Value(Info.Ctx.getIntRange(E->getType()),
                E->getType()->isUnsignedIntegerOrEnumerationType());
   if (!handleIntIntBinOp(Info, E, LHSVal.getInt(), E->getOpcode(),
                          RHSVal.getInt(), Value))
@@ -9672,7 +9672,7 @@ bool IntExprEvaluator::VisitBinaryOperator(const BinaryOperator *E) {
     APSInt ElemSize(llvm::APInt(65, (int64_t)ElementSize.getQuantity(), true),
                     false);
     APSInt TrueResult = (LHS - RHS) / ElemSize;
-    APSInt Result = TrueResult.trunc(Info.Ctx.getIntWidth(E->getType()));
+    APSInt Result = TrueResult.trunc(Info.Ctx.getIntRange(E->getType()));
 
     if (Result.extend(65) != TrueResult &&
         !HandleOverflow(Info, E, TrueResult, E->getType()))
@@ -9942,7 +9942,7 @@ bool IntExprEvaluator::VisitCastExpr(const CastExpr *E) {
       return false;
     bool Overflowed;
     llvm::APSInt Result = Src.convertToInt(
-        Info.Ctx.getIntWidth(DestType),
+        Info.Ctx.getIntRange(DestType),
         DestType->isSignedIntegerOrEnumerationType(), &Overflowed);
     if (Overflowed && !HandleOverflow(Info, E, Result, DestType))
       return false;
@@ -11765,7 +11765,7 @@ static ICEDiag CheckICE(const Expr* E, const ASTContext &Ctx) {
     if (isa<ExplicitCastExpr>(E)) {
       if (const FloatingLiteral *FL
             = dyn_cast<FloatingLiteral>(SubExpr->IgnoreParenImpCasts())) {
-        unsigned DestWidth = Ctx.getIntWidth(E->getType());
+        unsigned DestWidth = Ctx.getIntRange(E->getType());
         bool DestSigned = E->getType()->isSignedIntegerOrEnumerationType();
         APSInt IgnoredVal(DestWidth, !DestSigned);
         bool Ignored;

clang/lib/Sema/SemaTemplate.cpp
@@ -6931,7 +6931,7 @@ Sema::BuildExpressionFromIntegralTemplateArgument(const TemplateArgument &Arg,
   } else {
     auto IntValue = Arg.getAsIntegral();
     if (T->isIntCapType())
-      IntValue = IntValue.extOrTrunc(Context.getIntWidth(T));
+      IntValue = IntValue.extOrTrunc(Context.getIntRange(T));
     E = IntegerLiteral::Create(Context, IntValue, T, Loc);
   }
 

clang/test/SemaCXX/cheri/template-param-enum-uintcap.cpp
@@ -1,0 +1,10 @@
+// RUN: %cheri_purecap_cc1 -fsyntax-only -o - %s  -verify
+// expected-no-diagnostics
+enum { b = 3 };
+template <__uintcap_t c> class d {
+public:
+  enum { value = b * c };
+};
+d<1> e;
+static_assert(d<1>::value == 3, "");
+static_assert(d<2>::value == 6, "");