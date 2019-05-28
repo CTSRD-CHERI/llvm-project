ebbae6f8f0687c71f5423282196a0450497a94eb
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-05-28 12:06:11 +0100

    [Driver] Avoid unused command line argument for some CHERI options
    Passing -cheri-cap-table-abi to the linker is fine and should not warn.

--- clang/lib/Driver/ToolChains/FreeBSD.cpp
+++ clang/lib/Driver/ToolChains/FreeBSD.cpp
@@ -157,6 +157,16 @@ void freebsd::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   // And -cheri-uintcap=
   Args.ClaimAllArgs(options::OPT_cheri_uintcap_offset);
   Args.ClaimAllArgs(options::OPT_cheri_uintcap_addr);
+  // Also pretend that all the captable flags were used
+  Args.ClaimAllArgs(options::OPT_cheri_cap_table_abi);
+  Args.ClaimAllArgs(options::OPT_cheri_cap_table);
+  Args.ClaimAllArgs(options::OPT_no_cheri_cap_table);
+  Args.ClaimAllArgs(options::OPT_cheri_large_cap_table);
+  Args.ClaimAllArgs(options::OPT_no_cheri_large_cap_table);
+  Args.ClaimAllArgs(options::OPT_cheri_cap_tls_abi);
+  // Various other CHERI flags can also be passed to the linker without warning:
+  Args.ClaimAllArgs(options::OPT_cheri_data_dependent_provenance);
+  Args.ClaimAllArgs(options::OPT_cheri_bounds_EQ);
 
   // Silence warning for "clang -g foo.o -o foo"
   Args.ClaimAllArgs(options::OPT_g_Group);
--- clang/test/Driver/cheri/no-warning-cheri-flag-unused.c
+++ clang/test/Driver/cheri/no-warning-cheri-flag-unused.c
@@ -1,4 +1,18 @@
 // RUN: touch %t.o
-// RUN: %clang -g -integrated-as -Werror --target=cheri-unknown-freebsd -msoft-float -cheri=128 -Wl,--no-warn-mismatch -EB -mabi=purecap -Wl,-melf64btsmip_cheri_fbsd -fuse-ld=lld -Wl,-preemptible-caprelocs=elf -Wl,-z,norelro -Wl,--version-script=Version.map -shared -Wl,-x -Wl,--fatal-warnings -Wl,--warn-shared-textrel -o libprocstat.so.1 -Wl,-soname,libprocstat.so.1 %t.o -### 2>&1 | FileCheck %s
+// Check that cheri flags don't cause warnings when linking:
+// RUN: %clang -g -integrated-as -Werror -Werror=unused-command-line-argument --target=cheri-unknown-freebsd -msoft-float \
+// RUN:   -cheri=128 \
+// RUN:   -no-cheri-cap-table \
+// RUN:   -cheri-cap-table \
+// RUN:   -cheri-cap-table-abi=plt \
+// RUN:   -cheri-cap-tls-abi=legacy \
+// RUN:   -cheri \
+// RUN:   -cheri-uintcap=offset \
+// RUN:   -cheri-uintcap=addr \
+// RUN:   -cheri \
+// RUN:   -cheri-bounds=subobject-safe \
+// RUN:   -mxcaptable \
+// RUN:   -no-mxcaptable \
+// RUN:   -mabi=purecap -shared %t.o -### 2>&1 | FileCheck %s
 // CHECK-NOT: [-Werror,
 
