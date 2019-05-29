c8097df229a52bf8120fbaefa1038a01acef7503
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-05-29 20:18:57 +0100

    Fix test that depended on a local file that wasn't being created

--- lld/test/ELF/cheri/cap-table/non-preemptible-fn-ptr-unique-plt-abi.c
+++ lld/test/ELF/cheri/cap-table/non-preemptible-fn-ptr-unique-plt-abi.c
@@ -15,9 +15,8 @@
 // DISAS: 00000000000001e0: R_MIPS_CHERI_CAPCALL20/R_MIPS_NONE/R_MIPS_NONE check_if_default
 
 // RUN: %cheri128_purecap_cc1 -mllvm -cheri-cap-table-abi=plt -emit-obj -o %t2.o -DOTHER_FILE %s
-// RUN: ld.lld -shared %t-shlib.o -o %t2.o
-// RUN: llvm-objdump --syms  %t2.o | FileCheck %s -check-prefix OTHER_FILE-SYMS
-// OTHER_FILE-SYMS: 0000000000010000 g F .text 00000034 check_if_default
+// RUN: llvm-objdump --syms %t2.o | FileCheck %s -check-prefix OTHER_FILE-SYMS
+// OTHER_FILE-SYMS: 0000000000000000 g F .text 00000034 check_if_default
 
 // Check that we emit a R_CHERI_CAPABILITY relocation instead of __cap_relocs for shlib/pie/dynamically linked exe
 // Also check that we only emit exactly one fake symbol for each function pointer!
@@ -27,22 +26,21 @@
 // Should be the same for -Bsymbolic
 // RUN: ld.lld -Bsymbolic -shared %t.o %t2.o -o %t.so --verbose-cap-relocs
 // RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t.so | FileCheck %s -check-prefixes CHECK,CHECK-SHLIB,CHECK-NO-VERSION-SCRIPT
-// And for -pie:
-// RUN: ld.lld -pie %t.o %t2.o -o %t-pie.exe  --verbose-cap-relocs
-// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t-pie.exe | FileCheck %s -check-prefixes CHECK,CHECK-NO-VERSION-SCRIPT
 
 // Create an empty .so file just to force non-static linking:
 // RUN: %cheri128_purecap_cc1 -mllvm -cheri-cap-table-abi=plt -emit-obj -o %t-empty.o -xc /dev/null
 // RUN: ld.lld -shared %t-empty.o -o %t-empty-shlib.so
+// RUN: ld.lld -pie %t.o %t2.o %t-empty-shlib.so -o %t-pie.exe  --verbose-cap-relocs
+// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t-pie.exe | FileCheck %s -check-prefixes CHECK,CHECK-NONPREEMPTIBLE
 // RUN: ld.lld %t.o %t2.o %t-empty-shlib.so -o %t.exe  --verbose-cap-relocs
-// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t.exe | FileCheck %s -check-prefixes CHECK,CHECK-NO-VERSION-SCRIPT
+// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t.exe | FileCheck %s -check-prefixes CHECK,CHECK-NONPREEMPTIBLE
 
 // CHECK-LABEL: Dynamic Relocations {
 // With the version script default_callback comes first
-// CHECK-VERSION-SCRIPT-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE __cheri_fnptr_default_callback 0x0
-// CHECK-VERSION-SCRIPT-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE __cheri_fnptr_default_callback 0x0
+// CHECK-NONPREEMPTIBLE-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE __cheri_fnptr_default_callback 0x0
+// CHECK-NONPREEMPTIBLE-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE __cheri_fnptr_default_callback 0x0
 // one reference in .captable
-// CHECK-VERSION-SCRIPT-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE __cheri_fnptr_default_callback 0x0
+// CHECK-NONPREEMPTIBLE-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE __cheri_fnptr_default_callback 0x0
 // We need exactly one R_MIPS_CHERI_CAPABILITY relocation against static_callback to ensure that
 // the dynamic linker creates a unique trampoline
 // two global references to static_callback:
@@ -58,7 +56,7 @@
 // CHECK-NO-VERSION-SCRIPT-NEXT:   R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE default_callback 0x0
 
 // We need a R_MIPS_CHERI_CAPABILITY_CALL relocation against use_callback to call a function (potentially) in another DSO
-// CHECK-NEXT:     R_MIPS_CHERI_CAPABILITY_CALL/R_MIPS_NONE/R_MIPS_NONE check_if_default 0x0
+// CHECK-SHLINEXT:     R_MIPS_CHERI_CAPABILITY_CALL/R_MIPS_NONE/R_MIPS_NONE check_if_default 0x0
 // CHECK-NEXT:  }
 // CHECK-LABEL: DynamicSymbols [
 // CHECK-NEXT:   Symbol {
@@ -70,18 +68,18 @@
 // CHECK-NEXT:     Other: 0
 // CHECK-NEXT:     Section: Undefined (0x0)
 // CHECK-NEXT:   }
-// With the version script we also need to add a fake symbol for default_callback
-// CHECK-VERSION-SCRIPT-NEXT:   Symbol {
-// CHECK-VERSION-SCRIPT-NEXT:     Name: __cheri_fnptr_default_callback ({{.+}})
-// CHECK-VERSION-SCRIPT-NEXT:     Value: 0x10000
-// CHECK-VERSION-SCRIPT-NEXT:     Size: 12
-// CHECK-VERSION-SCRIPT-NEXT:     Binding: Local (0x0)
-// CHECK-VERSION-SCRIPT-NEXT:     Type: Function (0x2)
-// CHECK-VERSION-SCRIPT-NEXT:     Other [ (0x2)
-// CHECK-VERSION-SCRIPT-NEXT:       STV_HIDDEN (0x2)
-// CHECK-VERSION-SCRIPT-NEXT:     ]
-// CHECK-VERSION-SCRIPT-NEXT:     Section: .text (0x
-// CHECK-VERSION-SCRIPT-NEXT:   }
+// With the version script/when building an executable we also need to add a fake symbol for default_callback
+// CHECK-NONPREEMPTIBLE-NEXT:   Symbol {
+// CHECK-NONPREEMPTIBLE-NEXT:     Name: __cheri_fnptr_default_callback ({{.+}})
+// CHECK-NONPREEMPTIBLE-NEXT:     Value: 0x{{12001|1}}0000
+// CHECK-NONPREEMPTIBLE-NEXT:     Size: 12
+// CHECK-NONPREEMPTIBLE-NEXT:     Binding: Local (0x0)
+// CHECK-NONPREEMPTIBLE-NEXT:     Type: Function (0x2)
+// CHECK-NONPREEMPTIBLE-NEXT:     Other [ (0x2)
+// CHECK-NONPREEMPTIBLE-NEXT:       STV_HIDDEN (0x2)
+// CHECK-NONPREEMPTIBLE-NEXT:     ]
+// CHECK-NONPREEMPTIBLE-NEXT:     Section: .text (0x
+// CHECK-NONPREEMPTIBLE-NEXT:   }
 // CHECK-NEXT:   Symbol {
 // Check that we added a "fake" symbol for the local return1 symbol:
 // CHECK-NEXT:     Name: __cheri_fnptr_static_callback ({{.+}})
@@ -94,15 +92,17 @@
 // CHECK-NEXT:     ]
 // CHECK-NEXT:     Section: .text (0x
 // CHECK-NEXT:   }
-// CHECK-NEXT:  Symbol {
-// CHECK-NEXT:    Name: check_if_default (
-// CHECK-NEXT:    Value: 0x0
-// CHECK-NEXT:    Size: 0
-// CHECK-NEXT:    Binding: Global (0x1)
-// CHECK-NEXT:    Type: Function (0x2)
-// CHECK-NEXT:    Other: 0
-// CHECK-NEXT:    Section: Undefined (0x0)
-// CHECK-NEXT:  }
+// CHECK-SHLIB-NEXT:  Symbol {
+// CHECK-SHLIB-NEXT:    Name: check_if_default (
+// CHECK-SHLIB-NEXT:    Value: 0x{{12001|1}}0270
+// CHECK-SHLIB-NEXT:    Size: 52
+// CHECK-SHLIB-NEXT:    Binding: Global (0x1)
+// CHECK-SHLIB-NEXT:    Type: Function (0x2)
+// CHECK-SHLIB-NEXT:    Other [ (0x3)
+// CHECK-SHLIB-NEXT:      STV_PROTECTED (0x3)
+// CHECK-SHLIB-NEXT:    ]
+// CHECK-SHLIB-NEXT:    Section: .text (0x
+// CHECK-SHLIB-NEXT:  }
 // CHECK-NO-VERSION-SCRIPT-NEXT:  Symbol {
 // CHECK-NO-VERSION-SCRIPT-NEXT:    Name: default_callback (
 // CHECK-NO-VERSION-SCRIPT-NEXT:    Value: {{0x12001|0x1}}0000
@@ -115,17 +115,18 @@
 // CHECK-NEXT: ]
 // __cap_relocs should be empty other than the one global reference to callbacks
 // CHECK-NEXT: CHERI __cap_relocs [
+// CHECK-NEXT:   0x{{.+}} (check_if_default@CAPTABLE) Base: 0x{{.+}} (check_if_default+0) Length: 52 Perms: Function
 // CHECK-NEXT:   0x{{.+}} (callbacks@CAPTABLE.3) Base: 0x{{.+}} (callbacks+0) Length: 64 Perms: Constant
 // CHECK-NEXT: ]
 // CHECK-NEXT: CHERI .captable [
 // CHECK-NEXT:   0x0 default_callback@CAPTABLE R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE against {{default_callback|__cheri_fnptr_default_callback}}
-// CHECK-NEXT:   0x10 check_if_default@CAPTABLE R_MIPS_CHERI_CAPABILITY_CALL/R_MIPS_NONE/R_MIPS_NONE against check_if_default
+// CHECK-NEXT:   0x10 check_if_default@CAPTABLE
 // CHECK-NEXT:   0x20 static_callback@CAPTABLE.2 R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE against __cheri_fnptr_static_callback
 // CHECK-NEXT:   0x30 callbacks@CAPTABLE.3
 // CHECK-NEXT: ]
 
 // no need for trampolines in static executable
-// RUN: ld.lld %t.o %t-shlib.o -o %t-static.exe --verbose-cap-relocs 2>&1 | FileCheck %s -check-prefix STATIC-MESSAGE
+// RUN: ld.lld %t.o %t2.o -o %t-static.exe --verbose-cap-relocs 2>&1 | FileCheck %s -check-prefix STATIC-MESSAGE
 // STATIC-MESSAGE: Do not need function pointer trampoline for default_callback in static binary
 // STATIC-MESSAGE-NEXT: Do not need function pointer trampoline for static_callback in static binary
 // STATIC-MESSAGE-NEXT: Do not need function pointer trampoline for default_callback in static binary
@@ -134,7 +135,6 @@
 // STATIC-MESSAGE-NEXT: Do not need function pointer trampoline for static_callback in static binary
 // STATIC-MESSAGE-EMPTY:
 
-// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t-static.exe
 // RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t-static.exe | FileCheck %s -check-prefix STATIC
 // STATIC-LABEL: Dynamic Relocations {
 // STATIC-NEXT:  }
@@ -160,12 +160,12 @@
 // Check that we don't crash when a version script marks a symbol as non-preemptible:
 // RUN: echo "VERSION_1.0 { local: *; };" > %t.script
 // RUN: ld.lld -shared %t.o %t2.o %t-empty-shlib.so --verbose-cap-relocs -o %t-version-script.so --version-script %t.script
-// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t-version-script.so | FileCheck %s -check-prefixes CHECK,CHECK-VERSION-SCRIPT
+// RUN: llvm-readobj --dyn-symbols --dyn-relocations --cap-relocs --cap-table %t-version-script.so | FileCheck %s -check-prefixes CHECK,CHECK-NONPREEMPTIBLE
 
 // FIXME: this should also be a runtime test in rtld
 
 typedef int (*func_ptr)(void);
-_Bool check_if_default(func_ptr ptr);
+__attribute__((visibility("protected"))) _Bool check_if_default(func_ptr ptr);
 int default_callback(void);
 
 #ifdef OTHER_FILE
