a3edab4181df27f3b9442d3a23c73d134ea4fc09
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-06-13 13:31:20 +0100

    Remove files that should not exist
    Bad merge

--- clang/test/CodeGen/CHERI/weak-call.c
+++
@@ -1,8 +0,0 @@
-// RUN: %cheri_purecap_cc1 -std=gnu99 -ftls-model=local-exec -o - -O0 -emit-llvm  %s
-// calling a weak function used to crash the compiler
-
-void a(void);
-#pragma weak a
-void b(void) {
-  a();
-}
