06a03c4ee16786275a7b21f3b709e04e1d75eea7
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-07-01 11:27:26 +0100

    Fix debug-info test case

--- clang/test/CodeGen/cheri/cheri-debug-info.c
+++ clang/test/CodeGen/cheri/cheri-debug-info.c
@@ -5,15 +5,14 @@
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -emit-obj -O0 -o %t-cheri.o %s
 // RUN: %cheri_cc1 -mllvm -cheri-cap-table-abi=pcrel "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -emit-obj -O0 -o %t-cheri.o %s
 
-
 // Generate a .o file and compare the debug info:
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -emit-obj -O0 -o %t-cheri.o %s -mllvm -debug-only=dwarfdebug
 // RUN: %clang_cc1 -triple=mips64-unknown-freebsd "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -emit-obj -O0 -o %t-mips.o %s
 // RUN: %cheri_cc1 "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -emit-obj -O0 -o %t-hybrid.o %s
 
 // Check that hybrid and MIPS generated asm is the same:
-// RUN: %clang_cc1 -triple=mips64-unknown-freebsd "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -S -O0 -o %t-mips.s %s
-// RUN: %cheri_cc1 "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -S -O0 -o %t-hybrid.s %s
+// RUN: %clang_cc1 -triple=mips64-unknown-freebsd "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb" -mstack-alignment=32  -S -O0 -o %t-mips.s %s
+// RUN: %cheri_cc1 "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb" -mstack-alignment=32 -S -O0 -o %t-hybrid.s %s
 // Should be the same other than some ELF flags:
 // RUN: diff -u %t-mips.s %t-hybrid.s
 
@@ -28,13 +27,13 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // CHECK-IR: ret i32 %{{.+}}, !dbg !28
 // Both MIPS and purecap should have the same number of metadata nodes:
 // CHECK-IR: !13 = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: !4, size: {{64|128|256}})
-// CHECK-IR: !14 = !DILocalVariable(name: "i", arg: 1, scope: !9, file: !10, line: 20, type: !13)
+// CHECK-IR: !14 = !DILocalVariable(name: "i", arg: 1, scope: !9, file: !10, line: [[#FOO_DECL_LINE:]], type: !13)
 // check that we have both j variables listed:
-// CHECK-IR: !19 = !DILocalVariable(name: "j", scope: !20, file: !10, line: 22, type: !4)
-// CHECK-IR: !20 = distinct !DILexicalBlock(scope: !17, file: !10, line: 21, column: 9)
-// CHECK-IR: !23 = !DILocalVariable(name: "j", scope: !24, file: !10, line: 25, type: !4)
-// CHECK-IR: !24 = distinct !DILexicalBlock(scope: !17, file: !10, line: 24, column: 7)
-// CHECK-IR: !28 = !DILocation(line: 27, column: 2, scope: !9)
+// CHECK-IR: !19 = !DILocalVariable(name: "j", scope: !20, file: !10, line: [[#FOO_DECL_LINE + 2]], type: !4)
+// CHECK-IR: !20 = distinct !DILexicalBlock(scope: !17, file: !10, line: [[#FOO_DECL_LINE + 1]], column: 9)
+// CHECK-IR: !23 = !DILocalVariable(name: "j", scope: !24, file: !10, line: [[#FOO_DECL_LINE + 5]], type: !4)
+// CHECK-IR: !24 = distinct !DILexicalBlock(scope: !17, file: !10, line: [[#FOO_DECL_LINE + 4]], column: 7)
+// CHECK-IR: !28 = !DILocation(line: [[#FOO_DECL_LINE + 7]], column: 2, scope: !9)
 }
 
 // Previoulsy llvm-dwarfdump would not handle MIPS relocations for a CHERI triple:
@@ -75,7 +74,7 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // CHERI-DEBUG-INFO-NEXT:           DW_AT_frame_base	(DW_OP_regx C11)
 // DEBUG-INFO-NEXT:                 DW_AT_name	("foo")
 // DEBUG-INFO-NEXT:                 DW_AT_decl_file	("{{.+}}/test/CodeGen/cheri/cheri-debug-info.c")
-// DEBUG-INFO-NEXT:                 DW_AT_decl_line	(20)
+// DEBUG-INFO-NEXT:                 DW_AT_decl_line	([[#FOO_DECL_LINE:]])
 // DEBUG-INFO-NEXT:                 DW_AT_prototyped	(0x01)
 // DEBUG-INFO-NEXT:                 DW_AT_type	([[INT_TYPE_INFO_ADDR]] "int")
 // DEBUG-INFO-NEXT:                 DW_AT_external	(0x01)
@@ -84,7 +83,7 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // DEBUG-INFO-NEXT:                   DW_AT_location	(DW_OP_fbreg +{{.+}})
 // DEBUG-INFO-NEXT:                   DW_AT_name	("i")
 // DEBUG-INFO-NEXT:                   DW_AT_decl_file	("{{.+}}/test/CodeGen/cheri/cheri-debug-info.c")
-// DEBUG-INFO-NEXT:                   DW_AT_decl_line	(20)
+// DEBUG-INFO-NEXT:                   DW_AT_decl_line	([[#FOO_DECL_LINE]])
 // DEBUG-INFO-NEXT:                   DW_AT_type	([[INTPTR_TYPE_INFO_ADDR:0x000.+]] "int*")
 // DEBUG-INFO-EMPTY:
 // DEBUG-INFO-NEXT: DW_TAG_lexical_block
@@ -95,7 +94,7 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // DEBUG-INFO-NEXT:                     DW_AT_location	(DW_OP_fbreg +{{.+}})
 // DEBUG-INFO-NEXT:                     DW_AT_name	("j")
 // DEBUG-INFO-NEXT:                     DW_AT_decl_file	("{{.+}}/test/CodeGen/cheri/cheri-debug-info.c")
-// DEBUG-INFO-NEXT:                     DW_AT_decl_line	(22)
+// DEBUG-INFO-NEXT:                     DW_AT_decl_line	([[#FOO_DECL_LINE + 2]])
 // DEBUG-INFO-NEXT:                     DW_AT_type	([[INT_TYPE_INFO_ADDR]] "int")
 // DEBUG-INFO-EMPTY:
 // DEBUG-INFO-NEXT:  NULL
@@ -108,7 +107,7 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // DEBUG-INFO-NEXT:                     DW_AT_location	(DW_OP_fbreg +{{.+}})
 // DEBUG-INFO-NEXT:                     DW_AT_name	("j")
 // DEBUG-INFO-NEXT:                     DW_AT_decl_file	("{{.+}}/CodeGen/cheri/cheri-debug-info.c")
-// DEBUG-INFO-NEXT:                     DW_AT_decl_line	(25)
+// DEBUG-INFO-NEXT:                     DW_AT_decl_line	([[#FOO_DECL_LINE + 5]])
 // DEBUG-INFO-NEXT:                     DW_AT_type	([[INT_TYPE_INFO_ADDR]] "int")
 // DEBUG-INFO-EMPTY:
 // DEBUG-INFO-NEXT:   NULL
@@ -126,7 +125,6 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 
 // Check the optimized debug info (should no longer have the two j variables but should have a register for i:
 
-// RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb"  -emit-obj -O2 -o - %s | llvm-dwarfdump -debug-info -
 // RUN: %cheri_purecap_cc1 -mllvm -cheri-cap-table-abi=pcrel "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb" \
 // RUN: -emit-obj -O2 -o - %s | llvm-dwarfdump -debug-info - | FileCheck %s -check-prefixes DEBUG-INFO-OPT,CHERI-DEBUG-INFO-OPT
 // RUN: %cheri_cc1 -mllvm -cheri-cap-table-abi=pcrel "-dwarf-column-info" "-debug-info-kind=standalone" "-dwarf-version=2" "-debugger-tuning=gdb" \
@@ -161,7 +159,7 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // MIPS-DEBUG-INFO-OPT-NEXT:            DW_AT_frame_base	(DW_OP_reg29 SP_64)
 // DEBUG-INFO-OPT-NEXT:                 DW_AT_name	("foo")
 // DEBUG-INFO-OPT-NEXT:                 DW_AT_decl_file	("{{.+}}cheri-debug-info.c")
-// DEBUG-INFO-OPT-NEXT:                 DW_AT_decl_line	(20)
+// DEBUG-INFO-OPT-NEXT:                 DW_AT_decl_line	([[#FOO_DECL_LINE:]])
 // DEBUG-INFO-OPT-NEXT:                 DW_AT_prototyped	(0x01)
 // DEBUG-INFO-OPT-NEXT:                 DW_AT_type	([[INT_ADDR]] "int")
 // DEBUG-INFO-OPT-NEXT:                 DW_AT_external	(0x01)
@@ -171,7 +169,7 @@ int foo(int* i) { // CHECK-IR:  call void @llvm.dbg.declare(metadata i32{{( addr
 // CHERI-DEBUG-INFO-OPT-NEXT:                   DW_AT_location	(DW_OP_regx C3)
 // DEBUG-INFO-OPT-NEXT:                         DW_AT_name	("i")
 // DEBUG-INFO-OPT-NEXT:                         DW_AT_decl_file	("{{.+}}cheri-debug-info.c")
-// DEBUG-INFO-OPT-NEXT:                         DW_AT_decl_line	(20)
+// DEBUG-INFO-OPT-NEXT:                         DW_AT_decl_line	([[#FOO_DECL_LINE]])
 // DEBUG-INFO-OPT-NEXT:                         DW_AT_type	([[INT_PTR_INFO_LOC:0x0000006(2|4)]] "int*")
 // DEBUG-INFO-OPT-EMPTY:
 // DEBUG-INFO-OPT-NEXT: 0x000000{{.+}}:     NULL
