CTSRD-CHERI/llvm-project

commit b7cfb54e6499931b5100486e5104ffb45d8dc1dd

Make the "m" constrain work by removing diff to upstream
This also showed that the CHERI register names in tablegen are not
accepted by the asm parser since they use uppercase C. Changed them to
use lowercase names.

llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -5292,8 +5292,7 @@ parseRegForInlineAsmConstraint(StringRef C, MVT VT) const {
   else if (Prefix == "$w") { // Parse $w0-$w31.
     RC = getRegClassFor((VT == MVT::Other) ? MVT::v16i8 : VT);
   } else if (Prefix == "$c") { // Parse $c0-$c31.
-    assert(Reg != 0 && "$c0 should not end up as an inline asm constraint");
-    Reg--; // C0 does not exist so we need to subtract one here
+    assert(Reg != 0 && "$cnull should not end up as an inline asm constraint");
     RC = getRegClassFor(CapType);
   } else { // Parse $0-$31.
     assert(Prefix == "$");

clang/lib/Basic/TargetInfo.cpp
@@ -583,9 +583,6 @@ bool TargetInfo::validateOutputConstraint(ConstraintInfo &Info) const {
       Info.setAllowsRegister();
       break;
     case 'm': // memory operand.
-      if (areAllPointersCapabilities())
-        break;
-      LLVM_FALLTHROUGH;
     case 'o': // offsetable memory operand.
     case 'V': // non-offsetable memory operand.
     case '<': // autodecrement memory operand.

clang/lib/CodeGen/CGStmt.cpp
@@ -1891,10 +1891,7 @@ CodeGenFunction::EmitAsmInputLValue(const TargetInfo::ConstraintInfo &Info,
     }
   } else {
     Arg = InputValue.getPointer();
-    // An 'm' constraint will have been rewritten to 'C' if the input expr is a
-    // CHERI capability type. However, don't make an indirect input.
-    if (!InputType->isCHERICapabilityType(getContext()))
-      ConstraintStr += '*';
+    ConstraintStr += '*';
   }
 
   return Arg;
@@ -1922,19 +1919,9 @@ llvm::Value* CodeGenFunction::EmitAsmInput(
       return llvm::ConstantInt::get(getLLVMContext(), Result.Val.getInt());
   }
 
-  if (Info.allowsRegister() || !Info.allowsMemory()) {
-    // CHERI: For references, return a pointer to the referenced value
-    if (const DeclRefExpr *DRE = dyn_cast<DeclRefExpr>(InputExpr->IgnoreParenNoopCasts(getContext()))) {
-      if (const VarDecl *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
-        if (const ReferenceType *RT = dyn_cast<ReferenceType>(VD->getType())) {
-          if (RT->isCHERICapability())
-            return EmitLValue(InputExpr).getPointer();
-        }
-      }
-    }
-    else if (CodeGenFunction::hasScalarEvaluationKind(InputExpr->getType()))
+  if (Info.allowsRegister() || !Info.allowsMemory())
+    if (CodeGenFunction::hasScalarEvaluationKind(InputExpr->getType()))
       return EmitScalarExpr(InputExpr);
-  }
   if (InputExpr->getStmtClass() == Expr::CXXThisExprClass)
     return EmitScalarExpr(InputExpr);
   InputExpr = InputExpr->IgnoreParenNoopCasts(getContext());
@@ -2220,21 +2207,6 @@ void CodeGenFunction::EmitAsmStmt(const AsmStmt &S) {
         InputConstraint, *InputExpr->IgnoreParenNoopCasts(getContext()),
         getTarget(), CGM, S, false /* No EarlyClobber */);
 
-    // CHERI: Rewrite 'r' and 'm' constraints to C if the input expr is a capability
-    if (Info.allowsRegister() || Info.allowsMemory()) {
-      if(InputExpr->getType()->isCHERICapabilityType(getContext())) {
-        std::size_t rfound = InputConstraint.find("r");
-        std::size_t mfound = InputConstraint.find("m");
-        if (rfound != std::string::npos && mfound != std::string::npos) {
-          InputConstraint.replace(rfound, 1, "C");
-          InputConstraint.erase(mfound, 1);
-        } else if (rfound != std::string::npos)
-          InputConstraint.replace(rfound, 1, "C");
-        else if (mfound != std::string::npos)
-          InputConstraint.replace(mfound, 1, "C");
-      }
-    }
-
     std::string ReplaceConstraint (InputConstraint);
     llvm::Value *Arg = EmitAsmInput(Info, InputExpr, Constraints);
 
@@ -2411,8 +2383,9 @@ void CodeGenFunction::EmitAsmStmt(const AsmStmt &S) {
     // ResultTypeRequiresCast.size() elements of RegResults.
     if ((i < ResultTypeRequiresCast.size()) && ResultTypeRequiresCast[i]) {
       unsigned Size = getContext().getTypeSize(ResultRegQualTys[i]);
-      Address A = Builder.CreateBitCast(Dest.getAddress(),
-                                        ResultRegTypes[i]->getPointerTo());
+      Address A = Builder.CreateBitCast(
+          Dest.getAddress(),
+          ResultRegTypes[i]->getPointerTo(Dest.getAddress().getAddressSpace()));
       QualType Ty = getContext().getIntTypeForBitwidth(Size, /*Signed*/ false);
       if (Ty.isNull()) {
         const Expr *OutExpr = S.getOutputExpr(i);

clang/test/CodeGenCXX/cheri/inlineasm-constraint.cpp
@@ -1,0 +1,74 @@
+// NOTE: Assertions have been autogenerated by utils/update_cc_test_checks.py
+// RUN: %cheri_cc1 -emit-llvm -O1 %s -o -  | FileCheck %s -check-prefix HYBRID
+// RUN: %cheri_purecap_cc1 -emit-llvm -O1 %s -o -  | FileCheck %s -check-prefix PURECAP
+
+template <class Tp>
+__attribute__((always_inline)) void DoNotOptimize(Tp& value) {
+#ifdef __CHERI_PURE_CAPABILITY__
+  asm volatile("clb $zero, $zero, %0" : "+r,m"(value) : : "memory");
+#else
+  asm volatile("lb $zero, %0" : "+r,m"(value) : : "memory");
+#endif
+// asm volatile("" : "+m,r"(value) : : "memory");
+}
+
+char DummyData4[4];
+char DummyData8[8];
+char DummyData16[16];
+char DummyData32[32];
+char DummyData64[64];
+
+// HYBRID-LABEL: @_Z4testv(
+// HYBRID-NEXT:  entry:
+// HYBRID-NEXT:    [[TMP0:%.*]] = load i32, i32* bitcast ([4 x i8]* @DummyData4 to i32*), align 1
+// HYBRID-NEXT:    tail call void asm sideeffect "lb $$zero, $0", "=*r|m,0,~{memory},~{$1}"([4 x i8]* nonnull @DummyData4, i32 [[TMP0]]) #1, !srcloc !2
+// HYBRID-NEXT:    [[TMP1:%.*]] = load i64, i64* bitcast ([8 x i8]* @DummyData8 to i64*), align 1
+// HYBRID-NEXT:    tail call void asm sideeffect "lb $$zero, $0", "=*r|m,0,~{memory},~{$1}"([8 x i8]* nonnull @DummyData8, i64 [[TMP1]]) #1, !srcloc !2
+// HYBRID-NEXT:    tail call void asm sideeffect "lb $$zero, $0", "=*r|m,*0,~{memory},~{$1}"([16 x i8]* nonnull @DummyData16, [16 x i8]* nonnull @DummyData16) #1, !srcloc !2
+// HYBRID-NEXT:    tail call void asm sideeffect "lb $$zero, $0", "=*r|m,*0,~{memory},~{$1}"([32 x i8]* nonnull @DummyData32, [32 x i8]* nonnull @DummyData32) #1, !srcloc !2
+// HYBRID-NEXT:    tail call void asm sideeffect "lb $$zero, $0", "=*r|m,*0,~{memory},~{$1}"([64 x i8]* nonnull @DummyData64, [64 x i8]* nonnull @DummyData64) #1, !srcloc !2
+// HYBRID-NEXT:    [[TMP2:%.*]] = load i8, i8* getelementptr inbounds ([4 x i8], [4 x i8]* @DummyData4, i64 0, i64 0), align 1, !tbaa !3
+// HYBRID-NEXT:    ret i8 [[TMP2]]
+//
+// PURECAP-LABEL: @_Z4testv(
+// PURECAP-NEXT:  entry:
+// PURECAP-NEXT:    [[TMP0:%.*]] = load i32, i32 addrspace(200)* bitcast ([4 x i8] addrspace(200)* @DummyData4 to i32 addrspace(200)*), align 1
+// PURECAP-NEXT:    tail call void asm sideeffect "clb $$zero, $$zero, $0", "=*r|m,0,~{memory},~{$1}"([4 x i8] addrspace(200)* nonnull @DummyData4, i32 [[TMP0]]) #1, !srcloc !2
+// PURECAP-NEXT:    [[TMP1:%.*]] = load i64, i64 addrspace(200)* bitcast ([8 x i8] addrspace(200)* @DummyData8 to i64 addrspace(200)*), align 1
+// PURECAP-NEXT:    tail call void asm sideeffect "clb $$zero, $$zero, $0", "=*r|m,0,~{memory},~{$1}"([8 x i8] addrspace(200)* nonnull @DummyData8, i64 [[TMP1]]) #1, !srcloc !2
+// PURECAP-NEXT:    tail call void asm sideeffect "clb $$zero, $$zero, $0", "=*r|m,*0,~{memory},~{$1}"([16 x i8] addrspace(200)* nonnull @DummyData16, [16 x i8] addrspace(200)* nonnull @DummyData16) #1, !srcloc !2
+// PURECAP-NEXT:    tail call void asm sideeffect "clb $$zero, $$zero, $0", "=*r|m,*0,~{memory},~{$1}"([32 x i8] addrspace(200)* nonnull @DummyData32, [32 x i8] addrspace(200)* nonnull @DummyData32) #1, !srcloc !2
+// PURECAP-NEXT:    tail call void asm sideeffect "clb $$zero, $$zero, $0", "=*r|m,*0,~{memory},~{$1}"([64 x i8] addrspace(200)* nonnull @DummyData64, [64 x i8] addrspace(200)* nonnull @DummyData64) #1, !srcloc !2
+// PURECAP-NEXT:    [[TMP2:%.*]] = load i8, i8 addrspace(200)* getelementptr inbounds ([4 x i8], [4 x i8] addrspace(200)* @DummyData4, i64 0, i64 0), align 1, !tbaa !3
+// PURECAP-NEXT:    ret i8 [[TMP2]]
+//
+char test() {
+  DoNotOptimize(DummyData4);
+  DoNotOptimize(DummyData8);
+  DoNotOptimize(DummyData16);
+  DoNotOptimize(DummyData32);
+  DoNotOptimize(DummyData64);
+  return DummyData4[0];
+}
+
+// HYBRID-LABEL: @_Z8test_ptrPv(
+// HYBRID-NEXT:  entry:
+// HYBRID-NEXT:    [[P_ADDR:%.*]] = alloca i8*, align 8
+// HYBRID-NEXT:    store i8* [[P:%.*]], i8** [[P_ADDR]], align 8, !tbaa !6
+// HYBRID-NEXT:    call void asm sideeffect "lb $$zero, $0", "=*r|m,0,~{memory},~{$1}"(i8** nonnull [[P_ADDR]], i8* [[P]]) #1, !srcloc !2
+// HYBRID-NEXT:    [[TMP0:%.*]] = load i8*, i8** [[P_ADDR]], align 8, !tbaa !6
+// HYBRID-NEXT:    ret i8* [[TMP0]]
+//
+//
+// PURECAP-LABEL: @_Z8test_ptrU3capPv(
+// PURECAP-NEXT:  entry:
+// PURECAP-NEXT:    [[P_ADDR:%.*]] = alloca i8 addrspace(200)*, align 16, addrspace(200)
+// PURECAP-NEXT:    store i8 addrspace(200)* [[P:%.*]], i8 addrspace(200)* addrspace(200)* [[P_ADDR]], align 16, !tbaa !6
+// PURECAP-NEXT:    call void asm sideeffect "clb $$zero, $$zero, $0", "=*r|m,0,~{memory},~{$1}"(i8 addrspace(200)* addrspace(200)* nonnull [[P_ADDR]], i8 addrspace(200)* [[P]]) #1, !srcloc !2
+// PURECAP-NEXT:    [[TMP0:%.*]] = load i8 addrspace(200)*, i8 addrspace(200)* addrspace(200)* [[P_ADDR]], align 16, !tbaa !6
+// PURECAP-NEXT:    ret i8 addrspace(200)* [[TMP0]]
+//
+void* test_ptr(void* p) {
+  DoNotOptimize(p);
+  return p;
+}

llvm/lib/Target/Mips/MipsRegisterInfo.td
@@ -300,38 +300,38 @@ let Namespace = "Mips" in {
   // For compatibility with GDB keep the DwarfRegNum for DDC as 72 and assign
   // the next one to cnull (not very useful in GDB anyway since it will always
   // be null).
-  def CNULL : CAP<0, "CNULL", []>, DwarfRegNum<[104]>;
-  def C1  : CAP<1, "C1", []>, DwarfRegNum<[73]>;
-  def C2  : CAP<2, "C2", []>, DwarfRegNum<[74]>;
-  def C3  : CAP<3, "C3", []>, DwarfRegNum<[75]>;
-  def C4  : CAP<4, "C4", []>, DwarfRegNum<[76]>;
-  def C5  : CAP<5, "C5", []>, DwarfRegNum<[77]>;
-  def C6  : CAP<6, "C6", []>, DwarfRegNum<[78]>;
-  def C7  : CAP<7, "C7", []>, DwarfRegNum<[79]>;
-  def C8  : CAP<8, "C8", []>, DwarfRegNum<[80]>;
-  def C9  : CAP<9, "C9", []>, DwarfRegNum<[81]>;
-  def C10 : CAP<10, "C10", []>, DwarfRegNum<[82]>;
-  def C11 : CAP<11, "C11", []>, DwarfRegNum<[83]>;
-  def C12 : CAP<12, "C12", []>, DwarfRegNum<[84]>;
-  def C13 : CAP<13, "C13", []>, DwarfRegNum<[85]>;
-  def C14 : CAP<14, "C14", []>, DwarfRegNum<[86]>;
-  def C15 : CAP<15, "C15", []>, DwarfRegNum<[87]>;
-  def C16 : CAP<16, "C16", []>, DwarfRegNum<[88]>;
-  def C17 : AliasCAP<17, "C17", [RA_64]>, DwarfRegNum<[89]>;
-  def C18 : AliasCAP<18, "C18", [S0_64]>, DwarfRegNum<[90]>;
-  def C19 : AliasCAP<19, "C19", [S1_64]>, DwarfRegNum<[91]>;
-  def C20 : AliasCAP<20, "C20", [S2_64]>, DwarfRegNum<[92]>;
-  def C21 : AliasCAP<21, "C21", [S3_64]>, DwarfRegNum<[93]>;
-  def C22 : AliasCAP<22, "C22", [S4_64]>, DwarfRegNum<[94]>;
-  def C23 : AliasCAP<23, "C23", [S5_64]>, DwarfRegNum<[95]>;
-  def C24 : AliasCAP<24, "C24", [FP_64]>, DwarfRegNum<[96]>;
-  def C25 : CAP<25, "C25", []>, DwarfRegNum<[97]>;
-  def C26 : AliasCAP<26, "C26", [GP_64]>, DwarfRegNum<[98]>;
-  def C27 : AliasCAP<27, "C27", [K0_64]>, DwarfRegNum<[99]>;
-  def C28 : AliasCAP<28, "C28", [K1_64]>, DwarfRegNum<[100]>;
-  def C29 : CAP<29, "C29", []>, DwarfRegNum<[101]>;
-  def C30 : CAP<30, "C30", []>, DwarfRegNum<[102]>;
-  def C31 : CAP<31, "C31", []>, DwarfRegNum<[103]>;
+  def CNULL : CAP<0, "cnull", []>, DwarfRegNum<[104]>;
+  def C1  : CAP<1, "c1", []>, DwarfRegNum<[73]>;
+  def C2  : CAP<2, "c2", []>, DwarfRegNum<[74]>;
+  def C3  : CAP<3, "c3", []>, DwarfRegNum<[75]>;
+  def C4  : CAP<4, "c4", []>, DwarfRegNum<[76]>;
+  def C5  : CAP<5, "c5", []>, DwarfRegNum<[77]>;
+  def C6  : CAP<6, "c6", []>, DwarfRegNum<[78]>;
+  def C7  : CAP<7, "c7", []>, DwarfRegNum<[79]>;
+  def C8  : CAP<8, "c8", []>, DwarfRegNum<[80]>;
+  def C9  : CAP<9, "c9", []>, DwarfRegNum<[81]>;
+  def C10 : CAP<10, "c10", []>, DwarfRegNum<[82]>;
+  def C11 : CAP<11, "c11", []>, DwarfRegNum<[83]>;
+  def C12 : CAP<12, "c12", []>, DwarfRegNum<[84]>;
+  def C13 : CAP<13, "c13", []>, DwarfRegNum<[85]>;
+  def C14 : CAP<14, "c14", []>, DwarfRegNum<[86]>;
+  def C15 : CAP<15, "c15", []>, DwarfRegNum<[87]>;
+  def C16 : CAP<16, "c16", []>, DwarfRegNum<[88]>;
+  def C17 : AliasCAP<17, "c17", [RA_64]>, DwarfRegNum<[89]>;
+  def C18 : AliasCAP<18, "c18", [S0_64]>, DwarfRegNum<[90]>;
+  def C19 : AliasCAP<19, "c19", [S1_64]>, DwarfRegNum<[91]>;
+  def C20 : AliasCAP<20, "c20", [S2_64]>, DwarfRegNum<[92]>;
+  def C21 : AliasCAP<21, "c21", [S3_64]>, DwarfRegNum<[93]>;
+  def C22 : AliasCAP<22, "c22", [S4_64]>, DwarfRegNum<[94]>;
+  def C23 : AliasCAP<23, "c23", [S5_64]>, DwarfRegNum<[95]>;
+  def C24 : AliasCAP<24, "c24", [FP_64]>, DwarfRegNum<[96]>;
+  def C25 : CAP<25, "c25", []>, DwarfRegNum<[97]>;
+  def C26 : AliasCAP<26, "c26", [GP_64]>, DwarfRegNum<[98]>;
+  def C27 : AliasCAP<27, "c27", [K0_64]>, DwarfRegNum<[99]>;
+  def C28 : AliasCAP<28, "c28", [K1_64]>, DwarfRegNum<[100]>;
+  def C29 : CAP<29, "c29", []>, DwarfRegNum<[101]>;
+  def C30 : CAP<30, "c30", []>, DwarfRegNum<[102]>;
+  def C31 : CAP<31, "c31", []>, DwarfRegNum<[103]>;
 
 
   // TODO: assign DWARF register numbers to the special hwregs

llvm/test/CodeGen/Mips/cheri/inlineasm-constraint.cpp
@@ -1,0 +1,103 @@
+// RUN: %cheri_cc1 -S -O0 %s -o - | FileCheck %s -check-prefix MIPS
+// RUN: %cheri_purecap_cc1 -S -O0 %s -o - | FileCheck %s -check-prefix PURECAP
+
+
+template <class Tp>
+__attribute__((always_inline)) void DoNotOptimize(Tp& value) {
+#ifdef __CHERI_PURE_CAPABILITY__
+  asm volatile("clb $zero, $zero, %0" : "+r,m"(value) : : "memory");
+#else
+  asm volatile("lb $zero, %0" : "+r,m"(value) : : "memory");
+#endif
+}
+
+char DummyData4[4];
+char DummyData8[8];
+char DummyData16[16];
+char DummyData32[32];
+char DummyData64[64];
+
+char test() {
+  DoNotOptimize(DummyData4);
+  DoNotOptimize(DummyData8);
+  DoNotOptimize(DummyData16);
+  DoNotOptimize(DummyData32);
+  DoNotOptimize(DummyData64);
+  return DummyData4[0];
+}
+
+// PURECAP-LABEL: _Z4testv:
+// PURECAP: clcbi	[[DATA4_PTR:\$c[0-9]+]], %captab20(DummyData4)($c1)
+// PURECAP-NEXT: csc	[[DATA4_PTR]], $zero, [[DATA4_ON_STACK:0\(\$c[0-9]+\)]]
+// PURECAP-NEXT: clc	[[DATA4:\$c[0-9]+]], $zero, [[DATA4_ON_STACK]]
+// PURECAP: #APP
+// PURECAP: clb	$zero, $zero, 0([[DATA4]])
+// PURECAP: #NO_APP
+
+// PURECAP: clcbi	[[DATA8_PTR:\$c[0-9]+]], %captab20(DummyData8)($c1)
+// PURECAP-NEXT: csc	[[DATA8_PTR]], $zero, [[DATA8_ON_STACK:0\(\$c[0-9]+\)]]
+// PURECAP-NEXT: clc	[[DATA8:\$c[0-9]+]], $zero, [[DATA8_ON_STACK]]
+// PURECAP: #APP
+// PURECAP: clb	$zero, $zero, 0([[DATA8]])
+// PURECAP: #NO_APP
+
+// PURECAP: clcbi	[[DATA16_PTR:\$c[0-9]+]], %captab20(DummyData16)($c1)
+// PURECAP-NEXT: csc	[[DATA16_PTR]], $zero, [[DATA16_ON_STACK:0\(\$c[0-9]+\)]]
+// PURECAP-NEXT: clc	[[DATA16:\$c[0-9]+]], $zero, [[DATA16_ON_STACK]]
+// PURECAP: #APP
+// PURECAP: clb	$zero, $zero, 0([[DATA16]])
+// PURECAP: #NO_APP
+
+// PURECAP: clcbi	[[DATA32_PTR:\$c[0-9]+]], %captab20(DummyData32)($c1)
+// PURECAP-NEXT: csc	[[DATA32_PTR]], $zero, [[DATA32_ON_STACK:0\(\$c[0-9]+\)]]
+// PURECAP-NEXT: clc	[[DATA32:\$c[0-9]+]], $zero, [[DATA32_ON_STACK]]
+// PURECAP: #APP
+// PURECAP: clb	$zero, $zero, 0([[DATA32]])
+// PURECAP: #NO_APP
+
+// PURECAP: clcbi	[[DATA64_PTR:\$c[0-9]+]], %captab20(DummyData64)($c1)
+// PURECAP-NEXT: csc	[[DATA64_PTR]], $zero, [[DATA64_ON_STACK:0\(\$c[0-9]+\)]]
+// PURECAP-NEXT: clc	[[DATA64:\$c[0-9]+]], $zero, [[DATA64_ON_STACK]]
+// PURECAP: #APP
+// PURECAP: clb	$zero, $zero, 0([[DATA64]])
+// PURECAP: #NO_APP
+
+// MIPS-LABEL: _Z4testv:
+// MIPS: ld	[[DATA4_PTR:\$[0-9]+]], %got_disp(DummyData4)([[GOT_PTR:\$[0-9]+]])
+// MIPS-NEXT: sd	[[DATA4_PTR]], [[DATA4_ON_STACK:8\(\$sp\)]]
+// MIPS-NEXT: ld	[[DATA4:\$[0-9]+]], [[DATA4_ON_STACK]]
+// MIPS-NEXT: sd	[[GOT_PTR]], 0($sp)
+// MIPS: #APP
+// MIPS: lb	$zero, 0([[DATA4]])
+// MIPS: #NO_APP
+// MIPS: ld	[[DATA8_PTR:\$[0-9]+]], %got_disp(DummyData8)($3)
+// MIPS-NEXT: sd	[[DATA8_PTR]], [[DATA8_ON_STACK:16\(\$sp\)]]
+// MIPS-NEXT: ld	[[DATA8:\$[0-9]+]], [[DATA8_ON_STACK]]
+// MIPS: #APP
+// MIPS: lb	$zero, 0([[DATA8]])
+// MIPS: #NO_APP
+// MIPS: ld	[[DATA16_PTR:\$[0-9]+]], %got_disp(DummyData16)($3)
+// MIPS-NEXT: sd	[[DATA16_PTR]], [[DATA16_ON_STACK:24\(\$sp\)]]
+// MIPS-NEXT: ld	[[DATA16:\$[0-9]+]], [[DATA16_ON_STACK]]
+// MIPS: #APP
+// MIPS: lb	$zero, 0([[DATA16]])
+// MIPS: #NO_APP
+// MIPS: ld	[[DATA32_PTR:\$[0-9]+]], %got_disp(DummyData32)($3)
+// MIPS-NEXT: sd	[[DATA32_PTR]], [[DATA32_ON_STACK:32\(\$sp\)]]
+// MIPS-NEXT: ld	[[DATA32:\$[0-9]+]], [[DATA32_ON_STACK]]
+// MIPS: #APP
+// MIPS: lb	$zero, 0([[DATA32]])
+// MIPS: #NO_APP
+// MIPS: ld	[[DATA64_PTR:\$[0-9]+]], %got_disp(DummyData64)($3)
+// MIPS-NEXT: sd	[[DATA64_PTR]], [[DATA64_ON_STACK:40\(\$sp\)]]
+// MIPS-NEXT: ld	[[DATA64:\$[0-9]+]], [[DATA64_ON_STACK]]
+// MIPS: #APP
+// MIPS: lb	$zero, 0([[DATA64]])
+// MIPS: #NO_APP
+
+
+
+void* test_ptr(void* p) {
+  DoNotOptimize(p);
+  return p;
+}

commit c749a1bbc2d25e4c22e1449c1e0787b06fd96bb2

Fix off-by-one bug that I accidentally re-introduced
This was caused by b7cfb54e6499931b5100486e5104ffb45d8dc1dd.
Improve the test and add assertions to make this more obvious

llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -5295,8 +5295,15 @@ parseRegForInlineAsmConstraint(StringRef C, MVT VT) const {
   else if (Prefix == "$w") { // Parse $w0-$w31.
     RC = getRegClassFor((VT == MVT::Other) ? MVT::v16i8 : VT);
   } else if (Prefix == "$c") { // Parse $c0-$c31.
-    assert(Reg != 0 && "$cnull should not end up as an inline asm constraint");
+    if (Reg == 0)
+      report_fatal_error("$c0 should not be used as an inline asm constraint");
     RC = getRegClassFor(CapType);
+    assert(RC == &Mips::CheriGPRRegClass);
+    assert(!RC->contains(Mips::CNULL));
+    assert(!RC->contains(Mips::DDC));
+    // Note: The CheriGPR register class does not contain CNULL so we have to
+    // subtrace one to get the actual register number.
+    Reg--;
   } else { // Parse $0-$31.
     assert(Prefix == "$");
     RC = getRegClassFor((VT == MVT::Other) ? MVT::i32 : VT);

llvm/test/CodeGen/Mips/cheri/inline-asm-clobber.ll
@@ -1,4 +1,4 @@
-; RUNNOT: %cheri_purecap_cc1 -xc -emit-llvm -o - -O2 %s
+; RUN: %cheri_purecap_llc -o - -O0 %s -stop-after=instruction-select | FileCheck %s -check-prefix MIR
 ; RUN: %cheri_purecap_llc -o - -O3 %s | FileCheck %s -enable-var-scope
 ;
 ; https://github.com/CTSRD-CHERI/llvm/issues/274
@@ -15,9 +15,13 @@
 
 ; The capability clobber registers were previously off by one: -> $c3 would save $c4, etc.
 
+
 define i32 @test_clobber_c2(i32 signext %intval, i8 addrspace(200)* %ptrval, i8 addrspace(200)* %ptrval2) local_unnamed_addr #0 {
 entry:
   tail call void asm sideeffect "syscall\0A", "~{$4},~{$c2},~{$1}"() #1
+  ; MIR-LABEL: name: test_clobber_c2
+  ; MIR: INLINEASM &"syscall\0A", 1, 12, implicit-def early-clobber $a0, 12, implicit-def early-clobber $c2, 12, implicit-def early-clobber $at
+  ; MIR: PseudoReturnCap
   %0 = load volatile i8, i8 addrspace(200)* %ptrval, align 1
   %1 = load volatile i8, i8 addrspace(200)* %ptrval2, align 1
   %conv = sext i8 %0 to i32
@@ -44,6 +48,9 @@ entry:
 define i32 @test_clobber_c3(i32 signext %intval, i8 addrspace(200)* %ptrval, i8 addrspace(200)* %ptrval2) local_unnamed_addr #0 {
 entry:
   tail call void asm sideeffect "syscall\0A", "~{$4},~{$c3},~{$1}"() #1
+  ; MIR-LABEL: name: test_clobber_c3
+  ; MIR: INLINEASM &"syscall\0A", 1, 12, implicit-def early-clobber $a0, 12, implicit-def early-clobber $c3, 12, implicit-def early-clobber $at
+  ; MIR: PseudoReturnCap
   %0 = load volatile i8, i8 addrspace(200)* %ptrval, align 1
   %1 = load volatile i8, i8 addrspace(200)* %ptrval2, align 1
   %conv = sext i8 %0 to i32
@@ -71,6 +78,9 @@ entry:
 define i32 @test_clobber_c4(i32 signext %intval, i8 addrspace(200)* %ptrval, i8 addrspace(200)* %ptrval2) local_unnamed_addr #0 {
 entry:
   tail call void asm sideeffect "syscall\0A", "~{$4},~{$c4},~{$1}"() #1
+  ; MIR-LABEL: name: test_clobber_c4
+  ; MIR: INLINEASM &"syscall\0A", 1, 12, implicit-def early-clobber $a0, 12, implicit-def early-clobber $c4, 12, implicit-def early-clobber $at
+  ; MIR: PseudoReturnCap
   %0 = load volatile i8, i8 addrspace(200)* %ptrval, align 1
   %1 = load volatile i8, i8 addrspace(200)* %ptrval2, align 1
   %conv = sext i8 %0 to i32
@@ -98,6 +108,9 @@ entry:
 define i32 @test_clobber_c3_c4(i32 signext %intval, i8 addrspace(200)* %ptrval, i8 addrspace(200)* %ptrval2) local_unnamed_addr #0 {
 entry:
   tail call void asm sideeffect "syscall\0A", "~{$4},~{$c3},~{$c4},~{$1}"() #1
+  ; MIR-LABEL: name: test_clobber_c3_c4
+  ; MIR: INLINEASM &"syscall\0A", 1, 12, implicit-def early-clobber $a0, 12, implicit-def early-clobber $c3, 12, implicit-def early-clobber $c4, 12, implicit-def early-clobber $at
+  ; MIR: PseudoReturnCap
   %0 = load volatile i8, i8 addrspace(200)* %ptrval, align 1
   %1 = load volatile i8, i8 addrspace(200)* %ptrval2, align 1
   %conv = sext i8 %0 to i32
@@ -106,7 +119,7 @@ entry:
   %add2 = add nsw i32 %conv2, %add
   ret i32 %add2
   ;
-  ; This function should save $c3 and the int arg:
+  ; This function should save $c3, $c4 and the int arg:
   ;
   ; CHECK-LABEL: test_clobber_c3_c4:
   ; CHECK: # %bb.0: