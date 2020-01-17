760b55601d4144cc48cf9c625cd6dc763ee6f7f6
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-01-17 16:58:41 +0000

    Update test for changed codegen

--- clang/test/CodeGen/cheri/fnptr-uintptr-cast-issue-338.c
+++ clang/test/CodeGen/cheri/fnptr-uintptr-cast-issue-338.c
@@ -8,9 +8,9 @@ struct a {
 
 // CHECK-LABEL: @test1(
 // CHECK-NEXT:  entry:
-// CHECK-NEXT:    [[TMP0:%.*]] = tail call i8 addrspace(200)* @llvm.cheri.ddc.get()
+// CHECK-NEXT:    [[TMP0:%.*]] = call i8 addrspace(200)* @llvm.cheri.ddc.get()
 // CHECK-NEXT:    [[TMP1:%.*]] = bitcast void (...) addrspace(200)* [[ARG_COERCE:%.*]] to i8 addrspace(200)*
-// CHECK-NEXT:    [[TMP2:%.*]] = tail call i64 @llvm.cheri.cap.to.pointer.i64(i8 addrspace(200)* [[TMP0]], i8 addrspace(200)* [[TMP1]])
+// CHECK-NEXT:    [[TMP2:%.*]] = call i64 @llvm.cheri.cap.to.pointer.i64(i8 addrspace(200)* [[TMP0]], i8 addrspace(200)* [[TMP1]])
 // CHECK-NEXT:    ret i64 [[TMP2]]
 //
 long test1(struct a arg) {
@@ -21,7 +21,7 @@ long test1(struct a arg) {
 // CHECK-LABEL: @test2(
 // CHECK-NEXT:  entry:
 // CHECK-NEXT:    [[TMP0:%.*]] = bitcast void (...) addrspace(200)* [[ARG_COERCE:%.*]] to i8 addrspace(200)*
-// CHECK-NEXT:    [[TMP1:%.*]] = tail call i64 @llvm.cheri.cap.offset.get.i64(i8 addrspace(200)* [[TMP0]])
+// CHECK-NEXT:    [[TMP1:%.*]] = call i64 @llvm.cheri.cap.offset.get.i64(i8 addrspace(200)* [[TMP0]])
 // CHECK-NEXT:    ret i64 [[TMP1]]
 //
 long test2(struct a arg) {
@@ -31,7 +31,7 @@ long test2(struct a arg) {
 // CHECK-LABEL: @test3(
 // CHECK-NEXT:  entry:
 // CHECK-NEXT:    [[TMP0:%.*]] = bitcast void (...) addrspace(200)* [[ARG_COERCE:%.*]] to i8 addrspace(200)*
-// CHECK-NEXT:    [[TMP1:%.*]] = tail call i64 @llvm.cheri.cap.address.get.i64(i8 addrspace(200)* [[TMP0]])
+// CHECK-NEXT:    [[TMP1:%.*]] = call i64 @llvm.cheri.cap.address.get.i64(i8 addrspace(200)* [[TMP0]])
 // CHECK-NEXT:    ret i64 [[TMP1]]
 //
 long test3(struct a arg) {
@@ -41,7 +41,7 @@ long test3(struct a arg) {
 // CHECK-LABEL: @test4(
 // CHECK-NEXT:  entry:
 // CHECK-NEXT:    [[TMP0:%.*]] = bitcast void (...) addrspace(200)* [[ARG_COERCE:%.*]] to i8 addrspace(200)*
-// CHECK-NEXT:    [[TMP1:%.*]] = tail call i64 @llvm.cheri.cap.to.pointer.i64(i8 addrspace(200)* [[TMP0]], i8 addrspace(200)* [[TMP0]])
+// CHECK-NEXT:    [[TMP1:%.*]] = call i64 @llvm.cheri.cap.to.pointer.i64(i8 addrspace(200)* [[TMP0]], i8 addrspace(200)* [[TMP0]])
 // CHECK-NEXT:    [[TMP2:%.*]] = inttoptr i64 [[TMP1]] to i8*
 // CHECK-NEXT:    ret i8* [[TMP2]]
 //
