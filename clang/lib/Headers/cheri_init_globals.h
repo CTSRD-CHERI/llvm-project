d27bbe2eda41cf8872b5335913e7ff3fc78cec9c
James Clarke <jrtc27@jrtc27.com>
2020-01-29 14:59:22 +0000

    cheri_init_globals.h: Don't warn about missing sentries on RISC-V

--- clang/lib/Headers/cheri_init_globals.h
+++ clang/lib/Headers/cheri_init_globals.h
@@ -187,13 +187,15 @@ cheri_init_globals_impl(const struct capreloc *start_relocs,
     }
     src = __builtin_cheri_offset_increment(src, reloc->offset);
     /* XXX: Permit on RISC-V once supported */
-#if __has_builtin(__builtin_cheri_seal_entry) && !defined(__riscv)
+#ifndef __riscv
+#if __has_builtin(__builtin_cheri_seal_entry)
     if ((reloc->permissions & function_reloc_flag) == function_reloc_flag) {
       /* Convert function pointers to sentries: */
       src = __builtin_cheri_seal_entry(src);
     }
 #else
 #warning "Sentries not supported, global function pointers will provide unnecessary privilege"
+#endif
 #endif
     *dest = src;
   }
