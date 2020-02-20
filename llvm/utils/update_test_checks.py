ab8323efaa2c40f2588d89ce3938089f8d4a67ab
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-20 17:47:59 +0000

    Fix update_test_checks spam

--- llvm/utils/update_test_checks.py
+++ llvm/utils/update_test_checks.py
@@ -215,7 +215,8 @@ def main():
     common.debug('Writing %d lines to %s...' % (len(output_lines), test))
 
     with open(test, 'wb') as f:
-      sys.stderr.writelines(['{}\n'.format(l) for l in output_lines])
+      if args.verbose:
+        sys.stderr.writelines(['{}\n'.format(l) for l in output_lines])
       f.writelines(['{}\n'.format(l).encode('utf-8') for l in output_lines])
 
 
