c59249565f27d79127d74825bc44506ba8e76a3b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-04-03 02:00:47 +0100

    Use not --crash for a crash testcase
    TODO: report upstream

--- clang/test/CodeGenCXX/cheri/cheri-x86-crash-2.cpp
+++ clang/test/CodeGenCXX/cheri/cheri-x86-crash-2.cpp
@@ -1,5 +1,4 @@
 // XXXAR: This was created by creduce from another crash bug. Looks like an upstream bug
 // TODO: report upstream
-// XFAIL: *
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-obj -mrelax-all -disable-free -main-file-name driver.cpp -mrelocation-model static -mthread-model posix -mframe-pointer=all -fmath-errno -masm-verbose -mconstructor-aliases -munwind-tables -fuse-init-array -target-cpu x86-64 -dwarf-column-info -debug-info-kind=limited -dwarf-version=4 -debugger-tuning=gdb -coverage-notes-file /local/scratch/alr48/cheri/build/llvm-test-suite-native-build/Bitcode/Benchmarks/Halide/bilateral_grid/CMakeFiles/halide_bilateral_grid.dir/driver.cpp.gcno -sys-header-deps -D NDEBUG -Werror=date-time -w -std=c++11 -fdeprecated-macro -ferror-limit 19 -fmessage-length 0 -fobjc-runtime=gcc -fcxx-exceptions -fexceptions -fdiagnostics-show-option -cheri-linker -x c++ %s -o - | FileCheck %s
+// RUN: not --crash %clang_cc1 %s -o -
 template <_Alloc> template <a...> void b template <a _Alloc> b<_Tp_Alloc>
