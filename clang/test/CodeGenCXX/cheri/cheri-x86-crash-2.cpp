f335464e5cebc5f898eb0d8cdd493b2b3eaa98cb
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-04-10 16:35:00 +0100

    Fix a test in release builds

--- clang/test/CodeGenCXX/cheri/cheri-x86-crash-2.cpp
+++ clang/test/CodeGenCXX/cheri/cheri-x86-crash-2.cpp
@@ -1,4 +1,5 @@
 // XXXAR: This was created by creduce from another crash bug. Looks like an upstream bug
 // TODO: report upstream
+// REQUIRES: asserts
 // RUN: not --crash %clang_cc1 %s -o -
 template <_Alloc> template <a...> void b template <a _Alloc> b<_Tp_Alloc>
