6bfc7595ce8d789d0cf666f5e52b6c94e38a6bef
Jessica Clarke <jrtc27@jrtc27.com>
2020-05-02 19:50:21 +0100

    cheri-capability-qualifier-declspec.cpp: Check current behaviour

--- clang/test/SemaCXX/cheri/cheri-capability-qualifier-declspec.cpp
+++ clang/test/SemaCXX/cheri/cheri-capability-qualifier-declspec.cpp
@@ -1,17 +1,19 @@
 // RUN: %cheri_cc1 -o - %s -fsyntax-only -verify
-// expected-no-diagnostics
 
 void * __capability test_decltype(void *p) {
   decltype(p) __capability c = (__cheri_tocap decltype(p) __capability)p;
   return c;
 }
 
-// TODO: Clang can't currently represent this and so falls back on
-// "__capability only applies to pointers; type here is 'T'". Ideally we would
-// support this.
-//template<typename T>
-//T __capability test_template(T p) {
-//  T __capability c = (__cheri_tocap T __capability)p;
-//  return c;
-//}
-//template void * __capability test_template<void *>(void *);
+// XXX: Clang can't currently represent this, but ideally we would support it.
+// Document the current behaviour and ensure we don't crash.
+template<typename T>
+T __capability test_template(T p) {
+  // expected-error@-1 {{capability only applies to pointers; type here is 'T'}}
+  // expected-note@-2 {{candidate template ignored: could not match 'void *(void *)' against 'void * __capability (void *)'}}}
+  T __capability c = (__cheri_tocap T __capability)p;
+  // expected-error@-1 {{capability only applies to pointers; type here is 'T'}}
+  // expected-error@-2 {{capability only applies to pointers; type here is 'T'}}
+  return c;
+}
+template void * __capability test_template<void *>(void *); // expected-error {{explicit instantiation of 'test_template' does not refer to a function template, variable template, member function, member class, or static data member}}
