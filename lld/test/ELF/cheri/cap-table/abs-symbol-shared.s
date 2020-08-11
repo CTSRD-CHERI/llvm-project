d49a9e8aba0068f9804e80176095e451072dc920
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-08-11 10:00:31 +0100

    Fix a broken LLD test that had XFAIL: *

--- lld/test/ELF/cheri/cap-table/abs-symbol-shared.s
+++ lld/test/ELF/cheri/cap-table/abs-symbol-shared.s
@@ -1,25 +1,24 @@
-# XFAIL: *
-# TODO: need to look at x86 behaviour
-
-# RUN: %cheri_purecap_llvm-mc -filetype=obj -defsym=MAIN_FILE=1 %s -o %t.o
+# RUN: %cheri_purecap_llvm-mc -filetype=obj %s -o %t.o
 # RUN: ld.lld --fatal-warnings -pie %t.o -o %t.exe
-# RUN: llvm-readobj --dyn-relocations -t %t.exe
+# RUN: llvm-readobj --dyn-relocations -t %t.exe | FileCheck %s --check-prefix=INTERPOSABLE
 # RUN: ld.lld --fatal-warnings -shared %t.o -o %t.so
-# RUN: llvm-readobj --dyn-relocations -t %t.so
+# RUN: llvm-readobj --dyn-relocations -t %t.so | FileCheck %s --check-prefix=INTERPOSABLE
 
 # RUN: echo "weak_symbol = 64;" > %t.script
 # RUN: ld.lld --fatal-warnings -pie -o %t.exe %t.o -T %t.script
-# RUN: llvm-readobj --dyn-relocations -t %t.exe
+# RUN: llvm-readobj --dyn-relocations -t %t.exe | FileCheck %s --check-prefix=DEFINED-EXE
 # RUN: ld.lld --fatal-warnings -shared -o %t.so %t.o -T %t.script
-# RUN: llvm-readobj --dyn-relocations -t %t.so
+# RUN: llvm-readobj --dyn-relocations -t %t.so | FileCheck %s --check-prefix=INTERPOSABLE
 
-# what happens with x86?
-# RUN: llvm-mc -triple=x86_64-unknown-freebsd -filetype=obj -defsym=MAIN_FILE=1 %s -o %t.o
-# RUN: echo "weak_symbol = 64;" > %t.script
-# RUN: ld.lld --fatal-warnings -pie -o %t.exe %t.o -T %t.script
-# RUN: llvm-readobj --dyn-relocations -t %t.exe
-# RUN: ld.lld --fatal-warnings -shared -o %t.so %t.o -T %t.script
-# RUN: llvm-readobj --dyn-relocations -t %t.so
+
+## Check that there is a dynamic relocation against the weak symbol
+## even though it is defined here.
+# INTERPOSABLE-LABEL: Dynamic Relocations {
+# INTERPOSABLE-NEXT:    R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE weak_symbol 0x0
+# INTERPOSABLE-NEXT:  }
+## However, in an executable where it is defined there is no need for dynamic relocation:
+# DEFINED-EXE-LABEL: Dynamic Relocations {
+# DEFINED-EXE-NEXT:  }
 
 .text
 .weak weak_symbol
