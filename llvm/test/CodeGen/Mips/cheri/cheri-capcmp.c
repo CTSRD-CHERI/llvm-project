13985bbc0eb3f93ccd3803d4d95b579ba861c2ce
Jessica Clarke <jrtc27@jrtc27.com>
2020-08-19 21:37:29 +0100

    Convert cheri-capcmp.ll to use update_llc_test_checks
    Also removed unnecessary elements.

--- llvm/test/CodeGen/Mips/cheri/cheri-capcmp.c
+++ llvm/test/CodeGen/Mips/cheri/cheri-capcmp.c
@@ -1,7 +1,8 @@
 // REQUIRES: clang
 // Check that the default comparison mode is inexact equals (ceq and not cexeq)
-// RUN: %cheri_cc1 %S/cheri-capcmp.ll -S -o - | FileCheck %S/cheri-capcmp.ll --check-prefixes=CHECK-INEXACT,CHECK
-// RUN: %cheri_cc1 -cheri-comparison=exact %S/cheri-capcmp.ll -S -o - | \
-// RUN:   FileCheck %S/cheri-capcmp.ll --check-prefixes=CHECK-EXACT,CHECK
-// RUN: %cheri_cc1 -cheri-comparison=address %S/cheri-capcmp.ll -S -o - | \
-// RUN:   FileCheck %S/cheri-capcmp.ll --check-prefixes=CHECK-INEXACT,CHECK
+// RUN: %cheri_cc1 %S/cheri-capcmp.ll -S -o - -O | \
+// RUN:   FileCheck %S/cheri-capcmp.ll --check-prefixes=CHECK-INEXACT
+// RUN: %cheri_cc1 -cheri-comparison=exact %S/cheri-capcmp.ll -S -o - -O | \
+// RUN:   FileCheck %S/cheri-capcmp.ll --check-prefixes=CHECK-EXACT
+// RUN: %cheri_cc1 -cheri-comparison=address %S/cheri-capcmp.ll -S -o - -O | \
+// RUN:   FileCheck %S/cheri-capcmp.ll --check-prefixes=CHECK-INEXACT
