66c064c477ac8bbfd0cad77dcf8010a2d2f3d252
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-09-23 14:59:04 +0100

    Extract a separate IR->ASM test from global-register-variable.c
    I (incorrectly) assumed that we need a REQUIRES: riscv-registered-target
    due to the validation of register names. Turns out this logic is duplicated
    in clang and therefore does not need the RISC-V backend. Drop the REQUIRES
    and factor out a new IR->ASM test.

--- clang/test/CodeGen/cheri/riscv/global-register-variable.c
+++ clang/test/CodeGen/cheri/riscv/global-register-variable.c
@@ -1,25 +1,33 @@
-// REQUIRES: riscv-registered-target
+// Please also update llvm/test/CodeGen/RISCV/cheri/read_write_register.ll when changing this test
+// RUN_TO_UPDATE_IR_TO_ASM_TEST: %riscv64_cheri_cc1 -emit-llvm %s -O2 -o %S/../../../../../llvm/test/CodeGen/RISCV/cheri/read_write_register.ll.template
 // Some projects (e.g. Linux kernel) like to use global register variables, check
 // that we can handle the naive port from "tp" -> "ctp"
 // See e.g. https://elixir.bootlin.com/linux/v5.8.10/source/arch/riscv/include/asm/current.h
 // RUN: %riscv64_cheri_purecap_cc1 -fsyntax-only %s -o - -verify -DCHECK_BAD
+// RUN: %riscv64_cheri_cc1 -emit-llvm %s -o %s.ll -O2
 // RUN: %riscv64_cheri_purecap_cc1 -emit-llvm %s -o - | FileCheck %s
 // RUN: %riscv64_cheri_cc1 -emit-llvm %s -o -  | FileCheck %s
-// RUN: %riscv64_cheri_cc1 -S %s -o - -O1
-// RUN: %riscv64_cheri_purecap_cc1 -S %s -o - -O1 | FileCheck %s  --check-prefixes ASM,PURECAP-ASM
-// RUN: %riscv64_cheri_cc1 -S %s -o - -O1 | FileCheck %s --check-prefixes ASM,HYBRID-ASM
 
 register long int_tp __asm__("tp");
 // CHECK-LABEL: define {{[^@]+}}@get_tp()
 // CHECK-NEXT:  entry:
 // CHECK-NEXT:    [[TMP0:%.*]] = call i64 @llvm.read_register.i64(metadata !0)
 // CHECK-NEXT:    ret i64 [[TMP0]]
-// ASM-LABEL:        get_tp:
-// ASM:              mv a0, tp
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM-NEXT:  ret
 long get_tp(void) {
   return int_tp;
+}
+
+// CHECK-LABEL: define {{[^@]+}}@set_tp
+// CHECK-SAME: (i64 [[VALUE:%[a-z0-9]+]])
+// CHECK-NEXT:  entry:
+// CHECK-NEXT:    [[VALUE_ADDR:%.*]] = alloca i64, align 8
+// CHECK-NEXT:    store i64 [[VALUE]], i64{{( addrspace\(200\))?}}* [[VALUE_ADDR]], align 8
+// CHECK-NEXT:    [[TMP0:%.*]] = load i64, i64{{( addrspace\(200\))?}}* [[VALUE_ADDR]], align 8
+// CHECK-NEXT:    call void @llvm.write_register.i64(metadata !0, i64 [[TMP0]])
+// CHECK-NEXT:    ret void
+//
+void set_tp(long value) {
+  int_tp = value;
 }
 
 register __uintcap_t cap_tp __asm__("ctp");
@@ -27,10 +35,6 @@ register __uintcap_t cap_tp __asm__("ctp");
 // CHECK-NEXT:  entry:
 // CHECK-NEXT:    [[TMP0:%.*]] = call i8 addrspace(200)* @llvm.read_register.p200i8(metadata !1)
 // CHECK-NEXT:    ret i8 addrspace(200)* [[TMP0]]
-// ASM-LABEL:        get_ctp:
-// ASM:              cmove ca0, ctp
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM-NEXT:  ret
 __uintcap_t get_ctp(void) {
   return cap_tp;
 }
@@ -44,19 +48,6 @@ __uintcap_t get_ctp(void) {
 // CHECK-NEXT:    ret void
 //
 // FIXME: This looks wrong, it does not actually set ctp
-// ASM-LABEL:        set_ctp:
-// PURECAP-ASM:      cincoffset	csp, csp, -16
-// PURECAP-ASM-NEXT: csc	ctp, 0(csp)
-// PURECAP-ASM-NEXT: cmove	ctp, ca0
-// PURECAP-ASM-NEXT: clc	ctp, 0(csp)
-// PURECAP-ASM-NEXT: cincoffset	csp, csp, 16
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM:       addi sp, sp, -16
-// HYBRID-ASM-NEXT:  sd tp, 8(sp)
-// HYBRID-ASM-NEXT:  cmove ctp, ca0
-// HYBRID-ASM-NEXT:  ld tp, 8(sp)
-// HYBRID-ASM-NEXT:  addi sp, sp, 16
-// HYBRID-ASM-NEXT:  ret
 void set_ctp(__uintcap_t value) {
   cap_tp = value;
 }
@@ -69,10 +60,6 @@ register struct StackPtr *__capability cap_sp __asm__("csp");
 // CHECK-NEXT:    [[TMP0:%.*]] = call i8 addrspace(200)* @llvm.read_register.p200i8(metadata !2)
 // CHECK-NEXT:    [[TMP1:%.*]] = bitcast i8 addrspace(200)* [[TMP0]] to %struct.StackPtr addrspace(200)*
 // CHECK-NEXT:    ret %struct.StackPtr addrspace(200)* [[TMP1]]
-// ASM-LABEL:        get_csp:
-// ASM:              cmove ca0, csp
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM-NEXT:  ret
 struct StackPtr *__capability get_csp(void) {
   return cap_sp;
 }
@@ -85,11 +72,6 @@ struct StackPtr *__capability get_csp(void) {
 // CHECK-NEXT:    [[TMP1:%.*]] = bitcast [[STRUCT_STACKPTR]] addrspace(200)* [[TMP0]] to i8 addrspace(200)*
 // CHECK-NEXT:    call void @llvm.write_register.p200i8(metadata !2, i8 addrspace(200)* [[TMP1]])
 // CHECK-NEXT:    ret void
-//
-// ASM-LABEL:        set_csp:
-// ASM:              cmove csp, ca0
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM-NEXT:  ret
 void set_csp(struct StackPtr *__capability value) {
   cap_sp = value;
 }
@@ -101,10 +83,6 @@ register __uintcap_t cap_gp __asm__("cgp");
 // CHECK-NEXT:    [[TMP0:%.*]] = call i8 addrspace(200)* @llvm.read_register.p200i8(metadata !3)
 // CHECK-NEXT:    [[TMP1:%.*]] = call i64 @llvm.cheri.cap.address.get.i64(i8 addrspace(200)* [[TMP0]])
 // CHECK-NEXT:    ret i64 [[TMP1]]
-// ASM-LABEL:        get_gp_addr:
-// ASM:              cgetaddr a0, cgp
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM-NEXT:  ret
 long get_gp_addr(void) {
   return cap_gp;
 }
@@ -118,21 +96,6 @@ long get_gp_addr(void) {
 // CHECK-NEXT:    [[TMP1:%.*]] = getelementptr i8, i8 addrspace(200)* null, i64 [[TMP0]]
 // CHECK-NEXT:    call void @llvm.write_register.p200i8(metadata !3, i8 addrspace(200)* [[TMP1]])
 // CHECK-NEXT:    ret void
-//
-// FIXME: This looks wrong, it does not actually set cgp
-// ASM-LABEL:        set_gp_addr:
-// PURECAP-ASM:      cincoffset	csp, csp, -16
-// PURECAP-ASM-NEXT: csc	cgp, 0(csp)
-// PURECAP-ASM-NEXT: cincoffset cgp, cnull, a0
-// PURECAP-ASM-NEXT: clc	cgp, 0(csp)
-// PURECAP-ASM-NEXT: cincoffset	csp, csp, 16
-// PURECAP-ASM-NEXT: cret
-// HYBRID-ASM:       addi sp, sp, -16
-// HYBRID-ASM-NEXT:  sd gp, 8(sp)
-// HYBRID-ASM-NEXT:  cincoffset cgp, cnull, a0
-// HYBRID-ASM-NEXT:  ld gp, 8(sp)
-// HYBRID-ASM-NEXT:  addi sp, sp, 16
-// HYBRID-ASM-NEXT:  ret
 void set_gp_addr(long value) {
   cap_gp = value;
 }
