121f00ff5c7b4396d1799a867912bd83265f80eb
Jessica Clarke <jrtc27@jrtc27.com>
2020-10-01 20:22:38 +0100

    Better type-checking for %lp format string arguments
    Previously it required the argument to be a (native) pointer. Now we
    require it to be a capability, but also permit pointers to capabilities
    for hybrid ABIs for compatibility with CheriBSD's use (and since they
    get passed the same way in the hybrid ABIs). This compatibility will be
    removed in future once CheriBSD stops using that style, which will
    likely only be once the Morello toolchain has updated to support
    capability varargs and includes this commit.

--- clang/test/Sema/format-strings-cheri.c
+++ clang/test/Sema/format-strings-cheri.c
@@ -1,5 +1,5 @@
-// RUN: %riscv64_cheri_cc1 -fsyntax-only -verify %s
-// RUN: %riscv64_cheri_purecap_cc1 -fsyntax-only -verify %s
+// RUN: %riscv64_cheri_cc1 -fsyntax-only -verify=expected,hybrid %s
+// RUN: %riscv64_cheri_purecap_cc1 -fsyntax-only -verify=expected,purecap %s
 
 int printf(const char *restrict, ...);
 
@@ -10,18 +10,22 @@ void test_plain_pointer(void *p) {
 }
 
 void test_long_pointer(void * __capability p) {
-  printf("%lp", &p);
-  printf("%#lp", &p);
-  printf("%.4lp", &p); // expected-warning{{precision used with 'p' conversion specifier, resulting in undefined behavior}}
-  printf("%#.4lp", &p);
+  printf("%lp", p);
+  printf("%#lp", p);
+  printf("%.4lp", p); // expected-warning{{precision used with 'p' conversion specifier, resulting in undefined behavior}}
+  printf("%#.4lp", p);
+  printf("%lp", &p); // XXX: Currently accepted for CheriBSD compatibility
+  printf("%lp", (void *)0); // hybrid-warning{{format specifies type 'void * __capability' but the argument has type 'void *'}}
+  printf("%lp", (void **)(void *)0); // hybrid-warning{{format specifies type 'void * __capability' but the argument has type 'void **'}}
+  printf("%lp", (int *)(void *)0); // hybrid-warning{{format specifies type 'void * __capability' but the argument has type 'int *'}}
 }
 
 void test_invalid_length_modifiers(void *p) {
-  printf("%hhp", &p); // expected-warning{{length modifier 'hh' results in undefined behavior or no effect with 'p' conversion specifier}}
-  printf("%hp", &p); // expected-warning{{length modifier 'h' results in undefined behavior or no effect with 'p' conversion specifier}}
-  printf("%llp", &p); // expected-warning{{length modifier 'll' results in undefined behavior or no effect with 'p' conversion specifier}}
-  printf("%jp", &p); // expected-warning{{length modifier 'j' results in undefined behavior or no effect with 'p' conversion specifier}}
-  printf("%tp", &p); // expected-warning{{length modifier 't' results in undefined behavior or no effect with 'p' conversion specifier}}
-  printf("%zp", &p); // expected-warning{{length modifier 'z' results in undefined behavior or no effect with 'p' conversion specifier}}
-  printf("%qp", &p); // expected-warning{{length modifier 'q' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%hhp", p); // expected-warning{{length modifier 'hh' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%hp", p); // expected-warning{{length modifier 'h' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%llp", p); // expected-warning{{length modifier 'll' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%jp", p); // expected-warning{{length modifier 'j' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%tp", p); // expected-warning{{length modifier 't' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%zp", p); // expected-warning{{length modifier 'z' results in undefined behavior or no effect with 'p' conversion specifier}}
+  printf("%qp", p); // expected-warning{{length modifier 'q' results in undefined behavior or no effect with 'p' conversion specifier}}
 }
