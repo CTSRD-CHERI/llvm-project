905b3de78827c4ae28c91dd4a31565866b5c745e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-01-23 19:45:14 +0000

    Remove %cheri256 substitutions from llvm/utils

--- llvm/utils/update_test_checks.py
+++ llvm/utils/update_test_checks.py
@@ -83,10 +83,8 @@ def main():
       if tool_cmd.startswith("%"):
         tool_cmd = tool_cmd.replace("%cheri_purecap_opt", "opt -mtriple=mips64-unknown-freebsd -target-abi purecap -relocation-model pic -mcpu=cheri128 -mattr=+cheri128")
         tool_cmd = tool_cmd.replace("%cheri128_purecap_opt", "opt -mtriple=mips64-unknown-freebsd -target-abi purecap -relocation-model pic -mcpu=cheri128 -mattr=+cheri128")
-        tool_cmd = tool_cmd.replace("%cheri256_purecap_opt", "opt -mtriple=mips64-unknown-freebsd -target-abi purecap -relocation-model pic -mcpu=cheri256 -mattr=+cheri256")
         tool_cmd = tool_cmd.replace("%cheri_opt", "opt -mtriple=mips64-unknown-freebsd -mcpu=cheri128 -mattr=+cheri128")
         tool_cmd = tool_cmd.replace("%cheri128_opt", "opt -mtriple=mips64-unknown-freebsd -mcpu=cheri128 -mattr=+cheri128")
-        tool_cmd = tool_cmd.replace("%cheri256_opt", "opt -mtriple=mips64-unknown-freebsd -mcpu=cheri256 -mattr=+cheri256")
         tool_cmd = tool_cmd.replace("%riscv32_cheri_purecap_opt", "opt -mtriple=riscv32-unknown-freebsd -target-abi il32pc64 -mattr=+xcheri,+cap-mode")
         tool_cmd = tool_cmd.replace("%riscv64_cheri_purecap_opt", "opt -mtriple=riscv64-unknown-freebsd -target-abi l64pc128 -mattr=+xcheri,+cap-mode")
         tool_cmd = tool_cmd.replace("%riscv32_cheri_opt", "opt -mtriple=riscv32-unknown-freebsd -mattr=+xcheri")
