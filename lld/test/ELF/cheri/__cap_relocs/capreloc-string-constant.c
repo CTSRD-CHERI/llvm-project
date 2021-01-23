f5442b66e022424fa02686c49c4dde2d9bbf19bd
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-01-23 13:45:12 +0000

    [ELF] Update a caprelocs tests that was disabled
    It depended on having the new FileCheck syntax (since the CHERI @EXPR
    syntax was removed) but I forgot to update it after merging the FileCheck
    patches.

--- lld/test/ELF/cheri/__cap_relocs/capreloc-string-constant.c
+++ lld/test/ELF/cheri/__cap_relocs/capreloc-string-constant.c
@@ -1,7 +1,6 @@
 // REQUIRES: clang
-// REQUIRES: filecheck_new_syntax, D60389
 
-// RUN: %cheri256_purecap_cc1 %s -emit-obj -o %t.o
+// RUN: %cheri_purecap_cc1 %s -emit-obj -o %t.o
 // RUN: llvm-readobj -r %t.o | FileCheck --check-prefix READOBJ %s
 
 // RUN: ld.lld -preemptible-caprelocs=legacy --no-relative-cap-relocs %t.o -static -o %t-static.exe 2>&1 | FileCheck -check-prefixes UNKNOWN_LENGTH %s
@@ -16,10 +15,6 @@
 // RUN: ld.lld -preemptible-caprelocs=legacy --no-relative-cap-relocs %t.o -shared -o %t.so
 // RUN: llvm-readobj -r -s --cap-relocs %t.so | FileCheck --check-prefixes DYNAMIC-RELOCS %s
 
-
-// FIXME: it would be good if we could set bounds here instead of having it as -1
-
-
 struct option {
     int id;
     const char* name;
@@ -31,8 +26,6 @@ struct option options_table[] = {
         { 3, "duplicate" },
         { 4, "duplicate" },
         { 5, "duplicate" + 1 },
-
-
 };
 
 // READOBJ:      Relocations [
@@ -52,16 +45,16 @@ struct option options_table[] = {
 // dynamic should have 10 relocations against the load address
 // DYNAMIC-RELOCS-LABEL: Relocations [
 // DYNAMIC-RELOCS-NEXT:   Section ({{.+}}) .rel.dyn {
-// DYNAMIC-RELOCS-NEXT:     0x10000 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10008 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10028 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10030 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10050 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10058 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10078 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x10080 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x100A0 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
-// DYNAMIC-RELOCS-NEXT:     0x100A8 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+// DYNAMIC-RELOCS-NEXT:     R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
 // DYNAMIC-RELOCS-NEXT:   }
 // DYNAMIC-RELOCS-NEXT: ]
 
@@ -73,14 +66,14 @@ struct option options_table[] = {
 // STATIC-NEXT:   SHF_MERGE (0x10)
 // STATIC-NEXT:   SHF_STRINGS (0x20)
 // STATIC-NEXT: ]
-// STATIC-NEXT: Address: [[#RODATA:12345]]
+// STATIC-NEXT: Address: 0x[[#%x,RODATA:0x10208]]
 
 // STATIC-LABEL: CHERI __cap_relocs [
-// STATIC-NEXT:    0x120010020 Base: 0x[[#RODATA]]      (<unknown symbol>+0) Length: 6 Perms: Constant
-// STATIC-NEXT:    0x120010060 Base: 0x[[#RODATA + 16]] (<unknown symbol>+4) Length: 7 Perms: Constant
-// STATIC-NEXT:    0x1200100a0 Base: 0x[[#RODATA + 6]]  (<unknown symbol>+0) Length: 10 Perms: Constant
-// STATIC-NEXT:    0x1200100e0 Base: 0x[[#RODATA + 6]]  (<unknown symbol>+0) Length: 10 Perms: Constant
-// STATIC-NEXT:    0x120010120 Base: 0x[[#RODATA + 6]]  (<unknown symbol>+1) Length: 10 Perms: Constant
+// STATIC-NEXT:    Base: 0x[[#RODATA]]      (<unknown symbol>+0) Length: 6 Perms: Constant
+// STATIC-NEXT:    Base: 0x[[#RODATA + 16]] (<unknown symbol>+4) Length: 7 Perms: Constant
+// STATIC-NEXT:    Base: 0x[[#RODATA + 6]]  (<unknown symbol>+0) Length: 10 Perms: Constant
+// STATIC-NEXT:    Base: 0x[[#RODATA + 6]]  (<unknown symbol>+0) Length: 10 Perms: Constant
+// STATIC-NEXT:    Base: 0x[[#RODATA + 6]]  (<unknown symbol>+1) Length: 10 Perms: Constant
 // STATIC-NEXT: ]
 
 
@@ -93,15 +86,15 @@ struct option options_table[] = {
 // DYNAMIC-NEXT:   SHF_MERGE (0x10)
 // DYNAMIC-NEXT:   SHF_STRINGS (0x20)
 // DYNAMIC-NEXT: ]
-// DYNAMIC-NEXT: Address: [[$RODATA:0x4D0]]
+// DYNAMIC-NEXT: Address: 0x[[#%X,RODATA:0x4E0]]
 
 // PIE exe amd shlib should have a dynamic relocations and only have the offset+length values filled in:
 // DYNAMIC-LABEL: CHERI __cap_relocs [
-// DYNAMIC-NEXT:    0x020020 Base: 0x[[@EXPR tolower(hex($RODATA))]]      (<unknown symbol>+0) Length: 6 Perms: Constant
-// DYNAMIC-NEXT:    0x020060 Base: 0x[[@EXPR tolower(hex($RODATA + 16))]] (<unknown symbol>+4) Length: 7 Perms: Constant
-// DYNAMIC-NEXT:    0x0200a0 Base: 0x[[@EXPR tolower(hex($RODATA + 6))]]  (<unknown symbol>+0) Length: 10 Perms: Constant
-// DYNAMIC-NEXT:    0x0200e0 Base: 0x[[@EXPR tolower(hex($RODATA + 6))]]  (<unknown symbol>+0) Length: 10 Perms: Constant
-// DYNAMIC-NEXT:    0x020120 Base: 0x[[@EXPR tolower(hex($RODATA + 6))]]  (<unknown symbol>+1) Length: 10 Perms: Constant
+// DYNAMIC-NEXT:    Base: 0x[[#%x,RODATA]]      (<unknown symbol>+0) Length: 6 Perms: Constant
+// DYNAMIC-NEXT:    Base: 0x[[#%x,RODATA + 16]] (<unknown symbol>+4) Length: 7 Perms: Constant
+// DYNAMIC-NEXT:    Base: 0x[[#%x,RODATA + 6]]  (<unknown symbol>+0) Length: 10 Perms: Constant
+// DYNAMIC-NEXT:    Base: 0x[[#%x,RODATA + 6]]  (<unknown symbol>+0) Length: 10 Perms: Constant
+// DYNAMIC-NEXT:    Base: 0x[[#%x,RODATA + 6]]  (<unknown symbol>+1) Length: 10 Perms: Constant
 // DYNAMIC-NEXT: ]
 
 
