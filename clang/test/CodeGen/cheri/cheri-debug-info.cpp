5251a31f2ddde296d37ac42d15a5a708a83a878b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-01-23 12:04:25 +0000

    cheri-debug-info.cpp: Test 64+128 instead of 128+256
    CHERI256 is no longer support, so test RV32+RV64 instead.

--- clang/test/CodeGen/cheri/cheri-debug-info.cpp
+++ clang/test/CodeGen/cheri/cheri-debug-info.cpp
@@ -1,13 +1,16 @@
 // RUN: %cheri128_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited -o - %s \
-// RUN:   | FileCheck %s --check-prefix=CHECK-HYBRID --check-prefix=CHECK-128
-// RUN: %cheri256_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited -o - %s \
-// RUN:   | FileCheck %s --check-prefix=CHECK-HYBRID --check-prefix=CHECK-256
-// RUN: %cheri128_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited \
-// RUN:   -target-abi purecap -o - %s \
-// RUN:   | FileCheck %s --check-prefix=CHECK-PUREABI --check-prefix=CHECK-128
-// RUN: %cheri256_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited \
-// RUN:   -target-abi purecap -o - %s \
-// RUN:   | FileCheck %s --check-prefix=CHECK-PUREABI --check-prefix=CHECK-256
+// RUN:   | FileCheck %s --check-prefixes=CHECK-HYBRID,CHECK-128 '-D#POINTER_SIZE=64'
+// RUN: %riscv64_cheri_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited -o - %s \
+// RUN:   | FileCheck %s --check-prefixes=CHECK-HYBRID,CHECK-128 '-D#POINTER_SIZE=64'
+// RUN: %riscv32_cheri_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited -o - %s \
+// RUN:   | FileCheck %s --check-prefixes=CHECK-HYBRID,CHECK-64 '-D#POINTER_SIZE=32'
+
+// RUN: %cheri128_purecap_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited \
+// RUN:   -o - %s | FileCheck %s --check-prefixes=CHECK-PUREABI,CHECK-128
+// RUN: %riscv64_cheri_purecap_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited \
+// RUN:   -o - %s | FileCheck %s --check-prefixes=CHECK-PUREABI,CHECK-128
+// RUN: %riscv32_cheri_purecap_cc1 --std=c++11 -emit-llvm -debug-info-kind=limited \
+// RUN:   -o - %s | FileCheck %s --check-prefixes=CHECK-PUREABI,CHECK-64
 
 // Check that CHERI capability pointer/references have the right size in debug
 // information.
@@ -19,15 +22,15 @@ char *__capability always_capability_pointer;
 char &__capability always_capability_reference = *always_capability_pointer;
 char &&__capability always_capability_rvalue_reference = 0;
 
-// CHECK-HYBRID-DAG: ![[DWARF_SIMPLE_POINTER:[0-9]+]] = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: !{{[0-9]+}}, size: 64)
-// CHECK-HYBRID-DAG: ![[DWARF_SIMPLE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_reference_type, baseType: !{{[0-9]+}}, size: 64)
-// CHECK-HYBRID-DAG: ![[DWARF_SIMPLE_RVALUE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_rvalue_reference_type, baseType: !{{[0-9]+}}, size: 64)
+// CHECK-HYBRID-DAG: ![[DWARF_SIMPLE_POINTER:[0-9]+]] = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: !{{[0-9]+}}, size: [[#POINTER_SIZE]])
+// CHECK-HYBRID-DAG: ![[DWARF_SIMPLE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_reference_type, baseType: !{{[0-9]+}}, size: [[#POINTER_SIZE]])
+// CHECK-HYBRID-DAG: ![[DWARF_SIMPLE_RVALUE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_rvalue_reference_type, baseType: !{{[0-9]+}}, size: [[#POINTER_SIZE]])
 // CHECK-128-DAG: ![[DWARF_CAPABILITY_POINTER:[0-9]+]] = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: !{{[0-9]+}}, size: 128)
 // CHECK-128-DAG: ![[DWARF_CAPABILITY_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_reference_type, baseType: !{{[0-9]+}}, size: 128)
 // CHECK-128-DAG: ![[DWARF_CAPABILITY_RVALUE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_rvalue_reference_type, baseType: !{{[0-9]+}}, size: 128)
-// CHECK-256-DAG: ![[DWARF_CAPABILITY_POINTER:[0-9]+]] = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: !{{[0-9]+}}, size: 256)
-// CHECK-256-DAG: ![[DWARF_CAPABILITY_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_reference_type, baseType: !{{[0-9]+}}, size: 256)
-// CHECK-256-DAG: ![[DWARF_CAPABILITY_RVALUE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_rvalue_reference_type, baseType: !{{[0-9]+}}, size: 256)
+// CHECK-64-DAG: ![[DWARF_CAPABILITY_POINTER:[0-9]+]] = !DIDerivedType(tag: DW_TAG_pointer_type, baseType: !{{[0-9]+}}, size: 64)
+// CHECK-64-DAG: ![[DWARF_CAPABILITY_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_reference_type, baseType: !{{[0-9]+}}, size: 64)
+// CHECK-64-DAG: ![[DWARF_CAPABILITY_RVALUE_REFERENCE:[0-9]+]] = !DIDerivedType(tag: DW_TAG_rvalue_reference_type, baseType: !{{[0-9]+}}, size: 64)
 
 // CHECK-HYBRID-DAG: ![[POINTER_VAR:[0-9]+]] = distinct !DIGlobalVariable(name: "pointer", scope: !{{[0-9]+}}, file: !{{[0-9]+}}, line: {{[0-9]+}}, type: ![[DWARF_SIMPLE_POINTER]], isLocal: false, isDefinition: true)
 // CHECK-HYBRID-DAG: ![[REFERENCE_VAR:[0-9]+]] = distinct !DIGlobalVariable(name: "reference", scope: !{{[0-9]+}}, file: !{{[0-9]+}}, line: {{[0-9]+}}, type: ![[DWARF_SIMPLE_REFERENCE]], isLocal: false, isDefinition: true)
