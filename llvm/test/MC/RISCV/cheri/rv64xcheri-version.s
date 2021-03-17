4a0879316c98fe9c171a3866f5f0dbc250be9912
Robert Norton <robert.norton@microsoft.com>
2021-03-17 17:16:11 +0000

    Add brackets around memory operand for cloadversion(s), cstoreversion and camocdecversion. This slightly uses GPCRMemAtomic which exists only to support memory operands without an immediate and could be renamed.

--- llvm/test/MC/RISCV/cheri/rv64xcheri-version.s
+++ llvm/test/MC/RISCV/cheri/rv64xcheri-version.s
@@ -11,21 +11,21 @@ cgetversion x3, c2
 # CHECK: encoding: [0xdb,0x00,0x31,0x04]
 # CHECK-DISABLED: error: instruction requires the following: CHERI Extension with memory versioning
 csetversion c1, c2, x3
-# CHECK: cloadversion    sp, cra
+# CHECK: cloadversion    sp, (cra)
 # CHECK: encoding: [0x5b,0x81,0x40,0xff]
 # CHECK-DISABLED: error: instruction requires the following: CHERI Extension with memory versioning
-cloadversion x2, c1
-# CHECK: cloadversions   sp, cra
+cloadversion x2, (c1)
+# CHECK: cloadversions   sp, (cra)
 # CHECK: encoding: [0x5b,0x81,0x50,0xff]
 # CHECK-DISABLED: error: instruction requires the following: CHERI Extension with memory versioning
-cloadversions x2, c1
-# CHECK: cstoreversion   ra, ctp
+cloadversions x2, (c1)
+# CHECK: cstoreversion   ra, (ctp)
 # CHECK: encoding: [0x5b,0x01,0x12,0xfc]
 # CHECK-DISABLED: error: instruction requires the following: CHERI Extension with memory versioning
-cstoreversion x1, c4
-# CHECK: camocdecversion ra, csp, cgp
+cstoreversion x1, (c4)
+# CHECK: camocdecversion ra, (csp), cgp
 # CHECK: encoding: [0xdb,0x00,0x31,0x06]
 # CHECK-DISABLED: error: instruction requires the following: CHERI Extension with memory versioning
-camocdecversion x1, c2, c3
+camocdecversion x1, (c2), c3
 
 
