5b2c89f6614d325f130d3bc9a5cafd5f290e9c16
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-03-22 10:04:33 +0000

    Fix tests that assume that llvm-readobj defaults to stdin
    Commit df952cb914eae5264603bd9fadffcc7626355c51 changed this case to emit
    an error instead.

--- lld/test/ELF/output-to-stdout.s
+++ lld/test/ELF/output-to-stdout.s
@@ -1,6 +1,6 @@
 # Make sure that using - as an output file uses stdout
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
-# RUN: ld.lld %t.o -o - | llvm-readobj -h | FileCheck %s
+# RUN: ld.lld %t.o -o - | llvm-readobj -h - | FileCheck %s
 # REQUIRES: x86
 
 .globl _start
--- llvm/test/MC/Mips/cheri/elf_eflags_cheri.s
+++ llvm/test/MC/Mips/cheri/elf_eflags_cheri.s
@@ -1,12 +1,12 @@
-# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri256 -filetype=obj %s -o -| llvm-readobj -h | FileCheck --check-prefix=CHERI256-NO-ABI %s
+# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri256 -filetype=obj %s -o - | llvm-readobj -h - | FileCheck --check-prefix=CHERI256-NO-ABI %s
 # CHERI256-NO-ABI: Flags [ (0x30C20004)
-# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri128 -filetype=obj %s -o -| llvm-readobj -h | FileCheck --check-prefix=CHERI128-NO-ABI %s
+# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri128 -filetype=obj %s -o - | llvm-readobj -h - | FileCheck --check-prefix=CHERI128-NO-ABI %s
 # CHERI128-NO-ABI: Flags [ (0x30C10004)
-# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri256 -filetype=obj -target-abi n64 %s -o -| llvm-readobj -h | FileCheck --check-prefix=CHERI256-N64 %s
+# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri256 -filetype=obj -target-abi n64 %s -o - | llvm-readobj -h - | FileCheck --check-prefix=CHERI256-N64 %s
 # CHERI256-N64: Flags [ (0x30C20004)
-# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri128 -filetype=obj -target-abi n64 %s -o -| llvm-readobj -h | FileCheck --check-prefix=CHERI128-N64 %s
+# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri128 -filetype=obj -target-abi n64 %s -o - | llvm-readobj -h - | FileCheck --check-prefix=CHERI128-N64 %s
 # CHERI128-N64: Flags [ (0x30C10004)
-# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri256 -position-independent -filetype=obj -target-abi purecap %s -o -| llvm-readobj -h | FileCheck --check-prefix=CHERI256-PURECAP %s
+# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri256 -position-independent -filetype=obj -target-abi purecap %s -o - | llvm-readobj -h - | FileCheck --check-prefix=CHERI256-PURECAP %s
 # CHERI256-PURECAP: Flags [ (0x30C2C006)
-# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri128 -position-independent -filetype=obj -target-abi purecap %s -o -| llvm-readobj -h | FileCheck --check-prefix=CHERI128-PURECAP %s
+# RUN: llvm-mc -triple=mips64-unknown-freebsd -mcpu=cheri128 -position-independent -filetype=obj -target-abi purecap %s -o - | llvm-readobj -h - | FileCheck --check-prefix=CHERI128-PURECAP %s
 # CHERI128-PURECAP: Flags [ (0x30C1C006)
