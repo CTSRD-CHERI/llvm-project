96bf5114e004b340da00b65215319b9859c20927
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-03-30 11:46:05 +0100

    Restore local test diff in aarch64-tme.cpp test
    I accidentally dropped this while merging 291ac7e622d542f8b25f74bc28051762edc90938

--- clang/test/CodeGen/aarch64-tme.cpp
+++ clang/test/CodeGen/aarch64-tme.cpp
@@ -29,7 +29,7 @@ void test_tme_funcs() {
 // CHECK: call void @llvm.aarch64.tcancel(i64 65535)
 
 // CHECK: declare i64 @llvm.aarch64.tstart() #1
-// CHECK: declare i64 @llvm.aarch64.ttest() #1
+// CHECK: declare i64 @llvm.aarch64.ttest() #2
 // CHECK: declare void @llvm.aarch64.tcommit() #1
 // CHECK: declare void @llvm.aarch64.tcancel(i64 immarg) #1
 
@@ -39,4 +39,4 @@ extern "C" void arm_feature_tme_defined() {}
 // CHECK: define{{.*}} void @arm_feature_tme_defined()
 
 // CHECK: attributes #1 = { nounwind willreturn }
-
+// CHECK: attributes #2 = { hassideeffects nounwind willreturn }
