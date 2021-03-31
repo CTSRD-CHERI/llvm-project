95e91ca67f69b443b1077929244e7e5ee74ce203
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-03-31 12:10:53 +0100

    Update test for changed -fdump-record-layouts output order

--- clang/test/CodeGenCXX/cheri/cheri-reference-member-size.cpp
+++ clang/test/CodeGenCXX/cheri/cheri-reference-member-size.cpp
@@ -1,26 +1,20 @@
 // RUN: %cheri_purecap_cc1 -emit-llvm \
-// RUN:  -fdump-record-layouts -verify %s -o /dev/null
-// RUN: %cheri_purecap_cc1 -emit-llvm \
-// RUN:  -fdump-record-layouts %s -o /dev/null 2>&1 | %cheri_FileCheck %s
+// RUN:  -fdump-record-layouts -verify %s -o /dev/null 2>&1 | %cheri_FileCheck %s
 // expected-no-diagnostics
 
-// CHECK:     *** Dumping AST Record Layout
-// CHECK-NEXT:  0 | class A (empty)
-// CHECK-NEXT:    | [sizeof=1, dsize=1, align=1,
-// CHECK-NEXT:    |  nvsize=1, nvalign=1]
-
-// CHECK:   LLVMType:%class.A = type { i8 }
-
-// CHECK:     *** Dumping AST Record Layout
+// CHECK-LABEL: *** Dumping AST Record Layout
 // CHECK-NEXT:  0 | class Foo
 // CHECK-NEXT:  0 |   class A & __capability _a
 // CHECK-NEXT:    | [sizeof=[[#CAP_SIZE]],
 // CHECK-SAME:       dsize=[[#CAP_SIZE]], align=[[#CAP_SIZE]],
 // CHECK-NEXT:    |  nvsize=[[#CAP_SIZE]], nvalign=[[#CAP_SIZE]]]
 
-//CHECK: LLVMType:%class.Foo = type { %class.A addrspace(200)* }
+// CHECK-LABEL: *** Dumping AST Record Layout
+// CHECK-NEXT:  0 | class A (empty)
+// CHECK-NEXT:    | [sizeof=1, dsize=1, align=1,
+// CHECK-NEXT:    |  nvsize=1, nvalign=1]
 
-// CHECK:      *** Dumping AST Record Layout
+// CHECK-LABEL:    *** Dumping AST Record Layout
 // CHECK-NEXT:     0 | class Bar
 // CHECK-NEXT:     0 |   class Foo (base)
 // CHECK-NEXT:     0 |     class A & __capability _a
@@ -28,20 +22,23 @@
 // CHECK-NEXT:       | [sizeof=[[#CAP_SIZE * 2]], dsize=[[#CAP_SIZE + 4]], align=[[#CAP_SIZE]],
 // CHECK-NEXT:       |  nvsize=[[#CAP_SIZE + 4]], nvalign=[[#CAP_SIZE]]]
 
-// CHECK:   LLVMType:%class.Bar = type <{ %class.Foo, i32, {{\[}}[[#CAP_SIZE - 4]] x i8] }>
+// CHECK-LABEL: *** Dumping IRgen Record Layout
+// CHECK: LLVMType:%class.A = type { i8 }
+// CHECK: LLVMType:%class.Foo = type { %class.A addrspace(200)* }
+// CHECK: LLVMType:%class.Bar = type <{ %class.Foo, i32, {{\[}}[[#CAP_SIZE - 4]] x i8] }>
 
-class A { };
+class A {};
 
 class Foo {
-  public:
-    A& _a;
-    Foo(A& a) : _a(a) { }
+public:
+  A &_a;
+  Foo(A &a) : _a(a) {}
 };
 
 class Bar : public Foo {
-  public:
-    int x;
-    Bar(A& a, int x) : Foo(a), x(x) { }
+public:
+  int x;
+  Bar(A &a, int x) : Foo(a), x(x) {}
 };
 
 void f() {
