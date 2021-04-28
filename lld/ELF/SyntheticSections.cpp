ac55a30370064b82c7f22c90d29fb9579c9f227d
Jessica Clarke <jrtc27@jrtc27.com>
2021-04-28 15:00:58 +0100

    [ELF] Fix DynamicReloc::getSymIndex after 908088aff20d
    This is a local assertion, so not needed upstream. Should fix linking
    libclang_rt.asan-mips64.so in CheriBSD.

--- lld/ELF/SyntheticSections.cpp
+++ lld/ELF/SyntheticSections.cpp
@@ -1651,8 +1651,10 @@ int64_t DynamicReloc::computeAddend() const {
 
 uint32_t DynamicReloc::getSymIndex(SymbolTableBaseSection *symTab) const {
   if (sym && !useSymVA) {
-    // It is fine to have a dynsymindex of 0 for the TLS module relocations
-    if (sym->dynsymIndex == 0 && type != target->tlsModuleIndexRel) {
+    // It is fine to have a dynsymindex of 0 for the TLS module and TP offset
+    // relocations as both are emitted for non-preemptible symbols in DSOs.
+    if (sym->dynsymIndex == 0 && type != target->tlsModuleIndexRel &&
+        type != target->tlsGotRel) {
       warn("DynsymIndex == 0 for " + toString(type) + " relocation against " +
            verboseToString(sym) + "+" + Twine(addend));
     }
