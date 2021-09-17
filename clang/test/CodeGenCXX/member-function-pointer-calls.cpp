e163ca66c0889755c44e1daede5f0a89edd2f2e2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-09-17 11:12:07 +0100

    [CHERI] Add some baseline tests for missed devirtualization
    We currently fail to inline member-pointer calls even if all the required
    information is present. This is most likely caused by either InstCombine
    or Transforms/Utils/CloneFunction.cpp since we fail to properly simplify
    functions while inlining them.

--- clang/test/CodeGenCXX/member-function-pointer-calls.cpp
+++ clang/test/CodeGenCXX/member-function-pointer-calls.cpp
@@ -5,6 +5,11 @@
 /// We should be able to optimize calls via the indirectly passed member pointers
 // RUN: %clang_cc1 %s -triple=x86_64-windows-gnu -emit-llvm -O3 -fno-experimental-new-pass-manager  -o - | FileCheck %s
 // RUN: %clang_cc1 %s -triple=x86_64-windows-gnu -emit-llvm -O3 -fexperimental-new-pass-manager  -o - | FileCheck %s
+// For purecap, we currently inline f() but fail to resolve the vtable entry to a direct call.
+// RUN: %riscv64_cheri_cc1 %s -emit-llvm -O3 -fno-experimental-new-pass-manager  -o - | FileCheck %s
+// RUN: %riscv64_cheri_cc1 %s -emit-llvm -O3 -fexperimental-new-pass-manager  -o - | FileCheck %s
+// RUN: %riscv64_cheri_purecap_cc1 %s -emit-llvm -O3 -fno-experimental-new-pass-manager -o - | FileCheck %s --check-prefix=PURECAP
+// RUN: %riscv64_cheri_purecap_cc1 %s -emit-llvm -O3 -fexperimental-new-pass-manager -o - | FileCheck %s --check-prefix=PURECAP
 struct A {
   virtual int vf1() { return 1; }
   virtual int vf2() { return 2; }
@@ -19,6 +24,11 @@ int f(A* a, int (A::*fp)()) {
 // CHECK: ret i32 1
 // MINGW64-LABEL: define dso_local i32 @_Z2g1v()
 // MINGW64: call i32 @_Z1fP1AMS_FivE(%struct.A* %{{.*}}, { i64, i64 }* %{{.*}})
+// PURECAP-LABEL: define{{.*}} i32 @_Z2g1v()
+// PURECAP-NOT:    ret i32
+// PURECAP:        [[RET:%.*]] = call signext i32 %memptr.virtualfn.i(
+// PURECAP:        ret i32 [[RET]]
+// PURECAP-NEXT: }
 int g1() {
   A a;
   return f(&a, &A::vf1);
@@ -29,6 +39,10 @@ int g1() {
 // CHECK: ret i32 2
 // MINGW64-LABEL: define dso_local i32 @_Z2g2v()
 // MINGW64: call i32 @_Z1fP1AMS_FivE(%struct.A* %{{.*}}, { i64, i64 }* %{{.*}})
+// PURECAP-LABEL: define{{.*}} i32 @_Z2g2v()
+// PURECAP-NEXT: _Z1fP1AMS_FivE.exit:
+// PURECAP-NEXT:   ret i32 2
+// PURECAP-NEXT: }
 int g2() {
   A a;
   return f(&a, &A::vf2);
