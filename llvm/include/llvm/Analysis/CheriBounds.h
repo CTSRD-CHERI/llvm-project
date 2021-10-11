93b488fa92d3903605c2c871ea4e0f5256602731
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-10-11 17:09:09 +0100

    [CheriNeedBoundsChecker][NFC] Avoid -Wshadow warnings

--- llvm/include/llvm/Analysis/CheriBounds.h
+++ llvm/include/llvm/Analysis/CheriBounds.h
@@ -6,7 +6,8 @@ namespace llvm {
 
 class CheriNeedBoundsChecker {
 public:
-  CheriNeedBoundsChecker(AllocaInst *AI, const DataLayout &DL) : I(AI), DL(DL) {
+  CheriNeedBoundsChecker(AllocaInst *AI, const DataLayout &DL)
+      : RootInst(AI), DL(DL) {
     auto AllocaSize = AI->getAllocationSizeInBits(DL);
     if (AllocaSize)
       MinSizeInBytes = *AllocaSize / 8;
@@ -14,7 +15,7 @@ public:
   }
   CheriNeedBoundsChecker(Instruction *I, Optional<uint64_t> MinSize,
                          const DataLayout &DL)
-      : I(I), DL(DL), MinSizeInBytes(MinSize) {
+      : RootInst(I), DL(DL), MinSizeInBytes(MinSize) {
     assert(I->getType()->isPointerTy());
     PointerAS = I->getType()->getPointerAddressSpace();
   }
@@ -33,7 +34,7 @@ private:
                                  const APInt &CurrentGEPOffset,
                                  unsigned Depth) const;
 
-  const Instruction *I;
+  const Instruction *RootInst;
   const DataLayout &DL;
   Optional<uint64_t> MinSizeInBytes;
   unsigned PointerAS = 0;
