62ede006ee18dcfc1e1f0eafede0abf6f1dc0fc8
Jessica Clarke <jrtc27@jrtc27.com>
2021-11-23 15:42:09 +0000

    libunwind: Remove bogus downstream assert
    This was added in 2017 in 4ae1b6a2998f without justification. There is
    already an assertion upstream that UnwindCursor does not exceed
    unw_cursor_t's size and, more recently, alignment, and the stronger
    assertion that they're always equal does not hold on i386, where
    unw_cursor_t is bigger than UnwindCursor. Not to mention that the assert
    should have been written as an inline static_assert like the others, not
    with this weird helper function.

--- libunwind/src/UnwindCursor.hpp
+++ libunwind/src/UnwindCursor.hpp
@@ -1222,13 +1222,10 @@ private:
 #endif
 };
 
-template <unsigned A, unsigned B> void size() { static_assert(A == B, "fail"); }
-
 template <typename A, typename R>
 UnwindCursor<A, R>::UnwindCursor(unw_context_t *context, A &as)
     : _addressSpace(as), _registers(context), _unwindInfoMissing(false),
       _isSignalFrame(false) {
-  size<sizeof(UnwindCursor<A, R>), sizeof(unw_cursor_t)>();
   static_assert((check_fit<UnwindCursor<A, R>, unw_cursor_t>::does_fit),
                 "UnwindCursor<> does not fit in unw_cursor_t");
   static_assert((alignof(UnwindCursor<A, R>) <= alignof(unw_cursor_t)),
