e6f3c46eb833aa507254f4d6b490a9236843173e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-11-17 14:53:49 +0000

    [ELF][CHERI] Remove a non-reduced test case
    A binary yaml2obj test case is not particularly helpful, especially
    considering that I actually checked in a reduced assembly test case at
    the same time.

--- lld/test/ELF/cheri/mips-libcxxrt-crash-cfi.s
+++ lld/test/ELF/cheri/mips-libcxxrt-crash-cfi.s
@@ -1,5 +1,5 @@
+## This used to crash ld.lld for CHERI MIPS
 # RUN: llvm-mc -filetype=obj -triple=mips64-unknown-freebsd %s -o %t.o
-# RUN: llvm-readobj -r %t.o
 # RUN: ld.lld --eh-frame-hdr --shared -z notext -o %t.so %t.o
 
 .cfi_startproc
