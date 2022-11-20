113fbb6aa5f3cde6531d7a0f004be9294337ea9a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-11-20 12:57:07 +0000

    Regerenate a test
    It was already auto-generated but apparently with a very old version of
    the update script.

--- clang/test/CodeGen/cheri/cheri-vla.c
+++ clang/test/CodeGen/cheri/cheri-vla.c
@@ -3,24 +3,29 @@
 
 extern void test(const char*);
 
+// CHECK-LABEL: define {{[^@]+}}@foo
+// CHECK-SAME: () local_unnamed_addr addrspace(200) #[[ATTR0:[0-9]+]] {
+// CHECK-NEXT:  entry:
+// CHECK-NEXT:    br label [[FOR_BODY:%.*]]
+// CHECK:       for.cond.cleanup:
+// CHECK-NEXT:    ret void
+// CHECK:       for.body:
+// CHECK-NEXT:    [[I_05:%.*]] = phi i64 [ 1, [[ENTRY:%.*]] ], [ [[INC:%.*]], [[FOR_BODY]] ]
+// CHECK-NEXT:    [[TMP0:%.*]] = call i8 addrspace(200)* @llvm.stacksave.p200i8()
+// CHECK-NEXT:    [[VLA:%.*]] = alloca i8, i64 [[I_05]], align 1, addrspace(200)
+// CHECK-NEXT:    [[SUB:%.*]] = add nsw i64 [[I_05]], -1
+// CHECK-NEXT:    [[ARRAYIDX:%.*]] = getelementptr inbounds i8, i8 addrspace(200)* [[VLA]], i64 [[SUB]]
+// CHECK-NEXT:    store i8 0, i8 addrspace(200)* [[ARRAYIDX]], align 1, !tbaa [[TBAA2:![0-9]+]]
+// CHECK-NEXT:    call void @test(i8 addrspace(200)* noundef nonnull [[VLA]]) #[[ATTR3:[0-9]+]]
+// CHECK-NEXT:    call void @llvm.stackrestore.p200i8(i8 addrspace(200)* [[TMP0]])
+// CHECK-NEXT:    [[INC]] = add nuw nsw i64 [[I_05]], 1
+// CHECK-NEXT:    [[EXITCOND_NOT:%.*]] = icmp eq i64 [[INC]], 32
+// CHECK-NEXT:    br i1 [[EXITCOND_NOT]], label [[FOR_COND_CLEANUP:%.*]], label [[FOR_BODY]], !llvm.loop [[LOOP5:![0-9]+]]
+//
 void foo(void) {
   for (long i = 1; i < 32; i++) {
     char vla[i];
     vla[i - 1] = '\0';
     test(vla);
   }
-  // CHECK-LABEL: define dso_local void @foo()
-  // CHECK: br label %[[FOR_BODY:.+]]
-  // CHECK: [[FOR_BODY]]:
-  // CHECK: [[I_05:%.+]] = phi i64 [ 1, %[[ENTRY:.+]] ], [ [[INC:%.+]], %[[FOR_BODY]] ]
-  // CHECK: [[VAR0:%.+]] = call i8 addrspace(200)* @llvm.stacksave.p200i8()
-  // CHECK: [[VLA:%.+]] = alloca i8, i64 [[I_05]], align 1
-  // CHECK: [[SUB:%.+]] = add nsw i64 [[I_05]], -1
-  // CHECK: [[ARRAYIDX:%.+]] = getelementptr inbounds i8, i8 addrspace(200)* [[VLA]], i64 [[SUB]]
-  // CHECK: store i8 0, i8 addrspace(200)* [[ARRAYIDX]], align 1, !tbaa !{{[0-9]+}}
-  // CHECK: call void @test(i8 addrspace(200)* noundef nonnull [[VLA]])
-  // CHECK: call void @llvm.stackrestore.p200i8(i8 addrspace(200)* [[VAR0]])
-  // CHECK: [[INC]] = add nuw nsw i64 [[I_05]], 1
-  // CHECK: [[EXITCOND:%.+]] = icmp eq i64 [[INC]], 32
-  // CHECK: br i1 [[EXITCOND]], label %[[FOR_COND_CLEANUP:.+]], label %[[FOR_BODY]]
 }
