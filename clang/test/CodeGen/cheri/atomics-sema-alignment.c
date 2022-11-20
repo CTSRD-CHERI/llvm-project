814a29ccd858e6e86682e41d2e33d4ed816756c2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-11-20 12:21:46 +0000

    Add noundef to atomics-sema-alignment.c

--- clang/test/CodeGen/cheri/atomics-sema-alignment.c
+++ clang/test/CodeGen/cheri/atomics-sema-alignment.c
@@ -30,15 +30,15 @@ void func1(void **p) {
   // PURECAP: atomicrmw xchg i8 addrspace(200)* addrspace(200)* %{{.+}}, i8 addrspace(200)* %{{.+}} seq_cst
   // PURECAP: cmpxchg i8 addrspace(200)* addrspace(200)* {{%.+}}, i8 addrspace(200)* {{%.+}}, i8 addrspace(200)* {{%.+}} seq_cst seq_cst
 
-  // HYBRID-LIBCALLS: call i64 @__atomic_load_8(i8* %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call void @__atomic_store_8(i8* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call i64 @__atomic_exchange_8(i8* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_8(i8* %{{.+}}, i8* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i64 @__atomic_load_8(i8* noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call void @__atomic_store_8(i8* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i64 @__atomic_exchange_8(i8* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_8(i8* noundef %{{.+}}, i8* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
 
   // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call void @__atomic_store_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call void @__atomic_store_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
 }
 
 void func2(int *__capability *p) {
@@ -54,16 +54,16 @@ void func2(int *__capability *p) {
   // PURECAP: atomicrmw add i32 addrspace(200)* addrspace(200)* %{{.+}}, i32 addrspace(200)* %{{.+}} seq_cst
   // HYBRID: atomicrmw add i32 addrspace(200)** %{{.+}}, i32 addrspace(200)* %{{.+}} seq_cst
 
-  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8* %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8* noundef %{{.+}}, i32{{.+}} 5)
   // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call void @__atomic_store_cap(i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call void @__atomic_store_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8* %{{.+}}, i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call void @__atomic_store_cap(i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call void @__atomic_store_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8* noundef %{{.+}}, i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
 
   int *__capability res;
   int *__capability res2;
@@ -87,16 +87,16 @@ void func3(__uintcap_t *p) {
   // PURECAP: atomicrmw add i8 addrspace(200)* addrspace(200)* %{{.+}}, i8 addrspace(200)* %{{.+}} seq_cst
   // HYBRID: atomicrmw add i8 addrspace(200)** %{{.+}}, i8 addrspace(200)* %{{.+}} seq_cst
 
-  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8* %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8* noundef %{{.+}}, i32{{.+}} 5)
   // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_load_cap(i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call void @__atomic_store_cap(i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call void @__atomic_store_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8* %{{.+}}, i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8* %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call void @__atomic_store_cap(i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call void @__atomic_store_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8* noundef %{{.+}}, i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call i8 addrspace(200)* @__atomic_fetch_add_cap(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
 
   __uintcap_t res;
   __uintcap_t res2;
@@ -118,14 +118,14 @@ void func4(long *p) {
   // HYBRID: atomicrmw xchg i64* %{{.+}}, i64 %{{.+}} seq_cst
   // HYBRID: cmpxchg i64* %{{.+}}, i64 %{{.+}}, i64 %{{.+}} seq_cst seq_cst
 
-  // HYBRID-LIBCALLS: call i64 @__atomic_load_8(i8* %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i64 @__atomic_load_8(i8* noundef %{{.+}}, i32{{.+}} 5)
   // PURECAP-LIBCALLS: call i64 @__atomic_load_8(i8 addrspace(200)* noundef %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call void @__atomic_store_8(i8* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call void @__atomic_store_8(i8 addrspace(200)* noundef %{{.+}}, i64 %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call i64 @__atomic_exchange_8(i8* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call i64 @__atomic_exchange_8(i8 addrspace(200)* noundef %{{.+}}, i64 %{{.+}}, i32{{.+}} 5)
-  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_8(i8* %{{.+}}, i8* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
-  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_8(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* %{{.+}}, i64 %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call void @__atomic_store_8(i8* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call void @__atomic_store_8(i8 addrspace(200)* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call i64 @__atomic_exchange_8(i8* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call i64 @__atomic_exchange_8(i8 addrspace(200)* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5)
+  // HYBRID-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_8(i8* noundef %{{.+}}, i8* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
+  // PURECAP-LIBCALLS: call{{.+}} i1 @__atomic_compare_exchange_8(i8 addrspace(200)* noundef %{{.+}}, i8 addrspace(200)* noundef %{{.+}}, i64 noundef %{{.+}}, i32{{.+}} 5, i32{{.+}} 5)
 
   long res;
   long res2;
