CTSRD-CHERI/llvm-project

commit f2af0bff150d004ebd63306c85e4792fa0f3fa88

[CheriBoundAllocas] Fix deprecated getElementType() calls

llvm/lib/CodeGen/CheriBoundAllocas.cpp
@@ -166,9 +166,8 @@ public:
       B.SetCurrentDebugLocation(AI->getDebugLoc());
 
       Align ForcedAlignment;
-      PointerType *AllocaTy = AI->getType();
-      assert(isCheriPointer(AllocaTy, &DL));
-      Type *AllocationTy = AllocaTy->getElementType();
+      assert(isCheriPointer(AI->getType(), &DL));
+      Type *AllocationTy = AI->getAllocatedType();
       Value *ArraySize = B.CreateZExtOrTrunc(AI->getArraySize(), SizeTy);
 
       // Create a new (AS 0) alloca
@@ -310,7 +309,7 @@ public:
         Instruction *AllocaI8 = cast<Instruction>(B.CreateBitCast(AI, I8CapTy));
         Value *SingleBoundedAlloc =
             B.CreateCall(SetBoundsIntrin, {AllocaI8, Size});
-        SingleBoundedAlloc = B.CreateBitCast(SingleBoundedAlloc, AllocaTy);
+        SingleBoundedAlloc = B.CreateBitCast(SingleBoundedAlloc, AI->getType());
         for (Use *U : UsesThatNeedBounds) {
           U->set(SingleBoundedAlloc);
         }
@@ -362,7 +361,7 @@ public:
             // just so we can do the setbounds in a different basic block.
             Value *AllocaI8 = B.CreateBitCast(AI, I8CapTy);
             auto WithBounds = B.CreateCall(SetBoundsIntrin, {AllocaI8, Size});
-            BoundedAlloca = B.CreateBitCast(WithBounds, AllocaTy);
+            BoundedAlloca = B.CreateBitCast(WithBounds, AI->getType());
             ReplacedUses.insert({{I, IncomingBB}, BoundedAlloca});
           } else {
             // Multiple uses in the same instruction -> reuse existing call.

llvm/test/Transforms/CheriBoundAllocas/pad-static-alloca.ll
@@ -1,4 +1,4 @@
-; NOTE: Assertions have been autogenerated by utils/update_test_checks.py
+; NOTE: Assertions have been autogenerated by utils/update_test_checks.py UTC_ARGS: --force-update
 ; RUN: %cheri_purecap_opt -cheri-bound-allocas -S < %s | FileCheck %s --check-prefix OPT128
 ; RUN: %riscv64_cheri_purecap_opt -cheri-bound-allocas -S < %s | FileCheck %s --check-prefix OPT128
 target datalayout = "E-m:m-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128-A200-P200-G200"
@@ -30,8 +30,8 @@ define void @pad_large() local_unnamed_addr addrspace(200) {
 ; OPT128-NEXT:    [[TMP1:%.*]] = bitcast { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP0]] to [16388 x i8] addrspace(200)*
 ; OPT128-NEXT:    [[TMP2:%.*]] = bitcast { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP0]] to i8 addrspace(200)*
 ; OPT128-NEXT:    [[TMP3:%.*]] = call i8 addrspace(200)* @llvm.cheri.bounded.stack.cap.i64(i8 addrspace(200)* [[TMP2]], i64 16416)
-; OPT128-NEXT:    [[TMP4:%.*]] = bitcast i8 addrspace(200)* [[TMP3]] to [16388 x i8] addrspace(200)*
-; OPT128-NEXT:    [[PTR:%.*]] = getelementptr inbounds [16388 x i8], [16388 x i8] addrspace(200)* [[TMP4]], i64 0, i64 0
+; OPT128-NEXT:    [[TMP4:%.*]] = bitcast i8 addrspace(200)* [[TMP3]] to { [16388 x i8], [28 x i8] } addrspace(200)*
+; OPT128-NEXT:    [[PTR:%.*]] = getelementptr inbounds [16388 x i8], { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP4]], i64 0, i64 0
 ; OPT128-NEXT:    call void @keep_live(i8 addrspace(200)* nonnull [[PTR]])
 ; OPT128-NEXT:    ret void
 ;
@@ -47,7 +47,7 @@ define void @nopad_large() local_unnamed_addr addrspace(200) {
 ; OPT128-NEXT:  entry:
 ; OPT128-NEXT:    [[TMP0:%.*]] = alloca [16388 x i8], align 32, addrspace(200)
 ; OPT128-NEXT:    [[PTR:%.*]] = getelementptr inbounds [16388 x i8], [16388 x i8] addrspace(200)* [[TMP0]], i64 0, i64 0
-; OPT128-NEXT:    store volatile i8 0, i8 addrspace(200)* [[PTR]]
+; OPT128-NEXT:    store volatile i8 0, i8 addrspace(200)* [[PTR]], align 1
 ; OPT128-NEXT:    ret void
 ;
 entry:

commit ffa64713fce0226fc1d6155d3dda701ca1b717d1

[CheriBoundAllocas] Avoid creating invalid IR when inserting tail padding
This bug was introduced in f2af0bff150d004ebd63306c85e4792fa0f3fa88, where
I replaced AllocaTy with AI->getType() without noticing that the type can
in fact change. While reverting those changes also refactor the tail
padding code to avoid creating a new alloca and instead mutating the
existing one. Also prepare the code for opaque pointers where we will no
longer need the added bitcast instruction (but this is untested).

I should have noticed while looking at the pad-static-alloca.ll test diff
in f2af0bff150d004ebd63306c85e4792fa0f3fa88 but missed the type change.

llvm/lib/CodeGen/CheriBoundAllocas.cpp
@@ -168,6 +168,7 @@ public:
       Align ForcedAlignment;
       assert(isCheriPointer(AI->getType(), &DL));
       Type *AllocationTy = AI->getAllocatedType();
+      PointerType *AllocaPtrTy = AI->getType();
       Value *ArraySize = B.CreateZExtOrTrunc(AI->getArraySize(), SizeTy);
 
       // Create a new (AS 0) alloca
@@ -269,22 +270,26 @@ public:
             ArrayType::get(Type::getInt8Ty(F.getContext()),
                            static_cast<uint64_t>(TailPadding));
           Type *TypeWithPadding = StructType::get(AllocatedType, PaddingType);
-          auto *NewAI =
-            new AllocaInst(TypeWithPadding, AI->getType()->getAddressSpace(),
-                           nullptr, "", AI);
-          NewAI->takeName(AI);
-          NewAI->setAlignment(AI->getAlign());
-          NewAI->setUsedWithInAlloca(AI->isUsedWithInAlloca());
-          NewAI->setSwiftError(AI->isSwiftError());
-          NewAI->copyMetadata(*AI);
-
-          auto *NewPtr = new BitCastInst(NewAI, AI->getType(), "", AI);
-          AI->replaceAllUsesWith(NewPtr);
-          AI->eraseFromParent();
-          AI = NewAI;
-          Size =
-            ConstantInt::get(SizeTy,
-                             AllocaSize + static_cast<uint64_t>(TailPadding));
+          // Instead of cloning the alloca, mutate it in-place to avoid missing
+          // some important metadata (debug info/attributes/etc.).
+          AI->setAllocatedType(TypeWithPadding);
+          if (!AI->getType()->isOpaquePointerTy()) {
+            // Explicitly create a bitcast instruction to allow us to RAUW all
+            // uses after changing the type (not needed with opaque pointers).
+            // We have to use a NULL source temporarily since we can only use
+            // AI after calling AI->mutateType(), and RAUW will assert if
+            // called after AI->mutateType(), so we need this temporary.
+            auto *NewPtr = new BitCastInst(
+                ConstantPointerNull::get(AllocaPtrTy), AllocaPtrTy,
+                "without-tail-padding", AI->getNextNonDebugInstruction());
+            AI->replaceAllUsesWith(NewPtr);
+            AI->mutateType(
+                TypeWithPadding->getPointerTo(AI->getAddressSpace()));
+            // Finally, set bitcast source to AI
+            NewPtr->getOperandUse(0).set(AI);
+          }
+          Size = ConstantInt::get(
+              SizeTy, AllocaSize + static_cast<uint64_t>(TailPadding));
         }
       }
 
@@ -309,7 +314,7 @@ public:
         Instruction *AllocaI8 = cast<Instruction>(B.CreateBitCast(AI, I8CapTy));
         Value *SingleBoundedAlloc =
             B.CreateCall(SetBoundsIntrin, {AllocaI8, Size});
-        SingleBoundedAlloc = B.CreateBitCast(SingleBoundedAlloc, AI->getType());
+        SingleBoundedAlloc = B.CreateBitCast(SingleBoundedAlloc, AllocaPtrTy);
         for (Use *U : UsesThatNeedBounds) {
           U->set(SingleBoundedAlloc);
         }
@@ -361,7 +366,7 @@ public:
             // just so we can do the setbounds in a different basic block.
             Value *AllocaI8 = B.CreateBitCast(AI, I8CapTy);
             auto WithBounds = B.CreateCall(SetBoundsIntrin, {AllocaI8, Size});
-            BoundedAlloca = B.CreateBitCast(WithBounds, AI->getType());
+            BoundedAlloca = B.CreateBitCast(WithBounds, AllocaPtrTy);
             ReplacedUses.insert({{I, IncomingBB}, BoundedAlloca});
           } else {
             // Multiple uses in the same instruction -> reuse existing call.

llvm/test/Transforms/CheriBoundAllocas/pad-static-alloca.ll
@@ -27,11 +27,11 @@ define void @pad_large() local_unnamed_addr addrspace(200) {
 ; OPT128-LABEL: @pad_large(
 ; OPT128-NEXT:  entry:
 ; OPT128-NEXT:    [[TMP0:%.*]] = alloca { [16388 x i8], [28 x i8] }, align 32, addrspace(200)
-; OPT128-NEXT:    [[TMP1:%.*]] = bitcast { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP0]] to [16388 x i8] addrspace(200)*
-; OPT128-NEXT:    [[TMP2:%.*]] = bitcast { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP0]] to i8 addrspace(200)*
-; OPT128-NEXT:    [[TMP3:%.*]] = call i8 addrspace(200)* @llvm.cheri.bounded.stack.cap.i64(i8 addrspace(200)* [[TMP2]], i64 16416)
-; OPT128-NEXT:    [[TMP4:%.*]] = bitcast i8 addrspace(200)* [[TMP3]] to { [16388 x i8], [28 x i8] } addrspace(200)*
-; OPT128-NEXT:    [[PTR:%.*]] = getelementptr inbounds [16388 x i8], { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP4]], i64 0, i64 0
+; OPT128-NEXT:    [[WITHOUT_TAIL_PADDING:%.*]] = bitcast { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP0]] to [16388 x i8] addrspace(200)*
+; OPT128-NEXT:    [[TMP1:%.*]] = bitcast { [16388 x i8], [28 x i8] } addrspace(200)* [[TMP0]] to i8 addrspace(200)*
+; OPT128-NEXT:    [[TMP2:%.*]] = call i8 addrspace(200)* @llvm.cheri.bounded.stack.cap.i64(i8 addrspace(200)* [[TMP1]], i64 16416)
+; OPT128-NEXT:    [[TMP3:%.*]] = bitcast i8 addrspace(200)* [[TMP2]] to [16388 x i8] addrspace(200)*
+; OPT128-NEXT:    [[PTR:%.*]] = getelementptr inbounds [16388 x i8], [16388 x i8] addrspace(200)* [[TMP3]], i64 0, i64 0
 ; OPT128-NEXT:    call void @keep_live(i8 addrspace(200)* nonnull [[PTR]])
 ; OPT128-NEXT:    ret void
 ;
@@ -56,3 +56,31 @@ entry:
   store volatile i8 0, i8 addrspace(200)* %ptr
   ret void
 }
+
+;; A large alloca with a struct type - incorrect handling of the new alloca type
+;; resulted in invalid IR after catching up with API changes when merging to LLVM 14.
+
+%struct.snmp_pdu = type { [100 x %struct.asn_oid], i32 }
+%struct.asn_oid = type { [28 x i32] }
+
+@snmp_discover_engine_resp = addrspace(200) global %struct.snmp_pdu zeroinitializer, align 4
+declare void @snmp_pdu_free(%struct.snmp_pdu addrspace(200)* noundef) addrspace(200)
+
+define dso_local void @snmp_discover_engine(%struct.snmp_pdu addrspace(200)* noalias sret(%struct.snmp_pdu) align 4 %agg.result) addrspace(200) nounwind {
+; OPT128-LABEL: @snmp_discover_engine(
+; OPT128-NEXT:  entry:
+; OPT128-NEXT:    [[BYVAL_TEMP:%.*]] = alloca { [[STRUCT_SNMP_PDU:%.*]], [12 x i8] }, align 16, addrspace(200)
+; OPT128-NEXT:    [[WITHOUT_TAIL_PADDING:%.*]] = bitcast { [[STRUCT_SNMP_PDU]], [12 x i8] } addrspace(200)* [[BYVAL_TEMP]] to [[STRUCT_SNMP_PDU]] addrspace(200)*
+; OPT128-NEXT:    [[TMP0:%.*]] = bitcast { [[STRUCT_SNMP_PDU]], [12 x i8] } addrspace(200)* [[BYVAL_TEMP]] to i8 addrspace(200)*
+; OPT128-NEXT:    [[TMP1:%.*]] = call i8 addrspace(200)* @llvm.cheri.bounded.stack.cap.i64(i8 addrspace(200)* [[TMP0]], i64 11216)
+; OPT128-NEXT:    [[TMP2:%.*]] = bitcast i8 addrspace(200)* [[TMP1]] to [[STRUCT_SNMP_PDU]] addrspace(200)*
+; OPT128-NEXT:    call void @snmp_pdu_free([[STRUCT_SNMP_PDU]] addrspace(200)* [[TMP2]])
+; OPT128-NEXT:    ret void
+;
+entry:
+  %byval-temp = alloca %struct.snmp_pdu, align 4, addrspace(200)
+  call void @snmp_pdu_free(%struct.snmp_pdu addrspace(200)* %byval-temp)
+  ret void
+}
+
+declare void @llvm.memcpy.p200i8.p200i8.i64(i8 addrspace(200)* noalias nocapture writeonly, i8 addrspace(200)* noalias nocapture readonly, i64, i1 immarg) addrspace(200)