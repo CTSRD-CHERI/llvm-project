072e006927f2feb35164e81651871508864117be
Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-12-08 10:52:04 +0000

    Update clang/lib/Headers/cheriintrin.h

--- clang/lib/Headers/cheriintrin.h
+++ clang/lib/Headers/cheriintrin.h
@@ -1,4 +1,4 @@
-/*===---- cheriintrin.h - CHERI intrinsics ----- ---------------------------===
+/*===---- cheriintrin.h - CHERI intrinsics ---------------------------------===
  *
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
  * See https://llvm.org/LICENSE.txt for license information.
