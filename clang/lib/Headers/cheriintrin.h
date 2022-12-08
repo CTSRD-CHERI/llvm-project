0bab6c8928b1c8775abee02bb07ad96f42ffd2e7
Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-12-08 10:51:39 +0000

    Update clang/lib/Headers/cheriintrin.h

--- clang/lib/Headers/cheriintrin.h
+++ clang/lib/Headers/cheriintrin.h
@@ -1,5 +1,4 @@
 /*===---- cheriintrin.h - CHERI intrinsics ----- ---------------------------===
- * SPDX-License-Identifier: BSD-2-Clause
  *
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
  * See https://llvm.org/LICENSE.txt for license information.
