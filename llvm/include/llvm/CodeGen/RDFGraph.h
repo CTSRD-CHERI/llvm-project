ca7af1938f5edb1b1868dca52ab4d9d4145469d3
Jessica Clarke <jrtc27@jrtc27.com>
2023-01-14 01:47:28 +0000

    fixup! RDFGraph: Bump up node allocation size for 16-byte pointers

--- llvm/include/llvm/CodeGen/RDFGraph.h
+++ llvm/include/llvm/CodeGen/RDFGraph.h
@@ -371,8 +371,8 @@ namespace rdf {
   // This method significantly improved the build time, compared to using maps
   // (std::unordered_map or DenseMap) to translate between pointers and ids.
   struct NodeAllocator {
-    // Amount of storage for a single node.
-    enum { NodeMemSize = sizeof(void *) <= 8 ? 32 : 48 };
+    // Amount of storage for a single node; must be a power of two.
+    enum { NodeMemSize = sizeof(void *) <= 8 ? 32 : 64 };
 
     NodeAllocator(uint32_t NPB = 4096)
         : NodesPerBlock(NPB), BitsPerIndex(Log2_32(NPB)),
@@ -503,8 +503,8 @@ namespace rdf {
       Code_struct Code;
     };
   };
-  // The allocator allocates chunks of 3/482 bytes for each node. The fact that
-  // each node takes 32/48 bytes in memory is used for fast translation between
+  // The allocator allocates chunks of 32/64 bytes for each node. The fact that
+  // each node takes 32/64 bytes in memory is used for fast translation between
   // the node id and the node address.
   static_assert(sizeof(NodeBase) <= NodeAllocator::NodeMemSize,
         "NodeBase must be at most NodeAllocator::NodeMemSize bytes");
