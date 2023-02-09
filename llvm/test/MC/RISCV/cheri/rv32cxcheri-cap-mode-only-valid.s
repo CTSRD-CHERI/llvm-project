4f0182753487c3ddce2cfef883ba98caedfb88c7
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-02-09 14:15:08 +0000

    [CHERI-RISC-V] Add MC tests for compressed instructions
    The error messages are not always ideal, but improving those would be
    rather complicated. For example RV64 clc with a multiple of 8 immediate
    reports missing RV32I extension rather than invalid immediate.

---
+++ llvm/test/MC/RISCV/cheri/rv32cxcheri-cap-mode-only-valid.s
@@ -0,0 +1,26 @@
+# RUN: llvm-mc %s -triple=riscv32 -mattr=+c,+xcheri,+xcheri-rvc,+cap-mode -riscv-no-aliases -show-encoding \
+# RUN:     | FileCheck -check-prefixes=CHECK-ASM,CHECK-ASM-AND-OBJ %s
+# RUN: llvm-mc -filetype=obj -triple riscv32 -mattr=+c,+xcheri,+xcheri-rvc,+cap-mode < %s \
+# RUN:     | llvm-objdump -M no-aliases --mattr=+c,+xcheri,+xcheri-rvc,+cap-mode -d - \
+# RUN:     | FileCheck -check-prefixes=CHECK-OBJ,CHECK-ASM-AND-OBJ %s
+
+## Different encoding between RV64 and RV32 (see rv64cxcheri-cap-mode-only-valid.s)
+
+# CHECK-ASM-AND-OBJ: c.clccsp cra, 40(csp)
+# CHECK-ASM-SAME: encoding: [0xa2,0x70]
+c.clccsp cra, 40(csp)
+# CHECK-ASM-AND-OBJ: c.csccsp cra, 256(csp)
+# CHECK-ASM-SAME: encoding: [0x06,0xe2]
+c.csccsp cra, 256(csp)
+# CHECK-ASM-AND-OBJ: c.clc ca2, 16(ca0)
+# CHECK-ASM-SAME: encoding: [0x10,0x69]
+c.clc ca2, 16(ca0)
+# CHECK-ASM-AND-OBJ: c.csc ca5, 128(ca3)
+# CHECK-ASM-SAME: encoding: [0xdc,0xe2]
+c.csc ca5, 128(ca3)
+
+## C.JAL is only defined for RV32C:
+# CHECK-OBJ: c.cjal 0x806
+# CHECK-ASM: c.cjal 2046
+# CHECK-ASM-SAME: encoding: [0xfd,0x2f]
+c.cjal 2046
---
+++ llvm/test/MC/RISCV/cheri/rv64cxcheri-cap-mode-only-valid.s
@@ -0,0 +1,32 @@
+# RUN: llvm-mc %s -triple=riscv64 -mattr=+c,+xcheri,+xcheri-rvc,+cap-mode -riscv-no-aliases -show-encoding \
+# RUN:     | FileCheck -check-prefixes=CHECK-ASM,CHECK-ASM-AND-OBJ %s
+# RUN: llvm-mc -filetype=obj -triple riscv64 -mattr=+c,+xcheri,+xcheri-rvc,+cap-mode < %s \
+# RUN:     | llvm-objdump -M no-aliases --mattr=+c,+xcheri,+xcheri-rvc,+cap-mode -d - \
+# RUN:     | FileCheck -check-prefixes=CHECK-ASM-AND-OBJ %s
+
+## Different encoding between RV64 and RV32 (see rv32cxcheri-cap-mode-only-valid.s)
+# CHECK-ASM-AND-OBJ: c.clccsp cra, 32(csp)
+# CHECK-ASM-SAME: encoding: [0x82,0x30]
+c.clccsp cra, 32(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.csccsp cra, 256(csp)
+# CHECK-ASM-SAME: encoding: [0x06,0xa2]
+c.csccsp cra, 256(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.clc ca2, 16(ca0)
+# CHECK-ASM-SAME: encoding: [0x10,0x29]
+c.clc ca2, 16(ca0)
+# CHECK-ASM-AND-OBJ-NEXT: c.csc ca5, 128(ca3)
+# CHECK-ASM-SAME: encoding: [0xdc,0xa2]
+c.csc ca5, 128(ca3)
+## *D operations need RV64C:
+# CHECK-ASM-AND-OBJ-NEXT: c.cldcsp ra, 40(csp)
+# CHECK-ASM-SAME: encoding: [0xa2,0x70]
+c.cldcsp ra, 40(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.csdcsp ra, 256(csp)
+# CHECK-ASM-SAME: encoding: [0x06,0xe2]
+c.csdcsp ra, 256(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.cld a2, 16(ca0)
+# CHECK-ASM-SAME: encoding: [0x10,0x69]
+c.cld a2, 16(ca0)
+# CHECK-ASM-AND-OBJ-NEXT: c.csd a5, 128(ca3)
+# CHECK-ASM-SAME: encoding: [0xdc,0xe2]
+c.csd a5, 128(ca3)
