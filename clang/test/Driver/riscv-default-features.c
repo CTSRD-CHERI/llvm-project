e154e6e880657d9548342a038416c02c88f60e17
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-02-09 20:27:00 +0000

    [CHERI-RISC-V] Remove explicit handling of -m(no-)xcheri-rvc
    It is already handled in the call to handleTargetFeaturesGroup() below.
    There is also no need to always add -xcheri-rvc when we have +xcheri since
    not having +xcheri-rvc results in the same behaviour as the negative flag.

--- clang/test/Driver/riscv-default-features.c
+++ clang/test/Driver/riscv-default-features.c
@@ -3,12 +3,21 @@
 
 // RUN: %clang --target=riscv32-unknown-elf -march=rv32ixcheri -S -emit-llvm %s -o - | FileCheck %s -check-prefix=RV32-XCHERI
 // RUN: %clang --target=riscv64-unknown-elf -march=rv64ixcheri -S -emit-llvm %s -o - | FileCheck %s -check-prefix=RV64-XCHERI
+// RUN: %clang --target=riscv32-unknown-elf -march=rv32ixcheri -S -mxcheri-rvc -emit-llvm %s -o - | FileCheck %s -check-prefix=RV32-XCHERI-RVC
+// RUN: %clang --target=riscv64-unknown-elf -march=rv64ixcheri -S -mxcheri-rvc -emit-llvm %s -o - | FileCheck %s -check-prefix=RV64-XCHERI-RVC
+// RUN: %clang --target=riscv32-unknown-elf -march=rv32ixcheri -S -mno-xcheri-rvc -emit-llvm %s -o - | FileCheck %s -check-prefix=RV32-XCHERI-NORVC
+// RUN: %clang --target=riscv64-unknown-elf -march=rv64ixcheri -S -mno-xcheri-rvc -emit-llvm %s -o - | FileCheck %s -check-prefix=RV64-XCHERI-NORVC
 
 // RV32: "target-features"="+a,+c,+m,+relax,-save-restore"
 // RV64: "target-features"="+64bit,+a,+c,+m,+relax,-save-restore"
 
-// RV32-XCHERI: "target-features"="+relax,+xcheri,-save-restore,-xcheri-rvc
-// RV64-XCHERI: "target-features"="+64bit,+relax,+xcheri,-save-restore,-xcheri-rvc"
+// RV32-XCHERI: "target-features"="+relax,+xcheri,-save-restore"
+// RV64-XCHERI: "target-features"="+64bit,+relax,+xcheri,-save-restore"
+
+// RV32-XCHERI-RVC: "target-features"="+relax,+xcheri,+xcheri-rvc,-save-restore"
+// RV64-XCHERI-RVC: "target-features"="+64bit,+relax,+xcheri,+xcheri-rvc,-save-restore"
+// RV32-XCHERI-NORVC: "target-features"="+relax,+xcheri,-save-restore,-xcheri-rvc"
+// RV64-XCHERI-NORVC: "target-features"="+64bit,+relax,+xcheri,-save-restore,-xcheri-rvc"
 
 // Dummy function
 int foo(){
