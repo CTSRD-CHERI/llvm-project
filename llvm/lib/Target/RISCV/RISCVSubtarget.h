787a16b96718e185b6fd63e14741c97aa79a3c30
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-07-05 15:03:21 -0700

    [CHERI-RISC-V] Introduce a temporary +xcheri-v9-semantics feature
    When set, this feature allows the compiler to assume new capability
    semantics (tag-clearing instead of trapping and removal of DDC/PCC
    relocation). This will allow hoisting previously trapping instructions
    out of loops which cannot be done unconditionally now as we still want
    to support building and running the last CheriBSD release with the latest
    dev branch of the compiler. We therefore need an opt-in flag that can be
    set by the CheriBSD build system.

--- llvm/lib/Target/RISCV/RISCVSubtarget.h
+++ llvm/lib/Target/RISCV/RISCVSubtarget.h
@@ -100,6 +100,7 @@ private:
   bool HasRV64 = false;
   bool IsRV32E = false;
   bool HasCheri = false;
+  bool HasCheriISAv9Semantics = false;
   bool IsCapMode = false;
   bool EnableLinkerRelax = false;
   bool EnableRVCHintInstrs = true;
@@ -186,6 +187,7 @@ public:
   bool is64Bit() const { return HasRV64; }
   bool isRV32E() const { return IsRV32E; }
   bool hasCheri() const { return HasCheri; }
+  bool hasCheriISAv9Semantics() const { return HasCheriISAv9Semantics; }
   bool isCapMode() const { return IsCapMode; }
   bool enableLinkerRelax() const { return EnableLinkerRelax; }
   bool enableRVCHintInstrs() const { return EnableRVCHintInstrs; }
