4fa16288937be7c39504cfee7ed43387bb6e03d8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-07-06 16:41:30 -0700

    Extend the __capability qualifier tests for typeof() and decltype()
    This exposed a new crash: https://github.com/CTSRD-CHERI/llvm-project/issues/710

--- clang/test/SemaCXX/cheri/cheri-capability-qualifier-declspec.cpp
+++ clang/test/SemaCXX/cheri/cheri-capability-qualifier-declspec.cpp
@@ -3,5 +3,14 @@
 
 void * __capability test_decltype(void *p) {
   decltype(p) __capability c = (__cheri_tocap decltype(p) __capability)p;
+  static_assert(__is_same(typeof(c) *, void *__capability *), "");
+  static_assert(__is_same(decltype(c) &__capability, void *__capability &__capability), "");
+  void *__capability &cref = c;
+  static_assert(__is_same(decltype(cref) __capability, void *__capability &__capability), "");
+  static_assert(__is_same(__capability decltype(cref), void *__capability &__capability), "");
+
+  using intref = int &;
+  static_assert(__is_same(intref __capability, int &__capability), "");
+  static_assert(__is_same(__capability intref, int &__capability), "");
   return c;
 }
