9e2180a7258ba0762a16aacbfd6fcfb928a9987e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-07-06 16:41:30 -0700

    Extend the __capability qualifier tests for typeof() and decltype()
    This exposed a new crash: https://github.com/CTSRD-CHERI/llvm-project/issues/710

--- clang/test/Sema/cheri/cheri-capability-qualifier-declspec.c
+++ clang/test/Sema/cheri/cheri-capability-qualifier-declspec.c
@@ -1,7 +1,30 @@
-// RUN: %cheri_cc1 -o - %s -fsyntax-only -verify
+// RUN: %cheri_cc1 %s -fsyntax-only -verify
 // expected-no-diagnostics
 
-void * __capability test_typeof(void *p) {
+void *__capability test_typeof(void *p) {
   __typeof__(p) __capability c = (__cheri_tocap __typeof__(p) __capability)p;
+  _Static_assert(__builtin_types_compatible_p(typeof(c), void *__capability), "");
+  __capability __typeof__(p) c2 = (__cheri_tocap __capability __typeof__(p))p;
+  _Static_assert(__builtin_types_compatible_p(typeof(c2), void *__capability), "");
+  __capability __typeof__(p) *__capability c3 = (__cheri_tocap __capability __typeof__(p) *__capability)p;
+  _Static_assert(__builtin_types_compatible_p(typeof(c3), void *__capability *__capability), "");
   return c;
 }
+
+static int *global_intptr = 0;
+static __capability typeof(global_intptr) global_intcap = 0;
+_Static_assert(__builtin_types_compatible_p(typeof(global_intptr), int *), "");
+_Static_assert(__builtin_types_compatible_p(typeof(global_intcap), int *__capability), "");
+
+/// Crash reproducer for https://github.com/CTSRD-CHERI/llvm-project/issues/710
+// RUN: not --crash %cheri_cc1 %s -fsyntax-only -verify -DCRASH
+// REQUIRES: asserts
+#ifdef CRASH
+struct s {
+  int i;
+};
+static struct s *global_sptr = 0;
+static __capability typeof(global_sptr) global_scap = 0;
+_Static_assert(__builtin_types_compatible_p(typeof(global_sptr), struct s *), "");
+_Static_assert(__builtin_types_compatible_p(typeof(global_scap), struct s *__capability), "");
+#endif
