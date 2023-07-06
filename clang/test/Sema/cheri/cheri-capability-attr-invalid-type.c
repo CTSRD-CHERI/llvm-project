e2fae40445902baca3579085f4751a8b7c481b57
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-07-06 10:04:42 -0700

    Update a test to use -verify instead of checking for "no crash"
    Also drop the second non-reduced test for the same issue.

--- clang/test/Sema/cheri/cheri-capability-attr-invalid-type.c
+++ clang/test/Sema/cheri/cheri-capability-attr-invalid-type.c
@@ -1,28 +1,14 @@
-// RUN: not %cheri_cc1 -target-abi n64 -fsyntax-only %s 2>&1 | FileCheck %s
-// Test case for https://github.com/CTSRD-CHERI/clang/issues/157
-// We only check if the compiler crashes
+// RUN: %cheri_cc1 -target-abi n64 -fsyntax-only %s -verify
+/// Test case for https://github.com/CTSRD-CHERI/clang/issues/157
+/// Applying __capability qualifier to a typedef as part of typo correction used to crash.
 
-// CHECK: *
-
-#define a(b)                                                                   \
-  {                                                                            \
-    typedef __typeof__(b) c;                                                   \
-    __capability c d; }
+#define a(b)                 \
+  __extension__({            \
+    typedef __typeof__(b) c; \
+    __capability c d;        \
+  })
 int e() {
-  int *pipv = (a(pip));
+  int *__capability pipv = a(pip); // expected-error{{use of undeclared identifier 'pip'; did you mean 'pipv'?}}
+  // expected-error@-1{{initializing 'int * __capability' with an expression of incompatible type 'void'}}
+  // expected-note@-2{{'pipv' declared here}}
 }
-
-
-#  define ZEXPORT
-#  define FAR
-typedef unsigned char  Byte;
-#  define Bytef Byte FAR
-#define cheri_ptr_to_bounded_cap(ptr)   __extension__({ \
-        typedef __typeof__(ptr) __ptr_type;             \
-        typedef __capability __ptr_type __cap_type;     \
- z_stream;
-typedef z_stream FAR * __capability z_streamp;
-int ZEXPORT compress2 (dest, destLen, source, sourceLen, level)     Bytef *dest;
-{
-  z_stream stream;
-  z_streamp stream_cap = cheri_ptr_to_bounded_cap(&stream);
