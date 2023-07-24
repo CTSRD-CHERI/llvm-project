1402235cad0dc08d2a9c0534aa067a28d7145b0d
Irina Dudina <irina.dudina@ed.ac.uk>
2023-07-24 18:31:53 +0100

    [CHERI_CSA] ProvenanceSourceChecker: silence for hybrid mode

---
+++ clang/lib/StaticAnalyzer/Checkers/CHERI/CHERIUtils.h
@@ -0,0 +1,26 @@
+//===-- CHERIUtils.h  -------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LIB_STATICANALYZER_CHECKERS_CHERI_CHERIUTILS_H
+#define LLVM_CLANG_LIB_STATICANALYZER_CHECKERS_CHERI_CHERIUTILS_H
+
+#include "clang/StaticAnalyzer/Core/Checker.h"
+
+namespace clang {
+namespace ento {
+namespace cheri {
+
+bool isPureCapMode(const ASTContext &C);
+
+bool isPointerToCapTy(const QualType Type, ASTContext &Ctx);
+
+} // end of namespace: cheri
+} // end of namespace: ento
+} // end of namespace: clang
+
+#endif // LLVM_CLANG_LIB_STATICANALYZER_CHECKERS_CHERI_CHERIUTILS_H
