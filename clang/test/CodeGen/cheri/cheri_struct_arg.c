94b72922ef4b95512cda4e2cadf9539bd400a084
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-18 16:37:32 -0700

    Update two tests for opaque pointers default

--- clang/test/CodeGen/cheri/cheri_struct_arg.c
+++ clang/test/CodeGen/cheri/cheri_struct_arg.c
@@ -8,14 +8,14 @@ void *render_char(struct thingy);
 
 // Make sure that capabilities are passed as pointers but structs containing
 // multiple integers all have their values coerced to register-sized ints.
-// CHECK: define dso_local i8 addrspace(200)* @f(i8 addrspace(200)* nocapture noundef readnone %a, i64 inreg %b.coerce0, i64 inreg %b.coerce1, i64 inreg %b.coerce2, i64 inreg %b.coerce3)
+// CHECK: define dso_local ptr addrspace(200) @f(ptr addrspace(200) nocapture noundef readnone %a, i64 inreg %b.coerce0, i64 inreg %b.coerce1, i64 inreg %b.coerce2, i64 inreg %b.coerce3)
 void *f(void *a, struct thingy b)
 {
 // The magic numbers here are difficult to read in decimal, but are clearer in
 // hex: they're the high 32 bits set to the first of a pair of 32-bit values
 // and the low 32 bits set to the second value.
 //
-// CHECK: call i8 addrspace(200)* 
+// CHECK: call ptr addrspace(200) 
 // CHECK: (i64 inreg 1, i64 inreg 8589934595, i64 inreg 17179869189, i64 inreg 25769803776)
 	render_char((struct thingy){1,{2,3,4,5,6}});
 	return render_char(b);
@@ -29,8 +29,8 @@ typedef struct heim_octet_string {
 // This is a bit ugly, as we're copying the struct padding in registers, but
 // that's probably needed for stable memcmpy anyway.  This won't be as bad on
 // CHERI128.
-// CHERI256: declare void @h(i64 inreg, i64 inreg, i64 inreg, i64 inreg, i8 addrspace(200)* inreg)
-// CHERI128: declare void @h(i64 inreg, i64 inreg, i8 addrspace(200)* inreg)
+// CHERI256: declare void @h(i64 inreg, i64 inreg, i64 inreg, i64 inreg, ptr addrspace(200) inreg)
+// CHERI128: declare void @h(i64 inreg, i64 inreg, ptr addrspace(200) inreg)
 void h(heim_octet_string str);
 
 void g(void)
