d395b0ea9b74da2b339c78a35135016b1e6b0e3d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-18 16:03:45 -0700

    Update test for prototypes warning

--- clang/test/Sema/cheri/cheri-warn-no-fnproto.c
+++ clang/test/Sema/cheri/cheri-warn-no-fnproto.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only %s -verify=nowarn
+// RUN: %clang_cc1 -fsyntax-only %s -verify=nowarn -Wno-deprecated-non-prototype
 // nowarn-no-diagnostics
 // Should be enabled by -Wall,-Wcheri and -Wcheri-prototypes
 // RUN: %cheri_purecap_cc1 -Wcheri-prototypes -fsyntax-only %s -verify
@@ -8,19 +8,21 @@
 // RUN: %cheri_cc1 -Wall -fsyntax-only %s -verify
 
 int do_append(); // expected-note {{candidate function declaration needs parameter types}}
+// expected-warning@-1{{a function declaration without a prototype is deprecated}}
 extern int store_char();
 
 void pappend(char c) {
   do_append(c); // expected-warning {{call to function 'do_append' with no prototype may lead to run-time stack corruption}}
   // expected-note@-1{{Calling functions without prototypes is dangerous because on some architectures (e.g. pure-capability MIPS) integer and pointer arguments are passed in different registers. If the call includes a parameter that does not match the function definition, the function will read uninitialized values from the argument registers.}}
+  //expected-warning@-2{{passing arguments to 'do_append' without a prototype is deprecated in all versions of C and is not supported in C2x}}
 }
 
-int do_append(long c) {
+int do_append(long c) { // expected-note{{conflicting prototype is here}}
   return store_char();
 }
 
 static int kr();
-static int kr(arg)
+static int kr(arg) // expected-warning{{a function definition without a prototype is deprecated}}
   int arg;
 {
   return arg + 1;
