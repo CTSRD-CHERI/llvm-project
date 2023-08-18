20ce1b3b7be023dfec6b70018a36e26178df41d4
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-18 15:07:29 -0700

    Delete a bad test
    This should already be tested by the InstCombine tests. "Does not crash"
    tests are not useful unless they also check expected output.

--- clang/test/CodeGen/cheri/cheri-hash-buf-crash.c
+++
@@ -1,9 +0,0 @@
-// Reproducer for a crash in the new folding intrinsics
-// For some reason the generated IR does not crash when using llc, so let's keep this test in clang
-
-// RUN: %cheri_purecap_cc1 -triple cheri-unknown-freebsd -emit-obj -O2 -std=gnu99 -ftls-model=local-exec -x c -o /dev/null %s
-typedef a;
-typedef b;
-#define c(d) (b *)(a) d
-*f;
-e() { f = c(e); }
