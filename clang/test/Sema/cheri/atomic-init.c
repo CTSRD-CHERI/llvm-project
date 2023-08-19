2ecf22e548b04268b8b98a16639585fda563384f
Jessica Clarke <jrtc27@jrtc27.com>
2023-08-19 01:27:44 +0100

    [AST] Rework and simplify ASTContext::getNonProvenanceCarryingType
    This now preserves typedefs where possible, and the generalisation
    allows us to remove the special enum case from its caller.

--- clang/test/Sema/cheri/atomic-init.c
+++ clang/test/Sema/cheri/atomic-init.c
@@ -8,3 +8,21 @@
 // CHECK-NEXT:     ImplicitCastExpr {{.*}} <col:22> '__intcap __attribute__((cheri_no_provenance))':'__intcap' <IntegralCast>
 // CHECK-NEXT:       IntegerLiteral {{.*}} <col:22> 'int' 0
 _Atomic __intcap x = 0;
+
+/// Check we preserve typedefs where possible
+typedef __intcap T;
+typedef _Atomic T AT;
+// CHECK:      VarDecl {{.*}} <{{.*}}:[[@LINE+4]]:1, col:8> col:4 y 'AT':'_Atomic(T)' cinit
+// CHECK-NEXT:   ImplicitCastExpr {{.*}} <col:8> '_Atomic(T __attribute__((cheri_no_provenance)))' <NonAtomicToAtomic>
+// CHECK-NEXT:     ImplicitCastExpr {{.*}} <col:8> '__intcap __attribute__((cheri_no_provenance))':'__intcap' <IntegralCast>
+// CHECK-NEXT:       IntegerLiteral {{.*}} <col:8> 'int' 0
+AT y = 0;
+
+/// Check that we don't duplicate attributes when preserving typdefs
+typedef __intcap __attribute__((cheri_no_provenance)) TNP;
+typedef _Atomic TNP ATNP;
+// CHECK:      VarDecl {{.*}} <{{.*}}:[[@LINE+4]]:1, col:10> col:6 z 'ATNP':'_Atomic(TNP)' cinit
+// CHECK-NEXT:   ImplicitCastExpr {{.*}} <col:10> 'ATNP':'_Atomic(TNP)' <NonAtomicToAtomic>
+// CHECK-NEXT:     ImplicitCastExpr {{.*}} <col:10> '__intcap __attribute__((cheri_no_provenance))':'__intcap' <IntegralCast>
+// CHECK-NEXT:       IntegerLiteral {{.*}} <col:10> 'int' 0
+ATNP z = 0;
