17f73e5961af2ee3a63bdb67029f44eb7eacf2fa
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-28 09:54:43 -0700

    Drop MIPS assembly checks from Clang test
    We have sufficient coverage of lowering IR atomics to assembly now, there
    is no need to do this in clang tests.

--- clang/test/CodeGenCXX/cheri/cheri-std-unexpected-atomicrmw.cpp
+++ clang/test/CodeGenCXX/cheri/cheri-std-unexpected-atomicrmw.cpp
@@ -1,8 +1,6 @@
-// REQUIRES: mips-registered-target
 
 // RUN: %cheri_purecap_cc1 -emit-llvm -std=c++11 -fsyntax-only -verify %s
 // RUN: %cheri_purecap_cc1 -emit-llvm -std=c++11 -o - %s | %cheri_FileCheck %s
-// RUN: %cheri_purecap_cc1 -O2 -mllvm -cheri-cap-table-abi=pcrel -std=c++11 -S -o - %s | %cheri_FileCheck -check-prefix=ASM -enable-var-scope %s
 // RUN: %cheri_purecap_cc1 -std=c++11 -ast-dump %s | %cheri_FileCheck -check-prefix=AST %s
 // reduced testcase for libcxx exception_fallback.ipp/new_handler_fallback.ipp
 
@@ -35,33 +33,12 @@ handler set_handler_atomic(handler func) noexcept {
   // CHECK: [[VAR_1:%.+]] = load void () addrspace(200)*, void () addrspace(200)* addrspace(200)* %.atomictmp, align [[#CAP_SIZE]]
   // CHECK: atomicrmw xchg void () addrspace(200)* addrspace(200)* @_ZL9__handler, void () addrspace(200)* [[VAR_1]] seq_cst
   return __atomic_exchange_n(&__handler, func, __ATOMIC_SEQ_CST);
-
-  // ASM-LABEL: _Z18set_handler_atomicPFvvE:
-  // ASM:      clcbi   $c2, %captab20(_ZL9__handler)($c{{.+}})
-  // ASM-NEXT: sync
-  // ASM-NEXT: .LBB2_1: # %entry
-  // ASM-NEXT: # =>This Inner Loop Header: Depth=1
-  // ASM-NEXT: cllc    $c1, $c2
-  // ASM-NEXT: cscc    $1, $c3, $c2
-  // ASM-NEXT: beqz    $1, .LBB2_1
-  // ASM-NEXT: nop
-  // ASM-NEXT: # %bb.2: # %entry
-  // ASM-NEXT: sync
-  // ASM: .end _Z18set_handler_atomicPFvvE
 }
 
 handler get_handler_atomic() noexcept {
   // CHECK-LABEL: @_Z18get_handler_atomicv(
   // CHECK: load atomic void () addrspace(200)*, void () addrspace(200)* addrspace(200)* @_ZL9__handler seq_cst
   return __atomic_load_n(&__handler, __ATOMIC_SEQ_CST);
-
-  // ASM-LABEL: _Z18get_handler_atomicv:
-  // This should just use a load + sync
-  // ASM: clcbi   [[HANDLER_ADDR:\$c[0-9]+]], %captab20(_ZL9__handler)($c{{.+}})
-  // ASM: clc     $c3, $zero, 0([[HANDLER_ADDR]])
-  // ASM: sync
-  // ASM: .end _Z18get_handler_atomicv
-
 }
 
 __attribute__((__require_constant_initialization__)) static _Atomic(handler) __atomic_handler;
@@ -77,20 +54,6 @@ handler set_handler_c11_atomic(handler func) noexcept {
   // CHECK: [[VAR_1:%.+]] = load void () addrspace(200)*, void () addrspace(200)* addrspace(200)* %.atomictmp, align [[#CAP_SIZE]]
   // CHECK: atomicrmw xchg void () addrspace(200)* addrspace(200)* @_ZL16__atomic_handler, void () addrspace(200)* [[VAR_1]] seq_cst
   return __c11_atomic_exchange(&__atomic_handler, func, __ATOMIC_SEQ_CST);
-
-  // ASM-LABEL: _Z22set_handler_c11_atomicPFvvE:
-  // ASM:      clcbi   $c2, %captab20(_ZL16__atomic_handler)($c{{.+}})
-  // ASM-NEXT: sync
-  // ASM-NEXT: .LBB4_1: # %entry
-  // ASM-NEXT: # =>This Inner Loop Header:
-  // ASM-NEXT: cllc    $c1, $c2
-  // ASM-NEXT: cscc    $1, $c3, $c2
-  // ASM-NEXT: beqz    $1, .LBB4_1
-  // ASM-NEXT: nop
-  // ASM-NEXT: # %bb.2: # %entry
-  // ASM-NEXT: sync
-  // ASM: .end _Z22set_handler_c11_atomicPFvvE
-
 }
 
 handler get_handler_c11_atomic() noexcept {
@@ -98,10 +61,4 @@ handler get_handler_c11_atomic() noexcept {
   // CHECK: load atomic void () addrspace(200)*, void () addrspace(200)* addrspace(200)* @_ZL16__atomic_handler seq_cst
 
   return __c11_atomic_load(&__atomic_handler, __ATOMIC_SEQ_CST);
-  // ASM-LABEL: _Z22get_handler_c11_atomicv:
-  // This should just use a load + sync
-  // ASM: clcbi   [[HANDLER_ADDR:\$c[0-9]+]], %captab20(_ZL16__atomic_handler)($c{{.+}})
-  // ASM: clc     $c3, $zero, 0([[HANDLER_ADDR]])
-  // ASM: sync
-  // ASM: .end _Z22get_handler_c11_atomicv
 }
