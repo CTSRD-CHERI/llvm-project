3ffb53a2f0f7700817bfb8ee5a8c9fa4c3b5bc2f
Irina Dudina <irina.dudina@ed.ac.uk>
2023-09-15 17:59:52 +0100

    [CHERI_CSA] Enable alpha.core.PointerSub by default for CHERI

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -3244,6 +3244,7 @@ static void RenderAnalyzerOptions(const ArgList &Args, ArgStringList &CmdArgs,
         (Triple.isRISCV() && tools::riscv::isCheriPurecap(Args, Triple))) {
       CmdArgs.push_back("-analyzer-checker=cheri");
       CmdArgs.push_back("-analyzer-checker=optin.portability.PointerAlignment");
+      CmdArgs.push_back("-analyzer-checker=alpha.core.PointerSub");
     }
 
     // Default nullability checks.
