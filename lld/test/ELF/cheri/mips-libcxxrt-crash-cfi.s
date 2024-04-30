29ee45e89c05a99b4e6e1fbd16fed65d939bf2b8
Petr Vesely <petr.vesely@codasip.com>
2024-04-30 12:56:40 +0100

    [CHERI][lld] Fix cheri tests after upstream merge
    Commit b2559f2f5ca950e917253b156bfd08ede5577cd5 reduces the amount
    of padding needed for the .eh-frame section. This commit updates
    the tests to match the new addresses.

--- lld/test/ELF/cheri/mips-libcxxrt-crash-cfi.s
+++ lld/test/ELF/cheri/mips-libcxxrt-crash-cfi.s
@@ -1,10 +1,17 @@
-## This used to crash ld.lld for CHERI MIPS
-# REQUIRES: asserts
+## This used to crash ld.lld for CHERI MIPS with "internal linker error: wrote incorrect addend value 0x1043000000000 instead of 0x10430"
+## The crash no longer occurs after b2559f2f5ca950e917253b156bfd08ede5577cd5
 # RUN: llvm-mc -filetype=obj -triple=mips64-unknown-freebsd --position-independent %s -o %t.o
 # RUN: ld.lld --eh-frame-hdr --shared -z notext -o %t.so %t.o
 # RUN: llvm-mc -filetype=obj -triple=mips64-unknown-freebsd %s -o %t-nopic.o
-# RUN: not ld.lld --eh-frame-hdr --shared -z notext -o %t.so %t-nopic.o 2>&1 | FileCheck %s --check-prefix=ERR
-# ERR: internal linker error: wrote incorrect addend value 0x1043000000000 instead of 0x10430 for dynamic relocation R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE
+# RUN: ld.lld --eh-frame-hdr --shared -z notext -o %t.so %t-nopic.o
+# RUN: llvm-readobj --hex-dump=.eh_frame %t.so | FileCheck %s
+
+# CHECK-LABEL: Hex dump of section '.eh_frame':
+# CHECK-NEXT: 0x000003f0 00000010 00000000 017a5200 01781f01 .........zR..x..
+# CHECK-NEXT: 0x00000400 0c0d1d00 00000018 00000018 00000000 ................
+# CHECK-NEXT: 0x00000410 00010430 00000000 00000004 00000000 ...0............
+#                           ^ expected addend of 0x10430
+# CHECK-NEXT: 0x00000420 00000000                            ....
 
 .text
 a:
