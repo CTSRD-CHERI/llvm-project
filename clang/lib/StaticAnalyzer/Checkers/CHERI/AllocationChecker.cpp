cac85bb7de84ebbf0ff65faa87dcda83d76abf5c
Irina Dudina <irina.dudina@ed.ac.uk>
2024-05-31 10:26:11 +0100

    [CHERI_CSA] AllocationChecker: disable for non-purecap

--- clang/lib/StaticAnalyzer/Checkers/CHERI/AllocationChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/CHERI/AllocationChecker.cpp
@@ -252,6 +252,9 @@ ExplodedNode *AllocationChecker::emitAllocationPartitionWarning(
 
 void AllocationChecker::checkPostStmt(const CastExpr *CE,
                                       CheckerContext &C) const {
+  if (!isPureCapMode(C.getASTContext()))
+    return;
+
   if (CE->getCastKind() != CK_BitCast)
     return;
   SVal SrcVal = C.getSVal(CE->getSubExpr());
@@ -322,6 +325,9 @@ void AllocationChecker::checkPostStmt(const CastExpr *CE,
 
 void AllocationChecker::checkPreCall(const CallEvent &Call,
                                      CheckerContext &C) const {
+  if (!isPureCapMode(C.getASTContext()))
+    return;
+
   if (IgnoreFnSet.contains(Call) || CheriBoundsFnSet.contains(Call))
     return;
 
@@ -362,6 +368,9 @@ void AllocationChecker::checkPreCall(const CallEvent &Call,
 
 void AllocationChecker::checkPostCall(const CallEvent &Call,
                                       CheckerContext &C) const {
+  if (!isPureCapMode(C.getASTContext()))
+    return;
+
   if (!CheriBoundsFnSet.contains(Call))
     return;
   const MemRegion *MR = C.getSVal(Call.getArgExpr(0)).getAsRegion();
@@ -381,6 +390,9 @@ void AllocationChecker::checkPostCall(const CallEvent &Call,
 
 void AllocationChecker::checkBind(SVal L, SVal V, const Stmt *S,
                                   CheckerContext &C) const {
+  if (!isPureCapMode(C.getASTContext()))
+    return;
+
   const MemRegion *Dst = L.getAsRegion();
   if (!Dst || !isa<FieldRegion>(Dst))
     return;
@@ -402,6 +414,9 @@ void AllocationChecker::checkBind(SVal L, SVal V, const Stmt *S,
 
 void AllocationChecker::checkEndFunction(const ReturnStmt *RS,
                                          CheckerContext &C) const {
+  if (!isPureCapMode(C.getASTContext()))
+    return;
+
   if (!RS)
     return;
   const Expr *RV = RS->getRetValue();
