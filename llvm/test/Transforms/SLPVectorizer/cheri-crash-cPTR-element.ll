CTSRD-CHERI/llvm-project

commit 7aa7f2ec8f274a247d55df0c0afa02d72ae9220e

[CodeGen] Rework MVT representation of capabilities and add type inference
This more closely aligns how capabilites are handled with iPTR and iN
types, introducing a new cPTR wildcard and fixed-width cN types that it
maps to, with iPTRAny being iPTR and cPTR. As a result we no longer need
to duplicate patterns for each fixed-width type in certain cases, and
the few upstream MIPS tests we've long marked as XFAIL'ed now pass.

As part of this, a few of the APIs around MVTs have been renamed from
fat pointer terminology to use capability instead. Where relatively
straightforward to do these have then been adopted throughout, but some
are pervasive and have deprecated aliases under the old names, whose
uses should be incrementally reduced.

llvm/lib/CodeGen/ValueTypes.cpp
@@ -159,14 +159,11 @@ std::string EVT::getEVTString() const {
              getVectorElementType().getEVTString();
     if (isInteger())
       return "i" + utostr(getSizeInBits());
+    if (isCapability())
+      return "c" + utostr(getSizeInBits());
     if (isFloatingPoint())
       return "f" + utostr(getSizeInBits());
     llvm_unreachable("Invalid EVT!");
-  case MVT::iFATPTR64: return "iFATPTR64";
-  case MVT::iFATPTR128: return "iFATPTR128";
-  case MVT::iFATPTR256: return "iFATPTR256";
-  case MVT::iFATPTR512: return "iFATPTR512";
-  case MVT::iFATPTRAny: return "iFATPTRAny";
   case MVT::bf16:      return "bf16";
   case MVT::ppcf128:   return "ppcf128";
   case MVT::isVoid:    return "isVoid";
@@ -537,11 +534,10 @@ Type *EVT::getTypeForEVT(LLVMContext &Context) const {
   case MVT::nxv8f64:
     return ScalableVectorType::get(Type::getDoubleTy(Context), 8);
   case MVT::Metadata: return Type::getMetadataTy(Context);
-  case MVT::iFATPTR64:
-  case MVT::iFATPTR128:
-  case MVT::iFATPTR256:
-  case MVT::iFATPTR512:
-  case MVT::iFATPTRAny:
+  case MVT::c64:
+  case MVT::c128:
+  case MVT::c256:
+    // XXX: Hard-coded AS
     return PointerType::get(Type::getInt8Ty(Context), 200);
   }
   // clang-format on
@@ -571,7 +567,7 @@ MVT MVT::getVT(Type *Ty, bool HandleUnknown){
   case Type::PointerTyID: {
     // FIXME: require a DataLayout here!
     if (isCheriPointer(Ty, nullptr))
-      return MVT(MVT::iFATPTRAny);
+      return MVT(MVT::cPTR);
     return MVT(MVT::iPTR);
   }
   case Type::FixedVectorTyID:

llvm/include/llvm/CodeGen/TargetLowering.h
@@ -367,7 +367,7 @@ public:
                    uint32_t AS) const {
 #endif
     if (DL.isFatPointer(AS))
-      return MVT::getFatPointerVT(DL.getPointerSizeInBits(AS));
+      return MVT::getCapabilityVT(DL.getPointerSizeInBits(AS));
     return MVT::getIntegerVT(DL.getPointerSizeInBits(AS));
   }
 
@@ -382,7 +382,7 @@ public:
                    uint32_t AS) const {
 #endif
     if (DL.isFatPointer(AS))
-      return MVT::getFatPointerVT(DL.getPointerSizeInBits(AS));
+      return MVT::getCapabilityVT(DL.getPointerSizeInBits(AS));
     return MVT::getIntegerVT(DL.getPointerSizeInBits(AS));
   }
 
@@ -3477,7 +3477,7 @@ protected:
   bool EnableExtLdPromotion;
 
   /// The type to use for CHERI capabilities (if supported)
-  /// Should be one of iFATPTR64/128/256
+  /// Should be one of c64/c128/c256
   MVT CapType = MVT();
 
   /// Whether the CHERI capability type supports precise bounds for any

llvm/include/llvm/CodeGen/ValueTypes.h
@@ -144,10 +144,14 @@ namespace llvm {
       return isSimple() ? V.isInteger() : isExtendedInteger();
     }
 
-    /// Return true if this is a fat pointer type.
-    bool isFatPointer() const {
-      return isSimple() ? V.isFatPointer() : false;
+    /// Return true if this is a capability type.
+    bool isCapability() const {
+      // TODO: isExtendedCapability?
+      return isSimple() ? V.isCapability() : false;
     }
+
+    /// Return true if this is a capability type. Deprecated.
+    bool isFatPointer() const { return isCapability(); }
 
     /// Return true if this is an integer, but not a vector.
     bool isScalarInteger() const {
@@ -213,7 +217,7 @@ namespace llvm {
     /// Return true if this is an overloaded type for TableGen.
     bool isOverloaded() const {
       return (V == MVT::iAny || V == MVT::fAny || V == MVT::vAny ||
-              V == MVT::iPTRAny || V == MVT::iFATPTRAny);
+              V == MVT::iPTRAny);
     }
 
     /// Return true if the bit size is a multiple of 8.

llvm/include/llvm/CodeGen/ValueTypes.td
@@ -226,11 +226,12 @@ def externref : ValueType<0,    182>;  // WebAssembly's externref type
 def x86amx    : ValueType<8192, 183>;  // X86 AMX value
 def i64x8     : ValueType<512,  184>;  // 8 Consecutive GPRs (AArch64)
 
-def iFATPTR64 : ValueType<64,   !add(i64x8.Value, 1)>;       // 64-bit fat pointer.
-def iFATPTR128: ValueType<128,  !add(iFATPTR64.Value, 1)>;   // 128-bit fat pointer.
-def iFATPTR256: ValueType<256,  !add(iFATPTR128.Value, 1)>;  // 256-bit fat pointer.
-def iFATPTR512: ValueType<512,  !add(iFATPTR256.Value, 1)>;  // 512-bit fat pointer.
-def iFATPTRAny: ValueType<0,    !add(iFATPTR512.Value, 1)>;  // Any fat pointer.
+def c64  : ValueType<64,  !add(i64x8.Value, 1)>;  // 64-bit capability value
+def c128 : ValueType<128, !add(c64.Value,   1)>;  // 128-bit capability value
+def c256 : ValueType<256, !add(c128.Value,  1)>;  // 256-bit capability value
+
+// Pseudo valuetype mapped to the current capability pointer size.
+def cPTR       : ValueType<0, 247>;
 
 def token      : ValueType<0, 248>;  // TokenTy
 def MetadataVT : ValueType<0, 249>;  // Metadata

llvm/include/llvm/IR/Intrinsics.td
@@ -187,8 +187,8 @@ class LLVMAnyPointerType<LLVMType elty>
   let isAny = true;
 }
 
-class LLVMFatPointerType<LLVMType elty>
-  : LLVMType<iFATPTRAny>{
+class LLVMCapabilityType<LLVMType elty>
+  : LLVMType<cPTR>{
   LLVMType ElTy = elty;
 }
 
@@ -261,7 +261,7 @@ def llvm_ppcf128_ty    : LLVMType<ppcf128>;
 def llvm_ptr_ty        : LLVMPointerType<llvm_i8_ty>;             // i8*
 def llvm_ptrptr_ty     : LLVMPointerType<llvm_ptr_ty>;            // i8**
 def llvm_anyptr_ty     : LLVMAnyPointerType<llvm_i8_ty>;          // (space)i8*
-def llvm_fatptr_ty     : LLVMFatPointerType<llvm_i8_ty>;          // (space)i8*
+def llvm_cap_ty        : LLVMCapabilityType<llvm_i8_ty>;          // (space)i8*
 def llvm_anyptrptr_ty  : LLVMAnyPointerType<llvm_ptr_ty>;         // i8*(space)*
 def llvm_empty_ty      : LLVMType<OtherVT>;                       // { }
 def llvm_descriptor_ty : LLVMPointerType<llvm_empty_ty>;          // { }*

llvm/include/llvm/IR/IntrinsicsCHERICap.td
@@ -14,150 +14,150 @@
 
 def int_cheri_cap_length_get :
   Intrinsic<[llvm_anyint_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_base_get :
   Intrinsic<[llvm_anyint_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 
 def int_cheri_cap_perms_and :
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_anyint_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_anyint_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_perms_get :
   Intrinsic<[llvm_anyint_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_flags_set :
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_anyint_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_anyint_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_flags_get :
   Intrinsic<[llvm_anyint_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_high_set :
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_anyint_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_anyint_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_high_get :
   Intrinsic<[llvm_anyint_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_bounds_set :
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_anyint_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_anyint_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_bounds_set_exact :
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_anyint_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_anyint_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_type_get :
   Intrinsic<[llvm_anyint_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_tag_get  :
   ClangBuiltin<"__builtin_cheri_tag_get">,
   Intrinsic<[llvm_i1_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_sealed_get  :
   ClangBuiltin<"__builtin_cheri_sealed_get">,
   Intrinsic<[llvm_i1_ty],
-            [llvm_fatptr_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 
 def int_cheri_cap_tag_clear  :
   ClangBuiltin<"__builtin_cheri_tag_clear">,
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 
 def int_cheri_cap_seal :
   ClangBuiltin<"__builtin_cheri_seal">,
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_fatptr_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_conditional_seal :
   ClangBuiltin<"__builtin_cheri_conditional_seal">,
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_fatptr_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_unseal  :
   ClangBuiltin<"__builtin_cheri_unseal">,
-  Intrinsic<[llvm_fatptr_ty],
-            [llvm_fatptr_ty, llvm_fatptr_ty],
+  Intrinsic<[llvm_cap_ty],
+            [llvm_cap_ty, llvm_cap_ty],
             [IntrNoMem, IntrWillReturn]>;
  def int_cheri_cap_seal_entry:
    ClangBuiltin<"__builtin_cheri_seal_entry">,
-   Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty],
+   Intrinsic<[llvm_cap_ty], [llvm_cap_ty],
     [IntrNoMem, IntrWillReturn]>;
 
 def int_cheri_cap_perms_check :
-  Intrinsic<[], [llvm_fatptr_ty, llvm_anyint_ty],
+  Intrinsic<[], [llvm_cap_ty, llvm_anyint_ty],
             [IntrNoMem, IntrHasSideEffects, IntrWillReturn]>;
 def int_cheri_cap_type_check :
   ClangBuiltin<"__builtin_cheri_type_check">,
-  Intrinsic<[], [llvm_fatptr_ty, llvm_fatptr_ty],
+  Intrinsic<[], [llvm_cap_ty, llvm_cap_ty],
             [IntrNoMem, IntrHasSideEffects, IntrWillReturn]>;
 def int_cheri_cap_subset_test:
   ClangBuiltin<"__builtin_cheri_subset_test">,
-  Intrinsic<[llvm_i1_ty], [llvm_fatptr_ty, llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_i1_ty], [llvm_cap_ty, llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_equal_exact:
   ClangBuiltin<"__builtin_cheri_equal_exact">,
-  Intrinsic<[llvm_i1_ty], [llvm_fatptr_ty, llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_i1_ty], [llvm_cap_ty, llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 
 def int_cheri_stack_cap_get :
   ClangBuiltin<"__builtin_cheri_stack_get">,
-  Intrinsic<[llvm_fatptr_ty], [], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_ddc_get :
   ClangBuiltin<"__builtin_cheri_global_data_get">,
-  Intrinsic<[llvm_fatptr_ty], [], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_pcc_get :
   ClangBuiltin<"__builtin_cheri_program_counter_get">,
-  Intrinsic<[llvm_fatptr_ty], [], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [], [IntrNoMem, IntrWillReturn]>;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Offset-related intrinsics
 ////////////////////////////////////////////////////////////////////////////////
 
 def int_cheri_cap_offset_set:
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_offset_get:
-  Intrinsic<[llvm_anyint_ty], [llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_anyint_ty], [llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_diff:
-  Intrinsic<[llvm_anyint_ty], [llvm_fatptr_ty,llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_anyint_ty], [llvm_cap_ty,llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Pointer / capability interoperation intrinsics
 ////////////////////////////////////////////////////////////////////////////////
 def int_cheri_cap_address_get:
-  Intrinsic<[llvm_anyint_ty], [llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_anyint_ty], [llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_address_set:
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
 
 
 def int_cheri_cap_to_pointer:
-  Intrinsic<[llvm_anyint_ty], [llvm_fatptr_ty, llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_anyint_ty], [llvm_cap_ty, llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_from_pointer:
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Building capabilities from untagged data
 ////////////////////////////////////////////////////////////////////////////////
 def int_cheri_cap_build:
   ClangBuiltin<"__builtin_cheri_cap_build">,
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_cap_type_copy:
   ClangBuiltin<"__builtin_cheri_cap_type_copy">,
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_fatptr_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_cap_ty], [IntrNoMem, IntrWillReturn]>;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Tag memory accessor functions
 ////////////////////////////////////////////////////////////////////////////////
 def int_cheri_cap_load_tags:
-  Intrinsic<[llvm_anyint_ty], [llvm_fatptr_ty], [IntrReadMem]>;
+  Intrinsic<[llvm_anyint_ty], [llvm_cap_ty], [IntrReadMem]>;
 
 /// Round length to compressed capability precision:
 def int_cheri_round_representable_length :
@@ -177,6 +177,6 @@ def int_cheri_representable_alignment_mask :
 /// NB: They *only* has this additional meaning at the IR level; at the DAG and
 ///     MIR levels they cannot be assumed safe like at the IR level.
 def int_cheri_bounded_stack_cap :
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
 def int_cheri_bounded_stack_cap_dynamic :
-  Intrinsic<[llvm_fatptr_ty], [llvm_fatptr_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;
+  Intrinsic<[llvm_cap_ty], [llvm_cap_ty, llvm_anyint_ty], [IntrNoMem, IntrWillReturn]>;

commit f15bb20c68da32ecbb7b6dca4c31a9e6b6e8c443

[IR][CodeGen] Add new SizedCapabilityType for use in CodeGen
EVT::getTypeForEVT currently returns a PointerType for MVT::cN, but this
has a couple of issues. The first issue is we have to hard-code the
address space, though that's not such a big deal given we do that
elsewhere too. The bigger issue is that, when we later pass that to
MVT::getVT or EVT::getEVT, it doesn't know what the right size is, so
returns MVT::cPTR instead, which is not a true value type, and is
supposed to only be used by TableGen. This has been seen to confuse
TargetLoweringBase::getTypeConversion, as when presented with a vector
of capability pointers it can end up trying to recreate a smaller vector
of the same type, but this trips up various assertions for the MVT::cPTR
as both the IR methods and the code here are expecting to be dealing
with actual value types.

Borrowing the idea of TypedPointerType (DXILPointerTyID) a bit,
introduce a new IR type, SizedCapabilityType, to represent a fixed-size
capability during CodeGen, which allows lossless roundtripping from MVT
to Type and back.

This fixes building cheritest, which has crashed since the introduction
of cPTR due to cPTR not being a value type, mirroring iPTR, unlike the
old iFATPTRAny which was its own weird beast, but wouldn't have tripped
up these assertions. It probably didn't do the most sensible things
though.

Fixes: 7aa7f2ec8f27 ("[CodeGen] Rework MVT representation of capabilities and add type inference")

llvm/include/llvm/IR/DataLayout.h
@@ -756,6 +756,8 @@ inline TypeSize DataLayout::getTypeSizeInBits(Type *Ty) const {
                        getTypeSizeInBits(VTy->getElementType()).getFixedSize();
     return TypeSize(MinBits, EltCnt.isScalable());
   }
+  case Type::SizedCapabilityTyID:
+    return TypeSize::Fixed(cast<SizedCapabilityType>(Ty)->getBitWidth());
   default:
     llvm_unreachable("DataLayout::getTypeSizeInBits(): Unsupported type");
   }

llvm/include/llvm/IR/DerivedTypes.h
@@ -705,6 +705,41 @@ public:
   }
 };
 
+/// Class to represent fixed-size capability types during CodeGen.
+class SizedCapabilityType : public Type {
+  friend class LLVMContextImpl;
+
+protected:
+  explicit SizedCapabilityType(LLVMContext &C, unsigned NumBits)
+      : Type(C, SizedCapabilityTyID) {
+    setSubclassData(NumBits);
+  }
+
+public:
+  /// This enum is just used to hold constants we need for SizedCapabilityType.
+  enum {
+    MIN_CAP_BITS = 64,       ///< Minimum number of bits that can be specified
+    MAX_CAP_BITS = 256       ///< Maximum number of bits that can be specified
+      ///< Note that bit width is stored in the Type classes SubclassData field
+      ///< which has 24 bits.
+  };
+
+  /// This static method is the primary way of constructing an IntegerType.
+  /// If an IntegerType with the same NumBits value was previously instantiated,
+  /// that instance will be returned. Otherwise a new one will be created. Only
+  /// one instance with a given NumBits value is ever created.
+  /// Get or create an IntegerType instance.
+  static SizedCapabilityType *get(LLVMContext &C, unsigned NumBits);
+
+  /// Get the number of bits in this IntegerType
+  unsigned getBitWidth() const { return getSubclassData(); }
+
+  /// Methods for support type inquiry through isa, cast, and dyn_cast.
+  static bool classof(const Type *T) {
+    return T->getTypeID() == SizedCapabilityTyID;
+  }
+};
+
 Type *Type::getExtendedType() const {
   assert(
       isIntOrIntVectorTy() &&

llvm/include/llvm/IR/Type.h
@@ -82,6 +82,7 @@ public:
     FixedVectorTyID,    ///< Fixed width SIMD vector type
     ScalableVectorTyID, ///< Scalable SIMD vector type
     DXILPointerTyID,    ///< DXIL typed pointer used by DirectX target
+    SizedCapabilityTyID,///< Fixed-size CHERI capability type
   };
 
 private:
@@ -225,6 +226,11 @@ public:
   /// True if this is an instance of PointerType.
   bool isPointerTy() const { return getTypeID() == PointerTyID; }
 
+  /// True if this is an instance of PointerType.
+  bool isSizedCapabilityTy() const {
+    return getTypeID() == SizedCapabilityTyID;
+  }
+
   /// True if this is an instance of an opaque PointerType.
   bool isOpaquePointerTy() const;
 
@@ -272,8 +278,8 @@ public:
   bool isSized(SmallPtrSetImpl<Type*> *Visited = nullptr) const {
     // If it's a primitive, it is always sized.
     if (getTypeID() == IntegerTyID || isFloatingPointTy() ||
-        getTypeID() == PointerTyID || getTypeID() == X86_MMXTyID ||
-        getTypeID() == X86_AMXTyID)
+        getTypeID() == PointerTyID || getTypeID() == SizedCapabilityTyID ||
+        getTypeID() == X86_MMXTyID || getTypeID() == X86_AMXTyID)
       return true;
     // If it is not something that can have a size (e.g. a function or label),
     // it doesn't have a size.

llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
@@ -1037,6 +1037,8 @@ void ModuleBitcodeWriter::writeTypeTable() {
     }
     case Type::DXILPointerTyID:
       llvm_unreachable("DXIL pointers cannot be added to IR modules");
+    case Type::SizedCapabilityTyID:
+      llvm_unreachable("Fixed-size capabilities cannot be added to IR modules");
     }
 
     // Emit the finished record.

llvm/lib/CodeGen/ValueTypes.cpp
@@ -535,10 +535,11 @@ Type *EVT::getTypeForEVT(LLVMContext &Context) const {
     return ScalableVectorType::get(Type::getDoubleTy(Context), 8);
   case MVT::Metadata: return Type::getMetadataTy(Context);
   case MVT::c64:
+    return SizedCapabilityType::get(Context, 64);
   case MVT::c128:
+    return SizedCapabilityType::get(Context, 128);
   case MVT::c256:
-    // XXX: Hard-coded AS
-    return PointerType::get(Type::getInt8Ty(Context), 200);
+    return SizedCapabilityType::get(Context, 256);
   }
   // clang-format on
 }
@@ -555,6 +556,8 @@ MVT MVT::getVT(Type *Ty, bool HandleUnknown){
     return MVT::isVoid;
   case Type::IntegerTyID:
     return getIntegerVT(cast<IntegerType>(Ty)->getBitWidth());
+  case Type::SizedCapabilityTyID:
+    return getCapabilityVT(cast<SizedCapabilityType>(Ty)->getBitWidth());
   case Type::HalfTyID:      return MVT(MVT::f16);
   case Type::BFloatTyID:    return MVT(MVT::bf16);
   case Type::FloatTyID:     return MVT(MVT::f32);
@@ -565,9 +568,8 @@ MVT MVT::getVT(Type *Ty, bool HandleUnknown){
   case Type::FP128TyID:     return MVT(MVT::f128);
   case Type::PPC_FP128TyID: return MVT(MVT::ppcf128);
   case Type::PointerTyID: {
-    // FIXME: require a DataLayout here!
-    if (isCheriPointer(Ty, nullptr))
-      return MVT(MVT::cPTR);
+    // NB: Removing this upstream, so ensure we haven't made it worse
+    assert(!isCheriPointer(Ty, nullptr));
     return MVT(MVT::iPTR);
   }
   case Type::FixedVectorTyID:

llvm/lib/IR/AsmWriter.cpp
@@ -618,6 +618,9 @@ void TypePrinting::print(Type *Ty, raw_ostream &OS) {
     // extra dependencies we just print the pointer's address here.
     OS << "dxil-ptr (" << Ty << ")";
     return;
+  case Type::SizedCapabilityTyID:
+    OS << 'c' << cast<SizedCapabilityType>(Ty)->getBitWidth();
+    return;
   }
   llvm_unreachable("Invalid TypeID");
 }

llvm/lib/IR/Core.cpp
@@ -543,6 +543,8 @@ LLVMTypeKind LLVMGetTypeKind(LLVMTypeRef Ty) {
     return LLVMScalableVectorTypeKind;
   case Type::DXILPointerTyID:
     llvm_unreachable("DXIL pointers are unsupported via the C API");
+  case Type::SizedCapabilityTyID:
+    llvm_unreachable("Fixed-size capabilities are unsupported via the C API");
   }
   llvm_unreachable("Unhandled TypeID.");
 }

llvm/lib/IR/DataLayout.cpp
@@ -843,6 +843,8 @@ Align DataLayout::getAlignment(Type *Ty, bool abi_or_pref) const {
   }
   case Type::X86_AMXTyID:
     return Align(64);
+  case Type::SizedCapabilityTyID:
+    return Align(cast<SizedCapabilityType>(Ty)->getBitWidth() / 8);
   default:
     llvm_unreachable("Bad type for getAlignment!!!");
   }

llvm/lib/IR/LLVMContextImpl.h
@@ -1484,6 +1484,7 @@ public:
   DenseMap<std::pair<Type *, ElementCount>, VectorType *> VectorTypes;
   DenseMap<Type *, PointerType *> PointerTypes; // Pointers in AddrSpace = 0
   DenseMap<std::pair<Type *, unsigned>, PointerType *> ASPointerTypes;
+  SmallDenseMap<unsigned, SizedCapabilityType *, 1> SizedCapabilityTypes;
 
   /// ValueHandles - This map keeps track of all of the value handles that are
   /// watching a Value*.  The Value::HasValueHandle bit is used to know

llvm/lib/IR/Type.cpp
@@ -182,6 +182,8 @@ TypeSize Type::getPrimitiveSizeInBits() const {
     assert(!ETS.isScalable() && "Vector type should have fixed-width elements");
     return {ETS.getFixedSize() * EC.getKnownMinValue(), EC.isScalable()};
   }
+  case Type::SizedCapabilityTyID:
+    return TypeSize::Fixed(cast<SizedCapabilityType>(this)->getBitWidth());
   default: return TypeSize::Fixed(0);
   }
 }
@@ -674,7 +676,7 @@ VectorType *VectorType::get(Type *ElementType, ElementCount EC) {
 
 bool VectorType::isValidElementType(Type *ElemTy) {
   return ElemTy->isIntegerTy() || ElemTy->isFloatingPointTy() ||
-         ElemTy->isPointerTy();
+         ElemTy->isPointerTy() || ElemTy->isSizedCapabilityTy();
 }
 
 //===----------------------------------------------------------------------===//
@@ -684,8 +686,9 @@ bool VectorType::isValidElementType(Type *ElemTy) {
 FixedVectorType *FixedVectorType::get(Type *ElementType, unsigned NumElts) {
   assert(NumElts > 0 && "#Elements of a VectorType must be greater than 0");
   assert(isValidElementType(ElementType) && "Element type of a VectorType must "
-                                            "be an integer, floating point, or "
-                                            "pointer type.");
+                                            "be an integer, floating point, "
+                                            "pointer, or sized capability "
+                                            "type.");
 
   auto EC = ElementCount::getFixed(NumElts);
 
@@ -706,8 +709,9 @@ ScalableVectorType *ScalableVectorType::get(Type *ElementType,
                                             unsigned MinNumElts) {
   assert(MinNumElts > 0 && "#Elements of a VectorType must be greater than 0");
   assert(isValidElementType(ElementType) && "Element type of a VectorType must "
-                                            "be an integer, floating point, or "
-                                            "pointer type.");
+                                            "be an integer, floating point, "
+                                            "pointer, or sized capability "
+                                            "type.");
 
   auto EC = ElementCount::getScalable(MinNumElts);
 
@@ -783,4 +787,21 @@ bool PointerType::isValidElementType(Type *ElemTy) {
 
 bool PointerType::isLoadableOrStorableType(Type *ElemTy) {
   return isValidElementType(ElemTy) && !ElemTy->isFunctionTy();
+}
+
+//===----------------------------------------------------------------------===//
+//                     SizedCapabilityType Implementation
+//===----------------------------------------------------------------------===//
+
+SizedCapabilityType *SizedCapabilityType::get(LLVMContext &C,
+                                              unsigned NumBits) {
+  assert(NumBits >= MIN_CAP_BITS && "bitwidth too small");
+  assert(NumBits <= MAX_CAP_BITS && "bitwidth too large");
+
+  SizedCapabilityType *&Entry = C.pImpl->SizedCapabilityTypes[NumBits];
+
+  if (!Entry)
+    Entry = new (C.pImpl->Alloc) SizedCapabilityType(C, NumBits);
+
+  return Entry;
 }

llvm/lib/Target/DirectX/DXILWriter/DXILBitcodeWriter.cpp
@@ -1031,6 +1031,7 @@ void DXILBitcodeWriter::writeTypeTable() {
     case Type::BFloatTyID:
     case Type::X86_AMXTyID:
     case Type::TokenTyID:
+    case Type::SizedCapabilityTyID:
       llvm_unreachable("These should never be used!!!");
       break;
     case Type::VoidTyID:

llvm/lib/Target/Hexagon/HexagonTargetObjectFile.cpp
@@ -333,6 +333,7 @@ unsigned HexagonTargetObjectFile::getSmallestAddressableSize(const Type *Ty,
   case Type::X86_AMXTyID:
   case Type::TokenTyID:
   case Type::DXILPointerTyID:
+  case Type::SizedCapabilityTyID:
     return 0;
   }
 

llvm/test/Transforms/SLPVectorizer/cheri-crash-cPTR-element.ll
@@ -1,0 +1,34 @@
+; NOTE: Assertions have been autogenerated by utils/update_test_checks.py
+; RUN: opt -mattr=+cheri128 -mcpu=beri -passes="function(slp-vectorizer)" -S < %s | FileCheck %s
+
+target datalayout = "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128"
+target triple = "mips64c128-unknown-freebsd"
+
+;; This previously crashed in TargetLoweringBase due to creating a vector which
+;; gave an element type of MVT::cPTR when queried rather than MVT::c128.
+define ptr @cmemcpy(ptr addrspace(200) %0, ptr addrspace(200) %1, i1 %tobool) {
+; CHECK-LABEL: @cmemcpy(
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    br i1 [[TOBOOL:%.*]], label [[IF_END12:%.*]], label [[DO_BODY45:%.*]]
+; CHECK:       if.end12:
+; CHECK-NEXT:    br label [[DO_BODY45]]
+; CHECK:       do.body45:
+; CHECK-NEXT:    [[SRC2_0:%.*]] = phi ptr addrspace(200) [ [[TMP0:%.*]], [[IF_END12]] ], [ [[TMP1:%.*]], [[ENTRY:%.*]] ]
+; CHECK-NEXT:    [[DST1_0:%.*]] = phi ptr addrspace(200) [ [[TMP1]], [[IF_END12]] ], [ [[TMP0]], [[ENTRY]] ]
+; CHECK-NEXT:    [[TMP2:%.*]] = load i64, ptr addrspace(200) [[SRC2_0]], align 8
+; CHECK-NEXT:    store i64 [[TMP2]], ptr addrspace(200) [[DST1_0]], align 8
+; CHECK-NEXT:    ret ptr null
+;
+entry:
+  br i1 %tobool, label %if.end12, label %do.body45
+
+if.end12:
+  br label %do.body45
+
+do.body45:
+  %src2.0 = phi ptr addrspace(200) [ %0, %if.end12 ], [ %1, %entry ]
+  %dst1.0 = phi ptr addrspace(200) [ %1, %if.end12 ], [ %0, %entry ]
+  %2 = load i64, ptr addrspace(200) %src2.0, align 8
+  store i64 %2, ptr addrspace(200) %dst1.0, align 8
+  ret ptr null
+}