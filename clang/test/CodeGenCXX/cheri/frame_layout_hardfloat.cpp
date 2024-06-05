cbc5ba142060195b24a8780a5a516204f5b6bcc5
Petr Vesely <petr.vesely@codasip.com>
2024-06-05 18:14:22 +0100

    [CHERI] Address review feedback

--- clang/test/CodeGenCXX/cheri/frame_layout_hardfloat.cpp
+++
@@ -1,5 +0,0 @@
-// REQUIRES: mips-registered-target
-// RUN: %cheri_purecap_cc1 -target-cpu mips4 -std=c++11 -fdeprecated-macro -fcxx-exceptions -fexceptions -o - -O0  -S %s
-// REQUIRES: asserts
-
-void a(double) {}
