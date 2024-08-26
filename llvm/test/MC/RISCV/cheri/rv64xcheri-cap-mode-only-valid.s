83e280b68025aad4d9172a4d6d4d65e475b12e62
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-08-25 22:23:09 -0700

    [CHERI-RISC-V] Add more MC tests for loads/stores
    We were not checking capability mode loads/stores, only
    compressed ones and atomics.

---
+++ llvm/test/MC/RISCV/cheri/rv64xcheri-cap-mode-only-valid.s
@@ -0,0 +1,25 @@
+# RUN: llvm-mc %s -triple=riscv64 -mattr=+xcheri,+cap-mode -riscv-no-aliases -show-encoding \
+# RUN:     | FileCheck -check-prefixes=CHECK-ASM,CHECK-ASM-AND-OBJ %s
+# RUN: llvm-mc -filetype=obj -triple riscv64 -mattr=+xcheri,+cap-mode < %s \
+# RUN:     | llvm-objdump -M no-aliases --mattr=+xcheri,+cap-mode -d - \
+# RUN:     | FileCheck -check-prefixes=CHECK-ASM-AND-OBJ %s
+
+# Capability load/store uses different encodings for RV32 vs RV64
+
+# CHECK-ASM-AND-OBJ: clc ca2, 17(ca0)
+# CHECK-ASM-SAME: encoding: [0x0f,0x26,0x15,0x01]
+clc ca2, 17(ca0)
+# CHECK-ASM-AND-OBJ: csc ca5, 25(ca3)
+# CHECK-ASM-SAME: encoding: [0xa3,0xcc,0xf6,0x00]
+csc ca5, 25(ca3)
+
+# CHECK-ASM-AND-OBJ: cld a2, 17(ca0)
+# CHECK-ASM-SAME: encoding: [0x03,0x36,0x15,0x01]
+cld a2, 17(ca0)
+# CHECK-ASM-AND-OBJ: csd a5, 25(ca3)
+# CHECK-ASM-SAME: encoding: [0xa3,0xbc,0xf6,0x00]
+csd a5, 25(ca3)
+
+# CHECK-ASM-AND-OBJ: clwu a2, 17(ca0)
+# CHECK-ASM-SAME: encoding: [0x03,0x66,0x15,0x01]
+clwu a2, 17(ca0)
