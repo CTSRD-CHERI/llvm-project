c01ae975cea7b49b18fec6aabb83ca391456f3a3
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-08-25 23:14:42 -0700

    [CHERI-RISC-V] Allow load/store mnemonics without the c prefix [2/n]
    As a follow-up to the last commit this includes the aliases without an
    immediate offset.

--- llvm/test/MC/RISCV/cheri/rv32xcheri-cap-mode-valid.s
+++ llvm/test/MC/RISCV/cheri/rv32xcheri-cap-mode-valid.s
@@ -50,3 +50,32 @@ clbu ra, 17(csp)
 # CHECK-ASM-AND-OBJ: clhu ra, 17(csp)
 # CHECK-ASM-SAME: encoding: [0x83,0x50,0x11,0x01]
 clhu ra, 17(csp)
+
+# CHECK-ASM-AND-OBJ:  clb a2, 0(ca0)
+# CHECK-ASM-SAME: encoding: [0x03,0x06,0x05,0x00]
+clb a2, (ca0)
+# CHECK-ASM-AND-OBJ: csb a5, 0(ca3)
+# CHECK-ASM-SAME: encoding: [0x23,0x80,0xf6,0x00]
+csb a5, (ca3)
+
+# CHECK-ASM-AND-OBJ: clh a2, 0(ca0)
+# CHECK-ASM-SAME: encoding: [0x03,0x16,0x05,0x00]
+clh a2, (ca0)
+# CHECK-ASM-AND-OBJ: csh a5, 0(ca3)
+# CHECK-ASM-SAME: encoding: [0x23,0x90,0xf6,0x00]
+csh a5, (ca3)
+
+# CHECK-ASM-AND-OBJ: clw ra, 0(csp)
+# CHECK-ASM-SAME: encoding: [0x83,0x20,0x01,0x00]
+clw ra, (csp)
+# CHECK-ASM-AND-OBJ: csw ra, 0(csp)
+# CHECK-ASM-SAME: encoding: [0x23,0x20,0x11,0x00]
+csw ra, (csp)
+
+# CHECK-ASM-AND-OBJ: clbu ra, 0(csp)
+# CHECK-ASM-SAME: encoding: [0x83,0x40,0x01,0x00]
+clbu ra, (csp)
+
+# CHECK-ASM-AND-OBJ: clhu ra, 0(csp)
+# CHECK-ASM-SAME: encoding: [0x83,0x50,0x01,0x00]
+clhu ra, (csp)
