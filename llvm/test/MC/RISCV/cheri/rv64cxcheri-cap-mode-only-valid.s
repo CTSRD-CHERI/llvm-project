843088b9b8d39489ab8179821162e1ff801cd20e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-08-26 23:14:50 -0700

    [CHERI-RISC-V] Allow load/store mnemonics without the c prefix [4/n]
    This adds support for compressed instructions. Does not yet include Zcb
    or floating-point loads/stores.

--- llvm/test/MC/RISCV/cheri/rv64cxcheri-cap-mode-only-valid.s
+++ llvm/test/MC/RISCV/cheri/rv64cxcheri-cap-mode-only-valid.s
@@ -8,25 +8,49 @@
 # CHECK-ASM-AND-OBJ: c.clccsp cra, 32(csp)
 # CHECK-ASM-SAME: encoding: [0x82,0x30]
 c.clccsp cra, 32(csp)
+# CHECK-ASM-AND-OBJ: c.clccsp cra, 32(csp)
+# CHECK-ASM-SAME: encoding: [0x82,0x30]
+c.lcsp cra, 32(csp)
 # CHECK-ASM-AND-OBJ-NEXT: c.csccsp cra, 256(csp)
 # CHECK-ASM-SAME: encoding: [0x06,0xa2]
 c.csccsp cra, 256(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.csccsp cra, 256(csp)
+# CHECK-ASM-SAME: encoding: [0x06,0xa2]
+c.scsp cra, 256(csp)
 # CHECK-ASM-AND-OBJ-NEXT: c.clc ca2, 16(ca0)
 # CHECK-ASM-SAME: encoding: [0x10,0x29]
 c.clc ca2, 16(ca0)
+# CHECK-ASM-AND-OBJ-NEXT: c.clc ca2, 16(ca0)
+# CHECK-ASM-SAME: encoding: [0x10,0x29]
+c.lc ca2, 16(ca0)
 # CHECK-ASM-AND-OBJ-NEXT: c.csc ca5, 128(ca3)
 # CHECK-ASM-SAME: encoding: [0xdc,0xa2]
 c.csc ca5, 128(ca3)
+# CHECK-ASM-AND-OBJ-NEXT: c.csc ca5, 128(ca3)
+# CHECK-ASM-SAME: encoding: [0xdc,0xa2]
+c.sc ca5, 128(ca3)
 ## *D operations need RV64C:
 # CHECK-ASM-AND-OBJ-NEXT: c.cldcsp ra, 40(csp)
 # CHECK-ASM-SAME: encoding: [0xa2,0x70]
 c.cldcsp ra, 40(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.cldcsp ra, 40(csp)
+# CHECK-ASM-SAME: encoding: [0xa2,0x70]
+c.ldsp ra, 40(csp)
 # CHECK-ASM-AND-OBJ-NEXT: c.csdcsp ra, 256(csp)
 # CHECK-ASM-SAME: encoding: [0x06,0xe2]
-c.csdcsp ra, 256(csp)
+c.sdsp ra, 256(csp)
+# CHECK-ASM-AND-OBJ-NEXT: c.csdcsp ra, 256(csp)
+# CHECK-ASM-SAME: encoding: [0x06,0xe2]
+c.sdsp ra, 256(csp)
 # CHECK-ASM-AND-OBJ-NEXT: c.cld a2, 16(ca0)
 # CHECK-ASM-SAME: encoding: [0x10,0x69]
 c.cld a2, 16(ca0)
+# CHECK-ASM-AND-OBJ-NEXT: c.cld a2, 16(ca0)
+# CHECK-ASM-SAME: encoding: [0x10,0x69]
+c.ld a2, 16(ca0)
 # CHECK-ASM-AND-OBJ-NEXT: c.csd a5, 128(ca3)
 # CHECK-ASM-SAME: encoding: [0xdc,0xe2]
 c.csd a5, 128(ca3)
+# CHECK-ASM-AND-OBJ-NEXT: c.csd a5, 128(ca3)
+# CHECK-ASM-SAME: encoding: [0xdc,0xe2]
+c.sd a5, 128(ca3)
