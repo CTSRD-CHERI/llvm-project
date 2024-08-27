ef56748ae882c67cc5d4bf0d5481e7d3b1a23d06
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-08-26 23:45:39 -0700

    [CHERI-RISC-V] Allow load/store mnemonics without the c prefix [5/n]
    This adds the floating point loads and stores and should conclude this
    series of patches.

--- llvm/test/MC/RISCV/cheri/rv32fdxcheri-cap-mode-valid.s
+++ llvm/test/MC/RISCV/cheri/rv32fdxcheri-cap-mode-valid.s
@@ -13,6 +13,12 @@
 # CHECK-ASM-AND-OBJ: cfsd fa0, 40(ca0)
 # CHECK-ASM-SAME: encoding: [0x27,0x34,0xa5,0x02]
 cfsd fa0, 40(ca0)
+# CHECK-ASM-AND-OBJ: cfsd fa0, 40(ca0)
+# CHECK-ASM-SAME: encoding: [0x27,0x34,0xa5,0x02]
+fsd fa0, 40(ca0)
+# CHECK-ASM-AND-OBJ: cfld fa0, 40(ca0)
+# CHECK-ASM-SAME: encoding: [0x07,0x35,0x85,0x02]
+cfld fa0, 40(ca0)
 # CHECK-ASM-AND-OBJ: cfld fa0, 40(ca0)
 # CHECK-ASM-SAME: encoding: [0x07,0x35,0x85,0x02]
 cfld fa0, 40(ca0)
@@ -20,21 +26,38 @@ cfld fa0, 40(ca0)
 # CHECK-ASM-AND-OBJ: cfsw ft1, 40(ca0)
 # CHECK-ASM-SAME: encoding: [0x27,0x24,0x15,0x02]
 cfsw ft1, 40(ca0)
+# CHECK-ASM-AND-OBJ: cfsw ft1, 40(ca0)
+# CHECK-ASM-SAME: encoding: [0x27,0x24,0x15,0x02]
+fsw ft1, 40(ca0)
 # CHECK-ASM-AND-OBJ: cflw ft1, 40(ca0)
 # CHECK-ASM-SAME: encoding: [0x87,0x20,0x85,0x02]
 cflw ft1, 40(ca0)
-
+# CHECK-ASM-AND-OBJ: cflw ft1, 40(ca0)
+# CHECK-ASM-SAME: encoding: [0x87,0x20,0x85,0x02]
+flw ft1, 40(ca0)
 
 # CHECK-ASM-AND-OBJ: cfsd fa0, 0(ca0)
 # CHECK-ASM-SAME: encoding: [0x27,0x30,0xa5,0x00]
 cfsd fa0, (ca0)
+# CHECK-ASM-AND-OBJ: cfsd fa0, 0(ca0)
+# CHECK-ASM-SAME: encoding: [0x27,0x30,0xa5,0x00]
+fsd fa0, (ca0)
 # CHECK-ASM-AND-OBJ: cfld fa0, 0(ca0)
 # CHECK-ASM-SAME: encoding: [0x07,0x35,0x05,0x00]
 cfld fa0, (ca0)
+# CHECK-ASM-AND-OBJ: cfld fa0, 0(ca0)
+# CHECK-ASM-SAME: encoding: [0x07,0x35,0x05,0x00]
+fld fa0, (ca0)
 
 # CHECK-ASM-AND-OBJ: cfsw ft1, 0(ca0)
 # CHECK-ASM-SAME: encoding: [0x27,0x20,0x15,0x00]
 cfsw ft1, (ca0)
+# CHECK-ASM-AND-OBJ: cfsw ft1, 0(ca0)
+# CHECK-ASM-SAME: encoding: [0x27,0x20,0x15,0x00]
+fsw ft1, (ca0)
 # CHECK-ASM-AND-OBJ: cflw ft1, 0(ca0)
 # CHECK-ASM-SAME: encoding: [0x87,0x20,0x05,0x00]
 cflw ft1, (ca0)
+# CHECK-ASM-AND-OBJ: cflw ft1, 0(ca0)
+# CHECK-ASM-SAME: encoding: [0x87,0x20,0x05,0x00]
+flw ft1, (ca0)
