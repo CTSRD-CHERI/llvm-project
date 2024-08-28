beaccc5639aeae43d79c18817a933f21669a971f
Jessica Clarke <jrtc27@jrtc27.com>
2024-08-28 17:11:15 +0100

    [CodeGen] Create IFUNCs in the program address space, not hard-coded 0 (#105726)
    Commit 0d527e56a5ee ("GlobalIFunc: Make ifunc respect function address
    spaces") added support for this within LLVM, but Clang does not properly
    honour the target's address spaces when creating IFUNCs, crashing with
    RAUW and verifier assertion failures when compiling C code on a target
    with a non-zero program address space, so fix this.
    
    (cherry picked from commit 73e0aa58666f235bef091735a5f189882acc1148)

--- clang/test/CodeGen/ifunc.c
+++ clang/test/CodeGen/ifunc.c
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -triple i386-unknown-linux-gnu -emit-llvm -o - %s | FileCheck %s
 // RUN: %clang_cc1 -triple i386-unknown-linux-gnu -O2 -emit-llvm -o - %s | FileCheck %s
+// RUN: %clang_cc1 -triple avr-unknown-unknown -emit-llvm -o - %s | FileCheck %s --check-prefix=AVR
 
 int foo(int) __attribute__ ((ifunc("foo_ifunc")));
 
@@ -36,6 +37,9 @@ void* goo_ifunc(void) {
 }
 // CHECK: @foo = ifunc i32 (i32), ptr @foo_ifunc
 // CHECK: @goo = ifunc void (), ptr @goo_ifunc
+
+// AVR: @foo = ifunc i16 (i16), ptr addrspace(1) @foo_ifunc
+// AVR: @goo = ifunc void (), ptr addrspace(1) @goo_ifunc
 
 // CHECK: call i32 @foo(i32
 // CHECK: call void @goo()
