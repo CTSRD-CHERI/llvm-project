CTSRD-CHERI/cheribsd

libunwind: Use APIs exposed by RTLD to unwind the trusted stack
Assembly stubs for _rtld_unw_{get,set}context are no longer needed.

Due to the significantly simplified implementation, the
LIBUNWIND_CHERI_C18N_SUPPORT option has been removed and c18n support is
now included by default for supported architectures (currently Morello
only).

contrib/subrepo-cheri-libunwind/src/UnwindCursor.hpp
@@ -86,6 +86,7 @@ extern "C" _Unwind_Reason_Code __libunwind_seh_personality(
 #include "Registers.hpp"
 #include "RWMutex.hpp"
 #include "Unwind-EHABI.h"
+#include "CompartmentInfo.hpp"
 
 namespace libunwind {
 
@@ -1293,6 +1294,10 @@ private:
                            _registers, _isSignalFrame);
   }
 #endif // defined(_LIBUNWIND_SUPPORT_TBTAB_UNWIND)
+
+  int stepThroughIfAtC18NBoundary(R &registers) {
+    return CompartmentInfo<A, R>::unwindIfAtBoundary(registers);
+  }
 
   A               &_addressSpace;
   R                _registers;
@@ -2866,6 +2871,11 @@ int UnwindCursor<A, R>::step() {
               _LIBUNWIND_ARM_EHABI
 #endif
   }
+
+  // If we are at a compartment boundary, step through it by asking RTLD to
+  // restore registers from the trusted stack.
+  if (result == UNW_STEP_SUCCESS)
+    result = stepThroughIfAtC18NBoundary(_registers);
 
   // update info based on new PC
   if (result == UNW_STEP_SUCCESS) {

CTSRD-CHERI/llvm-project

libunwind/src/UnwindCursor.hpp
@@ -86,6 +86,7 @@ extern "C" _Unwind_Reason_Code __libunwind_seh_personality(
 #include "Registers.hpp"
 #include "RWMutex.hpp"
 #include "Unwind-EHABI.h"
+#include "CompartmentInfo.hpp"
 
 namespace libunwind {
 
@@ -1293,6 +1294,10 @@ private:
                            _registers, _isSignalFrame);
   }
 #endif // defined(_LIBUNWIND_SUPPORT_TBTAB_UNWIND)
+
+  int stepThroughIfAtC18NBoundary() {
+    return CompartmentInfo<A, R>::unwindIfAtBoundary(_registers);
+  }
 
   A               &_addressSpace;
   R                _registers;
@@ -2866,6 +2871,11 @@ int UnwindCursor<A, R>::step() {
               _LIBUNWIND_ARM_EHABI
 #endif
   }
+
+  // If we are at a compartment boundary, step through it by asking RTLD to
+  // restore registers from the trusted stack.
+  if (result == UNW_STEP_SUCCESS)
+    result = stepThroughIfAtC18NBoundary();
 
   // update info based on new PC
   if (result == UNW_STEP_SUCCESS) {