ebe7fea0a5a4d13dad0582eafa2b8484795b9391
John Baldwin <jhb@FreeBSD.org>
2024-11-20 17:36:25 -0500

    lld: Implement Target::getCheriRequiredAlignment for CHERI RISC-V
    This just uses cheri-compressed-cap.h directly.  Probably the existing
    helper methods in llvm/lib/Target/RISCV/MCTargetDesc should be moved
    out to llvm/Support so they can be reused here.

--- lld/ELF/Arch/RISCV.cpp
+++ lld/ELF/Arch/RISCV.cpp
@@ -12,6 +12,7 @@
 #include "SyntheticSections.h"
 #include "Target.h"
 #include "llvm/Support/TimeProfiler.h"
+#include "llvm/CHERI/cheri-compressed-cap/cheri_compressed_cap.h"
 
 using namespace llvm;
 using namespace llvm::object;
@@ -28,6 +29,7 @@ public:
   uint32_t calcEFlags() const override;
   bool calcIsCheriAbi() const override;
   int getCapabilitySize() const override;
+  uint64_t getCheriRequiredAlignment(uint64_t len) const override;
   int64_t getImplicitAddend(const uint8_t *buf, RelType type) const override;
   void writeGotHeader(uint8_t *buf) const override;
   void writeGotPlt(Compartment *c, uint8_t *buf, const Symbol &s) const override;
@@ -139,6 +141,13 @@ static uint32_t getEFlags(InputFile *f) {
 
 int RISCV::getCapabilitySize() const {
   return config->is64 ? 16 : 8;
+}
+
+uint64_t RISCV::getCheriRequiredAlignment(uint64_t len) const {
+  if (config->is64)
+    return cc128_get_required_alignment(len);
+  else
+    return cc64_get_required_alignment(len);
 }
 
 uint32_t RISCV::calcEFlags() const {
