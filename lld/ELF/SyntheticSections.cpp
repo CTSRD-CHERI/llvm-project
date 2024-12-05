0e08cd6f140ea2b7f3a6bca7e8b5a9984d4dedc4
John Baldwin <jhb@FreeBSD.org>
2024-12-05 16:09:02 -0500

    lld: Fix a few more in.<foo> to use per-compartment sections

--- lld/ELF/SyntheticSections.cpp
+++ lld/ELF/SyntheticSections.cpp
@@ -1838,6 +1838,7 @@ void RelocationBaseSection::partitionRels() {
 
 void RelocationBaseSection::finalizeContents() {
   SymbolTableBaseSection *symTab = getPartition().dynSymTab.get();
+  Compartment *c = compartment;
 
   // When linking glibc statically, .rel{,a}.plt contains R_*_IRELATIVE
   // relocations due to IFUNC (e.g. strcpy). sh_link will be set to 0 in that
@@ -1847,7 +1848,7 @@ void RelocationBaseSection::finalizeContents() {
   else
     getParent()->link = 0;
 
-  if (in.relaPlt.get() == this && in.gotPlt->getParent()) {
+  if (relaPlt(c) == this && gotPlt(c)->getParent()) {
     getParent()->flags |= ELF::SHF_INFO_LINK;
     // For CheriABI we use the captable as the sh_info value
     if (config->isCheriAbi && in.mipsCheriCapTable &&
@@ -1855,10 +1856,10 @@ void RelocationBaseSection::finalizeContents() {
       assert(in.mipsCheriCapTable->getParent()->sectionIndex != UINT32_MAX);
       getParent()->info = in.mipsCheriCapTable->getParent()->sectionIndex;
     } else {
-      getParent()->info = in.gotPlt->getParent()->sectionIndex;
+      getParent()->info = gotPlt(c)->getParent()->sectionIndex;
     }
   }
-  if (in.relaIplt.get() == this && in.igotPlt->getParent()) {
+  if (relaIplt(c) == this && igotPlt(c)->getParent()) {
     getParent()->flags |= ELF::SHF_INFO_LINK;
     // For CheriABI we use the captable as the sh_info value
     if (config->isCheriAbi && in.mipsCheriCapTable &&
@@ -1866,7 +1867,7 @@ void RelocationBaseSection::finalizeContents() {
       assert(in.mipsCheriCapTable->getParent()->sectionIndex != UINT32_MAX);
       getParent()->info = in.mipsCheriCapTable->getParent()->sectionIndex;
     } else {
-      getParent()->info = in.igotPlt->getParent()->sectionIndex;
+      getParent()->info = igotPlt(c)->getParent()->sectionIndex;
     }
   }
   if (in.relaTgot.get() == this && in.tgot->getParent()) {
