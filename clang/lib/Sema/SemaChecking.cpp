db905a14013ab981bce153079c3904954886f905
Petr Vesely <petr.vesely@codasip.com>
2024-12-09 14:49:48 +0100

    Revert "[CHERI] Fix builtin overflow for uintcap types"
    This reverts commit 43fe4b8390c985c23ec53aa7f9e79551d61f7e6b.

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -457,7 +457,7 @@ static bool SemaBuiltinOverflow(Sema &S, CallExpr *TheCall,
     TheCall->setArg(I, Arg.get());
 
     QualType Ty = Arg.get()->getType();
-    if (!Ty->isIntegerType() || Ty->isIntCapType()) {
+    if (!Ty->isIntegerType()) {
       S.Diag(Arg.get()->getBeginLoc(), diag::err_overflow_builtin_must_be_int)
           << Ty << Arg.get()->getSourceRange();
       return true;
