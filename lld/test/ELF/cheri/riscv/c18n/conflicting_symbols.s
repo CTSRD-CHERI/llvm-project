ea6a7a4e652b1b3b9bc1e26c9f55975d8fe12e17
John Baldwin <jhb@FreeBSD.org>
2025-03-04 16:51:36 -0500

    lld: Add missing spaces to some error messages

--- lld/test/ELF/cheri/riscv/c18n/conflicting_symbols.s
+++ lld/test/ELF/cheri/riscv/c18n/conflicting_symbols.s
@@ -4,7 +4,7 @@
 # RUN: %riscv64_cheri_purecap_llvm-mc -filetype=obj %t/one.s -o %t/one.o
 # RUN: not ld.lld --shared --verbose-c18n --compartment-policy=%t/compartments.json %t/one.o -o %t/conflicting_symbols.so.0 2>&1 | FileCheck %s
 
-# CHECK: one.o:.textassigned to compartment one by symbol foo and compartment two by symbol bar
+# CHECK: one.o:.text assigned to compartment one by symbol foo and compartment two by symbol bar
 
 #--- one.s
 
