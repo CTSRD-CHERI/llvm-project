veselypeta/cherillvm

[CHERI] Support Cheri RISCV Lazy Binding

lld/ELF/Arch/Cheri.cpp
@@ -767,7 +767,8 @@ uint32_t CheriCapTableSection::getIndex(const Symbol &sym,
           Twine(Entries.FirstIndex) + ": " +
           Twine(*it->second.Index - Entries.FirstIndex));
 #endif
-  return *it->second.index - entries.firstIndex;
+  return (*it->second.index - entries.firstIndex) +
+         (in.plt->isNeeded() ? target->cheriCapTableHeaderEntriesNum : 0);
 }
 
 uint32_t CheriCapTableSection::getDynTlsOffset(const Symbol &sym) const {
@@ -845,6 +846,9 @@ uint64_t CheriCapTableSection::assignIndices(uint64_t startIndex,
 
     uint32_t index = *cti.index;
     assert(index >= startIndex && index < startIndex + entries.size());
+    if (in.plt->isNeeded()) {
+      assert(index >= target->cheriCapTableHeaderEntriesNum);
+    }
     Symbol *targetSym = it.first;
 
     StringRef name = targetSym->getName();
@@ -913,7 +917,9 @@ uint64_t CheriCapTableSection::assignIndices(uint64_t startIndex,
 template <class ELFT>
 void CheriCapTableSection::assignValuesAndAddCapTableSymbols() {
   // First assign the global indices (which will usually be the only ones)
-  uint64_t assignedEntries = assignIndices<ELFT>(0, globalEntries, "");
+  uint64_t assignedEntries =
+      partitionGlobalEntries() ? target->cheriCapTableHeaderEntriesNum : 0;
+  assignedEntries += assignIndices<ELFT>(assignedEntries, globalEntries, "");
   if (LLVM_UNLIKELY(config->capTableScope != CapTableScopePolicy::All)) {
     assert(assignedEntries == 0 && "Should not have any global entries in"
                                    " per-file/per-function captable mode");
@@ -997,6 +1003,32 @@ void CheriCapTableSection::assignValuesAndAddCapTableSymbols() {
   }
 
   valuesAssigned = true;
+}
+
+bool CheriCapTableSection::partitionGlobalEntries() {
+  assert(!valuesAssigned && "Values should not be assigned yet");
+  bool containsPltSymbols = false;
+  if (config->emachine != EM_RISCV)
+    return containsPltSymbols;
+  CaptableMap globalMapCopy = globalEntries;
+  globalEntries.map.clear();
+  for (const auto &it : globalMapCopy.map) {
+    if (!it.first->isInPlt())
+      continue;
+    auto inserted = globalEntries.map.insert(it);
+    (void)inserted;
+    assert(inserted.second && "Entry should not have already been inserted.");
+    containsPltSymbols = true;
+  }
+
+  for (const auto &it : globalMapCopy.map) {
+    if (it.first->isInPlt())
+      continue;
+    auto inserted = globalEntries.map.insert(it);
+    (void)inserted;
+    assert(inserted.second && "Entry should not have already been inserted.");
+  }
+  return containsPltSymbols;
 }
 
 template void

CTSRD-CHERI/llvm-project

lld/ELF/Arch/Cheri.cpp
@@ -906,6 +906,9 @@ uint64_t MipsCheriCapTableSection::assignIndices(uint64_t startIndex,
 
     uint32_t index = *cti.index;
     assert(index >= startIndex && index < startIndex + entries.size());
+    if (in.plt->isNeeded()) {
+      assert(index >= target->cheriCapTableHeaderEntriesNum);
+    }
     Symbol *targetSym = it.first;
 
     StringRef name = targetSym->getName();
@@ -974,7 +977,9 @@ uint64_t MipsCheriCapTableSection::assignIndices(uint64_t startIndex,
 template <class ELFT>
 void MipsCheriCapTableSection::assignValuesAndAddCapTableSymbols() {
   // First assign the global indices (which will usually be the only ones)
-  uint64_t assignedEntries = assignIndices<ELFT>(0, globalEntries, "");
+  uint64_t assignedEntries =
+      partitionGlobalEntries() ? target->cheriCapTableHeaderEntriesNum : 0;
+  assignedEntries += assignIndices<ELFT>(assignedEntries, globalEntries, "");
   if (LLVM_UNLIKELY(config->capTableScope != CapTableScopePolicy::All)) {
     assert(assignedEntries == 0 && "Should not have any global entries in"
                                    " per-file/per-function captable mode");
@@ -1057,6 +1062,32 @@ void MipsCheriCapTableSection::assignValuesAndAddCapTableSymbols() {
   }
 
   valuesAssigned = true;
+}
+
+bool MipsCheriCapTableSection::partitionGlobalEntries() {
+  assert(!valuesAssigned && "Values should not be assigned yet");
+  bool containsPltSymbols = false;
+  if (config->emachine != EM_RISCV)
+    return containsPltSymbols;
+  CaptableMap globalMapCopy = globalEntries;
+  globalEntries.map.clear();
+  for (const auto &it : globalMapCopy.map) {
+    if (!it.first->isInPlt())
+      continue;
+    auto inserted = globalEntries.map.insert(it);
+    (void)inserted;
+    assert(inserted.second && "Entry should not have already been inserted.");
+    containsPltSymbols = true;
+  }
+
+  for (const auto &it : globalMapCopy.map) {
+    if (it.first->isInPlt())
+      continue;
+    auto inserted = globalEntries.map.insert(it);
+    (void)inserted;
+    assert(inserted.second && "Entry should not have already been inserted.");
+  }
+  return containsPltSymbols;
 }
 
 template void