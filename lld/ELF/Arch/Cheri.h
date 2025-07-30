CTSRD-CHERI/llvm-project

commit 950bd6958008d636998893251c075279dab9bf93

[NFCI][ELF][CHERI] Teach addSymbolReloc to handle capabilities and use

lld/ELF/Arch/Cheri.h
@@ -305,6 +305,10 @@ void addRelativeCapabilityRelocation(
     llvm::PointerUnion<Symbol *, InputSectionBase *> symOrSec, int64_t addend,
     RelExpr expr, RelType type);
 
+void addSymbolCapabilityRelocation(RelType dynType, RelocationBaseSection &rel,
+                                   InputSectionBase &isec, uint64_t offsetInSec,
+                                   Symbol &sym, int64_t addend = 0);
+
 void addCapabilityRelocation(
     llvm::PointerUnion<Symbol *, InputSectionBase *> target, RelType type,
     InputSectionBase *sec, uint64_t offset, RelExpr expr, int64_t addend,

lld/ELF/Arch/Cheri.cpp
@@ -950,14 +950,6 @@ void addCapabilityRelocation(
     return;
   }
 
-  if (sym->isFunc() && addend != 0)
-    warn("capability relocation with non-zero addend (0x" +
-         llvm::utohexstr(addend) + ") against preemptible function " +
-         toString(*sym) + "; this may not be supported by the runtime linker" +
-         getLocationMessage(*sec, *sym, offset));
-
-  if (!dynRelSec)
-    dynRelSec = mainPart->relaDyn.get();
   if (needTrampoline && config->verboseCapRelocs)
     message("Using trampoline for function pointer against " +
             verboseToString(sym));
@@ -972,12 +964,9 @@ void addCapabilityRelocation(
     assert(newSym->visibility() == llvm::ELF::STV_HIDDEN);
     sym = newSym; // Make the relocation point to the newly added symbol
   }
-  // .chericap initialises the memory to 0xcacacaca not 0, so if writing
-  // addends we still need to write even it if zero.
-  // TODO: Stop doing this in the assembler and drop this hack
-  dynRelSec->addReloc(DynamicReloc::AgainstSymbol, type, *sec, offset, *sym,
-                      addend, R_ADDEND,
-                      /*addendRelType=*/type, /*writeZero=*/true);
+  if (!dynRelSec)
+    dynRelSec = mainPart->relaDyn.get();
+  dynRelSec->addSymbolReloc(type, *sec, offset, *sym, addend, type);
 }
 
 void addNullDerivedCapability(Symbol &sym, InputSectionBase &sec,

lld/ELF/SyntheticSections.cpp
@@ -1690,8 +1690,24 @@ RelocationBaseSection::RelocationBaseSection(StringRef name, uint32_t type,
 void RelocationBaseSection::addSymbolReloc(
     RelType dynType, InputSectionBase &isec, uint64_t offsetInSec, Symbol &sym,
     int64_t addend, std::optional<RelType> addendRelType) {
+  bool isCap =
+      dynType == target->cheriCapRel || dynType == target->cheriCapCallRel;
+  if (isCap && sym.isFunc() && addend != 0)
+    warn("capability relocation with non-zero addend (0x" +
+         llvm::utohexstr(addend) + ") against preemptible function " +
+         toString(sym) + "; this may not be supported by the runtime linker" +
+         getLocationMessage(isec, sym, offsetInSec));
+
+  // .chericap initialises the memory to 0xcacacaca not 0, so if writing
+  // addends we still need to write even it if zero (and must have an
+  // addendRelType in order to do so).
+  // TODO: Stop doing this in the assembler and drop this hack
+  bool writeZero = isCap;
+  if (writeZero && !addendRelType)
+    addendRelType = dynType;
   addReloc(DynamicReloc::AgainstSymbol, dynType, isec, offsetInSec, sym, addend,
-           R_ADDEND, addendRelType ? *addendRelType : target->noneRel);
+           R_ADDEND, addendRelType ? *addendRelType : target->noneRel,
+           writeZero);
 }
 
 void RelocationBaseSection::addAddendOnlyRelocIfNonPreemptible(

commit c73ddb704361cf094394dfc599612b215f5a5499

[NFC][ELF][CHERI] Remove stray prototype
Left over from a previous version of the commit.

Fixes: 950bd6958008 ("[NFCI][ELF][CHERI] Teach addSymbolReloc to handle capabilities and use")

lld/ELF/Arch/Cheri.h
@@ -305,10 +305,6 @@ void addRelativeCapabilityRelocation(
     InputSectionBase &isec, uint64_t offsetInSec,
     llvm::PointerUnion<Symbol *, InputSectionBase *> symOrSec, int64_t addend,
     RelExpr expr, RelType type);
-
-void addSymbolCapabilityRelocation(RelType dynType, RelocationBaseSection &rel,
-                                   InputSectionBase &isec, uint64_t offsetInSec,
-                                   Symbol &sym, int64_t addend = 0);
 } // namespace elf
 } // namespace lld
 