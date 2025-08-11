fd8bb84e2a6cfad64e3f68c99109531e3147ad7d
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2025-08-11 14:26:39 +0100

    libunwind: Unwind across c18n compartment boundaries on RISC-V

--- libunwind/src/CompartmentInfo.hpp
+++ libunwind/src/CompartmentInfo.hpp
@@ -26,8 +26,26 @@ struct CompartmentInfo {
 
   static int unwindIfAtBoundary(R &registers) {
 #ifdef _LIBUNWIND_HAS_CHERI_LIB_C18N
-#ifdef _LIBUNWIND_TARGET_AARCH64
     struct dl_c18n_compart_state state;
+
+#ifdef _LIBUNWIND_TARGET_AARCH64
+    static constexpr int fp_num = UNW_AARCH64_FP;
+    static constexpr int callee_saved[] = {
+      UNW_ARM64_C19, UNW_ARM64_C20, UNW_ARM64_C21, UNW_ARM64_C22, UNW_ARM64_C23,
+      UNW_ARM64_C24, UNW_ARM64_C26, UNW_ARM64_C27, UNW_ARM64_C28, UNW_ARM64_C29
+    };
+#elif defined(_LIBUNWIND_TARGET_RISCV)
+    static constexpr int fp_num = UNW_RISCV_X8;
+    static constexpr int callee_saved[] = {
+      UNW_RISCV_X9, UNW_RISCV_X18, UNW_RISCV_X19, UNW_RISCV_X20, UNW_RISCV_X21,
+      UNW_RISCV_X22, UNW_RISCV_X23, UNW_RISCV_X24, UNW_RISCV_X25, UNW_RISCV_X26,
+      UNW_RISCV_X27
+    };
+#endif
+    static_assert(sizeof(callee_saved) / sizeof(*callee_saved) ==
+                  sizeof(state.regs) / sizeof(*state.regs),
+                  "unexpected number of saved registers");
+
     pint_t pc = registers.getIP();
     pint_t tf = registers.getTrustedStack();
 
@@ -41,7 +59,7 @@ struct CompartmentInfo {
     registers.setTrustedStack(tf);
     CHERI_DBG("C18N: SET TRUSTED STACK %#p\n", (void *)tf);
 
-    registers.setFP((pint_t)state.fp);
+    registers.setCapabilityRegister(fp_num, (pint_t)state.fp);
     CHERI_DBG("C18N: SET FP %#p\n", state.fp);
 
     registers.setSP((pint_t)state.sp);
@@ -51,13 +69,12 @@ struct CompartmentInfo {
     CHERI_DBG("C18N: SET IP: %#p\n", state.pc);
 
     for (size_t i = 0; i < sizeof(state.regs) / sizeof(*state.regs); ++i) {
-      registers.setCapabilityRegister(UNW_ARM64_C19 + i, (pint_t)state.regs[i]);
-      CHERI_DBG("C18N: SET REGISTER: %lu (%s): %#p\n",
-                UNW_ARM64_C19 + i,
-                registers.getRegisterName(UNW_ARM64_C19 + i),
+      registers.setCapabilityRegister(callee_saved[i], (pint_t)state.regs[i]);
+      CHERI_DBG("C18N: SET REGISTER: %d (%s): %#p\n",
+                callee_saved[i],
+                registers.getRegisterName(callee_saved[i]),
                 state.regs[i]);
     }
-#endif
 #endif // _LIBUNWIND_HAS_CHERI_LIB_C18N
     return UNW_STEP_SUCCESS;
   }
