2404e6d21dc916de657cb636486eb6221e2158ff
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-15 01:39:21 +0100

    [Transforms] Move createLogCheriSetBoundsPass to Transforms/Utils.h
    This lets us drop dependencies on any CodeGen headers. Also add a
    LogCheriSetBoundsID like the other passes.

--- llvm/include/llvm/CodeGen/Passes.h
+++ llvm/include/llvm/CodeGen/Passes.h
@@ -495,9 +495,6 @@ namespace llvm {
   /// This pass frees the memory occupied by the MachineFunction.
   FunctionPass *createFreeMachineFunctionPass();
 
-  /// This pass logs information on generated CSetBounds calls
-  FunctionPass *createLogCheriSetBoundsPass();
-
   /// This pass performs outlining on machine instructions directly before
   /// printing assembly.
   ModulePass *createMachineOutlinerPass(bool RunOnAllFunctions = true);
