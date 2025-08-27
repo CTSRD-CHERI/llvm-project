CTSRD-CHERI/llvm-project

commit 2d52aa89a7bed25c75e6b2e82061f9641e11bf3c

[ELF][RISCV] Support R_RISCV_CHERI_CAPABILITY_CODE and R_RISCV_FUNC_RELATIVE
Note that we only use R_RISCV_FUNC_RELATIVE for CheriABI binaries as
it's redundant for plain RISC-V and we would not be able to produce
binaries that work with existing upstream RISC-V runtimes.

lld/ELF/Arch/RISCV.cpp
@@ -110,8 +110,11 @@ RISCV::RISCV() {
   copyRel = R_RISCV_COPY;
   pltRel = R_RISCV_JUMP_SLOT;
   relativeRel = R_RISCV_RELATIVE;
+  if (config->isCheriAbi)
+    relativeFuncRel = R_RISCV_FUNC_RELATIVE;
   iRelativeRel = R_RISCV_IRELATIVE;
   symbolicCapRel = R_RISCV_CHERI_CAPABILITY;
+  symbolicCodeCapRel = R_RISCV_CHERI_CAPABILITY_CODE;
   if (config->is64) {
     symbolicRel = R_RISCV_64;
     tlsModuleIndexRel = R_RISCV_TLS_DTPMOD64;
@@ -353,6 +356,7 @@ RelExpr RISCV::getRelExpr(const RelType type, const Symbol &s,
   case R_RISCV_RELAX:
     return config->relax ? R_RELAX_HINT : R_NONE;
   case R_RISCV_CHERI_CAPABILITY:
+  case R_RISCV_CHERI_CAPABILITY_CODE:
     return R_ABS_CAP;
   // TODO: Deprecate and eventually remove these
   case R_RISCV_CHERI_CAPTAB_PCREL_HI20:
@@ -576,6 +580,7 @@ void RISCV::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
     return; // Ignored (for now)
 
   case R_RISCV_CHERI_CAPABILITY:
+  case R_RISCV_CHERI_CAPABILITY_CODE:
     // Write a word within the capability
     if (config->is64)
       write64le(loc, val);

lld/ELF/Arch/Cheri.cpp
@@ -178,7 +178,7 @@ std::string CheriCapRelocLocation::toString() const {
  return SymbolAndOffset(section, offset).verboseToString();
 }
 
-void CheriCapRelocsSection::addCapReloc(CheriCapRelocLocation loc,
+void CheriCapRelocsSection::addCapReloc(bool isCode, CheriCapRelocLocation loc,
                                         const SymbolAndOffset &target,
                                         int64_t capabilityOffset,
                                         Symbol *sourceSymbol) {
@@ -210,7 +210,7 @@ void CheriCapRelocsSection::addCapReloc(CheriCapRelocLocation loc,
     return;
   }
 
-  addEntry(loc, {target, capabilityOffset});
+  addEntry(loc, {isCode, target, capabilityOffset});
 }
 
 static uint64_t getTargetSize(const CheriCapRelocLocation &location,
@@ -317,6 +317,7 @@ template <class ELFT> struct CapRelocPermission {
   static const uint64_t function = permissionBit(1);
   static const uint64_t readOnly = permissionBit(2);
   static const uint64_t indirect = permissionBit(3);
+  static const uint64_t code     = permissionBit(4);
   // clang-format on
 };
 
@@ -349,7 +350,7 @@ void CheriCapRelocsSection::writeToImpl(uint8_t *buf) {
 
     // The target VA is the base address of the capability, so symbol + 0
     uint64_t targetVA;
-    bool isFunc, isGnuIFunc, isTls;
+    bool isFunc, isGnuIFunc, isTls, isCode = reloc.isCode;
     OutputSection *os;
     if (Symbol *s = dyn_cast<Symbol *>(realTarget.symOrSec)) {
       targetVA = realTarget.sym()->getVA(0);
@@ -365,6 +366,10 @@ void CheriCapRelocsSection::writeToImpl(uint8_t *buf) {
       isTls = isec->type == STT_TLS;
       os = isec->getOutputSection();
     }
+    if (isCode && !isFunc)
+      errorOrWarn("code relocation against non-function symbol " +
+                  realTarget.verboseToString() + "\n>>> referenced by " +
+                  location.toString());
     uint64_t targetSize = getTargetSize(location, realTarget);
     uint64_t targetOffset = reloc.capabilityOffset + realTarget.offset;
     uint64_t permissions = 0;
@@ -373,6 +378,8 @@ void CheriCapRelocsSection::writeToImpl(uint8_t *buf) {
       permissions |= CapRelocPermission<ELFT>::function;
       if (isGnuIFunc)
         permissions |= CapRelocPermission<ELFT>::indirect;
+      if (isCode)
+        permissions |= CapRelocPermission<ELFT>::code;
     } else if (os) {
       assert(!isTls);
       // if ((OS->getPhdrFlags() & PF_W) == 0) {
@@ -937,10 +944,12 @@ void addRelativeCapabilityRelocation(
                                       type);
     return;
   }
+  bool isCode = type == target->symbolicCodeCapRel;
   assert(!sym || !sym->isPreemptible);
   assert(!config->useRelativeElfCheriRelocs &&
          "relative ELF capability relocations not currently implemented");
-  in.capRelocs->addCapReloc({&isec, offsetInSec}, {symOrSec, 0u}, addend);
+  in.capRelocs->addCapReloc(isCode, {&isec, offsetInSec}, {symOrSec, 0u},
+                            addend);
 }
 
 } // namespace elf

lld/ELF/Arch/Cheri.h
@@ -60,12 +60,18 @@ struct CheriCapRelocLocation {
 };
 
 struct CheriCapReloc {
+  // Indicates this is a relocation where the symbol is a function symbol but
+  // we do not want any interposition, as this reference is for a specific
+  // instruction within the function. Normal references to functions should not
+  // set this.
+  bool isCode;
   // We can't use a plain Symbol* here as capabilities to string constants
   // will be e.g. `.rodata.str + 0x90` -> need to store offset as well
   SymbolAndOffset target;
   int64_t capabilityOffset;
   bool operator==(const CheriCapReloc &other) const {
-    return target == other.target && capabilityOffset == other.capabilityOffset;
+    return isCode == other.isCode && target == other.target &&
+           capabilityOffset == other.capabilityOffset;
   }
 };
 
@@ -75,8 +81,9 @@ public:
   bool isNeeded() const override { return !relocsMap.empty(); }
   size_t getSize() const override { return relocsMap.size() * entsize; }
   void writeTo(uint8_t *buf) override;
-  void addCapReloc(CheriCapRelocLocation loc, const SymbolAndOffset &target,
-                   int64_t capabilityOffset, Symbol *sourceSymbol = nullptr);
+  void addCapReloc(bool isCode, CheriCapRelocLocation loc,
+                   const SymbolAndOffset &target, int64_t capabilityOffset,
+                   Symbol *sourceSymbol = nullptr);
 
 private:
   template <class ELFT> void writeToImpl(uint8_t *);
@@ -88,8 +95,10 @@ private:
             " does not match existing one:\n>   Existing: " +
             it.first->second.target.verboseToString() +
             ", cap offset=" + Twine(it.first->second.capabilityOffset) +
+            ", is code=" + Twine(it.first->second.isCode) +
             "\n>   New:     " + relocation.target.verboseToString() +
-            ", cap offset=" + Twine(relocation.capabilityOffset));
+            ", cap offset=" + Twine(relocation.capabilityOffset) +
+            ", is code=" + Twine(relocation.isCode));
     }
     return it.second;
   }

lld/ELF/Relocations.cpp
@@ -903,8 +903,11 @@ static void addRelativeReloc(InputSectionBase &isec, uint64_t offsetInSec,
       part.relrDyn->relocs.push_back({&isec, offsetInSec});
     return;
   }
-  part.relaDyn->addRelativeReloc<shard>(target->relativeRel, isec, offsetInSec,
-                                        sym, addend, type, expr);
+  RelType relativeType = target->relativeRel;
+  if (target->relativeFuncRel && sym.isFunc())
+    relativeType = *target->relativeFuncRel;
+  part.relaDyn->addRelativeReloc<shard>(relativeType, isec, offsetInSec, sym,
+                                        addend, type, expr);
 }
 
 template <class PltSection, class GotPltSection>
@@ -921,7 +924,7 @@ static void addPltEntry(PltSection &plt, GotPltSection &gotPlt,
     }
 
     addRelativeCapabilityRelocation(gotPlt, sym.getGotPltOffset(), &plt, 0,
-                                    R_ABS_CAP, *target->symbolicCapRel);
+                                    R_ABS_CAP, *target->symbolicCodeCapRel);
   }
 
   rel.addReloc({type, &gotPlt, sym.getGotPltOffset(),
@@ -1167,7 +1170,8 @@ void RelocationScanner::processAux(RelExpr expr, RelType type, uint64_t offset,
   if (canWrite) {
     RelType rel = target->getDynRel(type);
     if (oneof<R_GOT, R_LOONGARCH_GOT>(expr) ||
-        ((rel == target->symbolicRel || rel == target->symbolicCapRel) &&
+        ((rel == target->symbolicRel || rel == target->symbolicCapRel ||
+          type == target->symbolicCodeCapRel) &&
          !sym.isPreemptible)) {
       addRelativeReloc<true>(*sec, offset, sym, addend, expr, type);
       return;
@@ -1197,6 +1201,11 @@ void RelocationScanner::processAux(RelExpr expr, RelType type, uint64_t offset,
       // behaviour.
       if (config->emachine == EM_MIPS && expr != R_ABS_CAP)
         in.mipsGot->addEntry(*sec->file, sym, addend, expr);
+      return;
+    } else if (type == target->symbolicCodeCapRel) {
+      errorOrWarn("relocation " + toString(type) +
+                  " cannot be used against preemptible symbol '" +
+                  toString(sym) + "'" + getLocation(*sec, sym, offset));
       return;
     }
   }

lld/ELF/SyntheticSections.cpp
@@ -1735,9 +1735,11 @@ void RelocationBaseSection::partitionRels() {
   if (!combreloc)
     return;
   const RelType relativeRel = target->relativeRel;
-  numRelativeRelocs =
-      llvm::partition(relocs, [=](auto &r) { return r.type == relativeRel; }) -
-      relocs.begin();
+  const std::optional<RelType> relativeFuncRel = target->relativeFuncRel;
+  const auto *firstNonRelativeReloc = llvm::partition(relocs, [=](auto &r) {
+    return r.type == relativeRel || r.type == relativeFuncRel;
+  });
+  numRelativeRelocs = firstNonRelativeReloc - relocs.begin();
 }
 
 void RelocationBaseSection::finalizeContents() {

lld/ELF/Target.h
@@ -124,6 +124,7 @@ public:
   RelType gotRel;
   RelType pltRel;
   RelType relativeRel;
+  std::optional<RelType> relativeFuncRel;
   RelType iRelativeRel;
   RelType symbolicRel;
   RelType tlsDescRel;
@@ -134,6 +135,7 @@ public:
   std::optional<RelType> sizeRel;
   std::optional<RelType> symbolicCapRel;
   std::optional<RelType> symbolicCapCallRel;
+  std::optional<RelType> symbolicCodeCapRel;
   unsigned gotEntrySize = config->wordsize;
   unsigned pltEntrySize;
   unsigned pltHeaderSize;

lld/test/ELF/cheri/exception-table.ll
@@ -105,9 +105,9 @@
 ; RV64-RELOCS-NEXT:   0x002{{.+}} Base: 0x[[#%x,TEST2_ADDR]] (.L_Z5test2ll$local+72) Length: 124 Perms: Function
 ; Next one references the local symbol, and uses that length rather than the override:
 ; RV64-RELOCS-NEXT:   0x002{{.+}} Base: 0x[[#%x,TEST_WEAK_ADDR]] (.L_Z9test_weakll$local+28) Length: 52 Perms: Function
-; RV64-RELOCS-NEXT:   0x003{{.+}} Base: 0x[[#%x,PLT0_ADDR:]] (<unknown symbol>+0) Length: 80 Perms: Function
-; RV64-RELOCS-NEXT:   0x003{{.+}} Base: 0x[[#%x,PLT0_ADDR]] (<unknown symbol>+0) Length: 80 Perms: Function
-; RV64-RELOCS-NEXT:   0x003{{.+}} Base: 0x[[#%x,PLT0_ADDR]] (<unknown symbol>+0) Length: 80 Perms: Function
+; RV64-RELOCS-NEXT:   0x003{{.+}} Base: 0x[[#%x,PLT0_ADDR:]] (<unknown symbol>+0) Length: 80 Perms: Code
+; RV64-RELOCS-NEXT:   0x003{{.+}} Base: 0x[[#%x,PLT0_ADDR]] (<unknown symbol>+0) Length: 80 Perms: Code
+; RV64-RELOCS-NEXT:   0x003{{.+}} Base: 0x[[#%x,PLT0_ADDR]] (<unknown symbol>+0) Length: 80 Perms: Code
 ; RV64-RELOCS-NEXT: ]
 
 ; IR was generated from the following code:

lld/test/ELF/cheri/riscv/code-func-reloc.s
@@ -1,0 +1,30 @@
+# REQUIRES: riscv
+
+# RUN: %riscv64_cheri_purecap_llvm-mc -filetype=obj %s -o %t.o
+# RUN: ld.lld -shared -z separate-code %t.o -o %t.so
+# RUN: llvm-readobj -r --cap-relocs %t.so | FileCheck %s
+
+# CHECK:      .rela.dyn {
+# CHECK-NEXT:   0x3100 R_RISCV_FUNC_RELATIVE - 0x1000
+# CHECK-NEXT:   0x3108 R_RISCV_RELATIVE - 0x30D0
+# CHECK-NEXT: }
+# CHECK:      CHERI __cap_relocs [
+# CHECK-NEXT:   0x0030d0 (data) Base: 0x1000 (func+0) Length: 4 Perms: Function
+# CHECK-NEXT:   0x0030e0 Base: 0x1000 (func+0) Length: 4 Perms: Code
+# CHECK-NEXT:   0x0030f0 Base: 0x30d0 (data+0) Length: 64 Perms: Object
+# CHECK-NEXT: ]
+
+  .type func, @function
+func:
+  ret
+  .size func, . - func
+
+  .data
+  .type data, @object
+data:
+  .chericap func
+  .chericap %code(func)
+  .chericap data
+  .quad func
+  .quad data
+  .size data, . - data

commit 05c88511ae7c60ec12fc2e15dce91eba6783db04

[ELF][RISCV] Handle R_RISCV_FUNC_RELATIVE in getImplicitAddend
Otherwise linking with --apply-dynamic-relocs (as is done by U-Boot, or
due to -z rel which we don't support) when --check-dynamic-relocs is
enabled (which is the default in assertions builds) will give an
internal linker error when such a relocation is emitted.

Fixes: 2d52aa89a7be ("[ELF][RISCV] Support R_RISCV_CHERI_CAPABILITY_CODE and R_RISCV_FUNC_RELATIVE")

lld/ELF/Arch/RISCV.cpp
@@ -208,6 +208,7 @@ int64_t RISCV::getImplicitAddend(const uint8_t *buf, RelType type) const {
     return read64le(buf);
   case R_RISCV_RELATIVE:
   case R_RISCV_IRELATIVE:
+  case R_RISCV_FUNC_RELATIVE:
     return config->is64 ? read64le(buf) : read32le(buf);
   case R_RISCV_NONE:
   case R_RISCV_JUMP_SLOT: