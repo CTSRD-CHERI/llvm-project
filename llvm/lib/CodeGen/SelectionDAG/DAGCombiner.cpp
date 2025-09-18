CTSRD-CHERI/llvm-project

commit 05fd67ad059fddec929b0bd0f95a29be7dfd498c

[CodeGen][CHERI-Generic] Support commuting capability offsets when safe
If we know that both offsets are non-negative, or both are negative
(really non-positive but that is awkward to query in SelectionDAG as
most things are based on KnownBits), then we can commute the offsets as
we don't need to worry about introducing transient unrepresentability.

llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -2658,6 +2658,12 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
   if (isNullConstant(N1))
     return N0;
 
+  auto GetPTRADD2 = [this, &DL](SDValue &Base, SDValue &Off1, SDValue &Off2) {
+    SDValue Base2 = DAG.getPointerAdd(DL, Base, Off1);
+    AddToWorklist(Base2.getNode());
+    return DAG.getPointerAdd(DL, Base2, Off2);
+  };
+
   // Reassociate: (ptradd (ptradd x, y), z) -> (ptradd x, (add y, z)) if:
   //   * x is a null pointer; or
   //   * the add can be constant-folded; or
@@ -2670,6 +2676,14 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
   // patterns. Once we represent that with PTRMASK that will be less of a
   // concern, though we might still want to detect code not using the builtins
   // and canonicalise it to a PTRMASK.
+  //
+  // Commute: (ptradd (ptradd x, y), z) -> (ptradd (ptradd x, z), y) if:
+  //   * y and z have the same sign and y is a constant.
+  //
+  // This allows immediate addressing modes to be used. Note that we need to be
+  // careful to ensure we don't transiently become unrepresentable if the
+  // original DAG does not already do so, and this is the case if both PTRADDs
+  // have the same sign.
   if (N0.getOpcode() == ISD::PTRADD &&
       !reassociationCanBreakAddressingModePattern(ISD::PTRADD, DL, N, N0, N1)) {
     SDValue X = N0.getOperand(0);
@@ -2700,6 +2714,27 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
         (N0.hasOneUse() && Z.hasOneUse() &&
          !DAG.isConstantIntBuildVectorOrConstantInt(Z)))
       return DAG.getPointerAdd(DL, X, Add);
+    if (DAG.SignBitIsSame(Y, Z) && DAG.isConstantIntBuildVectorOrConstantInt(Y))
+      return GetPTRADD2(X, Z, Y);
+  }
+
+  // Transform: (ptradd x, (add y, z)) -> (ptradd (ptradd x, y), z) if:
+  //   * both y and z have the same sign and z is a constant.
+  //
+  // Transform: (ptradd x, (add y, z)) -> (ptradd (ptradd x, z), y) if:
+  //   * both y and z have the same sign and y is a constant.
+  //
+  // As above, this allows for immediate addressing modes.
+  if (N1.getOpcode() == ISD::ADD) {
+    SDValue X = N0;
+    SDValue Y = N1.getOperand(0);
+    SDValue Z = N1.getOperand(1);
+    if (DAG.SignBitIsSame(Y, Z)) {
+      if (DAG.isConstantIntBuildVectorOrConstantInt(Y))
+        return GetPTRADD2(X, Z, Y);
+      if (DAG.isConstantIntBuildVectorOrConstantInt(Z))
+        return GetPTRADD2(X, Y, Z);
+    }
   }
 
   return SDValue();

llvm/include/llvm/CodeGen/SelectionDAG.h
@@ -1983,6 +1983,14 @@ public:
   /// We use this predicate to simplify operations downstream.
   bool SignBitIsZero(SDValue Op, unsigned Depth = 0) const;
 
+  /// Return true if the sign bit of Op is known to be one.
+  /// We use this predicate to simplify operations downstream.
+  bool SignBitIsOne(SDValue Op, unsigned Depth = 0) const;
+
+  /// Return true if the sign bits of N0 and N1 are known to be the same.
+  /// We use this predicate to simplify operations downstream.
+  bool SignBitIsSame(SDValue N0, SDValue N1, unsigned Depth = 0) const;
+
   /// Return true if 'Op & Mask' is known to be zero.  We
   /// use this predicate to simplify operations downstream.  Op and Mask are
   /// known to be the same type.

llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -2630,6 +2630,20 @@ bool SelectionDAG::SignBitIsZero(SDValue Op, unsigned Depth) const {
   return MaskedValueIsZero(Op, APInt::getSignMask(BitWidth), Depth);
 }
 
+/// SignBitIsOne - Return true if the sign bit of Op is known to be one.  We
+/// use this predicate to simplify operations downstream.
+bool SelectionDAG::SignBitIsOne(SDValue Op, unsigned Depth) const {
+  unsigned BitWidth = Op.getScalarValueSizeInBits();
+  return MaskedValueIsAllOnes(Op, APInt::getSignMask(BitWidth), Depth);
+}
+
+/// SignBitIsSame - Return true if the sign bits of N0 and N1 are known to be
+/// the same.  We use this predicate to simplify operations downstream.
+bool SelectionDAG::SignBitIsSame(SDValue N0, SDValue N1, unsigned Depth) const {
+  return (SignBitIsZero(N0, Depth) && SignBitIsZero(N1, Depth)) ||
+         (SignBitIsOne(N0, Depth) && SignBitIsOne(N1, Depth));
+}
+
 /// MaskedValueIsZero - Return true if 'V & Mask' is known to be zero.  We use
 /// this predicate to simplify operations downstream.  Mask is known to be zero
 /// for bits that V cannot have.

llvm/test/CodeGen/CHERI-Generic/MIPS/ptradd-immediate.ll
@@ -34,18 +34,11 @@ define i32 @nneg_nneg(ptr addrspace(200) %p, i16 %x) {
 define i32 @neg_neg(ptr addrspace(200) %p, i16 %x) {
 ; PURECAP-LABEL: neg_neg:
 ; PURECAP:       # %bb.0:
-; PURECAP-NEXT:    daddiu $1, $zero, -1
-; PURECAP-NEXT:    xor $1, $4, $1
-; PURECAP-NEXT:    daddiu $2, $zero, 1
-; PURECAP-NEXT:    dsll $3, $2, 46
-; PURECAP-NEXT:    daddiu $3, $3, -1
-; PURECAP-NEXT:    dsll $3, $3, 16
-; PURECAP-NEXT:    or $1, $1, $3
-; PURECAP-NEXT:    dsll $2, $2, 62
-; PURECAP-NEXT:    daddiu $2, $2, -2
-; PURECAP-NEXT:    and $1, $1, $2
+; PURECAP-NEXT:    ori $1, $4, 1
+; PURECAP-NEXT:    andi $1, $1, 65535
+; PURECAP-NEXT:    dnegu $1, $1
 ; PURECAP-NEXT:    dsll $1, $1, 2
-; PURECAP-NEXT:    clw $2, $1, 0($c3)
+; PURECAP-NEXT:    clw $2, $1, -4($c3)
 ; PURECAP-NEXT:    cjr $c17
 ; PURECAP-NEXT:    nop
 ;

llvm/test/CodeGen/CHERI-Generic/RISCV32/ptradd-immediate.ll
@@ -12,9 +12,8 @@ define i32 @nneg_nneg(ptr addrspace(200) %p, i16 %x) {
 ; PURECAP-NEXT:    slli a1, a1, 16
 ; PURECAP-NEXT:    srli a1, a1, 16
 ; PURECAP-NEXT:    slli a1, a1, 2
-; PURECAP-NEXT:    addi a1, a1, 4
 ; PURECAP-NEXT:    cincoffset ca0, ca0, a1
-; PURECAP-NEXT:    lw a0, 0(ca0)
+; PURECAP-NEXT:    lw a0, 4(ca0)
 ; PURECAP-NEXT:    ret
 ;
 ; HYBRID-LABEL: nneg_nneg:
@@ -36,13 +35,13 @@ define i32 @nneg_nneg(ptr addrspace(200) %p, i16 %x) {
 define i32 @neg_neg(ptr addrspace(200) %p, i16 %x) {
 ; PURECAP-LABEL: neg_neg:
 ; PURECAP:       # %bb.0:
-; PURECAP-NEXT:    not a1, a1
-; PURECAP-NEXT:    andi a1, a1, -2
+; PURECAP-NEXT:    ori a1, a1, 1
+; PURECAP-NEXT:    slli a1, a1, 16
+; PURECAP-NEXT:    srli a1, a1, 16
+; PURECAP-NEXT:    neg a1, a1
 ; PURECAP-NEXT:    slli a1, a1, 2
-; PURECAP-NEXT:    lui a2, 1048512
-; PURECAP-NEXT:    or a1, a1, a2
 ; PURECAP-NEXT:    cincoffset ca0, ca0, a1
-; PURECAP-NEXT:    lw a0, 0(ca0)
+; PURECAP-NEXT:    lw a0, -4(ca0)
 ; PURECAP-NEXT:    ret
 ;
 ; HYBRID-LABEL: neg_neg:

llvm/test/CodeGen/CHERI-Generic/RISCV64/ptradd-immediate.ll
@@ -12,9 +12,8 @@ define i32 @nneg_nneg(ptr addrspace(200) %p, i16 %x) {
 ; PURECAP-NEXT:    slli a1, a1, 48
 ; PURECAP-NEXT:    srli a1, a1, 48
 ; PURECAP-NEXT:    slli a1, a1, 2
-; PURECAP-NEXT:    addi a1, a1, 4
 ; PURECAP-NEXT:    cincoffset ca0, ca0, a1
-; PURECAP-NEXT:    lw a0, 0(ca0)
+; PURECAP-NEXT:    lw a0, 4(ca0)
 ; PURECAP-NEXT:    ret
 ;
 ; HYBRID-LABEL: nneg_nneg:
@@ -36,13 +35,13 @@ define i32 @nneg_nneg(ptr addrspace(200) %p, i16 %x) {
 define i32 @neg_neg(ptr addrspace(200) %p, i16 %x) {
 ; PURECAP-LABEL: neg_neg:
 ; PURECAP:       # %bb.0:
-; PURECAP-NEXT:    not a1, a1
-; PURECAP-NEXT:    lui a2, 1048560
-; PURECAP-NEXT:    or a1, a1, a2
-; PURECAP-NEXT:    andi a1, a1, -2
+; PURECAP-NEXT:    ori a1, a1, 1
+; PURECAP-NEXT:    slli a1, a1, 48
+; PURECAP-NEXT:    srli a1, a1, 48
+; PURECAP-NEXT:    neg a1, a1
 ; PURECAP-NEXT:    slli a1, a1, 2
 ; PURECAP-NEXT:    cincoffset ca0, ca0, a1
-; PURECAP-NEXT:    lw a0, 0(ca0)
+; PURECAP-NEXT:    lw a0, -4(ca0)
 ; PURECAP-NEXT:    ret
 ;
 ; HYBRID-LABEL: neg_neg:

commit 511c1e0089b4112d4dfbc8b3b093c588aeedb8e9

[CodeGen] Don't create combine loops for PTRADD with opaque constants
We were implicitly assuming here that, if one of Y or Z is a constant,
either the other is not or we will have already (in the nested PTRADD
case) constant-folded both of them into one, and thus don't need to
consider the possibility. However, opaque constants exist and do not get
constant-folded, so we can end up cycling between PTRADD+ADD and
PTRADD+PTRADD. Catch this case by checking the other is not a constant
and skipping all but one combine if so.

No test is provided since I've yet to be able to trigger it in CHERI
LLVM, only downstream in Codasip LLVM and Morello LLVM (and only when
targeting CHERI-RISC-V even in Morello LLVM), both of which enable SCEV
and thus do interesting loop transformations.

Fixes: 05fd67ad059f ("[CodeGen][CHERI-Generic] Support commuting capability offsets when safe")

llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -2668,8 +2668,9 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
   //   * x is a null pointer; or
   //   * the add can be constant-folded; or
   //   * the add can be combined and z is not a constant; or
-  //   * y is a constant and z has one use; or
-  //   * y is a constant and (ptradd x, y) has one use; or
+  //   * y is a constant and z has one use and z is not a constant; or
+  //   * y is a constant and (ptradd x, y) has one use and z is not a
+  //     constant; or
   //   * (ptradd x, y) and z have one use and z is not a constant.
   //
   // Some of these overly-restrictive conditions are to not obfuscate CAndAddr
@@ -2678,12 +2679,15 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
   // and canonicalise it to a PTRMASK.
   //
   // Commute: (ptradd (ptradd x, y), z) -> (ptradd (ptradd x, z), y) if:
-  //   * y and z have the same sign and y is a constant.
+  //   * y and z have the same sign and only y is a constant.
   //
   // This allows immediate addressing modes to be used. Note that we need to be
   // careful to ensure we don't transiently become unrepresentable if the
   // original DAG does not already do so, and this is the case if both PTRADDs
   // have the same sign.
+  //
+  // Note we must be careful to handle the case that both are
+  // constants, as opaque constants will not be constant-folded.
   if (N0.getOpcode() == ISD::PTRADD &&
       !reassociationCanBreakAddressingModePattern(ISD::PTRADD, DL, N, N0, N1)) {
     SDValue X = N0.getOperand(0);
@@ -2706,15 +2710,18 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
     }
     LLVM_DEBUG(dbgs() << "visitPTRADD() add operand:"; Add.dump(&DAG));
     assert(Add->getOpcode() != ISD::DELETED_NODE && "Deleted Node used");
-    if (isNullConstant(X) ||
-        DAG.isConstantIntBuildVectorOrConstantInt(Add) ||
-        (VisitedAdd && !DAG.isConstantIntBuildVectorOrConstantInt(Z)) ||
-        (DAG.isConstantIntBuildVectorOrConstantInt(Y) && Z.hasOneUse()) ||
-        (DAG.isConstantIntBuildVectorOrConstantInt(Y) && N0.hasOneUse()) ||
-        (N0.hasOneUse() && Z.hasOneUse() &&
-         !DAG.isConstantIntBuildVectorOrConstantInt(Z)))
+    bool IsZConst = DAG.isConstantIntBuildVectorOrConstantInt(Z);
+    if (isNullConstant(X) || DAG.isConstantIntBuildVectorOrConstantInt(Add) ||
+        (VisitedAdd && !IsZConst) ||
+        (DAG.isConstantIntBuildVectorOrConstantInt(Y) && Z.hasOneUse() &&
+         !IsZConst) ||
+        (DAG.isConstantIntBuildVectorOrConstantInt(Y) && N0.hasOneUse() &&
+         !IsZConst) ||
+        (N0.hasOneUse() && Z.hasOneUse() && !IsZConst))
       return DAG.getPointerAdd(DL, X, Add);
-    if (DAG.SignBitIsSame(Y, Z) && DAG.isConstantIntBuildVectorOrConstantInt(Y))
+    if (DAG.SignBitIsSame(Y, Z) &&
+        DAG.isConstantIntBuildVectorOrConstantInt(Y) &&
+        !DAG.isConstantIntBuildVectorOrConstantInt(Z))
       return GetPTRADD2(X, Z, Y);
   }
 
@@ -2722,7 +2729,8 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
   //   * both y and z have the same sign and z is a constant.
   //
   // Transform: (ptradd x, (add y, z)) -> (ptradd (ptradd x, z), y) if:
-  //   * both y and z have the same sign and y is a constant.
+  //   * both y and z have the same sign and y is a constant (and
+  //     implicitly not z due to the previous transform).
   //
   // As above, this allows for immediate addressing modes.
   if (N1.getOpcode() == ISD::ADD) {
@@ -2730,10 +2738,10 @@ SDValue DAGCombiner::visitPTRADD(SDNode *N) {
     SDValue Y = N1.getOperand(0);
     SDValue Z = N1.getOperand(1);
     if (DAG.SignBitIsSame(Y, Z)) {
-      if (DAG.isConstantIntBuildVectorOrConstantInt(Y))
-        return GetPTRADD2(X, Z, Y);
       if (DAG.isConstantIntBuildVectorOrConstantInt(Z))
         return GetPTRADD2(X, Y, Z);
+      if (DAG.isConstantIntBuildVectorOrConstantInt(Y))
+        return GetPTRADD2(X, Z, Y);
     }
   }
 