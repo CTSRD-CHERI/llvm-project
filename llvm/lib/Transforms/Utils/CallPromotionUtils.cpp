CTSRD-CHERI/llvm-project

commit fb9fa3765325776d07c547b1b0668e6c749120f3

[UPSTREAM?][CallPromotion] Fix incorrect GEP APInt bitwidths
We should be using the index type not the size of the pointer. This makes
a difference for architectures such as CHERI where pointers have additional
metadata bits that are not part of the address.
I also noticed that CallPromotion was not working for CHERI purecap. This
is most likely caused by us changing the representation of member function
pointers and not having updated the internals to handle that.

llvm/lib/Transforms/Utils/CallPromotionUtils.cpp
@@ -540,14 +540,14 @@ bool llvm::tryPromoteCall(CallBase &CB) {
   if (!VTableEntryLoad)
     return false; // Not a vtable entry load.
   Value *VTableEntryPtr = VTableEntryLoad->getPointerOperand();
-  APInt VTableOffset(DL.getTypeSizeInBits(VTableEntryPtr->getType()), 0);
+  APInt VTableOffset(DL.getPointerAddrSizeInBits(VTableEntryPtr->getType()), 0);
   Value *VTableBasePtr = VTableEntryPtr->stripAndAccumulateConstantOffsets(
       DL, VTableOffset, /* AllowNonInbounds */ true);
   LoadInst *VTablePtrLoad = dyn_cast<LoadInst>(VTableBasePtr);
   if (!VTablePtrLoad)
     return false; // Not a vtable load.
   Value *Object = VTablePtrLoad->getPointerOperand();
-  APInt ObjectOffset(DL.getTypeSizeInBits(Object->getType()), 0);
+  APInt ObjectOffset(DL.getPointerAddrSizeInBits(Object->getType()), 0);
   Value *ObjectBase = Object->stripAndAccumulateConstantOffsets(
       DL, ObjectOffset, /* AllowNonInbounds */ true);
   if (!(isa<AllocaInst>(ObjectBase) && ObjectOffset == 0))
@@ -560,7 +560,8 @@ bool llvm::tryPromoteCall(CallBase &CB) {
       VTablePtrLoad, VTablePtrLoad->getParent(), BBI, 0, nullptr, nullptr);
   if (!VTablePtr)
     return false; // No vtable found.
-  APInt VTableOffsetGVBase(DL.getTypeSizeInBits(VTablePtr->getType()), 0);
+  APInt VTableOffsetGVBase(DL.getPointerAddrSizeInBits(VTablePtr->getType()),
+                           0);
   Value *VTableGVBase = VTablePtr->stripAndAccumulateConstantOffsets(
       DL, VTableOffsetGVBase, /* AllowNonInbounds */ true);
   GlobalVariable *GV = dyn_cast<GlobalVariable>(VTableGVBase);

llvm/test/Transforms/Inline/cheri-promote-call-bitwidth.ll
@@ -1,0 +1,71 @@
+; NOTE: Assertions have been autogenerated by utils/update_test_checks.py UTC_ARGS: --function-signature --scrub-attributes
+; RUN: opt -enable-new-pm=1 -S -passes=inline < %s | FileCheck %s
+; RUN: sed -e 's/addrspace(200)/addrspace(0)/g' -e 's/-A200-P200-G200//g' %s | opt -enable-new-pm=1 -S -passes=inline| FileCheck %s --check-prefix=HYBRID
+; This test case (reduce from compiling libunwind) previously triggered an assertion in the
+; inliner when trying to promote an indirect (virtual) call (inside llvm::tryPromoteCall)
+; Assertion failed: (BitWidth == DL.getIndexTypeSizeInBits(getType()) && "The offset bit width does not match the DL specification."), function stripAndAccumulateConstantOffsets
+target datalayout = "e-m:e-pf200:128:128:128:64-p:64:64-i64:64-i128:128-n64-S128-A200-P200-G200"
+target triple = "riscv64-unknown-freebsd13.0"
+
+%class.f = type <{ i32 (...) addrspace(200)* addrspace(200)*, %class.a, [15 x i8] }>
+%class.a = type { i8 }
+
+define void @test(%class.f addrspace(200)* %arg1, void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* %arg2) local_unnamed_addr addrspace(200) {
+; CHECK-LABEL: define {{[^@]+}}@test
+; CHECK-SAME: ([[CLASS_F:%.*]] addrspace(200)* [[ARG1:%.*]], void ([[CLASS_F]] addrspace(200)*) addrspace(200)* addrspace(200)* [[ARG2:%.*]]) local_unnamed_addr addrspace(200) {
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[TMP0:%.*]] = load void ([[CLASS_F]] addrspace(200)*) addrspace(200)*, void ([[CLASS_F]] addrspace(200)*) addrspace(200)* addrspace(200)* [[ARG2]], align 16
+; CHECK-NEXT:    call void [[TMP0]]([[CLASS_F]] addrspace(200)* [[ARG1]])
+; CHECK-NEXT:    ret void
+;
+; HYBRID-LABEL: define {{[^@]+}}@test
+; HYBRID-SAME: (%class.f* [[ARG1:%.*]], void (%class.f*)** [[ARG2:%.*]]) local_unnamed_addr {
+; HYBRID-NEXT:  entry:
+; HYBRID-NEXT:    [[TMP0:%.*]] = load void (%class.f*)*, void (%class.f*)** [[ARG2]], align 16
+; HYBRID-NEXT:    call void [[TMP0]](%class.f* [[ARG1]])
+; HYBRID-NEXT:    ret void
+;
+entry:
+  call void @call_fnptr(%class.f addrspace(200)* %arg1, void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* %arg2)
+  ret void
+}
+
+define internal void @call_fnptr(%class.f addrspace(200)* %this, void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* %arg) unnamed_addr addrspace(200) align 2 {
+entry:
+  %0 = load void (%class.f addrspace(200)*) addrspace(200)*, void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* %arg, align 16
+  call void %0(%class.f addrspace(200)* %this)
+  ret void
+}
+
+define void @test2(%class.f addrspace(200)* %this) local_unnamed_addr addrspace(200) {
+; CHECK-LABEL: define {{[^@]+}}@test2
+; CHECK-SAME: ([[CLASS_F:%.*]] addrspace(200)* [[THIS:%.*]]) local_unnamed_addr addrspace(200) {
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    [[TMP0:%.*]] = bitcast [[CLASS_F]] addrspace(200)* [[THIS]] to void ([[CLASS_F]] addrspace(200)*) addrspace(200)* addrspace(200)* addrspace(200)*
+; CHECK-NEXT:    [[VTABLE_I:%.*]] = load void ([[CLASS_F]] addrspace(200)*) addrspace(200)* addrspace(200)*, void ([[CLASS_F]] addrspace(200)*) addrspace(200)* addrspace(200)* addrspace(200)* [[TMP0]], align 16
+; CHECK-NEXT:    [[FN_I:%.*]] = load void ([[CLASS_F]] addrspace(200)*) addrspace(200)*, void ([[CLASS_F]] addrspace(200)*) addrspace(200)* addrspace(200)* [[VTABLE_I]], align 16
+; CHECK-NEXT:    call void [[FN_I]]([[CLASS_F]] addrspace(200)* [[THIS]])
+; CHECK-NEXT:    ret void
+;
+; HYBRID-LABEL: define {{[^@]+}}@test2
+; HYBRID-SAME: (%class.f* [[THIS:%.*]]) local_unnamed_addr {
+; HYBRID-NEXT:  entry:
+; HYBRID-NEXT:    [[TMP0:%.*]] = bitcast %class.f* [[THIS]] to void (%class.f*)***
+; HYBRID-NEXT:    [[VTABLE_I:%.*]] = load void (%class.f*)**, void (%class.f*)*** [[TMP0]], align 16
+; HYBRID-NEXT:    [[FN_I:%.*]] = load void (%class.f*)*, void (%class.f*)** [[VTABLE_I]], align 16
+; HYBRID-NEXT:    call void [[FN_I]](%class.f* [[THIS]])
+; HYBRID-NEXT:    ret void
+;
+entry:
+  call void @call_via_vtable(%class.f addrspace(200)* %this)
+  ret void
+}
+
+define internal void @call_via_vtable(%class.f addrspace(200)* %this) unnamed_addr addrspace(200) {
+entry:
+  %0 = bitcast %class.f addrspace(200)* %this to void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* addrspace(200)*
+  %vtable = load void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)*, void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* addrspace(200)* %0, align 16
+  %fn = load void (%class.f addrspace(200)*) addrspace(200)*, void (%class.f addrspace(200)*) addrspace(200)* addrspace(200)* %vtable, align 16
+  call void %fn(%class.f addrspace(200)* %this)
+  ret void
+}

commit 6723a4dd600bdcedd666d4be760155482d3afb12

[CallPromotion] Don't assume available loaded value is a pointer
We're doing store-to-load forwarding here but there's no guarantee that
the found value is a pointer (e.g. a memset(..., 0, ...) will forward a
value of 0 as a pointer-width integer, at least for non-CHERI). This
pulls in the bug fix in ab15976173e4 ("CallPromotionUtils: Correctly use
IndexSize when determining the bit width of pointer offsets. (#119483)")
upstream which was re-landed with this sole change to address the same
bug in the original attempt at upstreaming our downstream diff.

Fixes: fb9fa3765325 ("[UPSTREAM?][CallPromotion] Fix incorrect GEP APInt bitwidths")

llvm/lib/Transforms/Utils/CallPromotionUtils.cpp
@@ -587,7 +587,7 @@ bool llvm::tryPromoteCall(CallBase &CB) {
   BasicBlock::iterator BBI(VTablePtrLoad);
   Value *VTablePtr = FindAvailableLoadedValue(
       VTablePtrLoad, VTablePtrLoad->getParent(), BBI, 0, nullptr, nullptr);
-  if (!VTablePtr)
+  if (!VTablePtr || !VTablePtr->getType()->isPointerTy())
     return false; // No vtable found.
   APInt VTableOffsetGVBase(DL.getPointerAddrSizeInBits(VTablePtr->getType()),
                            0);