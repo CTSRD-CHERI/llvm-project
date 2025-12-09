CTSRD-CHERI/llvm-project

commit f0f9c370ea71e4523268c29491d28cbe0b475106

[ELF][CHERI] Fix missing reset() calls in InStruct::reset()
Now that these fields are unique_ptr<> we should delete them when
resetting the InStruct.

lld/ELF/SyntheticSections.cpp
@@ -3954,6 +3954,9 @@ void InStruct::reset() {
   attributes.reset();
   bss.reset();
   bssRelRo.reset();
+  capRelocs.reset();
+  cheriCapTableMapping.reset();
+  cheriCapTableMapping.reset();
   got.reset();
   gotPlt.reset();
   igotPlt.reset();

commit 3a2439cb57b7f1b88513af5967168221b209f691

[ELF] Add missing reset calls to InStruct::reset
One of the duplicated captable mapping calls was likely meant to be for
the captable itself.

Fixes: f0f9c370ea71 ("[ELF][CHERI] Fix missing reset() calls in InStruct::reset()")
Fixes: 86217f3233f7 ("[ELF] Support CHERI TGOT TLS")
Fixes: 7ac34fc2256d ("[ELF][CHERI] Output a PT_CHERI_PCC segment for CheriABI binaries and DSOs")

lld/ELF/SyntheticSections.cpp
@@ -4044,11 +4044,13 @@ void InStruct::reset() {
   riscvAttributes.reset();
   bss.reset();
   bssRelRo.reset();
-  mipsCheriCapTableMapping.reset();
-  mipsCheriCapTableMapping.reset();
   got.reset();
   gotPlt.reset();
   igotPlt.reset();
+  tgot.reset();
+  mipsCheriCapTable.reset();
+  pccPadding.reset();
+  mipsCheriCapTableMapping.reset();
   armCmseSGSection.reset();
   ppc64LongBranchTarget.reset();
   mipsAbiFlags.reset();
@@ -4064,6 +4066,8 @@ void InStruct::reset() {
   ibtPlt.reset();
   relaPlt.reset();
   relaIplt.reset();
+  relaTgot.reset();
+  tgotCapRelocs.reset();
   shStrTab.reset();
   strTab.reset();
   symTab.reset();