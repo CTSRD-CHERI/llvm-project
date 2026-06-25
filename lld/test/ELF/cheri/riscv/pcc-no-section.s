CTSRD-CHERI/llvm-project

commit 7ac34fc2256d05768425e0dda4c7e282a5e7bda2

[ELF][CHERI] Output a PT_CHERI_PCC segment for CheriABI binaries and DSOs
This segment bounds all of the executable sections as well as any GOTs
and other sections such as read-only data directly accessed via PCC.

The PT_CHERI_PCC segment is aligned and padded to ensure that the
segment bounds are exactly representable. The first section in the
segment is aligned based on the segment size, and a new .pad.cheri.pcc
section is inserted at the end of the segment to provide any needed
padding.

To simplify capreloc processing, function caprelocs use the bounds of
the containing PT_CHERI_PCC segment. Future relative capability
relocations should also use these bounds for function and code
capabilities.

This segment is not output for CHERI-MIPS when using the PLT or function
descriptor ABIs as those use a separate mechanism for setting bounds on
PCC.

Co-authored-by: Jessica Clarke <jrtc27@jrtc27.com>

lld/ELF/Relocations.cpp
@@ -1136,6 +1136,22 @@ void RelocationScanner::processAux(RelExpr expr, RelType type, uint64_t offset,
     }
   }
 
+  if (config->isCheriAbi && sym.isDefined() && (sec->flags & SHF_EXECINSTR) &&
+      oneof<R_PC, R_AARCH64_PAGE_PC>(expr)) {
+    OutputSection *osec = sym.getOutputSection();
+    if (osec == nullptr)
+      llvm_unreachable(
+          "PCC-accessed symbol defined in unsupported section type");
+    // TODO: Make this an error in future? Would need special relocation to
+    // allow bypassing for specific use cases (e.g. kernel startup code).
+    if ((osec->flags & SHF_WRITE) && !isRelroSection(osec))
+      warn("relocation " + toString(type) + " against symbol '" +
+           toString(sym) + "' in non-PCC section" +
+           getLocation(*sec, sym, offset));
+    else
+      osec->cheriPcc.store(true, std::memory_order_relaxed);
+  }
+
   // We were asked not to generate PLT entries for ifuncs. Instead, pass the
   // direct relocation on through.
   if (LLVM_UNLIKELY(isIfunc) && config->zIfuncNoplt) {

lld/ELF/Arch/Cheri.cpp
@@ -321,6 +321,19 @@ enum class CapRelocType {
   CODE,
 };
 
+bool isCapRelocTypeExec(CapRelocType type) {
+  switch (type) {
+  case CapRelocType::DATA:
+  case CapRelocType::RODATA:
+    return false;
+  case CapRelocType::FUNC:
+  case CapRelocType::IFUNC:
+  case CapRelocType::CODE:
+    return true;
+  }
+  llvm_unreachable("unknown CapRelocType");
+}
+
 static CapRelocType getTargetType(const SymbolAndOffset &target) {
   bool isFunc, isGnuIFunc, isTls;
   OutputSection *os;
@@ -428,6 +441,13 @@ void CheriCapRelocsSection::writeToImpl(uint8_t *buf) {
       targetType = CapRelocType::CODE;
     }
     uint64_t permissions = CapRelocPermission<ELFT>::encodeType(targetType);
+
+    // Use PCC bounds from the PT_CHERI_PCC segment.
+    if (PhdrEntry *ph = in.cheriBounds; ph && isCapRelocTypeExec(targetType)) {
+      targetOffset += targetVA - ph->p_vaddr;
+      targetVA = ph->p_vaddr;
+      targetSize = ph->p_memsz;
+    }
 
     // TODO: should we warn about symbols that are out-of-bounds?
     // mandoc seems to do it so I guess we need it
@@ -910,22 +930,8 @@ static void getMipsCheriAbiVariant(std::optional<unsigned> &abi,
   abi = static_cast<MipsAbiFlagsSection<ELFT> &>(sec).getCheriAbiVariant();
 }
 
-static bool needsCheriMipsTrampoline(RelType type, const Symbol &sym) {
-  // In the PLT ABI (and fndesc?) we have to use an elf relocation for function
-  // pointers to ensure that the runtime linker adds the required trampolines
-  // that sets $cgp:
-
+static bool isCheriMipsTrampolineAbi() {
   if (config->emachine != EM_MIPS)
-    return false;
-
-  if (!sym.isFunc() || type == *target->symbolicCapCallRel)
-    return false;
-
-  // In static binaries we do not need PLT stubs for function pointers since
-  // all functions share the same $cgp
-  // TODO: this is no longer true if we were to support dlopen() in static
-  // binaries
-  if (!hasDynamicLinker())
     return false;
 
   if (!in.mipsAbiFlags)
@@ -937,6 +943,27 @@ static bool needsCheriMipsTrampoline(RelType type, const Symbol &sym) {
     return false;
 
   if (*abi != DF_MIPS_CHERI_ABI_PLT && *abi != DF_MIPS_CHERI_ABI_FNDESC)
+    return false;
+
+  return true;
+}
+
+static bool needsCheriMipsTrampoline(RelType type, const Symbol &sym) {
+  // In the PLT ABI (and fndesc?) we have to use an elf relocation for function
+  // pointers to ensure that the runtime linker adds the required trampolines
+  // that sets $cgp:
+
+  if (!isCheriMipsTrampolineAbi())
+    return false;
+
+  if (!sym.isFunc() || type == *target->symbolicCapCallRel)
+    return false;
+
+  // In static binaries we do not need PLT stubs for function pointers since
+  // all functions share the same $cgp
+  // TODO: this is no longer true if we were to support dlopen() in static
+  // binaries
+  if (!hasDynamicLinker())
     return false;
 
   return true;
@@ -979,6 +1006,54 @@ void addRelativeCapabilityRelocation(
          "relative ELF capability relocations not currently implemented");
   in.capRelocs->addCapReloc(isCode, {&isec, offsetInSec}, {symOrSec, 0u},
                             addend);
+}
+
+// CHERI-MIPS using the PLT and fndesc ABIs uses a different mechanism for
+// determining the bounds of PCC.
+bool needsCheriPccSegment() { return !isCheriMipsTrampolineAbi(); }
+
+// Determine the required alignment for a single PT_CHERI_PCC segment.  Apply
+// the alignment to the first OutputSection and adjust the length of the padding
+// section to align the end of the segment.  Returns true if the alignment of
+// the first OutputSection changed or the size of the padding section changed.
+static bool alignPCCBounds(PhdrEntry *p, CheriPccPaddingSection &psec) {
+  OutputSection *first = p->firstSec;
+  OutputSection *last = p->lastSec;
+
+  if (!first)
+    return false;
+
+  assert(psec.getParent() == last && "padding section is not last");
+  assert(psec.isNeeded() && "padding section is not enabled");
+
+  // Ignore existing padding.
+  uint64_t size = last->getVA() - first->getVA();
+  uint64_t align = target->getCheriRequiredAlignment(size);
+  if (align == 0)
+    align = 1;
+  bool changed = false;
+  if (first->addralign < align) {
+    first->addralign = align;
+    if (first->ptLoad)
+      first->ptLoad->p_align =
+          std::max(first->ptLoad->p_align, first->addralign);
+    p->p_align = std::max(p->p_align, first->addralign);
+    changed = true;
+  }
+  uint64_t padSize = alignTo(size, align) - size;
+  if (psec.getSize() != padSize) {
+    psec.setSize(padSize);
+    changed = true;
+  }
+  return changed;
+}
+
+bool cheriCapabilityBoundsAlign() {
+  // Align the PT_CHERI_PCC segment.
+  bool changed = false;
+  if (in.cheriBounds)
+    changed |= alignPCCBounds(in.cheriBounds, *in.pccPadding);
+  return changed;
 }
 
 } // namespace elf

lld/ELF/Arch/Cheri.h
@@ -314,6 +314,14 @@ void addRelativeCapabilityRelocation(
     InputSectionBase &isec, uint64_t offsetInSec,
     llvm::PointerUnion<Symbol *, InputSectionBase *> symOrSec, int64_t addend,
     RelExpr expr, RelType type);
+
+bool needsCheriPccSegment();
+
+// Align OutputSections as needed to ensure the bounds of capabilities
+// such as PCC do not permit undesired access to portions of other
+// OutputSections.  Return true if the alignment of any OutputSection
+// was modified.
+bool cheriCapabilityBoundsAlign();
 } // namespace elf
 } // namespace lld
 

lld/ELF/OutputSections.h
@@ -72,6 +72,9 @@ public:
   uint64_t addr = 0;
   uint32_t shName = 0;
 
+  // Sections accessed using PCC on CHERI architectures.
+  std::atomic<bool> cheriPcc{false};
+
   void recordSection(InputSectionBase *isec);
   void commitSection(InputSection *isec);
   void finalizeInputSections();

lld/ELF/SyntheticSections.cpp
@@ -4069,6 +4069,7 @@ void InStruct::reset() {
   strTab.reset();
   symTab.reset();
   symTabShndx.reset();
+  cheriBounds = nullptr;
 }
 
 constexpr char kMemtagAndroidNoteName[] = "Android";

lld/ELF/SyntheticSections.h
@@ -1275,6 +1275,23 @@ public:
   size_t getSize() const override;
 };
 
+class CheriPccPaddingSection final : public SyntheticSection {
+public:
+  CheriPccPaddingSection()
+      : SyntheticSection(llvm::ELF::SHF_ALLOC, llvm::ELF::SHT_PROGBITS,
+                         /*alignment=*/1, ".pad.cheri.pcc") {}
+
+  void writeTo(uint8_t *buf) override {}
+  void markNeeded() { needed = true; }
+  bool isNeeded() const override { return needed; }
+  size_t getSize() const override { return size; }
+  void setSize(uint64_t len) { size = len; }
+
+private:
+  uint64_t size = 0;
+  bool needed = false;
+};
+
 InputSection *createInterpSection();
 MergeInputSection *createCommentSection();
 template <class ELFT> void splitSections();
@@ -1336,6 +1353,7 @@ struct InStruct {
   std::unique_ptr<IgotPltSection> igotPlt;
   std::unique_ptr<TgotSection> tgot;
   std::unique_ptr<MipsCheriCapTableSection> mipsCheriCapTable;
+  std::unique_ptr<CheriPccPaddingSection> pccPadding;
   std::unique_ptr<CheriCapRelocsSection> capRelocs;
   std::unique_ptr<CheriCapRelocsSection> tgotCapRelocs;
   // For per-file/per-function tables:
@@ -1360,6 +1378,8 @@ struct InStruct {
   std::unique_ptr<StringTableSection> strTab;
   std::unique_ptr<SymbolTableBaseSection> symTab;
   std::unique_ptr<SymtabShndxSection> symTabShndx;
+
+  PhdrEntry *cheriBounds;
 
   void reset();
 };

lld/ELF/Writer.cpp
@@ -61,6 +61,7 @@ private:
   void finalizeAddressDependentContent();
   void optimizeBasicBlockJumps();
   void sortInputSections();
+  void sortCheriPccPaddingSection();
   void sortOrphanSections();
   void finalizeSections();
   void checkExecuteOnly();
@@ -545,6 +546,11 @@ template <class ELFT> void elf::createSyntheticSections() {
   add(*in.plt);
   in.iplt = std::make_unique<IpltSection>();
   add(*in.iplt);
+
+  if (config->isCheriAbi && needsCheriPccSegment()) {
+    in.pccPadding = std::make_unique<CheriPccPaddingSection>();
+    add(*in.pccPadding);
+  }
 
   if (config->andFeatures)
     add(*make<GnuPropertySection>());
@@ -1477,6 +1483,50 @@ template <class ELFT> void Writer<ELFT>::sortInputSections() {
       sortSection(osd->osec, order);
 }
 
+// The CHERI PCC padding output section must be placed immediately after the
+// last section covered by the PCC bounds.
+template <class ELFT> void Writer<ELFT>::sortCheriPccPaddingSection() {
+  CheriPccPaddingSection *psec = in.pccPadding.get();
+  if (!psec->isNeeded())
+    return;
+
+  // First, find and remove the existing padding output section.
+  auto isPaddingSection = [&](SectionCommand *cmd) {
+    auto *to = dyn_cast<OutputDesc>(cmd);
+    return to != nullptr && psec->getParent() == &to->osec;
+  };
+  auto fromPos = llvm::find_if(script->sectionCommands, isPaddingSection);
+  assert(fromPos != script->sectionCommands.end() &&
+         "PCC padding section not found");
+  auto paddingSec = *fromPos;
+  script->sectionCommands.erase(fromPos);
+
+  // Second, find the last CHERI PCC output section.
+  auto isPccSection = [&](SectionCommand *cmd) {
+    auto *to = dyn_cast<OutputDesc>(cmd);
+    return to != nullptr && to->osec.cheriPcc;
+  };
+
+  auto insertPos = llvm::find_if(script->sectionCommands, isPccSection);
+  assert(insertPos != script->sectionCommands.end() &&
+         "did not find first PCC section");
+  for (;;) {
+    auto nextPos = std::find_if(insertPos + 1, script->sectionCommands.end(),
+                                isPccSection);
+    if (nextPos == script->sectionCommands.end())
+      break;
+    insertPos = nextPos;
+  }
+
+  // Change the flags of the padding output section to match the last CHERI PCC
+  // output section so it is treated as part of the same load segment.
+  cast<OutputDesc>(paddingSec)->osec.flags =
+      cast<OutputDesc>(*insertPos)->osec.flags;
+
+  // Insert the padding output section in its new location.
+  script->sectionCommands.insert(insertPos + 1, paddingSec);
+}
+
 template <class ELFT> void Writer<ELFT>::sortSections() {
   llvm::TimeTraceScope timeScope("Sort sections");
 
@@ -1508,6 +1558,9 @@ template <class ELFT> void Writer<ELFT>::sortSections() {
 
   if (script->hasSectionsCommand)
     sortOrphanSections();
+
+  if (in.pccPadding)
+    sortCheriPccPaddingSection();
 
   script->adjustSectionsAfterSorting();
 }
@@ -1690,6 +1743,12 @@ template <class ELFT> void Writer<ELFT>::finalizeAddressDependentContent() {
       break;
     }
 
+    if (config->isCheriAbi && !config->relocatable) {
+      if (changed)
+        script->assignAddresses();
+      changed |= cheriCapabilityBoundsAlign();
+    }
+
     if (config->fixCortexA53Errata843419) {
       if (changed)
         script->assignAddresses();
@@ -1826,6 +1885,81 @@ template <class ELFT> void Writer<ELFT>::optimizeBasicBlockJumps() {
   for (OutputSection *osec : outputSections)
     for (InputSection *is : getInputSections(*osec, storage))
       is->trim();
+}
+
+// Which output sections are always covered by CHERI PCC bounds.  This includes
+// executable sections, GOTs, and PCC padding.
+static bool isCheriBoundsSection(const OutputSection *sec) {
+  uint64_t flags = sec->flags;
+
+  // Non-allocatable sections are not mapped into memory.
+  if (!(flags & SHF_ALLOC))
+    return false;
+
+  // Executable sections are fetched via PCC.
+  if (flags & SHF_EXECINSTR)
+    return true;
+
+  // .got is accessed relative to PCC.
+  if (in.got && sec == in.got->getParent())
+    return true;
+  if (in.mipsGot && sec == in.mipsGot->getParent())
+    return true;
+
+  // .got.plt is accessed relative to PCC.
+  if (sec == in.gotPlt->getParent())
+    return true;
+  if (sec == in.igotPlt->getParent())
+    return true;
+
+  // CHERI-MIPS capability table is accessed relative to PCC.
+  if (in.mipsCheriCapTable && sec == in.mipsCheriCapTable->getParent())
+    return true;
+
+  // The PCC padding section is included in PCC bounds.
+  if (sec == in.pccPadding->getParent())
+    return true;
+
+  // XXX: CheriBSD's runtime loader assumes all read-only capabilities can be
+  // derived from PCC, so include all read-only sections as a workaround for
+  // now.  Once CheriBSD 25.03 is no longer supported, this can be removed.
+  if (sec->type == SHT_PROGBITS &&
+      (((flags & SHF_WRITE) == 0) || isRelroSection(sec)))
+    return true;
+
+  return false;
+}
+
+// Mark all output sections covered by CHERI PCC bounds.  In addition,
+// enable the padding section for the associated compartment.
+static void markCheriPccSections() {
+  // Mark padding section as needed as long as there is at least one executable
+  // input section.
+  for (InputSectionBase *s : ctx.inputSections) {
+    // Ignore unused synthetic sections
+    if (isa<SyntheticSection>(s)) {
+      auto *sec = cast<SyntheticSection>(s);
+      if (!(sec->getParent() && sec->isNeeded()))
+        continue;
+    }
+    // Ignore empty input sections
+    if (s->getSize() == 0)
+      continue;
+    if ((s->flags & (SHF_ALLOC | SHF_EXECINSTR)) == (SHF_ALLOC | SHF_EXECINSTR))
+      in.pccPadding->markNeeded();
+  }
+
+  // Mark all output sections accessed via PCC if there is at least one
+  // executable input section.
+  for (SectionCommand *cmd : script->sectionCommands) {
+    if (auto *osd = dyn_cast<OutputDesc>(cmd)) {
+      OutputSection &osec = osd->osec;
+      if (!in.pccPadding->isNeeded())
+        continue;
+      if (isCheriBoundsSection(&osec))
+        osec.cheriPcc.store(true, std::memory_order_relaxed);
+    }
+  }
 }
 
 // In order to allow users to manipulate linker-synthesized sections,
@@ -2093,6 +2227,8 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
   if (in.mipsGot)
     in.mipsGot->build();
 
+  if (in.pccPadding)
+    markCheriPccSections();
   removeUnusedSyntheticSections();
   script->diagnoseOrphanHandling();
   script->diagnoseMissingSGSectionAddress();
@@ -2180,6 +2316,7 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
     finalizeSynthetic(in.igotPlt.get());
     finalizeSynthetic(in.gotPlt.get());
     finalizeSynthetic(in.tgot.get());
+    finalizeSynthetic(in.pccPadding.get());
     finalizeSynthetic(in.relaIplt.get());
     finalizeSynthetic(in.relaPlt.get());
     finalizeSynthetic(in.relaTgot.get());
@@ -2463,7 +2600,10 @@ SmallVector<PhdrEntry *, 0> Writer<ELFT>::createPhdrs(Partition &part) {
   for (OutputSection *sec : outputSections) {
     if (sec->partition != partNo || !needsPtLoad(sec))
       continue;
-    if (isRelroSection(sec)) {
+    // Treat a CHERI PCC padding section as relro if it is preceded by a relro
+    // section.
+    if (isRelroSection(sec) ||
+        (in.pccPadding && inRelroPhdr && sec == in.pccPadding->getParent())) {
       inRelroPhdr = true;
       if (!relroEnd)
         relRo->add(sec);
@@ -2473,6 +2613,19 @@ SmallVector<PhdrEntry *, 0> Writer<ELFT>::createPhdrs(Partition &part) {
     } else if (inRelroPhdr) {
       inRelroPhdr = false;
       relroEnd = sec;
+    }
+  }
+
+  // Determine the sections PCC should cover.
+  if (in.pccPadding) {
+    in.cheriBounds = make<PhdrEntry>(PT_CHERI_PCC, PF_R | PF_X);
+
+    for (OutputSection *sec : outputSections) {
+      if (sec->partition != partNo || !needsPtLoad(sec))
+        continue;
+      if (!sec->cheriPcc.load(std::memory_order_relaxed))
+        continue;
+      in.cheriBounds->add(sec);
     }
   }
 
@@ -2543,6 +2696,11 @@ SmallVector<PhdrEntry *, 0> Writer<ELFT>::createPhdrs(Partition &part) {
 
   if (relRo->firstSec)
     ret.push_back(relRo);
+
+  if (in.cheriBounds) {
+    if (in.cheriBounds->firstSec)
+      ret.push_back(in.cheriBounds);
+  }
 
   // PT_GNU_EH_FRAME is a special section pointing on .eh_frame_hdr.
   if (part.ehFrame->isNeeded() && part.ehFrameHdr &&

lld/test/ELF/cheri/cap-table/cap-table-init-relocs.s
@@ -27,19 +27,19 @@ local_var:
 
 
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-LABEL: Sections:
-# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS: __cap_relocs  00000028 00000000000004b8 DATA
-# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS: .captable    00000020 00000000000204f0 DATA
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS: __cap_relocs  00000028 0000000000000500 DATA
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS: .captable    00000020 0000000000020540 DATA
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-LABEL: SYMBOL TABLE:
-# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS:       00000000000204f0 l     O .captable              0000000000000010 preemptible_var@CAPTABLE
-# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS:       0000000000020500 l     O .captable              0000000000000010 local_var@CAPTABLE
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS:       0000000000020540 l     O .captable              0000000000000010 preemptible_var@CAPTABLE
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS:       0000000000020550 l     O .captable              0000000000000010 local_var@CAPTABLE
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-LABEL: CAPABILITY RELOCATION RECORDS:
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-NEXT:  OFFSET           TYPE    VALUE
-# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-NEXT:  0000000000020500 DATA    0000000000030518 [0000000000030518-0000000000030520]
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-NEXT:  0000000000020550 DATA    0000000000030568 [0000000000030568-0000000000030570]
 
 # One relative reloc for the local __cap_reloc location and one R_MIPS_CHERI_CAPABILITY for the preemptible symbol
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS:      Relocations [
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:   Section (7) .rel.dyn {
-# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:     0x204F0 R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE preemptible_var{{$}}
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:     0x20540 R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE preemptible_var{{$}}
 #                                                       ^---- preemptible_var@CAPTABLE
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:   }
 # ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT: ]

commit 9458196a7a269b431739f609a37a3794616eaa58

[ELF][CHERI] Allow PCC-relative relocations against absolute symbols
These are rare and odd (regardless of CHERI they only make sense in
position-dependent executables, unless you want text relocations), but
they're no worse from a PCC bounds perspective than symbols in non-PCC
sections. They're also expressible in ELF so, even if we did want to
error rather than warn, llvm_unreachable would not be the right thing.
Just lump the two cases together here, and tweak the wording for the
warning so it covers this "no section" case.

Fixes: 7ac34fc2256d ("[ELF][CHERI] Output a PT_CHERI_PCC segment for CheriABI binaries and DSOs")
Fixes: https://github.com/CTSRD-CHERI/llvm-project/issues/824

lld/ELF/Relocations.cpp
@@ -1150,15 +1150,12 @@ void RelocationScanner::processAux(RelExpr expr, RelType type, uint64_t offset,
   if (config->isCheriAbi && sym.isDefined() && (sec->flags & SHF_EXECINSTR) &&
       isDirectPcExpr(expr)) {
     OutputSection *osec = sym.getOutputSection();
-    if (osec == nullptr)
-      llvm_unreachable(
-          "PCC-accessed symbol defined in unsupported section type");
     // TODO: Make this an error in future? Would need special relocation to
     // allow bypassing for specific use cases (e.g. kernel startup code).
-    if ((osec->flags & SHF_WRITE) &&
-        !isRelroSection(osec, /*ignoreZRelro=*/true))
+    if (osec == nullptr || ((osec->flags & SHF_WRITE) &&
+                            !isRelroSection(osec, /*ignoreZRelro=*/true)))
       warn("relocation " + toString(type) + " against symbol '" +
-           toString(sym) + "' in non-PCC section" +
+           toString(sym) + "' not in a PCC section" +
            getLocation(*sec, sym, offset));
     else
       osec->cheriPcc.store(true, std::memory_order_relaxed);

lld/test/ELF/cheri/riscv/pcc-no-section.s
@@ -1,0 +1,21 @@
+# REQUIRES: riscv
+
+# RUN: %riscv64_cheri_purecap_llvm-mc -filetype=obj %s -o %t.o
+# RUN: ld.lld %t.o --defsym abs=0x12000 -o %t
+# RUN: llvm-readelf -S %t | FileCheck %s --check-prefix=SEC
+# RUN: llvm-readelf -l %t | FileCheck %s --check-prefix=SEG
+# RUN: llvm-objdump -d --no-show-raw-insn %t | FileCheck %s --check-prefix=DIS
+
+## PCC-relative relocations against symbols not in a section
+## (such as absolute symbols) should be permitted like those
+## against symbols in non-PCC sections, but similarly won't
+## affect the bounds.
+
+# SEC: [ 1] .text             PROGBITS        0000000000011158 000158 000008 00  AX  0   0  4
+# SEG: CHERI_PCC      0x000158 0x0000000000011158 0x0000000000011158 0x000008 0x000008 R E 0x4
+
+## 0x12000 - 0x11158 = 1*4096 - 344
+# DIS:      11158: auipcc ca0, 1
+# DIS-NEXT:        cincoffset ca0, ca0, -344
+
+cllc ca0, abs