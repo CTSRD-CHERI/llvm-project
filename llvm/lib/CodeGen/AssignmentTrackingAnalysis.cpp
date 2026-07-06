CTSRD-CHERI/llvm-project

commit f562d50783d249ca59fd08b0fd47f4d4a5905373

[UPSTREAM][AssignmentTracking] Use index type when accumulating GEP offsets
This resulted in crashing tests in the CHERI target

llvm/lib/CodeGen/AssignmentTrackingAnalysis.cpp
@@ -213,7 +213,7 @@ void FunctionVarLocs::clear() {
 static std::pair<Value *, DIExpression *>
 walkToAllocaAndPrependOffsetDeref(const DataLayout &DL, Value *Start,
                                   DIExpression *Expression) {
-  APInt OffsetInBytes(DL.getTypeSizeInBits(Start->getType()), false);
+  APInt OffsetInBytes(DL.getIndexTypeSizeInBits(Start->getType()), false);
   Value *End =
       Start->stripAndAccumulateInBoundsConstantOffsets(DL, OffsetInBytes);
   SmallVector<uint64_t, 3> Ops;

commit 2370fc18cf84753896ceaa0af83b50aa0213fc4f

[Assignment Tracking] Allow integer types in walkToAllocaAndPrependOffsetDeref
CodeGenPrepare's production of this is rather dubious, but is still that
way upstream, so we need to deal with that reality. This case gets hit
when building CheriBSD soon-to-be-26.07's GENERIC-KASAN for amd64 and
arm64, though presumably the original case pre-reduction was not a null
but rather some shadow space address. No test case as debug info ones
are hard to reduce sensibly.

Fixes: f562d50783d2 ("[UPSTREAM][AssignmentTracking] Use index type when accumulating GEP offsets")

llvm/lib/CodeGen/AssignmentTrackingAnalysis.cpp
@@ -227,7 +227,15 @@ void FunctionVarLocs::clear() {
 static std::pair<Value *, DIExpression *>
 walkToAllocaAndPrependOffsetDeref(const DataLayout &DL, Value *Start,
                                   DIExpression *Expression) {
-  APInt OffsetInBytes(DL.getIndexTypeSizeInBits(Start->getType()), false);
+  // XXX: CodeGenPrepare will sometimes turn the address argument for a dbg
+  // assign from a pointer to an integer, e.g. inttoptr i64 0 to ptr will end
+  // up as i64 0 rather than ptr null as in the case of stores. This is true
+  // even for the newer #dbg_assign as of LLVM 22.
+  Type *StartTy = Start->getType();
+  unsigned IndexWidth = StartTy->isPointerTy()
+                            ? DL.getIndexTypeSizeInBits(Start->getType())
+                            : DL.getTypeSizeInBits(Start->getType());
+  APInt OffsetInBytes(IndexWidth, false);
   Value *End =
       Start->stripAndAccumulateInBoundsConstantOffsets(DL, OffsetInBytes);
   SmallVector<uint64_t, 3> Ops;