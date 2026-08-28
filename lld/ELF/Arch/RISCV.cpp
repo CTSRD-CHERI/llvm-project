CTSRD-CHERI/llvm-project

commit ff9e04b39fe987127f32d684bdb5de64b80856e7

[ELF][RISCV] Emit warning for deprecated CHERI relocations in input
This will quickly hit the warning limit for any real input that was
built with an old toolchain, but trying to rate-limit these specifically
to one-per-object is not worth the effort.

lld/ELF/Arch/RISCV.cpp
@@ -299,6 +299,12 @@ RelType RISCV::getDynRel(RelType type) const {
 
 RelExpr RISCV::getRelExpr(const RelType type, const Symbol &s,
                           const uint8_t *loc) const {
+  // TODO: Remove these
+  auto warnDeprecated = [=]() {
+    warn(getErrorLocation(loc) + "deprecated relocation (" + toString(type) +
+         ") against symbol '" + toString(s) +
+         "'; recompile with this toolchain");
+  };
   switch (type) {
   case R_RISCV_NONE:
     return R_NONE;
@@ -323,18 +329,22 @@ RelExpr RISCV::getRelExpr(const RelType type, const Symbol &s,
   case R_RISCV_SUB32:
   case R_RISCV_SUB64:
     return R_RISCV_ADD;
-  case R_RISCV_JAL:
   case R_RISCV_CHERI_CJAL:
+  case R_RISCV_CHERI_RVC_CJUMP:
+    warnDeprecated();
+    [[fallthrough]];
+  case R_RISCV_JAL:
   case R_RISCV_BRANCH:
   case R_RISCV_PCREL_HI20:
   case R_RISCV_RVC_BRANCH:
   case R_RISCV_RVC_JUMP:
-  case R_RISCV_CHERI_RVC_CJUMP:
   case R_RISCV_32_PCREL:
     return R_PC;
+  case R_RISCV_CHERI_CCALL:
+    warnDeprecated();
+    [[fallthrough]];
   case R_RISCV_CALL:
   case R_RISCV_CALL_PLT:
-  case R_RISCV_CHERI_CCALL:
   case R_RISCV_PLT32:
     return R_PLT_PC;
   case R_RISCV_GOT_HI20:
@@ -352,19 +362,23 @@ RelExpr RISCV::getRelExpr(const RelType type, const Symbol &s,
     return R_TPREL;
   case R_RISCV_ALIGN:
     return R_RELAX_HINT;
-  case R_RISCV_TPREL_ADD:
   case R_RISCV_CHERI_TPREL_CINCOFFSET:
+    warnDeprecated();
+    [[fallthrough]];
+  case R_RISCV_TPREL_ADD:
   case R_RISCV_RELAX:
     return config->relax ? R_RELAX_HINT : R_NONE;
   case R_RISCV_CHERI_CAPABILITY:
   case R_RISCV_CHERI_CAPABILITY_CODE:
     return R_ABS_CAP;
-  // TODO: Deprecate and eventually remove these
   case R_RISCV_CHERI_CAPTAB_PCREL_HI20:
+    warnDeprecated();
     return R_GOT_PC;
   case R_RISCV_CHERI_TLS_IE_CAPTAB_PCREL_HI20:
+    warnDeprecated();
     return R_GOT_PC;
   case R_RISCV_CHERI_TLS_GD_CAPTAB_PCREL_HI20:
+    warnDeprecated();
     return R_TLSGD_PC;
   default:
     error(getErrorLocation(loc) + "unknown relocation (" + Twine(type) +

commit ef47c327d15a990c7e537f2b9a19d6cf64f7d05b

[NFC][ELF][RISCV] Don't copy Symbol on every call to RISCV::getRelExpr
Despite s being a reference, the semantics here are to make a copy of
the object, which is a waste of time, and particularly dodgy to do when
processing relocations in parallel, as it technically introduces data
races on the atomic flags, and may become more problematic for
sub-object compartments.

Fixes: ff9e04b39fe9 ("[ELF][RISCV] Emit warning for deprecated CHERI relocations in input")

lld/ELF/Arch/RISCV.cpp
@@ -311,7 +311,7 @@ RelType RISCV::getDynRel(RelType type) const {
 RelExpr RISCV::getRelExpr(const RelType type, const Symbol &s,
                           const uint8_t *loc) const {
   // TODO: Remove these
-  auto warnDeprecated = [=]() {
+  auto warnDeprecated = [&]() {
     warn(getErrorLocation(loc) + "deprecated relocation (" + toString(type) +
          ") against symbol '" + toString(s) +
          "'; recompile with this toolchain");